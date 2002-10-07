6f0b67847b426c5bf946744594f4c417dcffab9d
Xavier Rubio-Campillo <xavier.rubio@ed.ac.uk>
2002-10-07 08:45:00 +0000

    + Added CLAM-LADSPA first implementation

---
+++ CLAM/examples/CLT/CLT_MonoPlugin.cxx
@@ -0,0 +1,4 @@
+#include "CLT_MonoPlugin.hxx"
+
+Audio CLT_MonoPlugin::sIBuffer = Audio();
+Audio CLT_MonoPlugin::sOBuffer = Audio();
---
+++ CLAM/examples/CLT/CLT_StereoPlugin.cxx
@@ -0,0 +1,6 @@
+#include "CLT_StereoPlugin.hxx"
+
+Audio CLT_StereoPlugin::sIBufferL = Audio();
+Audio CLT_StereoPlugin::sIBufferR = Audio();
+Audio CLT_StereoPlugin::sOBufferL = Audio();
+Audio CLT_StereoPlugin::sOBufferR = Audio();
