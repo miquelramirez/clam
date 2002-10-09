09ef70be7f17988551ed99d20aac32936e994e8c
xamat <xamat@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2002-10-09 17:55:56 +0000

    This class was outside CLAM's namespace

--- trunk/CLAM/src/Errors/ErrSoundFileIO.hxx
+++ trunk/CLAM/src/Errors/ErrSoundFileIO.hxx
@@ -1,11 +1,15 @@
 #ifndef __SoundFileIOError__
 #define __SoundFileIOError__
 
+namespace CLAM {
+
 class ErrSoundFileIO
 {
 public:
 	ErrSoundFileIO(char* str):mStr(str) {}
 	char* mStr;
 };
+
+};//CLAM
 
 #endif
