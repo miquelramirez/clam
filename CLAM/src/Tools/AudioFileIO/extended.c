9bb029d82feec9185dcc04a09cd8a11071769142
mdeboer <mdeboer@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2002-10-10 16:07:29 +0000

    use LGPL code from libsndfile for the ten byte float conversion

--- CLAM/src/Tools/AudioFileIO/extended.c
+++ CLAM/src/Tools/AudioFileIO/extended.c
@@ -1,178 +1,100 @@
 /*
- * C O N V E R T   T O   I E E E   E X T E N D E D
- */
+** This code is taken from aiff.c from libsndfile by Erik de Castro Lopo
+** http://www.zip.com.au/~erikd/libsndfile/libsndfile-1.0.1.tar.gz
+** 
+** This is a rough hack at converting from 80 bit IEEE float in AIFF header to an
+** int and back again. It assumes that all sample rates are between 1 and 800MHz,
+** which should be OK as other sound file formats use a 32 bit integer to store
+** sample rate.
+**
+** There is another (probably better) version in the source code to the SoX but it
+** has a copyright which probably prevents it from being allowable as GPL/LGPL.
+**
+** Another, very complete, implementation of the same conversions, written by
+** Ken Turkowski <turk@computer.org> is available at
+** http://www.worldserver.com/turk/opensource/ToFromIEEE.c.txt
+**
+** =========================================================================================
+**
+** Copyright (C) 1999-2002 Erik de Castro Lopo <erikd@zip.com.au>
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU Lesser General Public License as published by
+** the Free Software Foundation; either version 2.1 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU Lesser General Public License for more details.
+**
+** You should have received a copy of the GNU Lesser General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
 
-/* Copyright (C) 1988-1991 Apple Computer, Inc.
- * All rights reserved.
- *
- * Machine-independent I/O routines for IEEE floating-point numbers.
- *
- * NaN's and infinities are converted to HUGE_VAL or HUGE, which
- * happens to be infinity on IEEE machines.  Unfortunately, it is
- * impossible to preserve NaN's in a machine-independent way.
- * Infinities are, however, preserved on IEEE machines.
- *
- * These routines have been tested on the following machines:
- *    Apple Macintosh, MPW 3.1 C compiler
- *    Apple Macintosh, THINK C compiler
- *    Silicon Graphics IRIS, MIPS compiler
- *    Cray X/MP and Y/MP
- *    Digital Equipment VAX
- *
- *
- * Implemented by Malcolm Slaney and Ken Turkowski.
- *
- * Malcolm Slaney contributions during 1988-1990 include big- and little-
- * endian file I/O, conversion to and from Motorola's extended 80-bit
- * floating-point format, and conversions to and from IEEE single-
- * precision floating-point format.
- *
- * In 1991, Ken Turkowski implemented the conversions to and from
- * IEEE double-precision format, added more precision to the extended
- * conversions, and accommodated conversions involving +/- infinity,
- * NaN's, and denormalized numbers.
- */
+int
+tenbytefloat2int (unsigned char *bytes)
+{       int val = 3 ;
 
-#include <math.h>
+	if (bytes [0] & 0x80)   /* Negative number. */
+		return 0 ;
 
-#ifndef HUGE_VAL
-# define HUGE_VAL HUGE
-#endif /*HUGE_VAL*/
+	if (bytes [0] <= 0x3F)  /* Less than 1. */
+		return 1 ;
 
-# define FloatToUnsigned(f) \
-((unsigned long)(((long)(f - 2147483648.0)) + 2147483647L + 1))
+	if (bytes [0] > 0x40)   /* Way too big. */
+		return  0x4000000 ;
 
-void ConvertToIeeeExtended(double num, char* bytes)
-{
-    int    sign;
-    int expon;
-    double fMant, fsMant;
-    unsigned long hiMant, loMant;
+	if (bytes [0] == 0x40 && bytes [1] > 0x1C) /* Too big. */
+		return 800000000 ;
 
-    if (num < 0) {
-        sign = 0x8000;
-        num *= -1;
-    } else {
-        sign = 0;
-    }
+	/* Ok, can handle it. */
 
-    if (num == 0) {
-        expon = 0; hiMant = 0; loMant = 0;
-    }
-    else {
-        fMant = frexp(num, &expon);
-        if ((expon > 16384) || !(fMant < 1)) {    /* Infinity or NaN */
-            expon = sign|0x7FFF; hiMant = 0; loMant = 0; /* infinity */
-        }
-        else {    /* Finite */
-            expon += 16382;
-            if (expon < 0) {    /* denormalized */
-                fMant = ldexp(fMant, expon);
-                expon = 0;
-            }
-            expon |= sign;
-            fMant = ldexp(fMant, 32);          
-            fsMant = floor(fMant); 
-            hiMant = FloatToUnsigned(fsMant);
-            fMant = ldexp(fMant - fsMant, 32); 
-            fsMant = floor(fMant); 
-            loMant = FloatToUnsigned(fsMant);
-        }
-    }
-    
-    bytes[0] = (char)(expon >> 8);
-    bytes[1] = (char)expon;
-    bytes[2] = (char)(hiMant >> 24);
-    bytes[3] = (char)(hiMant >> 16);
-    bytes[4] = (char)(hiMant >> 8);
-    bytes[5] = (char)(hiMant);
-    bytes[6] = (char)(loMant >> 24);
-    bytes[7] = (char)(loMant >> 16);
-    bytes[8] = (char)(loMant >> 8);
-    bytes[9] = (char)loMant;
-}
+	val = (bytes [2] << 23) |
+	 (bytes [3] << 15) | (bytes [4] << 7)  | (bytes [5] >> 1) ;
+
+	val >>= (29 - bytes [1]) ;
+
+	return val ;
+} /* tenbytefloat2int */
 
 
-/*
- * C O N V E R T   F R O M   I E E E   E X T E N D E D  
- */
+void
+uint2tenbytefloat (unsigned int num, unsigned char *bytes)
+{	unsigned int mask = 0x40000000 ;
+	int		count ;
 
-/* 
- * Copyright (C) 1988-1991 Apple Computer, Inc.
- * All rights reserved.
- *
- * Machine-independent I/O routines for IEEE floating-point numbers.
- *
- * NaN's and infinities are converted to HUGE_VAL or HUGE, which
- * happens to be infinity on IEEE machines.  Unfortunately, it is
- * impossible to preserve NaN's in a machine-independent way.
- * Infinities are, however, preserved on IEEE machines.
- *
- * These routines have been tested on the following machines:
- *    Apple Macintosh, MPW 3.1 C compiler
- *    Apple Macintosh, THINK C compiler
- *    Silicon Graphics IRIS, MIPS compiler
- *    Cray X/MP and Y/MP
- *    Digital Equipment VAX
- *
- *
- * Implemented by Malcolm Slaney and Ken Turkowski.
- *
- * Malcolm Slaney contributions during 1988-1990 include big- and little-
- * endian file I/O, conversion to and from Motorola's extended 80-bit
- * floating-point format, and conversions to and from IEEE single-
- * precision floating-point format.
- *
- * In 1991, Ken Turkowski implemented the conversions to and from
- * IEEE double-precision format, added more precision to the extended
- * conversions, and accommodated conversions involving +/- infinity,
- * NaN's, and denormalized numbers.
- */
 
-#ifndef HUGE_VAL
-# define HUGE_VAL HUGE
-#endif /*HUGE_VAL*/
+	memset (bytes, 0, 10) ;
 
-# define UnsignedToFloat(u) \
-(((double)((long)(u - 2147483647L - 1))) + 2147483648.0)
+	if (num <= 1)
+	{	bytes [0] = 0x3F ;
+		bytes [1] = 0xFF ;
+		bytes [2] = 0x80 ;
+		return ;
+		} ;
 
-/****************************************************************
- * Extended precision IEEE floating-point conversion routine.
- ****************************************************************/
+	bytes [0] = 0x40 ;
 
-double ConvertFromIeeeExtended(unsigned char *bytes)
-{
-    double    f;
-    int    expon;
-    unsigned long hiMant, loMant;
-    
-    expon = ((bytes[0] & 0x7F) << 8) | (bytes[1] & 0xFF);
-    hiMant    =    ((unsigned long)(bytes[2] & 0xFF) << 24)
-            |    ((unsigned long)(bytes[3] & 0xFF) << 16)
-            |    ((unsigned long)(bytes[4] & 0xFF) << 8)
-            |    ((unsigned long)(bytes[5] & 0xFF));
-    loMant    =    ((unsigned long)(bytes[6] & 0xFF) << 24)
-            |    ((unsigned long)(bytes[7] & 0xFF) << 16)
-            |    ((unsigned long)(bytes[8] & 0xFF) << 8)
-            |    ((unsigned long)(bytes[9] & 0xFF));
+	if (num >= mask)
+	{	bytes [1] = 0x1D ;
+		return ;
+		} ;
 
-    if (expon == 0 && hiMant == 0 && loMant == 0) {
-        f = 0;
-    }
-    else {
-        if (expon == 0x7FFF) {    /* Infinity or NaN */
-            f = HUGE_VAL;
-        }
-        else {
-            expon -= 16383;
-            f  = ldexp(UnsignedToFloat(hiMant), expon-=31);
-            f += ldexp(UnsignedToFloat(loMant), expon-=32);
-        }
-    }
+	for (count = 0 ; count <= 32 ; count ++)
+	{	if (num & mask)
+			break ;
+		mask >>= 1 ;
+		} ;
 
-    if (bytes[0] & 0x80)
-        return -f;
-    else
-        return f;
-}
+	num <<= count + 1 ;
+	bytes [1] = 29 - count ;
+	bytes [2] = (num >> 24) & 0xFF ;
+	bytes [3] = (num >> 16) & 0xFF ;
+	bytes [4] = (num >>  8) & 0xFF ;
+	bytes [5] = num & 0xFF ;
+
+} /* uint2tenbytefloat */
+
 
