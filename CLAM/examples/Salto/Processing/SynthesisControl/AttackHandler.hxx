01f4a620a566e4b1459fc3a07a7a365199b1fb76
David García Garzón <vokimon@users.noreply.github.com>
2003-02-28 20:20:03 +0000

    Merged from devel for CLAM 0.4.2

--- CLAM/examples/Salto/Processing/SynthesisControl/AttackHandler.hxx
+++ CLAM/examples/Salto/Processing/SynthesisControl/AttackHandler.hxx
@@ -17,7 +17,7 @@ namespace SALTO
 
 		const ProcessingConfig &GetConfig() const { return mConfig; }
 
-		virtual const char* GetClassName()
+		const char* GetClassName() const
 		{
 			return "SALTO::AttackHandler";
 		}
