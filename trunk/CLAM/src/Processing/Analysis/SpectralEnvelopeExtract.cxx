c8fc3c78ff7e87169d06f0b3a3e0462795d08acb
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2003-02-28 17:49:52 +0000

    This SpectralEnvelopeExtract deprecates the one in rappid

---
+++ trunk/CLAM/src/Processing/Analysis/SpectralEnvelopeExtract.cxx
@@ -0,0 +1,211 @@
+/*
+ * Copyright (c) 2001-2002 MUSIC TECHNOLOGY GROUP (MTG)
+ *                         UNIVERSITAT POMPEU FABRA
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#include "Complex.hxx"
+#include "SpectralEnvelopeExtract.hxx"
+#include "ErrProcessingObj.hxx"
+
+#define CLASS "SpectralEnvelopeExtract"
+
+namespace CLAM {
+
+	/* The  Configuration object has at least to have a name */
+
+	void SpectralEnvelopeExtractConfig::DefaultInit()
+	{
+		AddAll();
+		UpdateData();
+		DefaultValues();
+	}
+
+	
+	void SpectralEnvelopeExtractConfig::DefaultValues()
+	{
+		SetName("SpectralEnvelopeExtract");
+		
+		SetInterpolationType(EInterpolation::eSpline);
+	}
+
+
+	/* Processing  object Method  implementations */
+
+	SpectralEnvelopeExtract::SpectralEnvelopeExtract()
+	{
+		Configure(SpectralEnvelopeExtractConfig());
+	}
+
+	SpectralEnvelopeExtract::SpectralEnvelopeExtract(const SpectralEnvelopeExtractConfig &c = SpectralEnvelopeExtractConfig())
+	{
+		Configure(c);
+	}
+
+	SpectralEnvelopeExtract::~SpectralEnvelopeExtract()
+	{}
+
+
+	/* Configure the Processing Object according to the Config object */
+
+	bool SpectralEnvelopeExtract::ConcreteConfigure(const ProcessingConfig& c) throw(std::bad_cast)
+	{
+
+		mConfig = dynamic_cast<const SpectralEnvelopeExtractConfig&>(c);
+		return true;
+	}
+
+	/* Setting Prototypes for faster processing */
+
+	bool SpectralEnvelopeExtract::SetPrototypes(const SpectralPeakArray& input,Spectrum& output)
+	{
+		return true;
+	}
+
+	bool SpectralEnvelopeExtract::SetPrototypes()
+	{
+		return true;
+	}
+	
+	bool SpectralEnvelopeExtract::UnsetPrototypes()
+	{
+		return true;
+	}
+
+	/* The supervised Do() function */
+
+	bool  SpectralEnvelopeExtract::Do(void) 
+	{
+		throw(ErrProcessingObj(CLASS"::Do(): Supervised mode not implemented"),this);
+		return false;
+	}
+
+	/* The  unsupervised Do() function */
+
+	bool  SpectralEnvelopeExtract::Do(const SpectralPeakArray& input, Spectrum& output)
+	{
+		output.SetScale(input.GetScale());
+		
+		CheckOutputType(output);
+		int i;
+		TSize nPeaks=input.GetnPeaks();
+		DataArray& magBuffer=input.GetMagBuffer();
+		DataArray& phaseBuffer=input.GetPhaseBuffer();
+		DataArray& freqBuffer=input.GetFreqBuffer();
+
+		Array<Point>& magPointArray=output.GetMagBPF().GetPointArray();
+		Array<Point>& phasePointArray=output.GetPhaseBPF().GetPointArray();
+
+		magPointArray.Resize(nPeaks+1);
+		magPointArray.SetSize(nPeaks+1);
+		phasePointArray.Resize(nPeaks+1);
+		phasePointArray.SetSize(nPeaks+1);
+		for(i=0;i<nPeaks;i++)
+		{
+			magPointArray[i+1].SetX(freqBuffer[i]);
+			magPointArray[i+1].SetY(magBuffer[i]);
+
+			phasePointArray[i+1].SetX(freqBuffer[i]);
+			phasePointArray[i+1].SetY(phaseBuffer[i]);
+
+		}
+	
+		if(input.GetScale()==EScale::eLog)
+		{
+		
+			//we now set first point (maybe we should do the same as with last point?)
+			magPointArray[0].SetX(0);
+			magPointArray[0].SetY(magBuffer[0]-3);
+			phasePointArray[0].SetX(0);
+			phasePointArray[0].SetY(0);
+			nPeaks++;
+			//we will now add points until we reach the spectral range
+			TData spectralRange=output.GetSpectralRange();
+			TData lastFreq=freqBuffer[nPeaks-2];
+			TData freqGap=lastFreq-freqBuffer[nPeaks-3];
+			TData currentFreq=lastFreq+freqGap;
+			TData currentMag=magBuffer[nPeaks-2];
+			while(currentFreq<spectralRange)
+			{
+				currentMag-=(currentFreq/lastFreq-1)*12;
+				magPointArray.AddElem(Point(currentFreq,currentMag));
+				phasePointArray.AddElem(Point(currentFreq,0));//check phase!!! this is not correct although may not be necessary
+				currentFreq+=freqGap;
+				nPeaks++;
+				
+			}
+			//we now add last point
+			magPointArray.AddElem(Point(spectralRange,currentMag-3));
+			phasePointArray.AddElem(Point(spectralRange,0));
+			nPeaks++;
+		}
+		else
+		{
+			//we now set first point (maybe we should do the same as with last point?)
+			magPointArray[0].SetX(0);
+			magPointArray[0].SetY(magBuffer[0]*0.5);
+			phasePointArray[0].SetX(0);
+			phasePointArray[0].SetY(0);
+			nPeaks++;
+			
+			//we will now add points until we reach the spectral range
+			TData spectralRange=output.GetSpectralRange();
+			TData lastFreq=freqBuffer[nPeaks-2];
+			TData freqGap=lastFreq-freqBuffer[nPeaks-3];
+			TData currentFreq=lastFreq+freqGap;
+			TData currentMag=magBuffer[nPeaks-2];
+			while(currentFreq<spectralRange)
+			{
+				currentMag*=pow(0.06,(currentFreq/lastFreq-1));
+				magPointArray.AddElem(Point(currentFreq,currentMag));
+				phasePointArray.AddElem(Point(currentFreq,0));//check phase!!! this is not correct although may not be necessary
+				currentFreq+=freqGap;
+				nPeaks++;
+				
+			}
+			//we now add last point
+			magPointArray.AddElem(Point(spectralRange,currentMag*0.5));
+			phasePointArray.AddElem(Point(spectralRange,0));
+			nPeaks++;
+
+
+		}
+		
+		output.SetSize(nPeaks);
+		output.GetMagBPF().UpdateSplineTable();
+		output.GetPhaseBPF().UpdateSplineTable();
+
+		return true;
+	}
+
+	bool SpectralEnvelopeExtract::CheckOutputType(Spectrum& out) 
+	{
+		SpecTypeFlags tmpFlags;
+		tmpFlags.bMagPhaseBPF=1;
+		tmpFlags.bMagPhase=0;
+		out.SetType(tmpFlags);
+
+		out.SetMagBPF(BPF(mConfig.GetInterpolationType()));
+		out.SetPhaseBPF(BPF(EInterpolation::eLinear));
+		
+		/* Clear the  output Array */
+
+		
+		return true;
+	}
+};//namespace CLAM
---
+++ trunk/CLAM/src/Processing/Analysis/SpectralEnvelopeExtract.hxx
@@ -0,0 +1,130 @@
+/*
+ * Copyright (c) 2001-2002 MUSIC TECHNOLOGY GROUP (MTG)
+ *                         UNIVERSITAT POMPEU FABRA
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#ifndef _SpectralEnvelope_Extract_
+#define _SpectralEnvelope_Extract_
+
+#include "Processing.hxx"
+#include "DynamicType.hxx"
+#include "Port.hxx"
+#include "Spectrum.hxx"
+#include "SpectralPeak.hxx"
+#include "SpectralPeakArray.hxx"
+
+namespace CLAM {
+
+
+	/** Configuration class for SpectralEnvelopeExtract objects
+	 *  @see ProcessingConfig, SpectralEnvelopeExtract
+	 */
+	class SpectralEnvelopeExtractConfig: public ProcessingConfig
+	{
+	public:
+		DYNAMIC_TYPE_USING_INTERFACE (SpectralEnvelopeExtractConfig, 2,ProcessingConfig);
+		/** Name of the SpectralEnvelopeExtract object*/
+		DYN_ATTRIBUTE (0, public, std::string, Name);
+		/** Type of interpolation for resultant Spectrum BPF*/
+		DYN_ATTRIBUTE (1, public, EInterpolation, InterpolationType);
+		
+	protected:
+		void DefaultInit();
+	public:
+	  /** Initialize configuration object with default values (name 'SpectralEnvelopeExtract', 513 bands, Magnitude treshold set to -80 and MaxSpectralPeaks setted to 100
+	   */
+		void DefaultValues();
+		~SpectralEnvelopeExtractConfig(){};
+		
+ 	}; 
+
+	/**
+	 *	Processing to extract spectral envelope from an array of spectral peaks.
+	 */
+	class SpectralEnvelopeExtract: public Processing {
+		SpectralEnvelopeExtractConfig mConfig;
+
+		/** This method returns the name of the object
+		 *  @return Char pointer with the name of object
+		 */
+		const char *GetClassName() const {return "SpectralEnvelopeExtract";}
+
+		/** Config change method
+		 *  @param The ProcessingConfig object
+		 *  @throw bad_cast exception when the argument is not an SpectralEnvelopeExtractConfig object.
+		 *  @return True if the cast has been commited correctly		 
+		 */
+		bool ConcreteConfigure(const ProcessingConfig&) throw(std::bad_cast);
+
+	public:
+		/** Base constructor of class. Calls Configure method with a SpectralEnvelopeExtractConfig initialised by default*/
+		SpectralEnvelopeExtract();
+		/** Constructor with an object of SpectralEnvelopeExtractConfig class by parameter
+		 *  @param c SpectralEnvelopeExtractConfig object created by the user
+*/
+		SpectralEnvelopeExtract(const SpectralEnvelopeExtractConfig &c);
+
+		/** Destructor of the class*/
+ 		~SpectralEnvelopeExtract();
+
+	       /** This method allows to obtain the SpectralEnvelopeExtractConfig object of the class
+		*  @return The configuration of the class
+		*/
+		const ProcessingConfig &GetConfig() const { return mConfig;}
+
+		/** Supervised Do() function, not implemented yet*/
+		bool Do(void);
+
+		/** Unsupervised Do function, receives an Spectrum object input, and the output of method is an SpectralPeakArray (passed by parameter)
+		 *  @param input The Spectral Peaks we want to extract the envelope from
+		 *  @param output A Spectrum in BPF form that represents the Spectral Envelope
+		 *  @return Boolean value, true if all has been executed correctly.
+		 */
+		bool Do(const SpectralPeakArray& input,Spectrum& output);
+
+		// Port interfaces.
+
+		/** Change the internal type state.  
+		 */
+
+		bool SetPrototypes(const SpectralPeakArray& input,Spectrum& output);
+
+		bool SetPrototypes();
+
+		bool UnsetPrototypes();
+
+		bool MayDisableExecution() const {return true;}
+
+		/** Not implemented yet*/
+		void StoreOn(Storage &s) {};
+
+	private:
+		
+		/** Checks if the output type has the correct size and attributes .
+		 *  If not, resize Buffers and changes control flags to set correctly the object
+		 *  @param out SpectrumSpectralPeakArray object to be checked
+		 *  @return true if success
+		 */
+		bool CheckOutputType(Spectrum& out);
+		
+ 	}; 
+
+};//namespace CLAM
+
+#endif // _SpectralEnvelope_Extract_
