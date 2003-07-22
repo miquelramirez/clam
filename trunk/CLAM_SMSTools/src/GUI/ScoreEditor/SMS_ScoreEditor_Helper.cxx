162f35f788c93d357eb8bd0e167acfa37813a159
xamat <xamat@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2003-07-22 09:32:31 +0000

    Merged changes from CLAM-devel-0_5_1-pre-rel. Release 0.5.1 on its way!

---
+++ trunk/CLAM_SMSTools/src/GUI/ScoreEditor/SMS_ScoreEditor_Helper.cxx
@@ -0,0 +1,18 @@
+#include "SMS_ScoreEditor_Helper.hxx"
+
+namespace CLAMVM
+{
+	void clampBPFValues( CLAM::BPF& bpf, double min, double max )
+	{
+		
+		for ( int i = 0; i < bpf.Size(); i++ )
+		{
+			if ( bpf.GetValueFromIndex( i ) < min )
+				bpf.SetValue( i, min );
+			else if ( bpf.GetValueFromIndex( i ) > max )
+				bpf.SetValue( i, max );						
+		}
+
+	}
+	
+}
