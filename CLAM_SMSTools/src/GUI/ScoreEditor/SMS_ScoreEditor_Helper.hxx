f261181cc7a76af2f3ad910ae10ce876f47aae18
xamat <xamat@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2003-07-22 09:32:31 +0000

    Merged changes from CLAM-devel-0_5_1-pre-rel. Release 0.5.1 on its way!

---
+++ CLAM_SMSTools/src/GUI/ScoreEditor/SMS_ScoreEditor_Helper.hxx
@@ -0,0 +1,12 @@
+#ifndef __SCOREEDITOR_HELPER__
+#define __SCOREEDITOR_HELPER__
+
+#include "BPF.hxx"
+
+namespace CLAMVM
+{
+	void clampBPFValues( CLAM::BPF& bpf, double min, double max );
+}
+
+
+#endif // SMS_ScoreEditor_Helper.hxx
