5e10aa877b73ca0a9b49556663318509af51b2ac
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2005-01-13 10:41:12 +0000

    config class was missing

--- CLAM/examples/MIDI_Synthesizer_example/MIDI_Synthesizer_example.cxx
+++ CLAM/examples/MIDI_Synthesizer_example/MIDI_Synthesizer_example.cxx
@@ -24,6 +24,7 @@
 A FLOW DIAGRAM TO DEMONSTRATE THIS EXAMPLE CAN BE FOUND IN
 CLAM-Docs/MIDI_Synthesizer_example (development-branch)
 */
+#include "MyInstrument.hxx"
 
 #include "AudioIn.hxx"
 #include "AudioOut.hxx"
@@ -58,169 +59,6 @@ public:
 	}
 };
 
-class MyInstrumentConfig: public ProcessingConfig
-{
-public:
-	DYNAMIC_TYPE_USING_INTERFACE (MyInstrumentConfig, 6, ProcessingConfig);
-	DYN_ATTRIBUTE (0, public, std::string, Name);
-	DYN_ATTRIBUTE (1, public, TData, AttackTime);
-	DYN_ATTRIBUTE (2, public, TData, DecayTime);
-	DYN_ATTRIBUTE (3, public, TData, SustainLevel);
-	DYN_ATTRIBUTE (4, public, TData , ReleaseTime);
-	DYN_ATTRIBUTE (5, public, TData , SampleRate);
-protected:
-	void DefaultInit(void);
-};
-
-class MyInstrument:public Instrument
-{
-public:
-		MyInstrumentConfig            mConfig;
-		ADSR                          mADSR;
-		Audio                         mEnvelope;
-		Oscillator                    mOscillator;
-		AudioMultiplier               mSampleMultiplier;
-		ControlMapper                 mMapperNote;
-		ControlMapper                 mMapperVel;
-		ControlMapper                 mMapperPitchBend;
-		ControlMultiplier             mFreqMultiplier;
-		InControlTmpl< MyInstrument >  mPitchBendIn;
-		OutControl                    mPitchBendOut;
-	
-		int UpdatePitchBend( TControlData value )
-		{
-			mPitchBendOut.SendControl( value );
-
-			return 0;
-		}
-
-		void LinkControls(void)
-		{
-			ConnectControls(mADSR, 0, *this, 0);
-
-			ConnectControls(*this, 1, mMapperNote, 0);
-			ConnectControls(*this, 2, mMapperVel, 0);
-			ConnectControls(*this, 3, mMapperPitchBend, 0);
-
-			ConnectControls(mMapperNote, 0, mFreqMultiplier, 0);
-			ConnectControls(mMapperPitchBend, 0, mFreqMultiplier, 1);
-			ConnectControls(mFreqMultiplier, 0, mOscillator, 0);
-			ConnectControls(mMapperVel, 0, mADSR, 0);
-		}
-
-	public:
-		MyInstrument():
-		  mPitchBendIn( "PitchBend", this, &MyInstrument::UpdatePitchBend ),
-		  mPitchBendOut( "PitchBendOut", this )
-		{
-			MyInstrumentConfig cfg;
-			Configure( cfg );
-		}
-
-		MyInstrument( const MyInstrumentConfig& c):
-		  mPitchBendIn( "PitchBend", this, &MyInstrument::UpdatePitchBend ),
-		  mPitchBendOut( "PitchBendOut", this )
-		{
-			  Configure( c );
-		}
-
-		~MyInstrument(){}
-
-		const ProcessingConfig &GetConfig() const { return mConfig; }
-
-		bool ConcreteConfigure( const ProcessingConfig& c );
-	
-		bool ConcreteStart();
-
-		bool Do(Audio& out) ;
-};
-
-
-void MyInstrumentConfig::DefaultInit(void)
-{
-	AddName();
-	AddAttackTime();
-	AddDecayTime(),
-	AddSustainLevel();
-	AddReleaseTime();
-	AddSampleRate();
-
-	UpdateData();
-
-	try
-	{
-		SetSampleRate( AudioManager::Current().SampleRate() );
-	}
-	catch(Err)
-	{
-		SetSampleRate( 8000 );
-	}
-
-}
-
-
-bool MyInstrument::ConcreteConfigure( const ProcessingConfig& c)
-{
-	CopyAsConcreteConfig(mConfig, c);
-
-	ADSRConfig ADSRCfg;
-	std::string tmp = mConfig.GetName() + ".ADSR";
-
-	ADSRCfg.SetAttackTime( mConfig.GetAttackTime() );
-	ADSRCfg.SetDecayTime( mConfig.GetDecayTime() );
-	ADSRCfg.SetSustainLevel( mConfig.GetSustainLevel() );
-	ADSRCfg.SetReleaseTime( mConfig.GetReleaseTime() );
-	ADSRCfg.SetSampleRate( mConfig.GetSampleRate() );
-
-	mADSR.Configure( ADSRCfg );
-
-	ControlMapperConfig MapperVelCfg;
-
-	MapperVelCfg.SetMapping( "linear" );
-	TData ptr1[] = {0.0 ,127.0 ,0.0 ,1.0};
- 	MapperVelCfg.SetArguments( DataArray( ptr1, 4 ) );
-	mMapperVel.Configure( MapperVelCfg );
-
-	ControlMapperConfig MapperNoteCfg;
-	
-	MapperNoteCfg.SetMapping( "NoteToFreq" );
-	TData ptr2[] = {69.0,440.0} ;
-	MapperNoteCfg.SetArguments( DataArray( ptr2, 2 ) );
-
-	mMapperNote.Configure( MapperNoteCfg );
-
-
-	ControlMapperConfig MapperPBendCfg;
-	MapperPBendCfg.SetMapping( "ValueToRatio" );
-	TData ptr3[] = { 12 } ;							
-	MapperPBendCfg.SetArguments( DataArray( ptr3, 1 ) );
-
-	mMapperPitchBend.Configure( MapperPBendCfg );
-
-	LinkControls();
-
-	return true;
-}
-
-bool MyInstrument::ConcreteStart()
-{
-	mOscillator.Start();
-	mADSR.Start();
-	mSampleMultiplier.Start();
-
-	return true;
-}
-
-bool MyInstrument::Do( Audio& out )
-{
-	mEnvelope.SetSize( out.GetSize() );
-
-	mOscillator.Do( out );
-	mADSR.Do( mEnvelope );
-	mSampleMultiplier.Do( out, mEnvelope, out );
-
-	return true;
-}
 
 void MyAudioApplication::ConfigureAndCheck(Processing& p,ProcessingConfig& cfg)
 {
@@ -331,7 +169,7 @@ void MyAudioApplication::AudioMain(void)
 
 		for (int i=0;i<nVoices;i++)
 		{
-			instruments.AddElem( new MyInstrument( instrumentCfg[i] ) );
+			instruments.AddElem( new CLAM::MyInstrument( instrumentCfg[i] ) );
 		}
 
 		// Dispatcher declaration
