02b2910365bfe97f25ab7c07cbfc2ef9eccf4407
David García Garzón <vokimon@users.noreply.github.com>
2005-01-13 22:40:55 +0000

    Viewing ports

--- CLAM/examples/Prototyper/PrototypeLoader.cxx
+++ CLAM/examples/Prototyper/PrototypeLoader.cxx
@@ -9,8 +9,14 @@
 #include "Thread.hxx"
 #include "AudioManager.hxx"
 #include "QtSlot2Control.hxx"
+#include "NetAudioPlot.hxx"
 #include "NetPeaksPlot.hxx"
+#include "NetSpectrumPlot.hxx"
+#include "NetFundPlot.hxx"
+#include "AudioPlotProcessing.hxx"
 #include "PeaksPlotProcessing.hxx"
+#include "SpectrumPlotProcessing.hxx"
+#include "FundPlotProcessing.hxx"
 
 
 class NetworkPlayer
@@ -59,6 +65,15 @@ public:
 	}
 };
 
+static std::string getMonitorNumber()
+{
+	static unsigned number = 0;
+	std::stringstream os;
+	os << number++;
+	return os.str();
+}
+
+
 class PrototypeLoader
 {
 	char * _networkFile;
@@ -66,89 +81,115 @@ class PrototypeLoader
 	QWidget * _mainWidget;
 	NetworkPlayer _player;
 public:
+	PrototypeLoader(char * networkFile)
+		: _networkFile(networkFile)
+		, _player(networkFile)
+	{
+		
+	}
+	QWidget * loadPrototype(char* uiFile)
+	{
+		_mainWidget = (QWidget *) QWidgetFactory::create( uiFile );
+		return _mainWidget;
+	}
+	void connectWithNetwork()
+	{
+		CLAM::Network & network = _player.Network();
+		QWidget * prototype = _mainWidget;
+		connectWidgetsWithControls(network,prototype);
+		connectWidgetsWithMappedControls(network,prototype);
+
+	//	connectWidgetsWithPorts<CLAM::VM::NetAudioPlot, CLAM::AudioPlotProcessing>
+	//		("OutPort__.*", "CLAM::VM::NetAudioPlot", "Audio Input");
+		connectWidgetsWithPorts<CLAM::VM::NetSpectrumPlot, CLAM::SpectrumPlotProcessing>
+			("OutPort__.*", "CLAM::VM::NetSpectrumPlot", "Spectrum Input");
+		connectWidgetsWithPorts<CLAM::VM::NetPeaksPlot, CLAM::PeaksPlotProcessing>
+			("OutPort__.*", "CLAM::VM::NetPeaksPlot", "Peaks Input");
+	//	connectWidgetsWithPorts<CLAM::VM::NetFundPlot, CLAM::FundPlotProcessing>
+	//		("OutPort__.*", "CLAM::VM::NetFundPlot", "Fundamental Input");
+	}
+	void Start()
+	{
+		_player.Start();
+	}
+
+private:
+	void substitute(std::string & subject, const char * pattern, const char * substitution)
+	{
+		for (std::string::size_type position = subject.find(pattern, 0);
+			position!=std::string::npos;
+			position = subject.find(pattern, position))
+		{
+			subject.replace(position, strlen(pattern), substitution);
+		}
+	}
+	std::string GetNetworkNameFromWidgetName(const char * widgetName)
+	{
+		std::string networkName(widgetName);
+		substitute(networkName,"___", " ");
+		substitute(networkName,"__", ".");
+		return networkName;
+	}
+
+	void connectWidgetsWithControls(CLAM::Network & network, QWidget * prototype)
+	{
+		QObjectList * widgets = prototype->queryList(0,"InControl__.*");
+		for (QObjectListIt it(*widgets); it.current(); ++it)
+		{
+			QWidget * aWidget = dynamic_cast<QWidget*>(it.current());
+			std::string controlName=GetNetworkNameFromWidgetName(aWidget->name() + 11);
+
+			std::cout << "Control: " << controlName << std::endl;
+
+			CLAM::InControl & receiver = network.GetInControlByCompleteName(controlName);
+			QtSlot2Control * notifier = new QtSlot2Control(controlName.c_str());
+			notifier->linkControl(receiver);
+			notifier->connect(aWidget,SIGNAL(valueChanged(int)),
+					  SLOT(sendControl(int)));
+		}
+
+	}
+
+	void connectWidgetsWithMappedControls(CLAM::Network & network, QWidget * prototype)
+	{
+		QObjectList * widgets = prototype->queryList(0,"InControlFloat__.*");
+		for (QObjectListIt it(*widgets); it.current(); ++it)
+		{
+			QWidget * aWidget = dynamic_cast<QWidget*>(it.current());
+			std::string controlName=GetNetworkNameFromWidgetName(aWidget->name() + 16);
+			std::cout << "0:1 Mapped Control: " << controlName << std::endl;
+
+			CLAM::InControl & receiver = network.GetInControlByCompleteName(controlName);
+			QtSlot2Control * notifier = new QtSlot2Control(controlName.c_str());
+			notifier->linkControl(receiver);
+			notifier->connect(aWidget,SIGNAL(valueChanged(int)),
+					  SLOT(sendMappedControl(int)));
+		}
+	}
+	template < typename PlotClass, typename MonitorClass >
+	void connectWidgetsWithPorts(char* prefix, char* plotClassName, char* monitorControlName)
+	{
+		CLAM::Network & network = _player.Network();
+		QWidget * prototype = _mainWidget;
+		QObjectList * widgets = prototype->queryList(plotClassName,prefix);
+		for (QObjectListIt it(*widgets); it.current(); ++it)
+		{
+			QWidget * aWidget = dynamic_cast<QWidget*>(it.current());
+			std::string controlName=GetNetworkNameFromWidgetName(aWidget->name() + 9);
+			std::cout << plotClassName << ": " << controlName << std::endl;
+
+			MonitorClass * monitor = new MonitorClass;
+			std::string monitorName = "PrototyperMonitor"+getMonitorNumber();
+			network.AddProcessing(monitorName,monitor);
+			network.ConnectPorts(controlName,monitorName+"."+monitorControlName);
+			PlotClass * plot = (PlotClass*) aWidget;
+			monitor->SetPlot(plot);
+			plot->Show();
+		}
+	}
+
 
 };
-void connectWidgetsWithControls(CLAM::Network & network, QWidget * prototype)
-{
-	QObjectList * widgets = prototype->queryList(0,"InControl::.*");
-	for (QObjectListIt it(*widgets); it.current(); ++it)
-	{
-		QWidget * aWidget = dynamic_cast<QWidget*>(it.current());
-		std::string controlName(aWidget->name() + 11);
-		for (std::string::size_type colonPosition = controlName.find("::", 0);
-			colonPosition!=std::string::npos;
-			colonPosition = controlName.find("::", colonPosition))
-		{
-			controlName.replace(colonPosition, 2, ".");
-		}
-		std::cout << "Control: " << controlName << std::endl;
-		std::cout << controlName << std::endl;
-
-		CLAM::InControl & receiver = network.GetInControlByCompleteName(controlName);
-		QtSlot2Control * notifier = new QtSlot2Control(controlName.c_str());
-		notifier->linkControl(receiver);
-		notifier->connect(aWidget,SIGNAL(valueChanged(int)),
-		                  SLOT(sendControl(int)));
-	}
-
-}
-
-void connectWidgetsWithMappedControls(CLAM::Network & network, QWidget * prototype)
-{
-	QObjectList * widgets = prototype->queryList(0,"InControlFloat::.*");
-	for (QObjectListIt it(*widgets); it.current(); ++it)
-	{
-		QWidget * aWidget = dynamic_cast<QWidget*>(it.current());
-		std::string controlName(aWidget->name() + 16);
-		for (std::string::size_type colonPosition = controlName.find("::", 0);
-			colonPosition!=std::string::npos;
-			colonPosition = controlName.find("::", colonPosition))
-		{
-			controlName.replace(colonPosition, 2, ".");
-		}
-		std::cout << "0:1 Mapped Control: " << controlName << std::endl;
-
-		CLAM::InControl & receiver = network.GetInControlByCompleteName(controlName);
-		QtSlot2Control * notifier = new QtSlot2Control(controlName.c_str());
-		notifier->linkControl(receiver);
-		notifier->connect(aWidget,SIGNAL(valueChanged(int)),
-		                  SLOT(sendMappedControl(int)));
-	}
-
-}
-void connectWidgetsWithPeaksPorts(CLAM::Network & network, QWidget * prototype)
-{
-	QObjectList * widgets = prototype->queryList("CLAM::VM::NetPeaksPlot","OutPort::.*");
-	for (QObjectListIt it(*widgets); it.current(); ++it)
-	{
-		QWidget * aWidget = dynamic_cast<QWidget*>(it.current());
-		std::string controlName(aWidget->name() + 9);
-		for (std::string::size_type colonPosition = controlName.find("::", 0);
-			colonPosition!=std::string::npos;
-			colonPosition = controlName.find("::", colonPosition))
-		{
-			controlName.replace(colonPosition, 2, ".");
-		}
-		controlName = "SMSPitchShift_11.Out SpectralPeaks";
-		std::cout << "Peak Port Monitor: " << controlName << std::endl;
-		CLAM::Processing * monitor = new CLAM::PeaksPlotProcessing;
-		network.AddProcessing("PrototyperMonitor",monitor);
-		network.ConnectPorts(controlName,"PrototyperMonitor.Peaks Input");
-	}
-
-}
-QWidget * loadPrototype(CLAM::Network & network, char* uiFile)
-{
-	QWidget * prototype = (QWidget *) QWidgetFactory::create( uiFile );
-	if (!prototype) return 0;
-
-	connectWidgetsWithControls(network,prototype);
-	connectWidgetsWithMappedControls(network,prototype);
-	connectWidgetsWithPeaksPorts(network,prototype);
-
-	return prototype;
-
-}
 
 
 
@@ -168,20 +209,20 @@ int main( int argc, char *argv[] )
 
 	QApplication app( argc, argv );
 
-	NetworkPlayer player(networkFile);
+	PrototypeLoader loader(networkFile);
 
 
-	QWidget * prototype = loadPrototype(player.Network(), uiFile);
-
+	QWidget * prototype = loader.loadPrototype(uiFile);
 	if (!prototype) return -1;
-
+	loader.connectWithNetwork();
 
 	// Set up the dynamic dialog here
 	app.setMainWidget( prototype );
 
 	prototype->show();
 
-	player.Start();
+
+	loader.Start();
 	int result = app.exec();
 
 	return result;
