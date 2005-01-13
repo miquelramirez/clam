316d5107ce5ba32a7b2ac23e6a11921830b9c34f
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2005-01-13 09:50:33 +0000

    Fully qualified names and senseful hints and help texts

--- trunk/CLAM/examples/QtDesignerPlugins/CLAMWidgetsPlugin.cxx
+++ trunk/CLAM/examples/QtDesignerPlugins/CLAMWidgetsPlugin.cxx
@@ -12,26 +12,26 @@ public:
 	QStringList keys() const
 	{
 		QStringList list;
-	        list << "NetAudioPlot";
-	        list << "NetSpectrumPlot";
-	        list << "NetPeaksPlot";
-	        list << "NetFundPlot";
+	        list << "CLAM::VM::NetAudioPlot";
+	        list << "CLAM::VM::NetSpectrumPlot";
+	        list << "CLAM::VM::NetPeaksPlot";
+	        list << "CLAM::VM::NetFundPlot";
 	        return list;
 	}
 	QWidget* create( const QString &key, QWidget* parent = 0, const char* name = 0 )
 	{
-		if (key=="NetAudioPlot") return new CLAM::VM::NetAudioPlot(parent);
-		if (key=="NetSpectrumPlot") return new CLAM::VM::NetSpectrumPlot(parent);
-		if (key=="NetPeaksPlot") return new CLAM::VM::NetPeaksPlot(parent);
-		if (key=="NetFundPlot") return new CLAM::VM::NetFundPlot(parent);
+		if (key=="CLAM::VM::NetAudioPlot") return new CLAM::VM::NetAudioPlot(parent);
+		if (key=="CLAM::VM::NetSpectrumPlot") return new CLAM::VM::NetSpectrumPlot(parent);
+		if (key=="CLAM::VM::NetPeaksPlot") return new CLAM::VM::NetPeaksPlot(parent);
+		if (key=="CLAM::VM::NetFundPlot") return new CLAM::VM::NetFundPlot(parent);
 		return 0;
 	}
 	QString includeFile( const QString& key) const
 	{
-		if (key=="NetAudioPlot") return "NetAudioPlot.hxx";
-		if (key=="NetSpectrumPlot") return "NetSpectrumPlot.hxx";
-		if (key=="NetPeaksPlot") return "NetPeaksPlot.hxx";
-		if (key=="NetFundPlot") return "NetFundPlot.hxx";
+		if (key=="CLAM::VM::NetAudioPlot") return "NetAudioPlot.hxx";
+		if (key=="CLAM::VM::NetSpectrumPlot") return "NetSpectrumPlot.hxx";
+		if (key=="CLAM::VM::NetPeaksPlot") return "NetPeaksPlot.hxx";
+		if (key=="CLAM::VM::NetFundPlot") return "NetFundPlot.hxx";
 		return QString::null;
 	}
 	QIconSet iconSet( const QString& key) const
@@ -40,34 +40,34 @@ public:
 	}
 	QString group( const QString& key) const
 	{
-		if (key=="NetAudioPlot") return "Plots";
-		if (key=="NetSpectrumPlot") return "Plots";
-		if (key=="NetPeaksPlot") return "Plots";
-		if (key=="NetFundPlot") return "Plots";
+		if (key=="CLAM::VM::NetAudioPlot") return "CLAM Plots";
+		if (key=="CLAM::VM::NetSpectrumPlot") return "CLAM Plots";
+		if (key=="CLAM::VM::NetPeaksPlot") return "CLAM Plots";
+		if (key=="CLAM::VM::NetFundPlot") return "CLAM Plots";
 		return QString::null;
 	}
 	QString toolTip( const QString& key) const
 	{
-		if (key=="NetAudioPlot") return "Mixer Slider";
-		if (key=="NetSpectrumPlot") return "Mixer Slider";
-		if (key=="NetPeaksPlot") return "Mixer Slider";
-		if (key=="NetFundPlot") return "Mixer Slider";
+		if (key=="CLAM::VM::NetAudioPlot") return "CLAM Network Audio Port Monitor";
+		if (key=="CLAM::VM::NetSpectrumPlot") return "CLAM Network Spectrum Port Monitor";
+		if (key=="CLAM::VM::NetPeaksPlot") return "CLAM Network Spectral Peaks Port Monitor";
+		if (key=="CLAM::VM::NetFundPlot") return "CLAM Network Fundamental Port Monitor";
 		return QString::null;
 	}
 	QString whatsThis( const QString& key) const
 	{
-		if (key=="NetAudioPlot") return "A Mixer device like slider";
-		if (key=="NetSpectrumPlot") return "A Mixer device like slider";
-		if (key=="NetPeaksPlot") return "A Mixer device like slider";
-		if (key=="NetFundPlot") return "A Mixer device like slider";
+		if (key=="CLAM::VM::NetAudioPlot") return "Use this widget to visualize Audio content from an outport of a CLAM processing object";
+		if (key=="CLAM::VM::NetSpectrumPlot") return "Use this widget to visualize Spectrum content from an outport of a CLAM processing object";
+		if (key=="CLAM::VM::NetPeaksPlot") return "Use this widget to visualize Spectral Peaks content from an outport of a CLAM processing object";
+		if (key=="CLAM::VM::NetFundPlot") return "Use this widget to visualize Fundamental content from an outport of a CLAM processing object";
 		return QString::null;
 	}
 	bool isContainer( const QString& key) const
 	{
-		if (key=="NetAudioPlot") return true;
-		if (key=="NetSpectrumPlot") return true;
-		if (key=="NetPeaksPlot") return true;
-		if (key=="NetFundPlot") return true;
+		if (key=="CLAM::VM::NetAudioPlot") return false;
+		if (key=="CLAM::VM::NetSpectrumPlot") return false;
+		if (key=="CLAM::VM::NetPeaksPlot") return false;
+		if (key=="CLAM::VM::NetFundPlot") return false;
 		return false;
 	}
 };
