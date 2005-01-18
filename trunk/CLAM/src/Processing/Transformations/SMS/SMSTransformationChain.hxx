3db9d939f6e373870e0c736ad1d7016f335fefa9
sroig <sroig@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2005-01-18 17:34:51 +0000

    Refactorings: - Merged ProcessingChain and SMSTransformationChain classes into SMSTransformationChain.

--- trunk/CLAM/src/Processing/Transformations/SMS/SMSTransformationChain.hxx
+++ trunk/CLAM/src/Processing/Transformations/SMS/SMSTransformationChain.hxx
@@ -25,7 +25,13 @@
 
 #include "ProcessingChain.hxx"
 #include "SMSTransformation.hxx"
-
+#include "ProcessingComposite.hxx"
+#include "ProcessingData.hxx"
+#include "InPort.hxx"
+#include "OutPort.hxx"
+#include "InControlTmplArray.hxx"
+#include "ProcessingFactory.hxx"
+#include "Array.hxx"
 
 namespace CLAM {
 
@@ -46,24 +52,75 @@ namespace CLAM {
 		
 	};
 
-	/** ProcessingChain particularized to the needs of SMSTransformations. It is an
-	 *	instance of the template class ProcessingChain, using Segment as the type for
-	 *	input and output ports. It implements the supervised Do and a helper method for
+		
+	/** Using Segment as the type for *	input and output ports. 
+	 * It implements the supervised Do and a helper method for
 	 *	updating the frame counter in the Segment.
-	 *	@see ProcessingChain<T>
 	 */
-	class SMSTransformationChain:public ProcessingChain<Segment>
+	class SMSTransformationChain: public ProcessingComposite
 	{
-		public:
-				
-		/** Default constructor. */
-		SMSTransformationChain(){}
-		/** Virtual Destructor. */
-		virtual ~SMSTransformationChain(){}
-		/** Supervised Do method. Does the same as the ProcessingChain Do method but adds a call
-		 *	to the counter updating method.
-		 *	@see ProcessingChain<T>::Do()
-		*/
+		
+		InControlTmplArray<SMSTransformationChain> *mpOnCtrlArray;
+
+		/** Temporal ProcessingData used as an internal node for intermediate Processing */
+		Array<Segment*> mpTmpDataArray;
+		/** Internal configuration. A pointer is used because polymorphism may be used on it */
+		ProcessingChainConfig* mpConfig;
+		Segment* mpChainInput;
+		Segment* mpChainOutput;
+
+
+	public:
+
+		void AttachIn(Segment& data) { mpChainInput = &data; }
+		void AttachOut(Segment& data) { mpChainOutput = &data; }
+		
+		SMSTransformationChain() : 
+			mpChainInput(0),
+			mpChainOutput(0)
+			{
+				mpConfig=NULL;
+				mpOnCtrlArray=NULL;
+			}
+		/** Destructor. Deletes temporal ProcessingData and Configuration and any previously
+		 *	allocated Processing chainee. 
+		 */
+		virtual ~SMSTransformationChain(){
+		
+			for(int i=0;i<mpTmpDataArray.Size();i++)
+				if(mpTmpDataArray[i]) delete mpTmpDataArray[i];
+			if (mpConfig) delete mpConfig;
+			
+			for(iterator obj=composite_begin();obj!=composite_end();obj++)
+			{
+				delete (*obj);
+			}
+		}
+		
+		virtual bool DoChildren()
+		{
+			CLAM_DEBUG_ASSERT(GetExecState() != Unconfigured &&
+		                  GetExecState() != Ready,
+		                  "ProcessingChain: Do(): Not in execution mode");
+
+			if (GetExecState() == Disabled)	return true;
+			
+			bool result=true;
+			int i=0;
+			//We iterate through all chainees and call their Do()
+			for (iterator obj=composite_begin(); obj!=composite_end(); obj++,i++)
+			{
+				Processing & proc = *(*obj);
+				if((*mpOnCtrlArray)[i].GetLastValue()||i==0||i==int(composite_size())-1)
+				//Note: First and last chainee's will always be active regartheless the value
+				//of their On control.
+				{
+					CLAM_DEBUG_ASSERT(proc.GetExecState()==Running, "child is not running" );
+					result &= proc.Do();
+				}
+			}
+			return result;
+		}
 		bool Do()
 		{
 			if(IsLastFrame())
@@ -72,27 +129,20 @@ namespace CLAM {
 				return false;
 			}
 			NextFrame();
-			return ProcessingChain<Segment>::Do();
+			return DoChildren();
 		}
-		bool Previously_ProcessingChainConcreteStart()
+
+		bool ConcreteStart()
 		{
-			
-			// init temp segments array
-			for(int i=0;i<mpTmpDataArray.Size();i++)
-				if(mpTmpDataArray[i])
-				{
-					delete mpTmpDataArray[i];
-					mpTmpDataArray[i]=NULL;
-				}
-			mpTmpDataArray.SetSize(0);
+			void InitSegmentArray(); 
 			
 			Segment* pCurrentData;
 			pCurrentData=new Segment(*mpChainInput);
 			mpTmpDataArray.AddElem(pCurrentData);
-			iterator obj;
 			
 			// set up connections:     inSegment<--trans-->outSegment
 			
+			iterator obj;
 			for(obj=composite_begin();obj!=composite_end();obj++)
 			{
 				//connecting ports for non-supervised mode
@@ -118,38 +168,106 @@ namespace CLAM {
 			SMSTransformation& concreteObj2 = dynamic_cast<SMSTransformation&>( *(*(obj)) );
 
 			concreteObj2.AttachOut( *mpChainOutput);
+
+			InitAllFrameIndex();
+			
 			return ProcessingComposite::ConcreteStart();
 
 		}
 		
-		bool ConcreteStart()
+		void InitSegmentArray() 
 		{
-//			bool ret= ProcessingChain<Segment>::ConcreteStart();
-			bool ret= Previously_ProcessingChainConcreteStart();
-			
-			int i;
-			for(i=0;i<mpTmpDataArray.Size();i++)
-				mpTmpDataArray[i]->mCurrentFrameIndex=0;
-			mpChainInput->mCurrentFrameIndex=0;
-			return ret;
+			for(int i=0;i<mpTmpDataArray.Size();i++)
+				if(mpTmpDataArray[i])
+					delete mpTmpDataArray[i];
+			mpTmpDataArray.SetSize(0);
 		}
 
-	protected:
+		void InitAllFrameIndex()
+		{
+			for(int i=0;i<mpTmpDataArray.Size();i++)
+				mpTmpDataArray[i]->mCurrentFrameIndex=0;
+			mpChainInput->mCurrentFrameIndex=0;
+		}
+
+		/** Accessor to On/off control. Turns on the specified control in the array.*/
+		void TurnOn(TIndex index)
+		{
+			(*mpOnCtrlArray)[index].DoControl(1);
+		}
+
+		/** Accessor to On/off control. Turns on the specified control in the array.*/
+		void TurnOff(TIndex index)
+		{
+			(*mpOnCtrlArray)[index].DoControl(0);
+		}
+
+		const ProcessingConfig& GetConfig() const
+		{
+			return *mpConfig;
+		}
+
+		bool ConcreteConfigure(const ProcessingConfig& c)
+		{
+			mpConfig=new ProcessingChainConfig(dynamic_cast<const ProcessingChainConfig&>(c));
+			bool result=true;
+			iterator obj;
+			
+			/** First we remove existing chainee if any*/
+			obj=composite_begin();
+			while(obj!=composite_end())
+			{
+				Remove(*(*obj));
+				obj=composite_begin();
+			}
+			
+			ProcessingChainConfig::iterator cfg;
+			for(cfg=mpConfig->ConfigList_begin();cfg!=mpConfig->ConfigList_end();cfg++)
+			{
+				AddChainee((*cfg).GetConcreteClassName());
+				obj=composite_end();
+				obj--;
+				result&=(*obj)->Configure((*cfg).GetConcreteConfig());
+			}
+			CLAM_ASSERT(mpConfig->GetConfigurations().size()==composite_size(),"Number of configurations should be the same as number of children");
+		
+			//TODO: right now there is no way to add or remove controls than to instantiate control array again
+			CLAM_ASSERT(mpConfig->GetOnArray().Size()==(int)composite_size(),"ProcessingChain::ConcreteConfigure: On array does not have same size as number of configurations");
+			TSize nControls=composite_size();
+			if(mpOnCtrlArray) delete mpOnCtrlArray;
+			mpOnCtrlArray= new InControlTmplArray<SMSTransformationChain>(nControls,"OnControlArray",this,NULL);
+			
+			for(int i=0;i<nControls;i++)
+			{
+				(*mpOnCtrlArray)[i].DoControl(mpConfig->GetOnArray()[i]);
+			}
+			return result;
+		}
+
+	private:
+
+		/** Adds a Processing chainee at the end of the chain. It instantiates a concrete Processing
+		 *	class using input string as a switch.
+		 */
+		bool AddChainee(const std::string& type)
+		{
+			Insert( *( ProcessingFactory::GetInstance().Create( type ) ) );
+						
+			return true;
+			
+		}
 		/** Helper method for updating frame counters both in ports and in internal data*/
 		void NextFrame()
 		{
 			mpChainInput->mCurrentFrameIndex++;
-			int i = mpChainInput->mCurrentFrameIndex++;
-			printf("TransChain::NextFrame input frame index=%d at=%x\t \n", i, int(&(mpChainInput->GetFramesArray()[i]) ));
+			mpChainInput->mCurrentFrameIndex++;
 		}
 		/** Returns true if current frame pointer at input port is pointing past the last
 		 *	frame in the segment
 		 */
 		bool IsLastFrame()
 		{
-			iterator obj;
-			int i=0;
-			for(obj=composite_begin(); obj!=composite_end(); i++, obj++)
+			for(iterator obj=composite_begin(); obj!=composite_end(); obj++)
 			{
 				SMSTransformation* transf=static_cast<SMSTransformation*>((*obj));
 				if(!transf->IsLastFrame()) return false;
@@ -161,6 +279,7 @@ namespace CLAM {
 
 	};
 
-};//namespace CLAM
+
+} // namespace CLAM
 
 #endif // _SMSTransformationChain_
