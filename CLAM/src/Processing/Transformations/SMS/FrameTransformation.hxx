3c0b17854a4906715646462897aeef8170b997c4
sroig <sroig@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2005-01-24 20:13:16 +0000

    *** empty log message ***

--- CLAM/src/Processing/Transformations/SMS/FrameTransformation.hxx
+++ CLAM/src/Processing/Transformations/SMS/FrameTransformation.hxx
@@ -1,20 +1,18 @@
-#ifndef _FrameTransformation_hxx
-#define _FrameTransformation_hxx
+#ifndef _FrameTransformation_
+#define _FrameTransformation_
 
 #include "Processing.hxx"
 
 namespace CLAM
 {
+	class Frame;
 
-class Frame;
+	class FrameTransformation : public Processing
+	{
+		public:
+			virtual bool Do(const Frame& in,Frame& out) = 0; 
+	};
 
-class FrameTransformation : public Processing
-{
-public:
-	virtual bool Do(const Frame& in, Frame& out) = 0;
-	
-};
-
-}
+} // namespace CLAM
 
 #endif
