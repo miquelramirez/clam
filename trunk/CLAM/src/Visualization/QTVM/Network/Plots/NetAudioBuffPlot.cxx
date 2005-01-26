08f04c0b3a780fd9d537c98aaf259963231ff218
imosquera <imosquera@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2005-01-26 19:41:38 +0000

    Adding audio buff plot

---
+++ trunk/CLAM/src/Visualization/QTVM/Network/Plots/NetAudioBuffPlot.cxx
@@ -0,0 +1,41 @@
+#include "NetAudioBuffPlotController.hxx"
+#include "NetAudioBuffPlot.hxx"
+
+namespace CLAM
+{
+	namespace VM
+	{
+		NetAudioBuffPlot::NetAudioBuffPlot(QWidget* parent, const char * name)
+			: NetPlot(parent,name)
+		{
+			InitNetAudioBuffPlot();
+		}
+
+		NetAudioBuffPlot::~NetAudioBuffPlot()
+		{
+		}
+
+		void NetAudioBuffPlot::SetData(const Audio& audio)
+		{
+			((NetAudioBuffPlotController*)_controller)->SetData(audio);
+		}
+
+		void NetAudioBuffPlot::SetDataColor(Color c)
+		{
+			((NetAudioBuffPlotController*)_controller)->SetDataColor(c);
+		}
+
+		void NetAudioBuffPlot::SetPlotController()
+		{
+			SetController(new NetAudioBuffPlotController());
+		}
+
+		void NetAudioBuffPlot::InitNetAudioBuffPlot()
+		{
+			SetPlotController();
+		}
+	}
+}
+
+// END
+
