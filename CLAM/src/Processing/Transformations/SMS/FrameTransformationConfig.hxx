66d85802e88c5d10ade0c5b4754da0d234538934
sroig <sroig@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2005-01-26 12:39:35 +0000

    *** empty log message ***

---
+++ CLAM/src/Processing/Transformations/SMS/FrameTransformationConfig.hxx
@@ -0,0 +1,18 @@
+#ifndef _FrameTransformationConfig_
+#define _FrameTransformationConfig_
+
+#include "Processing.hxx"
+
+namespace CLAM
+{
+
+	class FrameTransformationConfig : public ProcessingConfig
+	{
+		public:
+                DYNAMIC_TYPE_USING_INTERFACE (FrameTransformationConfig, 0,ProcessingConfig);
+
+	};
+
+} // namespace CLAM
+
+#endif
