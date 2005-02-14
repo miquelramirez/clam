2e4e238fafe1b87cac2c4fa68789b689df01c480
imosquera <imosquera@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2005-02-14 15:29:37 +0000

    adding methods to struct Color

--- trunk/CLAM/src/Visualization/QTVM/Utils/VMColor.cxx
+++ trunk/CLAM/src/Visualization/QTVM/Utils/VMColor.cxx
@@ -23,78 +23,110 @@
 
 namespace CLAM
 {
-	namespace VM
+    namespace VM
+    {
+	Color::Color()
 	{
-		Color VMColor::Red()
-		{
-			return Custom(255,0,0);
-		}
-
-		Color VMColor::Green()
-		{
-			return Custom(0,255,0);
-		}
-
-		Color VMColor::Blue()
-		{
-			return Custom(0,0,255);
-		}
-
-	    Color VMColor::Magenta()
-		{
-			return Custom(255,0,255);
-		}
-
-		Color VMColor::Cyan()
-		{
-			return Custom(0,255,255);
-		}
-
-		Color VMColor::White()
-		{
-			return Custom(255,255,255);
-		}
-
-		Color VMColor::Black()
-		{
-			return Custom(0,0,0);
-		}
-
-		Color VMColor::Yellow()
-		{
-			return Custom(255,255,0);
-		}
-
-		Color VMColor::Gray()
-		{
-			return Custom(127,127,127);
-		}
-		
-		Color VMColor::LightGray()
-		{
-			return Custom(185,185,185);
-		}
-
-		Color VMColor::Brown()
-		{
-			return Custom(153,102,31);
-		}
-
-	    Color VMColor::Orange()
-		{
-			return Custom(250,159,31);
-		}
-
-		Color VMColor::Custom(TUInt16 r, TUInt16 g, TUInt16 b)
-		{
-			Color c;
-			c.r = r;
-			c.g = g;
-			c.b = b;
-			return c;
-		}
-
+	    r=0; 
+	    g=0; 
+	    b=0;
 	}
+
+	Color::Color(TUInt16 red, TUInt16 green, TUInt16 blue)
+	{
+	    r=red;
+	    g=green;
+	    b=blue;
+	}
+
+	Color::Color(const Color& c)
+	{
+	    r=c.r;
+	    g=c.g;
+	    b=c.b;
+	}
+
+	Color::~Color()
+	{
+	}
+
+	void Color::operator=(Color c)
+	{
+	    r=c.r;
+	    g=c.g;
+	    b=c.b;
+	}
+
+	Color VMColor::Red()
+	{
+	    return Custom(255,0,0);
+	}
+
+	Color VMColor::Green()
+	{
+	    return Custom(0,255,0);
+	}
+
+	Color VMColor::Blue()
+	{
+	    return Custom(0,0,255);
+	}
+
+	Color VMColor::Magenta()
+	{
+	    return Custom(255,0,255);
+	}
+
+	Color VMColor::Cyan()
+	{
+	    return Custom(0,255,255);
+	}
+
+	Color VMColor::White()
+	{
+	    return Custom(255,255,255);
+	}
+
+	Color VMColor::Black()
+	{
+	    return Custom(0,0,0);
+	}
+
+	Color VMColor::Yellow()
+	{
+	    return Custom(255,255,0);
+	}
+
+	Color VMColor::Gray()
+	{
+	    return Custom(127,127,127);
+	}
+		
+	Color VMColor::LightGray()
+	{
+	    return Custom(185,185,185);
+	}
+
+	Color VMColor::Brown()
+	{
+	    return Custom(153,102,31);
+	}
+
+	Color VMColor::Orange()
+	{
+	    return Custom(250,159,31);
+	}
+
+	Color VMColor::Custom(TUInt16 r, TUInt16 g, TUInt16 b)
+	{
+	    Color c;
+	    c.r = r;
+	    c.g = g;
+	    c.b = b;
+	    return c;
+	}
+
+    }
 }
 
 // END
