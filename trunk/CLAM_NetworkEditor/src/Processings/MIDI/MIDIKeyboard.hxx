54cae54138f0c4869f884b4f76307997cc009979
sroig <sroig@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2005-02-17 20:05:28 +0000

    Added MIDI wrapper to provide quick MIDI-input functionality.

---
+++ trunk/CLAM_NetworkEditor/src/Processings/MIDI/MIDIKeyboard.hxx
@@ -0,0 +1,70 @@
+#ifndef _MIDIKeyboard_HXX_
+#define _MIDIKeyboard_HXX_
+
+#include "OutControl.hxx"
+#include "OutControlPublisher.hxx"
+#include "MIDIInControl.hxx"
+#include "MIDIClocker.hxx"
+#include "Processing.hxx"
+#include "ProcessingConfig.hxx"
+
+namespace CLAM 
+{
+
+class MIDIKeyboardConfig : public ProcessingConfig
+{
+	DYNAMIC_TYPE_USING_INTERFACE (MIDIKeyboardConfig, 1, ProcessingConfig);
+	DYN_ATTRIBUTE (0, public, std::string, MidiDevice);
+
+	void DefaultInit(void);
+};
+
+/* simple wrapper to provide quick and dirty access to MIDI devices in NetworkEditor */
+class MIDIKeyboard : public Processing
+{
+		MIDIKeyboardConfig mConfig;
+
+		TControlData mCurrentTime;
+		TControlData mCurrentTimeIncrement;
+
+		MIDIIOConfig mNoteInConfig;
+		MIDIInControl mNoteIn;
+
+		MIDIIOConfig mPitchBendInConfig;
+		MIDIInControl mPitchBendIn;
+
+		MIDIIOConfig mModulationConfig;
+		MIDIInControl mModulationIn;
+
+		MIDIClockerConfig mClockerConfig;
+		MIDIClocker mClocker;
+
+		OutControlPublisher mNoteOut;
+		OutControlPublisher mVelocityOut;
+		OutControlPublisher mPitchBendOut;
+		OutControlPublisher mModulationOut;
+
+	public:
+
+		const char* GetClassName() const { return "MIDIKeyboard"; }
+
+		MIDIKeyboard();
+		
+		MIDIKeyboard( ProcessingConfig& cfg );
+
+		bool ConcreteConfigure( const ProcessingConfig& cfg );
+
+		bool Do();
+
+		~MIDIKeyboard() {}
+
+		const ProcessingConfig& GetConfig() const { return mConfig; }
+
+	private:
+
+		bool ConfigureAndCheck( Processing& p, ProcessingConfig& cfg );
+};
+
+} // namespace CLAM 
+
+#endif
