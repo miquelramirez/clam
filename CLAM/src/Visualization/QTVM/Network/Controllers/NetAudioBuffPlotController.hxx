1b215f7161e688bce2760f9fdb4bfa9385300eb2
imosquera <imosquera@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2005-02-17 15:52:35 +0000

    assign a new renderer and avoid unnecessary copy of data

--- CLAM/src/Visualization/QTVM/Network/Controllers/NetAudioBuffPlotController.hxx
+++ CLAM/src/Visualization/QTVM/Network/Controllers/NetAudioBuffPlotController.hxx
@@ -2,7 +2,7 @@
 #define __NETAUDIOBUFFPLOTCONTROLLER__
 
 #include "Audio.hxx"
-#include "DataRenderer.hxx"
+#include "BuffDataRenderer.hxx"
 #include "NetPlotController.hxx"
 
 namespace CLAM
@@ -21,14 +21,12 @@ namespace CLAM
 
 	private:
 	    DataArray _cachedData;
-	    DataArray _processedData;
-	    DataRenderer _renderer;
+	    BuffDataRenderer _renderer;
 	    int _index;
 	    bool _first;
 	    TSize _frameSize;
 	    
 	    void AddData(const DataArray& data);
-	    void ProcessData();
 	  
 	    void Init(const TSize& frameSize);
 	    void FullView();
