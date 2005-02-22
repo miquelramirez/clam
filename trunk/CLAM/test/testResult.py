493fa881c5efbfd142c9b14c0d7f34eb972ee94d
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2005-02-22 17:22:44 +0000

    new sandboxes name for clam examples

--- trunk/CLAM/test/testResult.py
+++ trunk/CLAM/test/testResult.py
@@ -3,25 +3,29 @@
 class TestResult :
 	"info obtained from execution of a test"
 	def __init__(self) :
-		self.testname = "unnamed test"
-		self.isAutoTest = False
-		self.autoTestNFailures = 0
-		self.autoTestNErrors = 0
-		self.autoTestNTests = 0
-		self.debugCompilationOk = True
-		self.releaseCompilationOk = True
-		self.debugNWarnings = 0
-		self.releaseNWarnings = 0
-		self.isMandatory = True
-		self.reportedError = ""
+		self._currentConfig = "debug"
+		self._testname = "unnamed test"
+		self._isAutoTest = False
+		self._autoTestNFailures = 0
+		self._autoTestNErrors = 0
+		self._autoTestNTests = 0
+		self._debugCompilationOk = True
+		self._releaseCompilationOk = True
+		self._debugNWarnings = 0
+		self._releaseNWarnings = 0
+		self._isMandatory = True # TODO useit
+		self._reportedError = ""
 
+	def currentConfig(self, config) :
+		self._currentConfig = config
+		
 	def passed(self):
-		if not self.debugCompilationOk :
+		if not self._debugCompilationOk :
 			return False
-		if not self.releaseCompilationOk :
+		if not self._releaseCompilationOk :
 			return False
-		if self.isAutoTest :
-			return not self.autoTestNFailures and not self.autoTestNErrors
+		if self._isAutoTest :
+			return not self._autoTestNFailures and not self._autoTestNErrors
 		return True
 	def __checkConfiguration__(self, configuration):
 		if not configuration in ["debug", "release"] :
@@ -29,41 +33,41 @@ class TestResult :
 	def compilationStatus(self, configuration, ok):
 		self.__checkConfiguration__(configuration)
 		if configuration == "debug" :
-			self.debugCompilationOk = ok
+			self._debugCompilationOk = ok
 		else : # release
-			self.releaseCompilationOk = ok
+			self._releaseCompilationOk = ok
 		
 	def nWarnings(self, configuration, num):
 		self.__checkConfiguration__(configuration)
 		if configuration == "debug" :
-			self.debugNWarnings = num
+			self._debugNWarnings = num
 		else : # release
-			self.releaseNWarnings = num
+			self._releaseNWarnings = num
 
 	def reportError(self, configuration, error):
 		self.__checkConfiguration__(configuration)
-		self.reportedError = error
+		self._reportedError = error
 
 	def name(self, aName) :
-		self.testname = aName
+		self._testname = aName
 	
 	def nFailures(self, n) :
-		self.autoTestNFailures = n
+		self._autoTestNFailures = n
 	
 	def nErrors(self, n) :
-		self.autoTestNErrors = n
+		self._autoTestNErrors = n
 	
 	def nTests(self, n) :
-		self.autoTestNTests = n
+		self._autoTestNTests = n
 	
 	def getNFailures(self) :
-		return self.autoTestNFailures
+		return self._autoTestNFailures
 	
 	def getNErrors(self) :
-		return self.autoTestNErrors 
+		return self._autoTestNErrors 
 	
 	def getNTests(self) :
-		return self.autoTestNTests 
+		return self._autoTestNTests 
 
 #--------------------
 # Tests
@@ -75,16 +79,16 @@ class TestTestResult (unittest.TestCase) :
 		self.test = TestResult()
 
 	def testConstructor(self):
-		self.assertEqual("unnamed test", self.test.testname)
+		self.assertEqual("unnamed test", self.test._testname)
 
 	def testPassed_whenDebugErrors(self): 
 		"""passed should return false when debug errors"""
-		self.test.debugCompilationOk = False
+		self.test._debugCompilationOk = False
 		self.assertEqual(False, self.test.passed())
 	
 	def testPassed_whenReleaseErrors(self): 
 		"""passed should return false when release errors"""
-		self.test.releaseCompilationOk = False
+		self.test._releaseCompilationOk = False
 		self.assertEqual(False, self.test.passed())
 
 	def testPassed_whenNoErrorsSet(self):
@@ -92,21 +96,21 @@ class TestTestResult (unittest.TestCase) :
 
 	def testPassed_whenNoErrorsSetAndIsAutoTest_Failures(self):
 		"if autotest and tests not passed, should return false"
-		self.test.isAutoTest = True
+		self.test._isAutoTest = True
 		self.test.nFailures(1)
 		assert not self.test.passed()
 		self.assertEqual( 1, self.test.getNFailures() )
 	
 	def testPassed_whenNoErrorsSetAndIsAutoTest_Errors(self):
 		"if autotest and tests not passed, should return false"
-		self.test.isAutoTest = True
+		self.test._isAutoTest = True
 		self.test.nErrors(1)
 		assert not self.test.passed()
 		self.assertEqual( 1, self.test.getNErrors() )
 		
 	def testPassed_whenNoErrorsSet_IsAutoTest_Passed(self):
 		"passed autotests should return true"
-		self.test.isAutoTest = True
+		self.test._isAutoTest = True
 		assert self.test.passed()
 		
 	def testConfigurationStatus_whithInvalidConfiguration(self):
@@ -115,30 +119,30 @@ class TestTestResult (unittest.TestCase) :
 			self.test.compilationStatus, 'debbug', True)
 		
 	def testConfigurationStatus_DebugOk(self):
-		self.test.debugCompilationOk = False
+		self.test._debugCompilationOk = False
 		self.test.compilationStatus('debug', True)
-		assert self.test.debugCompilationOk
+		assert self.test._debugCompilationOk
 
 	def testConfigurationStatus_ReleaseNotOk(self):
-		self.test.debugCompilationOk = True
+		self.test._debugCompilationOk = True
 		self.test.compilationStatus('release', False)
-		assert not self.test.releaseCompilationOk
+		assert not self.test._releaseCompilationOk
 		
 	def testNWarnings_Debug(self):
 		self.test.nWarnings('debug', 2)
-		assert 2 == self.test.debugNWarnings
+		assert 2 == self.test._debugNWarnings
 	
 	def testNWarnings_Release(self):
 		self.test.nWarnings('release', 1)
-		assert 1 == self.test.releaseNWarnings
+		assert 1 == self.test._releaseNWarnings
 	
 	def testReportedError(self):
 		self.test.reportError('debug', 'un error')
-		assert 'un error' == self.test.reportedError
+		assert 'un error' == self.test._reportedError
 
 	def testName(self):
 		self.test.name('a test')
-		assert 'a test' == self.test.testname
+		assert 'a test' == self.test._testname
 
 
 if __name__ == "__main__":
