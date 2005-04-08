6344cf458b05a073ceb8afb1840f777caf89aaeb
xamat <xamat@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2005-04-08 23:25:30 +0000

    At least I was able to integrate my newest additions with Ismael's. It compiles and runs, but there are some strange things in the LLBPFS (one more tab) that I haven't been able to test completely.

--- trunk/CLAM_Annotator/src/Annotator.hxx
+++ trunk/CLAM_Annotator/src/Annotator.hxx
@@ -9,7 +9,7 @@
 
 #include "Audio.hxx"
 
-#include "LLDSchema.hxx"
+#include "Schema.hxx"
 #include "DescriptionScheme.hxx"
 #include "Pool.hxx"
 
@@ -33,117 +33,131 @@ class Annotator : public AnnotatorBase
   Q_OBJECT
   
 
-   	public:
-		Annotator( const std::string &, const AnnotatorDataFacade::StringList &, AnnotatorDataFacade &, QWidget * parent = 0, const char * name = 0, WFlags f = 0);
-                Annotator();
-                void initSongs(const std::string &, const std::vector<std::string>&);
-		void fillGlobalDescriptors( int index);
-		void songsClicked( QListViewItem * item);
-		void playPause();
-		void chooseColor();
-		void initFileMenu();
-		void initEditMenu();
+public:
+  Annotator( const std::string &, const AnnotatorDataFacade::StringList &, AnnotatorDataFacade &, QWidget * parent = 0, const char * name = 0, WFlags f = 0);
+  Annotator();
+  void initSongs(const std::string &, const std::vector<std::string>&);
+  void fillGlobalDescriptors( int index);
+  void songsClicked( QListViewItem * item);
+  void playPause();
+  void chooseColor();
+  void initFileMenu();
+  void initEditMenu();
 
-                void initProject();
-                void initInterface();
-	public slots:
-		void descriptorsTableChanged( int, int);
-		void fileNew();			
-		void fileOpen();
-		void addSongsToProject();
-		void addSongs( const AnnotatorDataFacade::StringList & );
-		void deleteAllSongsFromProject();
-
-                void deleteSongsFromProject();
-		void fileSaveAs();
-		void fileSave();
-		void fileMenuAboutToShow();
-		void editMenuAboutToShow();
-                void showAnalyzeWindow();
-                void analyze();
-                void analyzeAll();
-	protected:
-		void closeEvent( QCloseEvent * e);
-                bool event(QEvent* e);
-
-	private:
-		void value( const std::string & descriptor, std::string & descriptorValue ) const;
-		void createListOfGenres( QStringList &, const QString & ) const;
-		void createListOfTonalKeys( QStringList & list, const QString & value ) const;
-		bool somethingIsSelected() const;
-		void initSpliters();
-		void initView();
-		void initPlay();
-		void makeDescriptorTable();
-		void makeAnnotatorBrowserGLMenu();
-		void makeConnections();
-		void descriptor( std::string &, int);
-		void currentFile( std::string &);
-		void changeCurrentFile();
-		void markAllNoChanges();
-		void drawDescriptorsName();
-		void drawDescriptorsValue( int, bool );
-		void drawArtists( int, bool );
-		void drawTitle( int, bool );
-		void drawGenre( int, bool );
-		void drawDanceability( int, bool );
-		void drawTonality( int, bool );
-		void drawBPM( int, bool );
-		void drawDynamicComplexity( int, bool );
-//xamat
-                void initDataFacade();
-                void languageChange();
-                void initAudioWidget();
-                void initLLDescriptorsWidgets();
-                void removeLLDTabs();
-		void drawAudio(QListViewItem * item);
-		void loadAudioFile(const char* filename);
+  void initProject();
+  void initInterface();
+public slots:
+  void descriptorsTableChanged( int, int);
+  void fileNew();			
+  void fileOpen();
+  void addSongsToProject();
+  void addSongs( const AnnotatorDataFacade::StringList & );
   
-                void doAnalysis();
-                void drawLLDescriptors(int index);
-                void generateRandomEnvelopes();
-		CLAM::BPF generateRandomEnvelope();
+  void loadSongList();
+  void loadSchema();
+  void loadDescriptors();
 
-                void generateEnvelopesFromDescriptors();
-                CLAM::BPF generateEnvelopeFromDescriptor(const std::string& name);
+  void saveSongList();
+  void saveSchema();
+  void saveDescriptors();
+  void saveAll();
 
-                void LoadDescriptorPool();
+  void deleteSongsFromProject();
+  void fileSaveAs();
+  void fileSave();
+  void fileMenuAboutToShow();
+  void editMenuAboutToShow();
+  void showAnalyzeWindow();
+  void analyze();
+  void analyzeAll();
+protected:
+  void closeEvent( QCloseEvent * e);
+  bool event(QEvent* e);
 
-                double GetMinY(const CLAM::BPF& bpf);
-                double GetMaxY(const CLAM::BPF& bpf);
-
-	private:
-		bool mChanges;
-		AnnotatorDataFacade* mpData;
-		
-                SongFiles mSongFiles;
-                Project mProject;
-                std::string mProjectFileName;
-
-                typedef std::map< std::string, int > SongDescriptorsIndex;
-		SongDescriptorsIndex mSongDescriptorsIndex;
-		typedef std::vector< Song::Segments > SongSegments;
-		SongSegments mSongSegments;
+private:
+  void value( const std::string & descriptor, std::string & descriptorValue ) const;
+  void createListOfGenres( QStringList &, const QString & ) const;
+  void createListOfTonalKeys( QStringList & list, const QString & value ) const;
+  bool somethingIsSelected() const;
+  void initSpliters();
+  void initView();
+  void initPlay();
+  void makeDescriptorTable();
+  void makeAnnotatorBrowserGLMenu();
+  void makeConnections();
+  void descriptor( std::string &, int);
+  void currentFile( std::string &);
+  void changeCurrentFile();
+  void markAllNoChanges();
+  void drawDescriptorsName();
+  void drawDescriptorsValue( int, bool );
+  void drawArtists( int, bool );
+  void drawTitle( int, bool );
+  void drawGenre( int, bool );
+  void drawDanceability( int, bool );
+  void drawTonality( int, bool );
+  void drawBPM( int, bool );
+  void drawDynamicComplexity( int, bool );
 //xamat
-		CLAM::VM::QtAudioPlot* mpAudioPlot;
-                CLAM::Audio mCurrentAudio;
+  void initDataFacade();
+  void languageChange();
+  void initAudioWidget();
+  void initLLDescriptorsWidgets();
+  void removeLLDTabs();
+  void drawAudio(QListViewItem * item);
+  void loadAudioFile(const char* filename);
+  void deleteAllSongsFromProject();
+  
+  void doAnalysis();
+  void drawLLDescriptors(int index);
+  void drawHLD(int songIndex, const std::string& descriptorName, std::string& value, 
+	       bool computed );
+  void drawHLD(int songIndex, const std::string& descriptorName, float value, bool computed);
+  void drawHLD(int songIndex, const std::string& descriptorName, int value, bool computed);
+ 
+  int findHLDescriptorIndex(const std::string& name);
 
-                std::vector<CLAM::BPF> mBPFs;
-                std::vector<CLAM::VM::BPFEditor*> mBPFEditors;
+  void initEnvelopes();
+  void generateRandomEnvelopes();
+  CLAM::BPF generateRandomEnvelope();
+
+  void generateEnvelopesFromDescriptors();
+  CLAM::BPF generateEnvelopeFromDescriptor(const std::string& name);
+
+  void loadDescriptorPool();
+
+  double GetMinY(const CLAM::BPF& bpf);
+  double GetMaxY(const CLAM::BPF& bpf);
+
+private:
+  CLAM::VM::QtAudioPlot* mpAudioPlot;
+  CLAM::Audio mCurrentAudio;
+
+  CLAM_Annotator::Project mProject;
+  CLAM_Annotator::SongFiles mSongFiles;
+  std::vector<Song::Segments> mSongSegments;
+  std::string mProjectFileName;
+  std::map<std::string,int> mSongDescriptorsIndex;
+
+  bool mChanges;
+  AnnotatorDataFacade* mpData;
+  std::vector<CLAM::BPF> mBPFs;
+  std::vector<CLAM::VM::BPFEditor*> mBPFEditors;
+
                 
-                int mCurrentIndex;
+  int mCurrentIndex;
                 
-                std::vector<bool> mHaveLLDescriptors;
-                std::vector<bool> mHaveHLDescriptors;
+  std::vector<bool> mHaveLLDescriptors;
+  std::vector<bool> mHaveHLDescriptors;
                 
-                QVBoxLayout* mpTabLayout;
-                std::vector<QWidget*> mTabPages;
+  QVBoxLayout* mpTabLayout;
+  std::vector<QWidget*> mTabPages;
                
-                CLAM::LLDSchema mLLDSchema;
-                CLAM::DescriptionScheme mDescriptionScheme;
-                CLAM::DescriptionDataPool* mpDescriptorPool;
+  CLAM_Annotator::Schema mSchema;
+  CLAM::DescriptionScheme mDescriptionScheme;
+  CLAM::DescriptionDataPool* mpDescriptorPool;
 
-                QProgressDialog* mpProgressDialog;
+  QProgressDialog* mpProgressDialog;
 
                
 };
