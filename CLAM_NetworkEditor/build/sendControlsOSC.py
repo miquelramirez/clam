24a81df4c17efc945cf9ed2110a34812622acd33
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2005-04-13 15:39:01 +0000

    localhost or remote host now is chosed wich commandline

--- CLAM_NetworkEditor/build/sendControlsOSC.py
+++ CLAM_NetworkEditor/build/sendControlsOSC.py
@@ -4,6 +4,17 @@ import sys
 from OSC import Message
 
 if __name__ == "__main__":
+	if not (len(sys.argv)==1 or len(sys.argv)==2) :
+		print "usage: sendControlsOSC [host]\n"	
+		sys.exit(1)
+
+	if len(sys.argv)==2 :
+		host = sys.argv[1]
+		print "using host:", host
+	else:
+		host = ""
+		print "using local host"
+
 	procname="-"
 	print ">Processing name?"
 	procname=sys.stdin.readline()
@@ -16,6 +27,9 @@ if __name__ == "__main__":
 		value=float(sys.stdin.readline())
 
 		dest = "/"+procname[:-1]+"/"+ctrlname[:-1]
-		Message(dest, [value]).sendto("193.145.55.66", 7000)
+		if host :
+			Message(dest, [value]).sendto(host, 7000)
+		else :
+			Message(dest, [value]).sendlocal(7000)
 
 
