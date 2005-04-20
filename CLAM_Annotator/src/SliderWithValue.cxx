42e41a4913e09d275aa3f05d0ca83e9827a638af
imosquera <imosquera@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2005-04-20 12:47:09 +0000

    Now SliderWithValue is an abstract Base class for SliderWithIntValue and SliderWithFloatValue.

--- CLAM_Annotator/src/SliderWithValue.cxx
+++ CLAM_Annotator/src/SliderWithValue.cxx
@@ -7,24 +7,20 @@ SliderWithValue::SliderWithValue( QWidget * parent, const char * name, WFlags f)
 {
 	QHBoxLayout * layout = new QHBoxLayout(this);
 	mSlider = new QSlider(this, "slider");
-	mSlider->setMaxValue(10);
-	mSlider->setMinValue(0);
 	mSlider->setPageStep(1);
 	mSlider->setOrientation( Qt::Horizontal );
 	layout->addWidget( mSlider );
 	mValue = new QLabel(this, "value");
 	layout->addWidget( mValue );
-	connect( mSlider, SIGNAL( valueChanged(int) ), mValue, SLOT( setNum(int) ) );
 }
+
+SliderWithValue::~SliderWithValue()
+{
+}
+
 
 int SliderWithValue::value()
 {
-	return mSlider->value();
-}
-
-void SliderWithValue::setValue(int value)
-{
-	mSlider->setValue( value );
-	mValue->setText(QString().setNum( value ) );
+    return mSlider->value();
 }
 
