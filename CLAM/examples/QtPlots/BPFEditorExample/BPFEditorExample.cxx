d541e6ff571317389c6826872904f7c4a5d5c47a
imosquera <imosquera@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2005-04-25 13:11:45 +0000

    add player

--- CLAM/examples/QtPlots/BPFEditorExample/BPFEditorExample.cxx
+++ CLAM/examples/QtPlots/BPFEditorExample/BPFEditorExample.cxx
@@ -1,26 +1,37 @@
+#include "XMLStorage.hxx"
 #include "QtAppWrapper.hxx"
 #include "BPFEditor.hxx"
 
-using CLAM::TData;
-
 int main()
 {
+    CLAM::Melody melody;
+    CLAM::XMLStorage::Restore(melody,"../data/melody.xml");
+
+    CLAM::TData duration = melody.GetNoteArray()[melody.GetNumberOfNotes()-1].GetTime().GetEnd();
+
+    CLAM::TData max = -1E9;
+    CLAM::TData min = 1E9;
+
     CLAM::BPF bpf;
-    bpf.SetSize(0);
-    bpf.Insert(TData(0.0),TData(0.0));
-    bpf.Insert(TData(20.0),TData(20000.0));
-    bpf.Insert(TData(60.0),TData(11000.0));
-    bpf.Insert(TData(150.0),TData(11000.0));
-    bpf.Insert(TData(200.0),TData(0.0));
+
+    for(int i=0; i < melody.GetNumberOfNotes(); i++)
+    {
+	CLAM::TData value = melody.GetNoteArray()[i].GetFundFreq();
+	if(value < min) min = value;
+	if(value > max) max = value;
+	bpf.Insert(melody.GetNoteArray()[i].GetTime().GetBegin(), value);
+    }
+    
+    CLAM::TData span = max-min;
 
     CLAM::VM::QtAppWrapper::Init();
 
     CLAM::VM::BPFEditor editor;
     editor.Label("BPFEditor");
     editor.Geometry(100,100,600,400);
-    editor.SetXRange(0.0,200.0);
-    editor.SetYRange(0.0,22050.0);
-    editor.SetData(bpf);
+    editor.SetXRange(0.0,duration);
+    editor.SetYRange(double(min)-double(span)*0.1, double(max)+double(span)*0.1);
+    editor.SetData(bpf,duration);
     editor.Show();
 
     CLAM::VM::QtAppWrapper::Run();
