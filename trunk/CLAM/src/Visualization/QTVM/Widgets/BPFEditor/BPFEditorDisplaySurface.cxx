e8a0ba19d783686280712d2fcbd737c5d7b0dbb2
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2005-05-02 02:16:21 +0000

    Tabbed indentation

--- trunk/CLAM/src/Visualization/QTVM/Widgets/BPFEditor/BPFEditorDisplaySurface.cxx
+++ trunk/CLAM/src/Visualization/QTVM/Widgets/BPFEditor/BPFEditorDisplaySurface.cxx
@@ -4,165 +4,165 @@
 
 namespace CLAM
 {
-    namespace VM
-    {
-	BPFEditorDisplaySurface::BPFEditorDisplaySurface(QWidget* parent)
-	    : QGLWidget(parent), 
-	      mController(0),
-	      mWidth(0),mHeight(0),
-	      mDoResize(false)
+	namespace VM
 	{
-	    setMouseTracking(true);
-	    setFocusPolicy(StrongFocus);
-	    setAutoBufferSwap(false);
-	    SetBackgroundColor(0.0f,0.0f,0.0f);
-	}
-
-	BPFEditorDisplaySurface::~BPFEditorDisplaySurface()
-	{
-	    if(mController) delete mController;
-	}
-
-	void BPFEditorDisplaySurface::SetController(BPFEditorController* controller)
-	{
-	    mController = controller;
-	    
-	    connect(mController,SIGNAL(requestRefresh()),this,SLOT(updateGL()));
-	    connect(mController,SIGNAL(viewChanged(GLView)),this,SLOT(updateView(GLView)));
-	    connect(mController,SIGNAL(cursorChanged(QCursor)),this,SLOT(changeCursor(QCursor)));
-	}
-
-	void BPFEditorDisplaySurface::SetBackgroundColor(const float& r, const float& g, const float& b)
-	{
-	    mRed = r;
-	    mGreen = g;
-	    mBlue = b;
-
-	    updateGL();
-	}
-
-	void BPFEditorDisplaySurface::updateView(GLView view)
-	{
-	    mView.mLeft = view.mLeft;
-	    mView.mRight = view.mRight;
-	    mView.mBottom = view.mBottom;
-	    mView.mTop = view.mTop;
-	}
-
-	void BPFEditorDisplaySurface::changeCursor(QCursor cursor)
-	{
-	    setCursor(cursor);
-	}
-
-	void BPFEditorDisplaySurface::paintGL()
-	{
-	    if(!mController) return;
-	    if(mDoResize)
-	    {
-		glViewport(0,0,mWidth,mHeight);
-		mDoResize=false;
-	    }
-	    glMatrixMode(GL_PROJECTION);
-	    glLoadIdentity();
-	    glOrtho(mView.mLeft,mView.mRight,mView.mBottom,mView.mTop,-1.0,1.0);
-	    glMatrixMode(GL_MODELVIEW);
-	    glShadeModel(GL_FLAT);
-	    glClearColor(GLfloat(mRed),GLfloat(mGreen),GLfloat(mBlue),1.0);
-	    glClear(GL_COLOR_BUFFER_BIT);
-	    mController->Draw();
-	    swapBuffers();
-	}
-
-	void BPFEditorDisplaySurface::mousePressEvent(QMouseEvent* e)
-	{
-	    if(mController)
-	    {
-		if(e->button() == LeftButton)
+		BPFEditorDisplaySurface::BPFEditorDisplaySurface(QWidget* parent)
+			: QGLWidget(parent), 
+			  mController(0),
+			  mWidth(0),mHeight(0),
+			  mDoResize(false)
 		{
-		    mController->SetLeftButtonPressed(true);
-		    double xcoord = double(e->x());
-		    xcoord *= (mView.mRight-mView.mLeft);
-		    xcoord /= double(width());
-		    xcoord += mView.mLeft;
-		    double ycoord = double(-e->y())+double(height());
-		    ycoord *= (mView.mTop-mView.mBottom);
-		    ycoord /= double(height());
-		    ycoord += mView.mBottom;
-		    mController->SetPoint(TData(xcoord),TData(ycoord));
+			setMouseTracking(true);
+			setFocusPolicy(StrongFocus);
+			setAutoBufferSwap(false);
+			SetBackgroundColor(0.0f,0.0f,0.0f);
 		}
 
-		if(e->button() == RightButton)
+		BPFEditorDisplaySurface::~BPFEditorDisplaySurface()
 		{
-		    mController->SetRightButtonPressed(true);
-		}
-	    }
-	}
-
-	void BPFEditorDisplaySurface::mouseReleaseEvent(QMouseEvent* e)
-	{
-	    if(mController)
-	    {
-		if(e->button() == LeftButton)
-		{
-		    mController->SetLeftButtonPressed(false);
-		    double xcoord = double(e->x());
-		    xcoord *= (mView.mRight-mView.mLeft);
-		    xcoord /= double(width());
-		    xcoord += mView.mLeft;
-		    double ycoord = double(-e->y())+double(height());
-		    ycoord *= (mView.mTop-mView.mBottom);
-		    ycoord /= double(height());
-		    ycoord += mView.mBottom;
-		    mController->SetPoint(TData(xcoord),TData(ycoord));	    
+			if(mController) delete mController;
 		}
 
-		if(e->button() == RightButton)
+		void BPFEditorDisplaySurface::SetController(BPFEditorController* controller)
 		{
-		    mController->SetRightButtonPressed(false);
+			mController = controller;
+			
+			connect(mController,SIGNAL(requestRefresh()),this,SLOT(updateGL()));
+			connect(mController,SIGNAL(viewChanged(GLView)),this,SLOT(updateView(GLView)));
+			connect(mController,SIGNAL(cursorChanged(QCursor)),this,SLOT(changeCursor(QCursor)));
 		}
-	    }
-	}
 
-	void BPFEditorDisplaySurface::mouseMoveEvent(QMouseEvent* e)
-	{
-	    if(mController)
-	    {
-		double xcoord = double(e->x());
-		xcoord *= (mView.mRight-mView.mLeft);
-		xcoord /= double(width());
-		xcoord += mView.mLeft;
-		double ycoord = double(-e->y())+double(height());
-		ycoord *= (mView.mTop-mView.mBottom);
-		ycoord /= double(height());
-		ycoord += mView.mBottom;
-		mController->UpdatePoint(TData(xcoord),TData(ycoord));
-	    }
-	}
+		void BPFEditorDisplaySurface::SetBackgroundColor(const float& r, const float& g, const float& b)
+		{
+			mRed = r;
+			mGreen = g;
+			mBlue = b;
 
-	void BPFEditorDisplaySurface::leaveEvent(QEvent* e)
-	{
-	    if(mController)
-	    {
-		mController->MouseOverDisplay(false);
-	    }
-	}
+			updateGL();
+		}
 
-	void BPFEditorDisplaySurface::enterEvent(QEvent* e)
-	{
-	    if(mController)
-	    {
-		mController->MouseOverDisplay(true);
-	    }
+		void BPFEditorDisplaySurface::updateView(GLView view)
+		{
+			mView.mLeft = view.mLeft;
+			mView.mRight = view.mRight;
+			mView.mBottom = view.mBottom;
+			mView.mTop = view.mTop;
+		}
+
+		void BPFEditorDisplaySurface::changeCursor(QCursor cursor)
+		{
+			setCursor(cursor);
+		}
+
+		void BPFEditorDisplaySurface::paintGL()
+		{
+			if(!mController) return;
+			if(mDoResize)
+			{
+				glViewport(0,0,mWidth,mHeight);
+				mDoResize=false;
+			}
+			glMatrixMode(GL_PROJECTION);
+			glLoadIdentity();
+			glOrtho(mView.mLeft,mView.mRight,mView.mBottom,mView.mTop,-1.0,1.0);
+			glMatrixMode(GL_MODELVIEW);
+			glShadeModel(GL_FLAT);
+			glClearColor(GLfloat(mRed),GLfloat(mGreen),GLfloat(mBlue),1.0);
+			glClear(GL_COLOR_BUFFER_BIT);
+			mController->Draw();
+			swapBuffers();
+		}
+
+		void BPFEditorDisplaySurface::mousePressEvent(QMouseEvent* e)
+		{
+			if(mController)
+			{
+				if(e->button() == LeftButton)
+				{
+					mController->SetLeftButtonPressed(true);
+					double xcoord = double(e->x());
+					xcoord *= (mView.mRight-mView.mLeft);
+					xcoord /= double(width());
+					xcoord += mView.mLeft;
+					double ycoord = double(-e->y())+double(height());
+					ycoord *= (mView.mTop-mView.mBottom);
+					ycoord /= double(height());
+					ycoord += mView.mBottom;
+					mController->SetPoint(TData(xcoord),TData(ycoord));
+				}
+
+				if(e->button() == RightButton)
+				{
+					mController->SetRightButtonPressed(true);
+				}
+			}
+		}
+
+		void BPFEditorDisplaySurface::mouseReleaseEvent(QMouseEvent* e)
+		{
+			if(mController)
+			{
+				if(e->button() == LeftButton)
+				{
+					mController->SetLeftButtonPressed(false);
+					double xcoord = double(e->x());
+					xcoord *= (mView.mRight-mView.mLeft);
+					xcoord /= double(width());
+					xcoord += mView.mLeft;
+					double ycoord = double(-e->y())+double(height());
+					ycoord *= (mView.mTop-mView.mBottom);
+					ycoord /= double(height());
+					ycoord += mView.mBottom;
+					mController->SetPoint(TData(xcoord),TData(ycoord));			
+				}
+
+				if(e->button() == RightButton)
+				{
+					mController->SetRightButtonPressed(false);
+				}
+			}
+		}
+
+		void BPFEditorDisplaySurface::mouseMoveEvent(QMouseEvent* e)
+		{
+			if(mController)
+			{
+				double xcoord = double(e->x());
+				xcoord *= (mView.mRight-mView.mLeft);
+				xcoord /= double(width());
+				xcoord += mView.mLeft;
+				double ycoord = double(-e->y())+double(height());
+				ycoord *= (mView.mTop-mView.mBottom);
+				ycoord /= double(height());
+				ycoord += mView.mBottom;
+				mController->UpdatePoint(TData(xcoord),TData(ycoord));
+			}
+		}
+
+		void BPFEditorDisplaySurface::leaveEvent(QEvent* e)
+		{
+			if(mController)
+			{
+				mController->MouseOverDisplay(false);
+			}
+		}
+
+		void BPFEditorDisplaySurface::enterEvent(QEvent* e)
+		{
+			if(mController)
+			{
+				mController->MouseOverDisplay(true);
+			}
+		}
+		
+		void BPFEditorDisplaySurface::resizeEvent(QResizeEvent *e)
+		{
+			mWidth = e->size().width();
+			mHeight = e->size().height();
+			mDoResize = true;
+			if(mController) mController->DisplayDimensions(mWidth,mHeight);
+		}
 	}
-	
-	void BPFEditorDisplaySurface::resizeEvent(QResizeEvent *e)
-	{
-	    mWidth = e->size().width();
-	    mHeight = e->size().height();
-	    mDoResize = true;
-	    if(mController) mController->DisplayDimensions(mWidth,mHeight);
-	}
-    }
 }
 
 // END
