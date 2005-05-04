adf94ab468625c702ee857c1196413c7f46b7024
xoliver <xoliver@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2005-05-04 10:22:54 +0000

    Processing that can be used to send individual outcontrols via OSC

---
+++ trunk/CLAM/src/Processing/Controls/OSCSender.cxx
@@ -0,0 +1,36 @@
+#include "OSCSender.hxx"
+
+namespace CLAM
+{
+	int OSCSender::InputControlCB(TControlData val)
+	{
+		osc::OutboundPacketStream p( mBuffer, IP_MTU_SIZE);
+		p.Clear();
+		std::ostringstream header;
+		header <<mConf.GetPath();
+
+		p << osc::BeginMessage (header.str().c_str() ) 
+			<< val
+			<< osc::EndMessage;
+
+		mTransmitPort->Send (p.Data(), p.Size() );
+		
+		return 0;
+	}
+
+	bool OSCSender::ConcreteConfigure(const ProcessingConfig &c)
+	{
+		CopyAsConcreteConfig(mConf,c);
+
+		if (mTransmitPort==NULL)
+		{
+			//Use of InitializeNetworking is actually useless under POSIX because
+			// it does nothing -- should revise it if things change just in case
+			InitializeNetworking();
+			unsigned long hostAddress=GetHostByName(mConf.GetHostName().c_str());
+			mTransmitPort=new UdpTransmitPort (hostAddress, mConf.GetPort());
+		}
+	
+		return true;
+	}
+}
