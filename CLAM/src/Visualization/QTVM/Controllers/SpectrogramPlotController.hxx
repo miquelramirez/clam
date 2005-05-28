8a351b08c03cfe0efad5d1514cdf09b8dc513127
imosquera <imosquera@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2005-05-28 11:57:35 +0000

    SpectrogramPlotController class. First revision.

---
+++ CLAM/src/Visualization/QTVM/Controllers/SpectrogramPlotController.hxx
@@ -0,0 +1,75 @@
+#ifndef __SPECTROGRAMPLOTCONTROLLER__
+#define __SPECTROGRAMPLOTCONTROLLER__
+
+#include "Spectrum.hxx"
+#include "QtPalette.hxx"
+#include "SpectrogramRenderer.hxx"
+#include "SelPosPlotController.hxx"
+
+namespace CLAM
+{
+    namespace VM
+    {
+	class SpectrogramPlotController : public SelPosPlotController 
+	{
+	    Q_OBJECT
+
+	public:
+	    SpectrogramPlotController();
+	    ~SpectrogramPlotController();
+
+	    void SetData(const Array<Spectrum>& specMtx, const TData& dur);
+	    void SurfaceDimensions(int w,int h);
+	    void Draw();
+
+	    void SetMousePos(TData x,TData y);
+
+	    bool IsPlayable();
+
+	    std::vector<Color> GetColorScale(const int& size);
+	    std::vector<Color> GetGrayScale(const int& size);
+
+	    void SetRenderingMode(CLAM::VM::SonogramCM colorMap);
+
+	    int FFTSize() const;
+	    int TotalSlices() const;
+	    int SpectralRange() const;
+
+	    void LeaveMouse();
+
+	signals:
+	    void sendLabels(QString, QString, QString);
+
+	protected:
+	    void SetHBounds(const TData& left,const TData& right);
+	    void SetVBounds(const TData& bottom,const TData& top);
+
+	private:
+	    Array<Spectrum> mCacheData;
+	    SpectrogramRenderer mRenderer;
+	    TData mDuration;
+	    TData mSpectralRange;
+	    QtPalette mPalette;
+	    bool mMustProcessData;
+	    int mCurrentWidth;
+
+	    void ProcessData();
+	    void Colorize();
+
+	    float ClampToRange(TData value) const;
+	    
+	    void CacheData();
+	    void AdaptSpectralData();
+
+	    void FullView();
+
+	    void ComputeIndexes();
+
+	    QString TimeToStr(const TData& seconds);
+			
+	};
+    }
+}
+
+#endif
+
