466cb26d7c538895f9c213b75a6774c3d1a063b5
imosquera <imosquera@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2005-05-28 11:59:32 +0000

    SpectrogramRenderer class. First revision.

---
+++ CLAM/src/Visualization/QTVM/Renderers/SpectrogramRenderer.hxx
@@ -0,0 +1,45 @@
+#ifndef __SPECTROGRAMRENDERER__
+#define __SPECTROGRAMRENDERER__
+
+#include <vector>
+#include "Renderer.hxx"
+
+namespace CLAM
+{
+    namespace VM
+    {
+	enum SonogramCM { ColorSonogram=0, BlackWhiteSonogram };
+
+	class SpectrogramRenderer : public Renderer
+	{
+	public:
+	    SpectrogramRenderer();
+	    ~SpectrogramRenderer();
+
+	    
+	    void SetIndexes(const TIndex& leftIndex, const TIndex& rightIndex,
+			    const TIndex& bottomIndex, const TIndex& topIndex);
+
+	    void SetColorData(std::vector< std::vector<Color> >& cdata);
+	    void SetBWData(std::vector< std::vector<Color> >& bwdata);
+
+	    void Render();
+
+	    void SetRenderingMode(CLAM::VM::SonogramCM colorMap);
+	    void SetLineWidth(const float& lineWidth);
+
+	private:
+	    std::vector< std::vector<Color> > mColorData;
+	    std::vector< std::vector<Color> > mBWData;
+	    TIndex mLeftIndex, mRightIndex, mBottomIndex, mTopIndex;
+	    CLAM::VM::SonogramCM mColorMap;
+	    float mLineWidth;
+
+	    void DrawColorData();
+	    void DrawBWData();
+	};
+    }
+}
+
+#endif
+
