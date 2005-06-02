fd0134bd15311c6243f7a2fc71b22341eb35cc7a
imosquera <imosquera@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2005-06-02 16:08:57 +0000

    direct plot for QtSpectrogram.

---
+++ trunk/CLAM/src/Visualization/QTVM/DirectPlots/DPSpectrogram.hxx
@@ -0,0 +1,33 @@
+#ifndef __DPSPECTROGRAM__
+#define __DPSPECTROGRAM__
+
+#include <vector>
+#include <string>
+#include "Spectrum.hxx"
+
+namespace CLAM
+{
+    namespace VM
+    {
+
+	void PlotSpectrogram( const Array<Spectrum>& specMtx,
+			      const TData& duration,
+			      const std::string& label="",
+			      int x=100,
+			      int y=100,
+			      int w=600,
+			      int h=300 );
+
+	void PlotSpectrogram( const Array<Spectrum>& specMtx,
+			      const TData& duration,
+			      std::vector<unsigned>& marks,
+			      const std::string& label="",
+			      int x=100,
+			      int y=100,
+			      int w=600,
+			      int h=300 );
+    }
+}
+
+#endif
+
