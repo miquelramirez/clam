77287a8772289420efb49307e6266f9eef66ade5
imosquera <imosquera@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2005-06-14 19:07:49 +0000

    Identations.

--- trunk/CLAM/src/Visualization/QTVM/Widgets/BPFEditor/BPFEditor.hxx
+++ trunk/CLAM/src/Visualization/QTVM/Widgets/BPFEditor/BPFEditor.hxx
@@ -19,140 +19,140 @@ class QPopupMenu;
 
 namespace CLAM
 {
-    class Audio;
+	class Audio;
 
     namespace VM
     {
-	class Ruler;
-	class BPFEditorDisplaySurface;
-	class VScrollGroup;
-	class HScrollGroup;
+		class Ruler;
+		class BPFEditorDisplaySurface;
+		class VScrollGroup;
+		class HScrollGroup;
 
-	/**
-	 * Editable/auralizable plot.
-	 *
-	 * @ingroup QTVM
-	 */
+		/**
+		 * Editable/auralizable plot.
+		 *
+		 * @ingroup QTVM
+		 */
 
-	class BPFEditor : public QWidget, public PlayablePlot
-	{
-	    Q_OBJECT
+		class BPFEditor : public QWidget, public PlayablePlot
+		{
+			Q_OBJECT
 
-	public:
-	    BPFEditor(QWidget* parent=0, const char* name=0, int eFlags=CLAM::VM::AllowAll);
-	    ~BPFEditor();
+		public:
+			BPFEditor(QWidget* parent=0, const char* name=0, int eFlags=CLAM::VM::AllowAll);
+			~BPFEditor();
 	    
-	    void Label(const std::string& label);
-	    void Geometry(int x, int y, int w, int h);
+			void Label(const std::string& label);
+			void Geometry(int x, int y, int w, int h);
 
-	    void SetData(const BPF& bpf);
-	    BPF& GetData();
+			void SetData(const BPF& bpf);
+			BPF& GetData();
 
-	    void SetXRange(const double& min, const double& max, const EScale& scale=EScale::eLinear);
-	    void SetYRange(const double& min, const double& max, const EScale& scale=EScale::eLinear);
+			void SetXRange(const double& min, const double& max, const EScale& scale=EScale::eLinear);
+			void SetYRange(const double& min, const double& max, const EScale& scale=EScale::eLinear);
 
-	    void SetXScale(const EScale& scale);
-	    void SetYScale(const EScale& scale);
+			void SetXScale(const EScale& scale);
+			void SetYScale(const EScale& scale);
 
-	    Melody& GetMelody();
-	    MIDIMelody& GetMIDIMelody();
+			Melody& GetMelody();
+			MIDIMelody& GetMIDIMelody();
 	    
-	    void Show();
-	    void Hide();
+			void Show();
+			void Hide();
 
-	    void SetActivePlayer(bool active);
+			void SetActivePlayer(bool active);
 
-	    void SetAudioPtr(const Audio* audio);
+			void SetAudioPtr(const Audio* audio);
 	    
-        signals:
-	    void xValueChanged(int, float);
-	    void yValueChanged(int, float);
+		signals:
+			void xValueChanged(int, float);
+			void yValueChanged(int, float);
 
-	    void selectedXPos(double);
+			void selectedXPos(double);
 
-	    void currentPlayingTime(float);
-	    void stopPlaying(float);
+			void currentPlayingTime(float);
+			void stopPlaying(float);
 
-	public slots:
-	    void setHBounds(double, double);
-	    void selectPointFromXCoord(double);
+		public slots:
+			void setHBounds(double, double);
+			void selectPointFromXCoord(double);
 
-	    void switchColors();
+			void switchColors();
 
-	    void setRegionTime(MediaTime);
-	    void setRegionTime(float, float);
+			void setRegionTime(MediaTime);
+			void setRegionTime(float, float);
 
-	    void stopPendingTasks();
+			void stopPendingTasks();
 
-	    void playSimultaneously(bool);
+			void playSimultaneously(bool);
 
-	    void setCurrentPlayingTime(float);
-	    void receivedStopPlaying(float);
+			void setCurrentPlayingTime(float);
+			void receivedStopPlaying(float);
 
-	protected:
-	    void keyPressEvent(QKeyEvent* e);
-	    void keyReleaseEvent(QKeyEvent* e);
+		protected:
+			void keyPressEvent(QKeyEvent* e);
+			void keyReleaseEvent(QKeyEvent* e);
 
-	    void hideEvent(QHideEvent* e);
+			void hideEvent(QHideEvent* e);
 
-	private slots:
-	    void updateLabels(QString, QString);
-	    void setMaxVScroll(int);
-	    void setMaxHScroll(int);
+		private slots:
+			void updateLabels(QString, QString);
+			void setMaxVScroll(int);
+			void setMaxHScroll(int);
 
-	    void showPopupMenu();
-	    void activePlayer();
+			void showPopupMenu();
+			void activePlayer();
 
-	private:
-	    int mEFlags;
-	    bool mActivePlayer;
-	    Ruler *mXRuler, *mYRuler;
-	    BPFEditorController* mController;
-	    BPFEditorDisplaySurface* mDisplaySurface;
-	    QLabel *mXLabelInfo, *mYLabelInfo;
-	    QFrame* labelsContainer;
-	    QLabel *fixed_x_label, *fixed_y_label;
-	    QFont labelFont;
+		private:
+			int mEFlags;
+			bool mActivePlayer;
+			Ruler *mXRuler, *mYRuler;
+			BPFEditorController* mController;
+			BPFEditorDisplaySurface* mDisplaySurface;
+			QLabel *mXLabelInfo, *mYLabelInfo;
+			QFrame* labelsContainer;
+			QLabel *fixed_x_label, *fixed_y_label;
+			QFont labelFont;
 	    
-	    enum { EWhiteOverBlack = 0, EBlackOverWhite };
+			enum { EWhiteOverBlack = 0, EBlackOverWhite };
 	    
-	    int mColorScheme;
+			int mColorScheme;
 
-	    VScrollGroup *mVScroll;
-	    HScrollGroup *mHScroll;
-	    QBoxLayout *mainLayout, *topLayout, *middleLayout, *bottomLayout;
-	    QFrame *topLeftHole, *topRightHole, *bottomRightHole, *playerHole;
+			VScrollGroup *mVScroll;
+			HScrollGroup *mHScroll;
+			QBoxLayout *mainLayout, *topLayout, *middleLayout, *bottomLayout;
+			QFrame *topLeftHole, *topRightHole, *bottomRightHole, *playerHole;
 
-	    Melody mMelody;
-	    MIDIMelody mMIDIMelody;
+			Melody mMelody;
+			MIDIMelody mMIDIMelody;
 
-	    bool mHasPlayData;
-	    bool mWhiteOverBlackScheme;
+			bool mHasPlayData;
+			bool mWhiteOverBlackScheme;
 
-	    QPopupMenu* mPopupMenu;
+			QPopupMenu* mPopupMenu;
 
-	    Slotv1<TData> mSlotPlayingTimeReceived;
-	    Slotv1<TData> mSlotStopPlayingReceived;
+			Slotv1<TData> mSlotPlayingTimeReceived;
+			Slotv1<TData> mSlotStopPlayingReceived;
 
-	    void InitBPFEditor();
+			void InitBPFEditor();
 
-	    void SetScheme(int scheme);
-	    void WhiteOverBlack();
-	    void BlackOverWhite();
+			void SetScheme(int scheme);
+			void WhiteOverBlack();
+			void BlackOverWhite();
 
-	    void AdjustLeft(const double& min, const double& max, bool y_axis=true);
-	    void CreateVScroll();
-	    void CreateHScroll();
+			void AdjustLeft(const double& min, const double& max, bool y_axis=true);
+			void CreateVScroll();
+			void CreateHScroll();
 
-	    void ShowPlayer();
-	    void HidePlayer();
+			void ShowPlayer();
+			void HidePlayer();
 
-	    void InitPopupMenu();
+			void InitPopupMenu();
 	    
-	    void PlayingTime(TData time);
-	    void StopPlaying(TData time);
+			void PlayingTime(TData time);
+			void StopPlaying(TData time);
 	    
-	};
+		};
     }
 }
 
