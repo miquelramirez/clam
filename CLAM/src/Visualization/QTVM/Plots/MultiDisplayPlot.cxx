2a5d79b1dd443be1368a8b7adf9f3cd0a629d888
imosquera <imosquera@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2005-06-29 19:18:01 +0000

    multi display branch; first revision.

---
+++ CLAM/src/Visualization/QTVM/Plots/MultiDisplayPlot.cxx
@@ -0,0 +1,362 @@
+#include <qframe.h>
+#include <qpushbutton.h>
+#include <qtooltip.h>
+#include "Assert.hxx"
+#include "Ruler.hxx"
+#include "HScrollGroup.hxx"
+#include "PlotController.hxx"
+#include "DisplaySurface.hxx"
+#include "EditTagDialog.hxx"
+#include "MultiDisplayPlot.hxx"
+
+namespace CLAM
+{
+	namespace VM
+	{
+		MultiDisplayPlot::MultiDisplayPlot(QWidget* parent,  const char * name, WFlags f)
+			: QtPlot(parent,name,f)
+			, mMasterId(-1)
+			, mXRuler(0)
+			, mHScrollBar(0)
+			, mToggleContainer(0)
+			, mToggleButton(0)
+			, editTagDlg(0)
+		{
+		}
+
+		MultiDisplayPlot::~MultiDisplayPlot()
+		{
+		}
+
+		void MultiDisplayPlot::Label(const std::string& label)
+		{
+			setCaption(label.c_str());
+		}
+
+		void MultiDisplayPlot::Geometry(int x,int y,int w,int h)
+		{
+			setGeometry(x,y,w,h);
+		}
+
+		void MultiDisplayPlot::Show()
+		{
+			show();
+		}
+
+		void MultiDisplayPlot::Hide()
+		{
+			hide();
+		}
+
+		void MultiDisplayPlot::SetBackgroundColor(Color c)
+		{
+			double r = double(c.r)/255.0;
+			double g = double(c.g)/255.0;
+			double b = double(c.b)/255.0;
+
+			for(unsigned i=0; i < mSurfaces.size(); i++)
+			{
+				mSurfaces[i]->SetBackgroundColor(r,g,b);
+			}
+		}
+
+		void MultiDisplayPlot::SetDialColor(Color c)
+		{
+			for(unsigned i=0; i < mControllers.size(); i++)
+			{
+				mControllers[i]->SetDialColor(c);
+			}
+		}
+
+		void MultiDisplayPlot::SetMarks(std::vector<unsigned>& marks)
+		{
+			for(unsigned i=0; i < mControllers.size(); i++)
+			{
+				mControllers[i]->SetMarks(marks);
+			}
+		}
+
+		std::vector<unsigned>& MultiDisplayPlot::GetMarks()
+		{
+			CLAM_ASSERT( mMasterId >=0 && mMasterId < (int)mControllers.size(), 
+						 "MultiDisplayPlot::GetMarks: Invalid masterId." );
+			return mControllers[mMasterId]->GetMarks();
+		}
+
+		void MultiDisplayPlot::SetMarksColor(Color c)
+		{
+			for(unsigned i=0; i < mControllers.size(); i++)
+			{
+				mControllers[i]->SetMarksColor(c);
+			}
+		}
+
+		std::vector<QString> MultiDisplayPlot::GetSegmentationTags()
+		{
+			CLAM_ASSERT( mMasterId >=0 && mMasterId < (int)mControllers.size(), 
+						 "MultiDisplayPlot::GetSegmentationTags: Invalid masterId." );
+			return mControllers[mMasterId]->GetTags();
+		}
+
+		void MultiDisplayPlot::SetToggleColorOn(bool on)
+		{
+			if(!mToggleButton) return;
+			mToggleButton->setOn(on);
+		}
+
+		void MultiDisplayPlot::SwitchDisplayColors(bool sw)
+		{
+			(sw) ? mToggleButton->show() : mToggleButton->hide();
+		}
+
+		void MultiDisplayPlot::switchColors()
+		{
+			if(mToggleButton->isOn())
+			{
+				DisplayBackgroundBlack();
+			}
+			else
+			{
+				DisplayBackgroundWhite();	
+			}
+			emit switchColorsRequested();
+		}
+
+		void MultiDisplayPlot::SyncDialPos(const int& senderId)
+		{
+			CLAM_ASSERT( senderId >=0 && senderId < (int)mControllers.size(), 
+						 "MultiDisplayPlot::GetSyncDialPos: invalid senderId." );
+			for(unsigned i=0; i < mControllers.size(); i++)
+			{
+				if(i == (unsigned)senderId) continue;
+				connect(mControllers[senderId],SIGNAL(selectedXPos(double)),
+						mControllers[i],SLOT(setSelectedXPos(double)));
+			}
+		}
+
+		void MultiDisplayPlot::SyncInsertMarkBySampleIndex(const int& senderId)
+		{
+			CLAM_ASSERT( senderId >=0 && senderId < (int)mControllers.size(), 
+						 "MultiDisplayPlot::GetSyncInsertMarkBySampleIndex: invalid senderId." );
+			for(unsigned i=0; i < mControllers.size(); i++)
+			{
+				if(i == (unsigned)senderId) continue;
+				connect(mControllers[senderId],SIGNAL(insertedMark(unsigned)),
+						mControllers[i],SLOT(insertMark(unsigned)));
+			}
+			
+		}
+
+		void MultiDisplayPlot::SyncRemoveMarkBySampleIndex(const int& senderId)
+		{
+			CLAM_ASSERT( senderId >=0 && senderId < (int)mControllers.size(), 
+						 "MultiDisplayPlot::GetSyncRemoveMarkBySampleIndex: invalid senderId." );
+			for(unsigned i=0; i < mControllers.size(); i++)
+			{
+				if(i == (unsigned)senderId) continue;
+				connect(mControllers[senderId],SIGNAL(removedMark(int,unsigned)),
+						mControllers[i],SLOT(removeMark(int,unsigned)));
+			}
+					
+		}
+
+		void MultiDisplayPlot::SyncUpdateMarkBySampleIndex(const int& senderId)
+		{
+			CLAM_ASSERT( senderId >=0 && senderId < (int)mControllers.size(), 
+						 "MultiDisplayPlot::GetSyncUpdateMarkBySampleIndex: invalid senderId." );
+			for(unsigned i=0; i < mControllers.size(); i++)
+			{
+				if(i == (unsigned)senderId) continue;
+				connect(mControllers[senderId],SIGNAL(updatedMark(int,unsigned)),
+						mControllers[i],SLOT(updateMark(int,unsigned)));
+			}
+		}
+
+		void MultiDisplayPlot::SyncUpdateTag(const int& senderId)
+		{
+			CLAM_ASSERT( senderId >=0 && senderId < (int)mControllers.size(), 
+						 "MultiDisplayPlot::GetSyncUpdateTag: invalid senderId." );
+			for(unsigned i=0; i < mControllers.size(); i++)
+			{
+				if(i == (unsigned)senderId) continue;
+				connect(mControllers[senderId],SIGNAL(updatedTag(int,QString)),
+						mControllers[i],SLOT(updateTag(int,QString)));
+			}
+		}
+
+		void MultiDisplayPlot::SetMasterId(const int& mid)
+		{
+			CLAM_ASSERT( mControllers.size() > 0, 
+						 "MultiDisplayPlot::SetMasterId: master controller has not been created yet." );
+			CLAM_ASSERT( mid >=0 && mid < (int)mControllers.size(), 
+						 "MultiDisplayPlot::SetMasterId: invalid masterId." );
+			mMasterId = mid;
+		}
+
+		void MultiDisplayPlot::BindToSurfaces()
+		{
+			CLAM_ASSERT( mControllers.size() > 0, 
+						 "MultiDisplayPlot::BindToSurfaces: thre are not controllers to bind." );
+			CLAM_ASSERT( mControllers.size() == mSurfaces.size(), 
+						 "MultiDisplayPlot::BindToSurfaces: the number of controllers and surfaces must be the same." );
+			for(unsigned i=0; i < mControllers.size(); i++)
+			{
+				mSurfaces[i]->SetController(mControllers[i]);
+			}
+		}
+
+		void MultiDisplayPlot::SynchronizeHZoom()
+		{
+			CLAM_ASSERT(mMasterId >= 0,"MultiDisplayPlot::SyncHZoom: the master controller has not been created yet." );
+			for(unsigned i=0; i < mControllers.size(); i++)
+			{
+				if(i == (unsigned)mMasterId) continue;
+				connect(mControllers[mMasterId],SIGNAL(xRulerRange(double,double)),
+						mControllers[i],SLOT(setHBounds(double,double)));
+			}
+		}
+
+		void MultiDisplayPlot::keyPressEvent(QKeyEvent* e)
+		{
+			int key = e->key();
+			for(unsigned i=0; i < mControllers.size(); i++)
+			{
+				if(key == Qt::Key_Shift) mControllers[i]->SetKeyShiftPressed(true);
+				if(key == Qt::Key_Insert) mControllers[i]->SetKeyInsertPressed(true); 
+				if(key == Qt::Key_Delete) mControllers[i]->SetKeyDeletePressed(true);
+			}
+		}
+
+		void MultiDisplayPlot::keyReleaseEvent( QKeyEvent* e)
+		{
+			int key = e->key();
+			for(unsigned i=0; i < mControllers.size(); i++)
+			{
+				if(key == Qt::Key_Shift) mControllers[i]->SetKeyShiftPressed(false);
+				if(key == Qt::Key_Insert) mControllers[i]->SetKeyInsertPressed(false); 
+				if(key == Qt::Key_Delete) mControllers[i]->SetKeyDeletePressed(false);
+			}
+		}
+
+		void MultiDisplayPlot::CreateToggleColorFrame()
+		{
+			if(mToggleContainer) return;
+		   
+			QFont f("Sans",8);
+			f.setBold(true);
+			
+			mToggleContainer = new QFrame(this);
+			mToggleContainer->setFixedSize(XRULER_HEIGHT,XRULER_HEIGHT);
+			mToggleButton = new QPushButton(mToggleContainer);
+			mToggleButton->setFont(f);
+			mToggleButton->setFixedSize(25,25);
+			mToggleButton->setGeometry(0,8,25,25);
+			mToggleButton->setToggleButton(true);
+			mToggleButton->setText("CM");
+			QToolTip::add(mToggleButton,"switch display colors");
+			SwitchDisplayColors(false);
+
+			// connect
+			connect(mToggleButton,SIGNAL(clicked()),this,SLOT(switchColors()));
+		}
+
+		void MultiDisplayPlot::SetToggleColorFrameWidth(const int& w, bool center)
+		{
+			if(!mToggleContainer) return;
+			mToggleContainer->setFixedWidth(w);
+			if(!center) return;
+			int x = (mToggleContainer->width()-mToggleButton->width())/2;
+			mToggleButton->setGeometry(x,mToggleButton->y(),mToggleButton->width(),mToggleButton->height());
+		}
+			
+		void MultiDisplayPlot::CreateXRuler()
+		{
+			if(mXRuler) return;
+			mXRuler = new Ruler(this,CLAM::VM::Top);
+			mXRuler->setFixedHeight(XRULER_HEIGHT);
+		}
+
+		void MultiDisplayPlot::CreateHScrollGroup()
+		{
+			if(mHScrollBar) return;
+			mHScrollBar = new HScrollGroup(this);
+		}
+
+		void MultiDisplayPlot::ConnectXRuler()
+		{
+			CLAM_ASSERT( mXRuler,"MultiDisplayPlot::ConnectXRuler: mXRuler has not been created yet." );
+			CLAM_ASSERT( mMasterId >=0, "MultiDisplayPlot::ConnectXRuler: invalid masterId." );
+			connect(mControllers[mMasterId],SIGNAL(xRulerRange(double,double)),mXRuler,SLOT(updateRange(double,double)));
+		}
+
+		void MultiDisplayPlot::ConnectHScrollGroup()
+		{
+			CLAM_ASSERT( mHScrollBar,"MultiDisplayPlot::ConnectHScrollGroup: mHScrollBar has not been created yet." );
+			CLAM_ASSERT( mMasterId >=0, "MultiDisplayPlot::ConnectHScrollGroup: invalid masterId." );
+
+			connect(mHScrollBar,SIGNAL(zoomIn()),mControllers[mMasterId],SLOT(hZoomIn()));
+			connect(mHScrollBar,SIGNAL(zoomOut()),mControllers[mMasterId],SLOT(hZoomOut()));
+			connect(mHScrollBar,SIGNAL(scrollValueChanged(int)),mControllers[mMasterId],SLOT(updateHScrollValue(int)));
+
+			connect(mControllers[mMasterId],SIGNAL(hZoomRatio(double)),mHScrollBar,SLOT(updateZoomRatio(double)));
+			connect(mControllers[mMasterId],SIGNAL(hScrollValue(int)),mHScrollBar,SLOT(updateScrollValue(int)));
+			connect(mControllers[mMasterId],SIGNAL(hScrollMaxValue(int)),this,SLOT(setMaxHScroll(int)));
+
+		}
+
+		void MultiDisplayPlot::ConnectEditTagSlot()
+		{
+			for(unsigned i = 0; i < mControllers.size(); i++)
+			{
+				connect(mControllers[i],SIGNAL(requestSegmentationTag()),this,SLOT(showEditTagDialog()));
+			}
+
+		}
+
+		QWidget* MultiDisplayPlot::GetToggleColorFrame()
+		{
+			CLAM_ASSERT( mToggleContainer,"MultiDisplayPlot::GetToggleColorFrame: frame has not been created yet." );
+			return mToggleContainer;
+		}
+
+		QWidget* MultiDisplayPlot::GetXRuler()
+		{
+			CLAM_ASSERT( mXRuler,"MultiDisplayPlot::GetXRuler: mXRuler has not been created yet." );
+			return mXRuler;
+		}
+
+		QWidget* MultiDisplayPlot::GetHScrollGroup()
+		{
+			CLAM_ASSERT( mHScrollBar,"MultiDisplayPlot::GetHScrollBar: mHScrollBar has not been created yet." );
+			return mHScrollBar;
+		}
+
+		void MultiDisplayPlot::setMaxHScroll(int value)
+		{
+			int max = value-mSurfaces[0]->width();
+			if(max < 0) max=0;;
+			if(max >= 0 && mHScrollBar->GetMaxScrollValue() != max)
+			{
+				mHScrollBar->setMaxScrollValue(max);
+			}
+		}
+
+		void MultiDisplayPlot::showEditTagDialog()
+		{
+			if(editTagDlg)
+			{
+				delete editTagDlg;
+				editTagDlg=0;    
+			}
+
+			editTagDlg = new EditTagDialog(mControllers[mMasterId]->GetTag(),this);
+			if( editTagDlg->exec() == QDialog::Accepted )
+			{
+				mControllers[mMasterId]->SetSegmentationTag(editTagDlg->text());
+			}
+		}
+	}
+}
+
+// END
+
---
+++ CLAM/src/Visualization/QTVM/Plots/MultiDisplayPlot.hxx
@@ -0,0 +1,114 @@
+#ifndef __MULTIDISPLAYPLOT__
+#define __MULTIDISPLAYPLOT__
+
+#include "PlotController.hxx"
+#include "DisplaySurface.hxx"
+#include "QtPlot.hxx"
+
+class QFrame;
+class QPushButton;
+
+namespace CLAM
+{
+	namespace VM
+	{
+		class Ruler;
+		class HScrollGroup;
+		class EditTagDialog;
+
+		class MultiDisplayPlot : public QtPlot
+		{
+
+			Q_OBJECT
+
+		public:
+			MultiDisplayPlot(QWidget* parent=0,  const char * name = 0, WFlags f = 0);
+			virtual ~MultiDisplayPlot();
+
+			void Label(const std::string& label);
+			void Geometry(int x,int y,int w,int h);
+			void Show();
+			void Hide();
+
+			void SetBackgroundColor(Color c);
+			void SetDialColor(Color c);
+
+			virtual void SetMarks(std::vector<unsigned>& marks);
+			virtual std::vector<unsigned>& GetMarks();
+
+			void SetMarksColor(Color c);
+			std::vector<QString> GetSegmentationTags();
+
+			void SetToggleColorOn(bool on);
+			void SwitchDisplayColors(bool sw);
+
+		signals:
+			void switchColorsRequested();
+
+		public slots:
+			void switchColors();
+
+		protected:
+			std::vector<DisplaySurface*> mSurfaces;
+			std::vector<PlotController*> mControllers;
+
+			virtual void CreateControllers()=0;
+			virtual void CreateSurfaces()=0;
+
+			virtual void SynchronizeDialPos()=0;
+			virtual void SynchronizeInsertMark()=0;
+			virtual void SynchronizeRemoveMark()=0;
+			virtual void SynchronizeUpdateMark()=0;
+			virtual void SynchronizeUpdateTag()=0;
+
+			virtual void DisplayBackgroundBlack()=0;
+			virtual void DisplayBackgroundWhite()=0;
+
+			void SyncDialPos(const int& senderId);
+			void SyncInsertMarkBySampleIndex(const int& senderId);
+			void SyncRemoveMarkBySampleIndex(const int& senderId);
+			void SyncUpdateMarkBySampleIndex(const int& senderId);
+			void SyncUpdateTag(const int& senderId);
+
+			void SetMasterId(const int& mid);
+			void BindToSurfaces();
+			void SynchronizeHZoom();
+
+			void keyPressEvent(QKeyEvent* e);
+			void keyReleaseEvent( QKeyEvent* e);
+
+			void CreateToggleColorFrame();
+			void SetToggleColorFrameWidth(const int& w, bool center=false);
+			
+			void CreateXRuler();
+			void CreateHScrollGroup();
+			void ConnectXRuler();
+			void ConnectHScrollGroup();
+			void ConnectEditTagSlot();
+
+			QWidget* GetToggleColorFrame();
+			QWidget* GetXRuler();
+			QWidget* GetHScrollGroup();
+
+		private slots:
+			void setMaxHScroll(int);
+			void showEditTagDialog();
+
+		private:
+			int           mMasterId;
+			Ruler*        mXRuler;
+			HScrollGroup* mHScrollBar;
+			QFrame*       mToggleContainer;
+			QPushButton*  mToggleButton;
+
+			// edit tag dialog
+			EditTagDialog* editTagDlg;
+
+			enum { XRULER_HEIGHT=40 };
+		  
+		};
+	}
+}
+
+#endif
+
