8883ac6c71f0e10c5a414680577baca4945541c8
imosquera <imosquera@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2005-06-29 19:42:23 +0000

    spectral peaks renderer data.

---
+++ trunk/CLAM/src/Visualization/QTVM/Utils/FreqMagPoint.hxx
@@ -0,0 +1,41 @@
+#ifndef __FREQMAGPOINT__
+#define __FREQMAGPOINT__
+
+#include "DataTypes.hxx"
+
+namespace CLAM
+{
+	namespace VM
+	{
+		struct FreqMagPoint
+		{
+			TData freq;
+			TData mag;
+
+			FreqMagPoint()
+				: freq(TData(0.0))
+				, mag(TData(0.0))
+				{}
+
+			FreqMagPoint(const TData& f, const TData& m)
+				: freq(f)
+				, mag(m)
+				{}
+
+			FreqMagPoint(const FreqMagPoint& fmp)
+				: freq(fmp.freq)
+				, mag(fmp.mag)
+				{}
+
+			FreqMagPoint& operator=(const FreqMagPoint& fmp)
+				{
+					freq = fmp.freq;
+					mag = fmp.mag;
+					return *this;
+				}
+		};
+	}
+}
+
+#endif
+
