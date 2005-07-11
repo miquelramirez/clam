0b50abd4788e6b96b80f1986d19402ba03f4cba5
Miquel Ramírez <miquel.ramirez@gmail.com>
2005-07-11 09:48:21 +0000

    + ADDED: Test for pthread

--- CLAM/scons/libs/buildtools/package_checks.py
+++ CLAM/scons/libs/buildtools/package_checks.py
@@ -33,4 +33,36 @@ int main( int argc, char** argv )
 
 package_checks['check_xmlpp'] = ThoroughPackageCheck( 'libxml++', 'c++', 'xml++-2.6', xmlpp_test_code )
 
+# pthreads test
+pthread_test_code = """
+#include <pthread.h>
+#include <stdlib.h>
+
+#define NUM_THREADS 2
+
+void* print_hello(void *thread_id)
+{
+	pthread_exit(NULL);
+}
+
+int main(int argc, char *argv[])
+{
+	pthread_t threads[NUM_THREADS];
+	int rc, t;
+	for(t=0;t<NUM_THREADS;t++)
+	{
+		rc = pthread_create(&threads[t], NULL, print_hello, (void *)t);
+		if (rc)
+		{
+			exit(-1);
+		}
+	}
+
+	pthread_exit(NULL);
+}
+
+"""
+
+package_checks['check_pthread'] = ThoroughPackageCheck( 'pthread', 'c', None, pthread_test_code )
+
 
