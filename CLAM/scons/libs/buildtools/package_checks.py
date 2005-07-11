3c0807ea9966733cbc371c1bfbdd4694204ae9ed
Miquel Ramírez <miquel.ramirez@gmail.com>
2005-07-11 14:46:35 +0000

    + ADDED: Checks for pthreads, fftw and oscpack

--- CLAM/scons/libs/buildtools/package_checks.py
+++ CLAM/scons/libs/buildtools/package_checks.py
@@ -65,4 +65,117 @@ int main(int argc, char *argv[])
 
 package_checks['check_pthread'] = ThoroughPackageCheck( 'pthread', 'c', None, pthread_test_code )
 
+double_fftw_wo_prefix_test_code = """\
+#include <fftw.h>
+#include <stdio.h>
+
+int main(int argc, char** argv )
+{
+
+	fftw_create_plan(0,FFTW_FORWARD,0);
+	if (fftw_sizeof_fftw_real()!=sizeof(double))
+	{
+		fprintf(stderr, "expecting fftw to be using doubles, and it is using floats!\\n");
+		return -1;
+	}
+	return 0;
+
+}
+"""
+
+package_checks['check_fftw_double_wo_prefix'] = ThoroughPackageCheck( 'fftw using doubles without prefixed binaries/headers', 'c', None, double_fftw_wo_prefix_test_code )
+
+double_fftw_w_prefix_test_code = """\
+#include <dfftw.h>
+#include <stdio.h>
+
+int main(int argc, char** argv )
+{
+
+	fftw_create_plan(0,FFTW_FORWARD,0);
+	if (fftw_sizeof_fftw_real()!=sizeof(double))
+	{
+		fprintf(stderr, "expecting fftw to be using doubles, and it is using floats!\\n");
+		return -1;
+	}
+	return 0;
+
+}
+"""
+
+package_checks['check_fftw_double_w_prefix'] = ThoroughPackageCheck( 'fftw using doubles with prefixed binaries/headers', 'c', None, double_fftw_w_prefix_test_code )
+
+double_rfftw_wo_prefix_test_code = """\
+#include <rfftw.h>
+
+int main(int argc, char** argv )
+{
+
+	rfftw_create_plan(0,FFTW_FORWARD,0);
+	return 0;
+}
+"""
+
+package_checks['check_rfftw_double_wo_prefix'] = ThoroughPackageCheck( 'rfftw using doubles without prefixed binaries/headers', 'c', None, double_rfftw_wo_prefix_test_code )
+
+double_rfftw_w_prefix_test_code = """\
+#include <rfftw.h>
+
+int main(int argc, char** argv )
+{
+
+	rfftw_create_plan(0,FFTW_FORWARD,0);
+	return 0;
+}
+"""
+
+
+package_checks['check_rfftw_double_w_prefix'] = ThoroughPackageCheck( 'rfftw using doubles with prefixed binaries/headers', 'c', None, double_rfftw_w_prefix_test_code )
+
+float_fftw_w_prefix_test_code = """\
+#include <sfftw.h>
+#include <stdio.h>
+
+int main(int argc, char** argv )
+{
+
+	fftw_create_plan(0,FFTW_FORWARD,0);
+	if (fftw_sizeof_fftw_real()!=sizeof(float))
+	{
+		fprintf(stderr, "expecting fftw to be using floats, and it is using doubles!\\n");
+		return -1;
+	}
+	return 0;
+
+}
+"""
+
+package_checks['check_fftw_float_w_prefix'] = ThoroughPackageCheck( 'fftw using floats with prefixed binaries/headers', 'c', None, float_fftw_w_prefix_test_code )
+
+
+float_rfftw_w_prefix_test_code = """\
+#include <srfftw.h>
+
+int main(int argc, char** argv )
+{
+
+	rfftw_create_plan(0,FFTW_FORWARD,0);
+	return 0;
+}
+"""
+
+package_checks['check_rfftw_float_w_prefix'] = ThoroughPackageCheck( 'rfftw using floats with prefixed binaries/headers', 'c', None, float_rfftw_w_prefix_test_code )
+
+liboscpack_test_code = """\
+#include <oscpack/ip/NetworkingUtils.h>
+int main()
+{
+	InitializeNetworking();
+	TerminateNetworking();
+	return 0;
+}
+"""
+
+package_checks['check_oscpack'] = ThoroughPackageCheck( 'oscpack', 'c++', None, liboscpack_test_code )
+
 
