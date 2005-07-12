b91dfb22110ae11a978715c7149517f3bc357aa7
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2005-07-12 11:13:10 +0000

    Moved from src/Data/Descriptors/Pool

---
+++ trunk/CLAM/test/UnitTests/DescriptorsTests/DescriptionSchemeTest.cxx
@@ -0,0 +1,225 @@
+/*
+ * Copyright (c) 2004 MUSIC TECHNOLOGY GROUP (MTG)
+ *                         UNIVERSITAT POMPEU FABRA
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#include <cppunit/extensions/HelperMacros.h>
+#include "cppUnitHelper.hxx" // necessary for the custom assert
+
+#include "DescriptionScheme.hxx"
+#include "DataTypes.hxx"
+#include "Component.hxx"
+
+
+namespace CLAMTest
+{
+
+class DescriptionSchemeTest;
+
+CPPUNIT_TEST_SUITE_REGISTRATION( DescriptionSchemeTest );
+
+class DescriptionSchemeTest : public CppUnit::TestFixture
+{
+	CPPUNIT_TEST_SUITE( DescriptionSchemeTest );
+	CPPUNIT_TEST( testGetNScopes_withNoScopeRegistered );
+	CPPUNIT_TEST( testGetNScopes_withASingleAttribute );
+	CPPUNIT_TEST( testGetNScopes_withTwoAttributeOnDifferentScopes );
+	CPPUNIT_TEST( testGetNScopes_withTwoAttributeOnTheSameScope );
+	CPPUNIT_TEST( testGetScope_withNoScopeRegistered );
+	CPPUNIT_TEST( testGetScope_withARegisteredAttribute );
+	CPPUNIT_TEST( testGetScope_withTwoScopes );
+	CPPUNIT_TEST( testGetScopeByIndex_withIndexOutOfBounds );
+	CPPUNIT_TEST( testGetScopeIndex_withNoScopeRegistered );
+	CPPUNIT_TEST( testGetScopeIndex_withARegisteredAttribute );
+	CPPUNIT_TEST( testGetScopeIndex_withTwoScopes );
+	CPPUNIT_TEST( testGetScopeName_withTwoScopes );
+	CPPUNIT_TEST( testGetScopeName_withWrongNumber );
+	CPPUNIT_TEST_SUITE_END();
+
+public:
+	/// Common initialization, executed before each test method
+	void setUp() { }
+
+	/// Common clean up, executed after each test method
+	void tearDown() { }
+
+private:
+	void testGetNScopes_withNoScopeRegistered()
+	{
+		CLAM::DescriptionScheme scheme;
+		CPPUNIT_ASSERT_EQUAL(0u, scheme.GetNScopes());
+	}
+
+	void testGetNScopes_withASingleAttribute()
+	{
+		CLAM::DescriptionScheme scheme;
+		scheme.AddAttribute<CLAM::TData>("MyScope","MyAttribute");
+		CPPUNIT_ASSERT_EQUAL(1u, scheme.GetNScopes());
+	}
+
+	void testGetNScopes_withTwoAttributeOnDifferentScopes()
+	{
+		CLAM::DescriptionScheme scheme;
+		scheme.AddAttribute<CLAM::TData>("MyScope","MyAttribute");
+		scheme.AddAttribute<CLAM::TData>("YourScope","YourAttribute");
+		CPPUNIT_ASSERT_EQUAL(2u, scheme.GetNScopes());
+	}
+
+	void testGetNScopes_withTwoAttributeOnTheSameScope()
+	{
+		CLAM::DescriptionScheme scheme;
+		scheme.AddAttribute<CLAM::TData>("MyScope","MyAttribute");
+		scheme.AddAttribute<CLAM::TData>("MyScope","YourAttribute");
+		CPPUNIT_ASSERT_EQUAL(1u, scheme.GetNScopes());
+	}
+
+	void testGetScope_withNoScopeRegistered()
+	{
+		CLAM::DescriptionScheme scheme;
+		try
+		{
+			scheme.GetScope("NonExistent");
+			CPPUNIT_FAIL("Should have thrown an exception");
+		}
+		catch (CLAM::ErrAssertionFailed & err)
+		{
+			const std::string expected = "No scope registered with that name";
+			CPPUNIT_ASSERT_EQUAL(expected, std::string(err.what()));
+		}
+	}
+
+	void testGetScope_withARegisteredAttribute()
+	{
+		CLAM::DescriptionScheme scheme;
+		scheme.AddAttribute<CLAM::TData>("MyScope","MyAttribute");
+		scheme.AddAttribute<CLAM::TData>("MyScope","MyOtherAttribute");
+
+		const CLAM::DescriptionScope & spec = scheme.GetScope("MyScope");
+
+		CPPUNIT_ASSERT_EQUAL(0u,spec.GetIndex("MyAttribute"));
+		CPPUNIT_ASSERT_EQUAL(1u,spec.GetIndex("MyOtherAttribute"));
+	}
+
+	void testGetScope_withTwoScopes()
+	{
+		CLAM::DescriptionScheme scheme;
+		scheme.AddAttribute<CLAM::TData>("MyScope","MyAttribute");
+		scheme.AddAttribute<CLAM::TData>("YourScope","YourAttribute");
+		scheme.AddAttribute<int>("YourScope","YourIntAttribute");
+
+		const CLAM::DescriptionScope & myScope = scheme.GetScope("MyScope");
+		const CLAM::DescriptionScope & yourScope = scheme.GetScope("YourScope");
+
+		CPPUNIT_ASSERT_EQUAL(0u,myScope.GetIndex("MyAttribute"));
+		CPPUNIT_ASSERT_EQUAL(0u,yourScope.GetIndex("YourAttribute"));
+		CPPUNIT_ASSERT_EQUAL(1u,yourScope.GetIndex("YourIntAttribute"));
+	}
+
+	void testGetScopeByIndex_withIndexOutOfBounds()
+	{
+		CLAM::DescriptionScheme scheme;
+		try
+		{
+			scheme.GetScope(0);
+			CPPUNIT_FAIL("Should have thrown an exception");
+		}
+		catch (CLAM::ErrAssertionFailed & err)
+		{
+			const std::string expected = "Accessing an illegal scope index for the description scheme";
+			CPPUNIT_ASSERT_EQUAL(expected, std::string(err.what()));
+		}
+	}
+
+	void testGetScopeIndex_withNoScopeRegistered()
+	{
+		CLAM::DescriptionScheme scheme;
+		try
+		{
+			scheme.GetScopeIndex("NonExistent");
+			CPPUNIT_FAIL("Should have thrown an exception");
+		}
+		catch (CLAM::ErrAssertionFailed & err)
+		{
+			const std::string expected = "No scope registered with that name";
+			CPPUNIT_ASSERT_EQUAL(expected, std::string(err.what()));
+		}
+	}
+
+	void testGetScopeIndex_withARegisteredAttribute()
+	{
+		CLAM::DescriptionScheme scheme;
+		scheme.AddAttribute<CLAM::TData>("MyScope","MyAttribute");
+		scheme.AddAttribute<CLAM::TData>("MyScope","MyOtherAttribute");
+
+		unsigned scopeIndex = scheme.GetScopeIndex("MyScope");
+		const CLAM::DescriptionScope & scope = scheme.GetScope(scopeIndex);
+
+		CPPUNIT_ASSERT_EQUAL(0u,scope.GetIndex("MyAttribute"));
+		CPPUNIT_ASSERT_EQUAL(1u,scope.GetIndex("MyOtherAttribute"));
+	}
+
+	void testGetScopeIndex_withTwoScopes()
+	{
+		CLAM::DescriptionScheme scheme;
+		scheme.AddAttribute<CLAM::TData>("MyScope","MyAttribute");
+		scheme.AddAttribute<CLAM::TData>("YourScope","YourAttribute");
+		scheme.AddAttribute<int>("YourScope","YourIntAttribute");
+
+		unsigned myScopeIndex = scheme.GetScopeIndex("MyScope");
+		const CLAM::DescriptionScope & myScope = scheme.GetScope(myScopeIndex);
+		unsigned yourScopeIndex = scheme.GetScopeIndex("YourScope");
+		const CLAM::DescriptionScope & yourScope = scheme.GetScope(yourScopeIndex);
+
+		CPPUNIT_ASSERT_EQUAL(0u,myScope.GetIndex("MyAttribute"));
+		CPPUNIT_ASSERT_EQUAL(0u,yourScope.GetIndex("YourAttribute"));
+		CPPUNIT_ASSERT_EQUAL(1u,yourScope.GetIndex("YourIntAttribute"));
+	}
+
+	void testGetScopeName_withTwoScopes()
+	{
+		CLAM::DescriptionScheme scheme;
+		scheme.AddAttribute<CLAM::TData>("MyScope","MyAttribute");
+		scheme.AddAttribute<CLAM::TData>("YourScope","YourAttribute");
+		CPPUNIT_ASSERT_EQUAL(std::string("MyScope"), scheme.GetScopeName(0));
+		CPPUNIT_ASSERT_EQUAL(std::string("YourScope"), scheme.GetScopeName(1));
+	}
+
+	void testGetScopeName_withWrongNumber()
+	{
+		CLAM::DescriptionScheme scheme;
+		scheme.AddAttribute<CLAM::TData>("MyScope","MyAttribute");
+		try
+		{
+			scheme.GetScopeName(1);
+			CPPUNIT_FAIL("Should have thrown an exception");
+		}
+		catch (CLAM::ErrAssertionFailed & err)
+		{
+			const std::string expected = "Accessing an illegal scope index for the description scheme";
+			CPPUNIT_ASSERT_EQUAL(expected, std::string(err.what()));
+		}
+	}
+};
+
+
+
+
+
+
+} // namespace CLAMTest
---
+++ trunk/CLAM/test/UnitTests/DescriptorsTests/DescriptionScopeTest.cxx
@@ -0,0 +1,194 @@
+/*
+ * Copyright (c) 2004 MUSIC TECHNOLOGY GROUP (MTG)
+ *                         UNIVERSITAT POMPEU FABRA
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#include <cppunit/extensions/HelperMacros.h>
+#include "cppUnitHelper.hxx" // necessary for the custom assert
+
+#include "DescriptionScope.hxx"
+#include "DataTypes.hxx"
+
+namespace CLAMTest
+{
+
+class DescriptionScopeTest;
+
+CPPUNIT_TEST_SUITE_REGISTRATION( DescriptionScopeTest );
+
+class DescriptionScopeTest : public CppUnit::TestFixture
+{
+	CPPUNIT_TEST_SUITE( DescriptionScopeTest );
+	CPPUNIT_TEST( testGetIndex_whenEmpty );
+	CPPUNIT_TEST( testGetIndex_withOneInserted );
+	CPPUNIT_TEST( testGetIndex_withAWrongName );
+	CPPUNIT_TEST( testGetIndex_withSecondInsertedArray );
+	CPPUNIT_TEST( testAddAttribute_whenNameAlreadyAdded );
+	CPPUNIT_TEST( testAdding_DifferentTypes );
+	CPPUNIT_TEST( testCheckType_withOtherType );
+	CPPUNIT_TEST( testCheckType_withSameType );
+	CPPUNIT_TEST( testGetAttributeName_withTwoScopes );
+	CPPUNIT_TEST( testGetAttributeName_withWrongNumber );
+	CPPUNIT_TEST_SUITE_END();
+
+public:
+	/// Common initialization, executed before each test method
+	void setUp() { }
+
+	/// Common clean up, executed after each test method
+	void tearDown() { }
+
+private:
+	void testGetIndex_whenEmpty()
+	{
+		CLAM::DescriptionScope spec("TestScope");
+		try
+		{
+			spec.GetIndex("MyAttribute");
+			CPPUNIT_FAIL("Should have thrown an exception");
+		}
+		catch (CLAM::ErrAssertionFailed & err)
+		{
+			std::string expected = "Accessing an unexisting attribute 'TestScope':'MyAttribute'";
+			CPPUNIT_ASSERT_EQUAL(
+				expected,
+				std::string(err.what()));
+		}
+		CPPUNIT_ASSERT_EQUAL(0u,spec.GetNAttributes());
+	}
+
+	void testGetIndex_withOneInserted()
+	{
+		CLAM::DescriptionScope spec("TestScope");
+		spec.Add<CLAM::TData>("MyAttribute");
+		CPPUNIT_ASSERT_EQUAL(0u,spec.GetIndex("MyAttribute"));
+		CPPUNIT_ASSERT_EQUAL(1u,spec.GetNAttributes());
+	}
+
+	void testGetIndex_withAWrongName()
+	{
+		CLAM::DescriptionScope spec("TestScope");
+		spec.Add<CLAM::TData>("MyAttribute");
+		try
+		{
+			spec.GetIndex("Foo");
+			CPPUNIT_FAIL("Should have thrown an exception");
+		}
+		catch (CLAM::ErrAssertionFailed & err)
+		{
+			std::string expected = "Accessing an unexisting attribute 'TestScope':'Foo'";
+			CPPUNIT_ASSERT_EQUAL(
+				expected,
+				std::string(err.what()));
+		}
+		CPPUNIT_ASSERT_EQUAL(1u,spec.GetNAttributes());
+	}
+
+	void testGetIndex_withSecondInsertedArray()
+	{
+		CLAM::DescriptionScope spec("TestScope");
+		spec.Add<CLAM::TData>("MyAttribute");
+		spec.Add<CLAM::TData>("Foo");
+		CPPUNIT_ASSERT_EQUAL(0u,spec.GetIndex("MyAttribute"));
+		CPPUNIT_ASSERT_EQUAL(1u,spec.GetIndex("Foo"));
+		CPPUNIT_ASSERT_EQUAL(2u,spec.GetNAttributes());
+	}
+
+	void testAddAttribute_whenNameAlreadyAdded()
+	{
+		CLAM::DescriptionScope spec("TestScope");
+		spec.Add<CLAM::TData>("MyAttribute");
+		try
+		{
+			spec.Add<CLAM::TData>("MyAttribute");
+			CPPUNIT_FAIL("Should have thrown an exception");
+		}
+		catch (CLAM::ErrAssertionFailed & err)
+		{
+			std::string expected = "DescriptionScope::Add, Attribute already present";
+			CPPUNIT_ASSERT_EQUAL(
+				expected,
+				std::string(err.what()));
+		}
+		CPPUNIT_ASSERT_EQUAL(1u,spec.GetNAttributes());
+	}
+
+	void testAdding_DifferentTypes()
+	{
+		CLAM::DescriptionScope spec("TestScope");
+		spec.Add<CLAM::TData>("MyAttribute");
+		spec.Add<CLAM::TIndex>("Foo");
+		CPPUNIT_ASSERT_EQUAL(0u,spec.GetIndex("MyAttribute"));
+		CPPUNIT_ASSERT_EQUAL(1u,spec.GetIndex("Foo"));
+		CPPUNIT_ASSERT_EQUAL(2u,spec.GetNAttributes());
+	}
+
+	void testCheckType_withOtherType()
+	{
+		std::string expected = "Type Missmatch using a pool";
+		CLAM::DescriptionScope spec("TestScope");
+		spec.Add<CLAM::TData>("MyAttribute");
+		try
+		{
+			spec.CheckType(0,(CLAM::TIndex*)0);
+			CPPUNIT_FAIL("Should have thrown an exception");
+		}
+		catch (CLAM::ErrAssertionFailed & err)
+		{
+			CPPUNIT_ASSERT_EQUAL(
+				expected,
+				std::string(err.what()));
+		}
+	}
+
+	void testCheckType_withSameType()
+	{
+		CLAM::DescriptionScope spec("TestScope");
+		spec.Add<CLAM::TData>("MyAttribute");
+		spec.CheckType(0,(CLAM::TData*)0);
+	}
+
+	void testGetAttributeName_withTwoScopes()
+	{
+		CLAM::DescriptionScope scope("TestScope");
+		scope.Add< CLAM::TData >("MyAttribute");
+		scope.Add< CLAM::TData >("YourAttribute");
+		CPPUNIT_ASSERT_EQUAL(std::string("MyAttribute"), scope.GetAttributeName(0));
+		CPPUNIT_ASSERT_EQUAL(std::string("YourAttribute"), scope.GetAttributeName(1));
+	}
+
+	void testGetAttributeName_withWrongNumber()
+	{
+		CLAM::DescriptionScope scope("TestScope");
+		scope.Add< CLAM::TData >("MyAttribute");
+		try
+		{
+			scope.GetAttributeName(1);
+			CPPUNIT_FAIL("Should have thrown an exception");
+		}
+		catch (CLAM::ErrAssertionFailed & err)
+		{
+			const std::string expected = "GetAttributeName: Using a wrong index to look up an attribute name";
+			CPPUNIT_ASSERT_EQUAL(expected, std::string(err.what()));
+		}
+	}
+};
+
+
+} // namespace CLAMTest
