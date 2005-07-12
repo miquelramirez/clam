2f8fd7bfb151e6982480b2873b7f62756757d527
Miquel Ramírez <miquel.ramirez@gmail.com>
2005-07-12 15:09:30 +0000

    + Another milestone reached towards the CLAM binaries

--- CLAM/scons/libs/buildtools/file_retriever.py
+++ CLAM/scons/libs/buildtools/file_retriever.py
@@ -1,4 +1,4 @@
-import re, os
+import re, os, glob
 
 class FileRetriever :
 	__hdr_extensions = ['h', 'H', 'hxx', 'hpp']
@@ -47,13 +47,25 @@ class FileRetriever :
 
 	def scan( self ) :
 		for target in self.scantargets :
-			for file in os.listdir(target) :
-				if not self.is_blacklisted(file) :
-					if self.is_header(file) :
-						os.system( 'cp %s/%s include/CLAM'%(target,file) )
-						self.headers.append( 'include/CLAM/%s'%file )
-					if self.is_source(file) :
-						os.system( 'cp %s/%s src'%(target,file) )
-						self.sources.append( 'src/%s'%file )
+			if not os.path.isdir( target ) :
+				base = os.path.dirname(target)
+				for file in glob.glob(target ) :
+					if not self.is_blacklisted(file) :
+						if self.is_header(file) :
+							os.system( 'cp %s include/CLAM'%(file) )
+							self.headers.append( 'include/CLAM/%s'%os.path.split(file)[1] )
+						if self.is_source(file) :
+							os.system( 'cp %s src'%(file) )
+							self.sources.append( 'src/%s'%os.path.split(file)[1] )
+			else :
+
+				for file in os.listdir(target) :
+					if not self.is_blacklisted(file) :
+						if self.is_header(file) :
+							os.system( 'cp %s/%s include/CLAM'%(target,file) )
+							self.headers.append( 'include/CLAM/%s'%file )
+						if self.is_source(file) :
+							os.system( 'cp %s/%s src'%(target,file) )
+							self.sources.append( 'src/%s'%file )
 
 			
