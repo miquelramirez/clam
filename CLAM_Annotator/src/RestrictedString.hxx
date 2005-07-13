5b65401e99ecb9de0e6e87b7d76f17894b9349d1
David García Garzón <vokimon@users.noreply.github.com>
2005-07-13 14:57:31 +0000

    Indentation

--- CLAM_Annotator/src/RestrictedString.hxx
+++ CLAM_Annotator/src/RestrictedString.hxx
@@ -8,28 +8,28 @@
 
 namespace CLAM_Annotator{
 
-  /* We need this stupid class in order to differentiate from a regular string
-     when overloading functions*/
-  class RestrictedString:public CLAM::DynamicType
-  {
-    DYNAMIC_TYPE(RestrictedString,1);
-    DYN_ATTRIBUTE(0, public, std::string, String);
+	/* We need this stupid class in order to differentiate from a regular string
+	when overloading functions*/
+	class RestrictedString:public CLAM::DynamicType
+	{
+		DYNAMIC_TYPE(RestrictedString,1);
+		DYN_ATTRIBUTE(0, public, std::string, String);
 
-    void DefaultInit()
-    {
-      AddAll();
-      UpdateData();
-    }
-  public:
-    void operator = (const std::string& value)
-    {
-      SetString(value);
-    }
-    bool operator == (const std::string& value)
-    {
-      return (value == GetString());
-    }
-  };
+		void DefaultInit()
+		{
+			AddAll();
+			UpdateData();
+		}
+		public:
+		void operator = (const std::string& value)
+		{
+			SetString(value);
+		}
+		bool operator == (const std::string& value)
+		{
+			return (value == GetString());
+		}
+	};
 };
 
 #endif
