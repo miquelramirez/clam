c13cf1478bce4c911d193647c2851c91c4913501
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2005-07-13 16:54:23 +0000

    indentation

--- trunk/CLAM_Annotator/src/main.cxx
+++ trunk/CLAM_Annotator/src/main.cxx
@@ -6,14 +6,14 @@
 
 int main( int argc, char ** argv )
 {
-    QApplication app( argc, argv );
-    QSplashScreen splash( QPixmap::fromMimeSource("simac.png") );
-    splash.setCursor( QCursor(Qt::WaitCursor) );
-    splash.show();
-    splash.message("Loading data ... ");
-    Annotator * w = new Annotator("DefaultProject");
-    app.setMainWidget(w);
-    w->show();
-    splash.finish(w);
-    return app.exec();
+	QApplication app( argc, argv );
+	QSplashScreen splash( QPixmap::fromMimeSource("simac.png") );
+	splash.setCursor( QCursor(Qt::WaitCursor) );
+	splash.show();
+	splash.message("Loading data ... ");
+	Annotator * w = new Annotator("DefaultProject");
+	app.setMainWidget(w);
+	w->show();
+	splash.finish(w);
+	return app.exec();
 }
