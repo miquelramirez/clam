2f7467422cbd65db6d4191dc5e0924e0fc332399
mramirez <mramirez@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2005-07-14 16:50:53 +0000

    + ADDED: linker, so and real names for libs are now generated following Linux guidelines

--- trunk/CLAM/scons/libs/buildtools/__init__.py
+++ trunk/CLAM/scons/libs/buildtools/__init__.py
@@ -4,3 +4,14 @@ def store_headers_in_db(  db ) :
 	for _,_,headerlist in os.walk( 'include/CLAM' ) :
 		for header in headerlist :
 			db[header] = True
+
+def make_lib_names( source, target, env ) :
+	target_dir = os.path.dirname( str(target[0]) )
+	target_file = os.path.basename( str(target[0]) )
+	cwd = os.getcwd()
+	os.chdir( target_dir )
+	os.system( "ldconfig -n ." )
+	linkername = '.'.join(target_file.split('.')[0:2])
+	soname = '.'.join( target_file.split('.')[0:3])
+	os.system( "ln -sf %s %s"%(soname,linkername) )
+	os.chdir( cwd )
