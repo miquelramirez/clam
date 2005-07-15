7c3ad998ab19d4cc8bc8e160ffc9eff04cb16144
mramirez <mramirez@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2005-07-15 14:25:29 +0000

    + ADDED: installation

--- trunk/CLAM/scons/libs/buildtools/__init__.py
+++ trunk/CLAM/scons/libs/buildtools/__init__.py
@@ -14,4 +14,10 @@ def make_lib_names( source, target, env ) :
 	linkername = '.'.join(target_file.split('.')[0:2])
 	soname = '.'.join( target_file.split('.')[0:3])
 	os.system( "ln -sf %s %s"%(soname,linkername) )
+	os.system( "chmod 755 %s"%target_file )
+	os.system( "chmod 755 %s"%soname )
+	os.system( "chmod 755 %s"%linkername )
 	os.chdir( cwd )
+
+def make_lib_names_message( source, target, env ) :
+	return "Generate soname and linker name for %s"%target[0]
