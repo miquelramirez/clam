fc9d107e56676ec0a2fd49771d15362ca483087e
Miquel Ramírez <miquel.ramirez@gmail.com>
2005-07-25 14:31:53 +0000

    + FIXED: Weird problem with this class - linking networkeditor against binaries yield a 'missing typeinfo' error. This has been fixed by providing the definition of the class destructor (as well as other methods that would not be inlined no matter what) in a separate implementation file.

--- CLAM/src/Visualization/QTVM/Widgets/BPFEditor/QtEnvelopeEditor.hxx
+++ CLAM/src/Visualization/QTVM/Widgets/BPFEditor/QtEnvelopeEditor.hxx
@@ -19,31 +19,9 @@ Q_OBJECT
 
 public:
 
-	QtEnvelopeEditor(QWidget * parent)
-	{
-		mEmbededEditor = new Envelope_Point_Editor( parent );
-		mEmbededEditor->setMinimumSize( QSize( 350, 350) );
-		mMaxXBounded = false;
-		mMaxYBounded = false;
-		mMinYBounded = false;
-		mMinXBounded = false;
-	}
+	QtEnvelopeEditor(QWidget * parent);
 
-	void SetValue(const BPF & bpf)
-	{
-		if (mMaxYBounded) mEnvelope.set_maxY_value( mMaxY );
-		if (mMinYBounded) mEnvelope.set_minY_value( mMinY );
-		if (mMaxXBounded) mEnvelope.set_maxX_value( mMaxX );
-		if (mMinXBounded) mEnvelope.set_minX_value( mMinX );
-
-		for ( int i=0; i<bpf.Size(); i++ )
-		{
-			// TODO change this to floats for CLAM
-			mEnvelope.add_node_at_offset( bpf.GetXValue( i ), bpf.GetValueFromIndex( i ) );
-		}
-
-		mEmbededEditor->set_envelope( &mEnvelope );
-	}
+	void SetValue(const BPF & bpf);
 
 	void BoundMaxX(TData value)
 	{
@@ -89,23 +67,11 @@ public:
 		mMinXBounded = false;
 	}
 
-	BPF GetValue()
-	{
-		BPF value;
-		Envelope* env = mEmbededEditor->get_envelope();
-		value.SetSize(0);
-		for ( int i=0; i< env->get_node_count(); i++ )
-		{
-			// TODO change this to floats for CLAM
-			value.Insert( env->get_node_offset( i ), env->get_node_height( i ) );
-		}
-		return value;
-	}
+	BPF GetValue();
 
 	
-	~QtEnvelopeEditor() 
-	{
-	}
+	~QtEnvelopeEditor();
+
 private:
 	Envelope_Point_Editor * mEmbededEditor;
 	Envelope mEnvelope;
