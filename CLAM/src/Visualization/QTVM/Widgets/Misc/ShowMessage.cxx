2d4138250cb19dcd5744170d58dd731800a6e91f
imosquera <imosquera@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2005-07-29 17:20:56 +0000

    static class to show messages.

---
+++ CLAM/src/Visualization/QTVM/Widgets/Misc/ShowMessage.cxx
@@ -0,0 +1,38 @@
+#include <qmessagebox.h>
+#include "ShowMessage.hxx"
+
+namespace CLAM
+{
+	namespace VM
+	{
+		ShowMessage::Messages ShowMessage::mMessages;
+
+		void ShowMessage::NoMIDISupport()
+		{
+			if(!IsInitiated()) Init();
+			if(mMessages[NOMIDI]) return;
+			mMessages[NOMIDI] = true;
+			QMessageBox message("MIDI support not available",
+								"Seems that your system has not MIDI support. MIDI settings will be deactivated",
+								QMessageBox::Warning,
+								QMessageBox::Ok,
+								QMessageBox::NoButton,
+								QMessageBox::NoButton); 
+
+				message.exec();
+		}
+
+		void ShowMessage::Init()
+		{
+			for(unsigned i=0; i < MESSAGES; i++) mMessages.push_back(false);
+		}
+
+		bool ShowMessage::IsInitiated()
+		{
+			return mMessages.size();
+		}
+	}
+}
+
+// END
+
