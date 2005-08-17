9216cf149f5ab413d98f63dacd42f1750e3fbb15
imosquera <imosquera@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2005-08-17 15:12:55 +0000

    multiplayer: audio and fundamental frequency.

---
+++ CLAM/src/Visualization/QTVM/Widgets/Playable/QtSMSPlayer.cxx
@@ -0,0 +1,99 @@
+#include "Message.hxx"
+#include "APlayer.hxx"
+#include "FundPlayer.hxx"
+#include "QtSMSPlayer.hxx"
+
+namespace CLAM
+{
+	namespace VM
+	{
+		QtSMSPlayer::QtSMSPlayer(QWidget* parent)
+			: QtMultiPlayer(parent)
+			, mHasAudio(false)
+			, mHasFundamental(false)
+		{
+			AddPlayer(new APlayer());
+			AddPlayer(new FundPlayer());
+			AllPlayers(false);
+		}
+
+		QtSMSPlayer::~QtSMSPlayer()
+		{
+		}
+
+		void QtSMSPlayer::SetData(std::vector<const Audio*> data, bool setTime)
+		{
+			((APlayer*)mPlayers[AUDIO])->SetData(data,setTime);
+			mHasAudio = true;
+		}
+
+		void QtSMSPlayer::SetData(const Segment& segment)
+		{
+			((FundPlayer*)mPlayers[FUNDAMENTAL])->SetData(segment);
+			mHasFundamental = true;
+		}
+			
+		void QtSMSPlayer::SetLeftChannelMuted(bool b)
+		{
+			((APlayer*)mPlayers[AUDIO])->SetLeftChannelMuted(b);
+		}
+
+		void QtSMSPlayer::SetRightChannelMuted(bool b)
+		{
+			((APlayer*)mPlayers[AUDIO])->SetRightChannelMuted(b);
+		}
+
+		void QtSMSPlayer::Flush()
+		{
+			mHasAudio = false;
+			mHasFundamental = false;
+		}
+
+		void QtSMSPlayer::play()
+		{
+			if(mCurrentPlayer < AUDIO || mCurrentPlayer > FUNDAMENTAL) return;
+			int sentinel = LookIfAvailable(); 
+			switch(sentinel)
+			{
+				case AUDIO:
+					CLAM::VM::Message(QMessageBox::Critical,"QtSMSPlayer","The player has not audio data to play.");
+					break;
+				case FUNDAMENTAL:
+					CLAM::VM::Message(QMessageBox::Critical,"QtSMSPlayer","The player has not fundamental data to play.");
+					break;
+				default:
+					QtMultiPlayer::play();
+					break;
+			}
+			
+		}
+
+		void QtSMSPlayer::SetCurrentPlayer(int playerID)
+		{
+			if(playerID < AUDIO || playerID > FUNDAMENTAL)
+			{
+				CLAM::VM::Message(QMessageBox::Critical,"QtSMSPlayer","Player id out of range.");
+			}
+			else
+			{
+				mCurrentPlayer = playerID;
+			}
+		}
+
+		int QtSMSPlayer::LookIfAvailable()
+		{
+			if(mCurrentPlayer == AUDIO)
+			{
+				if(!mHasAudio) return AUDIO;
+			}
+			if(mCurrentPlayer == FUNDAMENTAL)
+			{
+				if(!mHasFundamental) return FUNDAMENTAL;
+			}
+			return PLAY;
+		}
+	}
+}
+
+// END
+
