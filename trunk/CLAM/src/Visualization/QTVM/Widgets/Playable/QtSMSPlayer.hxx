c7e30ceac0e63ef2864ed7e9c18b1ffa441da26f
imosquera <imosquera@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2005-08-17 15:12:55 +0000

    multiplayer: audio and fundamental frequency.

---
+++ trunk/CLAM/src/Visualization/QTVM/Widgets/Playable/QtSMSPlayer.hxx
@@ -0,0 +1,43 @@
+#ifndef __QTSMSPLAYER__
+#define __QTSMSPLAYER__
+
+#include "Segment.hxx"
+#include "QtMultiPlayer.hxx"
+
+namespace CLAM
+{
+	namespace VM
+	{
+		class QtSMSPlayer : public QtMultiPlayer
+		{
+			Q_OBJECT
+		public:
+			QtSMSPlayer(QWidget* parent=0);
+			~QtSMSPlayer();
+
+			void SetData(std::vector<const Audio*> data, bool setTime);
+			void SetData(const Segment& segment);
+			
+			void SetLeftChannelMuted(bool b);
+			void SetRightChannelMuted(bool b);
+
+			void SetCurrentPlayer(int playerID);
+
+			void Flush();
+
+		public slots:
+			void play();
+
+		private:
+			bool mHasAudio;
+			bool mHasFundamental;
+
+			enum { AUDIO=0, FUNDAMENTAL=1, PLAY=2 };
+
+			int LookIfAvailable();
+		};
+	}
+}
+
+#endif
+
