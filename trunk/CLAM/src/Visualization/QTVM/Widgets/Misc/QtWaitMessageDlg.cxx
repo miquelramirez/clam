cd4da0fba455f4d45d20d56509ce9cb7ae74dfc6
imosquera <imosquera@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2005-08-26 15:00:36 +0000

    *** empty log message ***

---
+++ trunk/CLAM/src/Visualization/QTVM/Widgets/Misc/QtWaitMessageDlg.cxx
@@ -0,0 +1,40 @@
+#include "QtWaitMessageDlg.hxx"
+#include "QtWaitMessageImpl.hxx"
+
+namespace CLAM
+{
+	namespace VM
+	{
+		QtWaitMessageDlg::QtWaitMessageDlg()
+		{
+			mWaitMessage = new QtWaitMessageImpl;
+			mWaitMessage->setGeometry(mWaitMessage->x(),mWaitMessage->y(),250,60);
+			mWaitMessage->setModal(true);
+		}
+
+		QtWaitMessageDlg::~QtWaitMessageDlg()
+		{
+			mWaitMessage->hide();
+			delete mWaitMessage;
+		}
+
+		void QtWaitMessageDlg::Caption(const std::string& caption)
+		{
+			mWaitMessage->setCaption(caption.c_str());
+		}
+
+		void QtWaitMessageDlg::LabelText(const std::string& label)
+		{
+			mWaitMessage->LabelText(label);
+		}
+
+		void QtWaitMessageDlg::Show()
+		{
+			mWaitMessage->show();
+		}
+	}
+
+}
+
+// END
+
---
+++ trunk/CLAM/src/Visualization/QTVM/Widgets/Misc/WaitMsgIndicator.cxx
@@ -0,0 +1,58 @@
+#include <qpainter.h>
+#include "Message.hxx"
+#include "WaitMsgIndicator.hxx"
+
+namespace CLAM
+{
+	namespace VM
+	{
+		WaitMsgIndicator::WaitMsgIndicator(QWidget* parent)
+			: QFrame(parent)
+			, mLeftXCoord(0)
+			, mState(MOV_TO_RIGHT)
+		{
+			setFrameStyle(QFrame::Panel | QFrame::Sunken);
+		}
+
+		WaitMsgIndicator::~WaitMsgIndicator()
+		{
+		}
+
+		void WaitMsgIndicator::paintEvent(QPaintEvent* pe)
+		{
+			QFrame::paintEvent(pe);
+			QColor brush(0,0,0);
+			QPainter painter(this);
+			painter.setBrush(brush);
+			painter.drawRect(mLeftXCoord,0,RECTWIDTH,height());
+		}
+
+		void WaitMsgIndicator::refresh()
+		{
+			if(mLeftXCoord < 0)
+			{
+				mState = MOV_TO_RIGHT;
+			}
+			else if(mLeftXCoord+RECTWIDTH > width())
+			{
+				mState = MOV_TO_LEFT;
+			}
+
+			switch(mState)
+			{
+				case MOV_TO_RIGHT:
+					mLeftXCoord += STEP;
+					break;
+				case MOV_TO_LEFT:
+					mLeftXCoord -= STEP;
+					break;
+				default:
+					break;
+			}
+			repaint();
+		}
+	}
+}
+
+// END
+
