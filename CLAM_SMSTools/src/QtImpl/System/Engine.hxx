14bfef53ffe444135b822a0b5d2ae74abfe4b0dc
imosquera <imosquera@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2005-08-29 15:38:28 +0000

    Facade for QtSMSTools

---
+++ CLAM_SMSTools/src/QtImpl/System/Engine.hxx
@@ -0,0 +1,72 @@
+#ifndef __ENGINE__
+#define __ENGINE__
+
+#include <string>
+#include "Thread.hxx"
+#include "QtProgress.hxx"
+#include "QtWaitMessage.hxx"
+#include "SMSBase.hxx"
+
+namespace QtSMS
+{
+	class ViewManager;
+
+	class Engine : public CLAM::SMSBase
+	{
+	public:
+		static Engine* Instance();
+		static ViewManager* DisplayManager();
+
+	    bool LoadConfiguration(const std::string& filename);
+		void StoreConfiguration(const std::string& filename);
+		bool LoadAnalysis(const std::string& filename);
+		void StoreAnalysis(const std::string& filename);
+		void StoreMelody(const std::string& filename);
+		void LoadTransformationScore(const std::string& filename);
+		void StoreTransformationScore(const std::string& filename);
+		void StoreOutputSound(const std::string& filename);
+		void StoreOutputSoundResidual(const std::string& filename);
+		void StoreOutputSoundSinusoidal(const std::string& filename);
+		void Analyze();
+		void ExtractMelody();
+		void DoTransformations();
+		void UndoTransformations();
+		void Synthesize();
+
+		CLAM::SMSAppState& State() { return GetState(); }
+	  
+	protected:
+		void Run(){ /* empty body */ }
+
+		CLAMGUI::Progress* CreateProgress(const char* title,float from,float to){
+			return new CLAMGUI::QtProgress(title,from,to);
+		}
+
+		CLAMGUI::WaitMessage* CreateWaitMessage(const char* title){
+			return new CLAMGUI::QtWaitMessage(title);
+		}
+
+	private:
+		Engine();
+			
+		static Engine* mInstance;
+
+		CLAM::Thread mThread;
+		std::string  mCurrentFileName;
+
+	    bool LoadInputSound();
+		bool LoadSound(const std::string& filename,CLAM::Segment& segment);
+		void StoreSound(const CLAM::Audio& audio,const std::string& filename);
+
+		void LaunchMethodOnThread(CBL::Functor0 method);
+
+		void DoStoreAnalysis();
+		void DoStoreMelody();
+		void DoMelodyExtraction();
+	};
+}
+
+
+
+#endif
+
