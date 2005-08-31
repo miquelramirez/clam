928db79f7f5c93d28dd949cc5db4c165d7cd8b96
David García Garzón <vokimon@users.noreply.github.com>
2005-08-31 16:34:01 +0000

    SongFiles class is dead

--- CLAM_Annotator/src/SongFiles.hxx
+++ CLAM_Annotator/src/SongFiles.hxx
@@ -12,25 +12,13 @@ namespace CLAM_Annotator{
 		DYN_ATTRIBUTE(0,public, std::string, SoundFile);
 		/**
 		 * Descriptor File Associated to the sound file. In most applications if this attribute
-		 * is missint the application is expected to add the .pool extension to the SoundFile name
+		 * is missing the application is expected to add the .pool extension to the SoundFile name
 		 */
 		DYN_ATTRIBUTE(1,public, std::string, PoolFile);
 		
 		void DefaultInit()
 		{
 			AddSoundFile();
-			UpdateData();
-		}
-	};
-
-	class SongFiles: public CLAM::DynamicType
-	{
-		DYNAMIC_TYPE(SongFiles,1);
-		DYN_CONTAINER_ATTRIBUTE(0,public, std::vector<Song>, FileNames,Name);
-		
-		void DefaultInit()
-		{
-			AddAll();
 			UpdateData();
 		}
 	};
