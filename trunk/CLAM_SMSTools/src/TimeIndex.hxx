13ebd5b6aed7f10f0aeb1a710deabedd3f8d825c
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2005-09-05 10:52:00 +0000

    Indentation

--- trunk/CLAM_SMSTools/src/TimeIndex.hxx
+++ trunk/CLAM_SMSTools/src/TimeIndex.hxx
@@ -1,20 +1,22 @@
 #ifndef __TIMEINDEX_H__
 #define __TIMEINDEX_H__
 
-#include "DynamicType.hxx"
 #include "ProcessingData.hxx"
 
-namespace CLAM {
+namespace CLAM
+{
 
-  class TimeIndex : public ProcessingData {
-  public:
-    DYNAMIC_TYPE_USING_INTERFACE(TimeIndex, 2, ProcessingData);
-    DYN_ATTRIBUTE (0,public, TData, Position);
-    DYN_ATTRIBUTE (1,public, TData, Weight);
+class TimeIndex : public ProcessingData 
+{
+public:
+	DYNAMIC_TYPE_USING_INTERFACE(TimeIndex, 2, ProcessingData);
+	DYN_ATTRIBUTE (0,public, TData, Position);
+	DYN_ATTRIBUTE (1,public, TData, Weight);
+protected:
+	void DefaultInit();
+};
 
-    void DefaultInit();
-  };
+} // namespace CLAM
 
-}
+#endif // __TIMEINDEX_H__
 
-#endif
