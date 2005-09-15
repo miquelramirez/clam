3a2b7d8316559a2977a333cda5090ab6a9dba65a
imosquera <imosquera@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2005-09-15 07:11:12 +0000

    First version for the MIDI file writer.

---
+++ trunk/CLAM/src/Tools/MIDIIO/File/MIDIWriter.hxx
@@ -0,0 +1,36 @@
+#ifndef __MIDIWRITER__
+#define __MIDIWRITER__
+
+#include <stdio.h>
+
+#define MThd 0x4d546864
+#define MTrk 0x4d54726b
+
+namespace MIDI
+{
+	class Song;
+	
+	class Writer
+	{
+	public:
+		Writer(const char* filename);
+		~Writer();
+
+		void Write(Song& s);
+
+	private:
+		FILE* mFile;
+		long  mBytesWritten;
+		int   mTracks;
+		int   mFormat;
+		int   mDivision;
+		
+		void WriteHeader();
+		void WriteVarLen(register unsigned long value);
+		void Write32Bit(unsigned long data);
+		void Write16Bit(int data);
+		void WriteCh(char c);
+	};
+}
+
+#endif
