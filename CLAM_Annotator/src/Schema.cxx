040acac6e6475c00a3ad9c80c8e9285214317a0b
David García Garzón <vokimon@users.noreply.github.com>
2005-09-19 11:23:50 +0000

    The make descriptor function is useless The findelement needs a scope

--- CLAM_Annotator/src/Schema.cxx
+++ CLAM_Annotator/src/Schema.cxx
@@ -16,26 +16,26 @@ namespace CLAM_Annotator
 			const std::string & scope = it2->GetScope();
 			if(type=="Int")
 				validated = validated && 
-					ValidateDescriptor(MakeDescriptor(
-						*pool.GetReadPool<int>(scope,name),name));
+					ValidateDescriptor(scope, name,
+						pool.GetReadPool<int>(scope,name));
 			else if(type=="Float")
 				validated = validated && 
-					ValidateDescriptor(MakeDescriptor(
-						*pool.GetReadPool<float>(scope,name),name));
+					ValidateDescriptor(scope, name,
+						pool.GetReadPool<float>(scope,name));
 			else if(type=="RestrictedString")
 				validated = validated && 
-					ValidateDescriptor(MakeDescriptor(
-						*pool.GetReadPool<RestrictedString>(scope,name),name));
+					ValidateDescriptor(scope, name,
+						pool.GetReadPool<RestrictedString>(scope,name));
 			else if(type=="String")
 				validated = validated &&
-					ValidateDescriptor(MakeDescriptor(
-						*pool.GetReadPool<CLAM::Text>(scope,name),name));
+					ValidateDescriptor(scope, name,
+						pool.GetReadPool<CLAM::Text>(scope,name));
 			CLAM_ASSERT(validated,"NotValidated");
 		}
 		return validated;
 	}
 
-	SchemaAttribute Schema::FindElement(const std::string& descriptorName) const
+	SchemaAttribute Schema::FindElement(const std::string& scope, const std::string& descriptorName) const
 	{
 		const std::list<SchemaAttribute> & hlds = GetAllAttributes();
 		std::list<SchemaAttribute>::const_iterator it;
