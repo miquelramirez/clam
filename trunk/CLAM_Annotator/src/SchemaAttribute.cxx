99b7b501cc049f16d98ee6a52883480546edc8d5
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2005-09-19 11:12:15 +0000

    Nicer XML Segmentation policy added as an attribute for segmentation the get type from value are not used anymore

--- trunk/CLAM_Annotator/src/SchemaAttribute.cxx
+++ trunk/CLAM_Annotator/src/SchemaAttribute.cxx
@@ -14,51 +14,27 @@ namespace CLAM_Annotator{
 		UpdateData();
 	}
 
-	bool SchemaAttribute::Validate(const Descriptor<CLAM_Annotator::RestrictedString>& descriptor)const
+	bool SchemaAttribute::Validate(const CLAM_Annotator::RestrictedString * descriptor) const
 	{
-		bool validated = true;
-		validated = (std::find(GetRestrictionValues().begin(),
-			GetRestrictionValues().end(),descriptor.GetValue().GetString())!=
-			GetRestrictionValues().end());
-		CLAM_DEBUG_ASSERT(validated,
-			std::string("Descriptor did not validate: " + descriptor.GetName()).c_str());
-		return validated;
-		
+		return std::find(GetRestrictionValues().begin(),
+			GetRestrictionValues().end(),descriptor[0].GetString())!=
+			GetRestrictionValues().end();
 	}
 	
-	bool SchemaAttribute::Validate (const Descriptor<int>& descriptor)const
+	bool SchemaAttribute::Validate (const int * descriptor) const
 	{
 		if (!HasiRange()) return true;
-		if (descriptor.GetValue()<GetiRange().GetMin()) return false;
-		if (descriptor.GetValue()>GetiRange().GetMax()) return false;
+		if (descriptor[0]<GetiRange().GetMin()) return false;
+		if (descriptor[0]>GetiRange().GetMax()) return false;
 		return true;
 	}
 	
-	bool SchemaAttribute::Validate (const Descriptor<float>& descriptor)const
+	bool SchemaAttribute::Validate (const float * descriptor) const
 	{
 		if (!HasfRange()) return true;
-		if (descriptor.GetValue()<GetfRange().GetMin()) return false;
-		if (descriptor.GetValue()>GetfRange().GetMax()) return false;
+		if (descriptor[0]<GetfRange().GetMin()) return false;
+		if (descriptor[0]>GetfRange().GetMax()) return false;
 		return true;
-	}
-
-	/**************** Auxiliary functions	**************/
-
-	std::string GetTypeFromValue(int i)
-	{
-		return "Int";
-	}
-	std::string GetTypeFromValue(float f)
-	{
-		return "Float";
-	}
-	std::string GetTypeFromValue(const CLAM::Text& s)
-	{
-		return "String";
-	}
-	std::string GetTypeFromValue(const CLAM_Annotator::RestrictedString& r)
-	{
-		return "RestrictedString";
 	}
 
 
--- trunk/CLAM_Annotator/src/SchemaAttribute.hxx
+++ trunk/CLAM_Annotator/src/SchemaAttribute.hxx
@@ -5,6 +5,7 @@
 
 #include <list>
 #include "Text.hxx"
+#include "Enum.hxx"
 
 #include "Pool.hxx"
 #include "Assert.hxx"
@@ -15,40 +16,101 @@
 
 namespace CLAM_Annotator{
 	
-	//TODO: type should be an enum not a string
+	class SegmentationPolicy : public CLAM::Enum
+	{
+	public:
+		SegmentationPolicy() : CLAM::Enum(ValueTable(), eUnsized) {}
+		SegmentationPolicy(tValue v) : CLAM::Enum(ValueTable(), v) {};
+		SegmentationPolicy(const std::string & s) : CLAM::Enum(ValueTable(), s) {};
+
+		virtual CLAM::Component * Species() const {return new SegmentationPolicy();}
+
+		typedef enum {
+			eUnsized,
+			eContinuous,
+			eDiscontinuous,
+			eOverlapping
+			
+		} tEnum;
+ 		static tEnumValue * ValueTable()
+ 		{
+ 			static tEnumValue sValueTable[] = 
+ 			{
+ 				{eUnsized,"Unsized"},
+ 				{eContinuous,"Continuous"},
+ 				{eDiscontinuous,"Discontinuous"},
+ 				{eOverlapping,"Overlapping"},
+ 				{0,NULL}
+ 			};
+ 			return sValueTable;
+ 		}
+	};
 	
 	class SchemaAttribute:public CLAM::DynamicType
 	{
-		DYNAMIC_TYPE(SchemaAttribute,7);
-		DYN_ATTRIBUTE(0, public, std::string, Name);
-		DYN_ATTRIBUTE(1, public, std::string, Scope);
-		//TODO: this should better be an enum
-		DYN_ATTRIBUTE(2, public, std::string, Type);
-		/*In case type is a RestrictedString, an optional list of enumeration values must be offered*/
+		DYNAMIC_TYPE(SchemaAttribute,8);
+		DYN_ATTRIBUTE(0, public, std::string, Name); ///< The attribute name, unique within the scope.
+		DYN_ATTRIBUTE(1, public, std::string, Scope); ///< The scope name at which the attribute sticks.
+		DYN_ATTRIBUTE(2, public, std::string, Type); ///< The type name. Should be at the Type factory.
+		/// To be added when type is RestrictedString, contains the available values
 		DYN_CONTAINER_ATTRIBUTE(3, public, std::list<std::string>, RestrictionValues, Restriction);
-		//In case type is a number we need to add a range
+		/// To be \b optionaly added when type is Float, contains the boundary values
 		DYN_ATTRIBUTE(4, public, Range<float>, fRange);
+		/// To be added when type is Int, contains the boundary values
 		DYN_ATTRIBUTE(5, public, Range<int>, iRange);
+		/// To be added when type is Segmentation, contains the Scope containing attributes for segments.
+		/// Empty string means no scope.
 		DYN_ATTRIBUTE(6, public, std::string, ChildScope);
+		DYN_ATTRIBUTE(7, public, SegmentationPolicy, SegmentationPolicy);
 		
 		void DefaultInit();
 		
 	public:
-		template <class T>
-		bool Validate (const Descriptor<T>& descriptor)const
+		bool Validate (const void * descriptors) const
 		{
 			return true;
 		}
-		bool Validate (const Descriptor<CLAM_Annotator::RestrictedString>& descriptor)const;
-		bool Validate (const Descriptor<int>& descriptor)const;
-		bool Validate (const Descriptor<float>& descriptor)const;
+		bool Validate (const CLAM_Annotator::RestrictedString * descriptors) const;
+		bool Validate (const int * descriptors) const;
+		bool Validate (const float * descriptors) const;
+
+		void StoreOn(CLAM::Storage & storage) const
+		{
+			typedef CLAM::XMLAdapter<std::string> Adapter;
+			Adapter adapter1(GetName(), "name", false);
+			storage.Store(adapter1);
+			Adapter adapter2(GetScope(), "scope", false);
+			storage.Store(adapter2);
+			Adapter adapter3(GetType(), "type", false);
+			storage.Store(adapter3);
+			StoreRestrictionValues(storage);
+			StorefRange(storage);
+			StoreiRange(storage);
+			StoreChildScope(storage);
+			StoreSegmentationPolicy(storage);
+		}
+		
+		void LoadFrom(CLAM::Storage & storage)
+		{
+			AddAll();
+			UpdateData();
+			typedef CLAM::XMLAdapter<std::string> Adapter;
+			Adapter adapter1(GetName(), "name", false);
+			if (!storage.Load(adapter1)) RemoveName();
+			Adapter adapter2(GetScope(), "scope", false);
+			if (!storage.Load(adapter2)) RemoveScope();
+			Adapter adapter3(GetType(), "type", false);
+			if (!storage.Load(adapter3)) RemoveType();
+			LoadRestrictionValues(storage);
+			LoadfRange(storage);
+			LoadiRange(storage);
+			LoadChildScope(storage);
+			LoadSegmentationPolicy(storage);
+			UpdateData();
+		}
+
 		
 	};
-
-	std::string GetTypeFromValue(int i);
-	std::string GetTypeFromValue(float f);
-	std::string GetTypeFromValue(const CLAM::Text& s);
-	std::string GetTypeFromValue(const CLAM_Annotator::RestrictedString& r);
 };
 
 #endif
