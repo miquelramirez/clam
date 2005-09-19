3217f988d99367f84a7dea3ce09dc8f179e2ce7a
imosquera <imosquera@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2005-09-19 07:35:58 +0000

    Example of usage for CLAM::MIDIFileReader/Writer.

---
+++ trunk/CLAM/examples/MIDIFileReadWriteExample.cxx
@@ -0,0 +1,72 @@
+#include <iostream>
+#include "MIDIFileReader.hxx"
+#include "MIDIFileWriter.hxx"
+
+using namespace CLAM;
+
+int main(int argc, char** argv)
+{
+	char* filein=0;
+	char* fileout=0;
+
+	switch(argc)
+	{
+		case 1:
+			filein="../../../../src/Tools/MIDIIO/File/test.mid";
+			fileout="midi_out.mid";
+			break;
+		case 2:
+			filein=argv[1];
+			fileout="midi_out.mid";
+			break;
+		case 3:
+			filein=argv[1];
+			fileout=argv[2];
+			break;
+	}
+
+	MIDISong song;
+	MIDIFileIOConfig cfg;
+	cfg.SetFileName(filein);
+	MIDIFileReader reader(cfg);
+
+	std::cout << "reading song from " << filein << std::endl;
+
+	reader.Start();
+	reader.Do(song);
+	reader.Stop();
+
+	cfg.SetFileName("original_file.mid");
+	MIDIFileWriter writer(cfg);
+
+	std::cout << "storing a copy in original_file.mid" << std::endl; 
+
+	writer.Start();
+	writer.Do(song);
+	writer.Stop();
+
+	std::cout << "increasing all notes two tones..." << std::endl;
+	for(int i=0; i < song.GetNumberOfTracks(); i++)
+	{
+		if(song.GetTracks()[i].GetChannel()==9) continue;
+		for(int j=0; j < song.GetTracks()[i].GetTrackMelody().GetNumberOfNotes(); j++)
+		{
+			int key = song.GetTracks()[i].GetTrackMelody().GetNoteArray()[j].GetKey();
+			song.GetTracks()[i].GetTrackMelody().GetNoteArray()[j].SetKey(key+4);
+		}
+	}
+
+	cfg.SetFileName(fileout);
+	writer.Configure(cfg);
+
+	std::cout << "storing the modified song in " << fileout << std::endl;
+	
+	writer.Start();
+	writer.Do(song);
+	writer.Stop();
+
+	return 0;
+}
+
+// END
+
