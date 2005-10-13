da10d3a94050e10d9f8e83cd7b9328f76284a0ec
uid39696 <uid39696@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2005-10-13 10:39:21 +0000

    license dialog

---
+++ CLAM_SMSTools/src/QtImpl/GUI/Dialogs/License/LicenseDlg.hxx
@@ -0,0 +1,17 @@
+#ifndef __LICENSEDLG__
+#define __LICENSEDLG__
+
+#include "LicenseDlgBase.h"
+
+namespace QtSMS
+{
+	class LicenseDlg : public LicenseDlgBase
+	{
+	public:
+		LicenseDlg(QWidget* parent=0);
+		~LicenseDlg();
+	};
+}
+
+#endif
+
