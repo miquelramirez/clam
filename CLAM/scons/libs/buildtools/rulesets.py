90c4999f51836975d5e48c9578110639b29c5e7b
xoliver <xoliver@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2005-11-16 09:01:59 +0000

    Added missing file (it was inside the new-header-style branch)

---
+++ CLAM/scons/libs/buildtools/rulesets.py
@@ -0,0 +1,58 @@
+from SCons.Action import *
+from buildtools import *
+
+def handle_preinclude ( env ):
+	if sys.platform != 'win32' :
+		env.Append(CCFLAGS='-include CLAM/%s'%env['preinclude'])
+	else:
+		env.Append(CCFLAGS='/FICLAM/%s'%env['preinclude'])
+	return
+
+def posix_lib_rules( name, version, headers, source_files, install_dirs, env) :
+	lib_descriptor = env.File( 'clam_'+name+'.pc' )
+
+	soname = 'libclam_'+name+'.so.%s'%version.split('.')[0]
+	linker_name = 'libclam_'+name+'.so'
+	env.Append(SHLINKFLAGS=['-Wl,-soname,%s'%soname ] )
+
+	lib = env.SharedLibrary( 'clam_' + name, source_files, SHLIBSUFFIX='.so.%s'%version )
+
+	soname_lib = env.SonameLink( soname, lib )
+	linkername_lib = env.LinkerNameLink( linker_name, soname_lib)
+
+	tgt = env.Alias( name, linkername_lib )
+
+	install_headers = env.Install( install_dirs.inc+'/CLAM', headers )
+	env.AddPostAction( install_headers, "chmod 644 $TARGET" )
+
+	install_lib = env.Install( install_dirs.lib, lib)
+	env.AddPostAction( install_lib, Action(make_lib_names, make_lib_names_message ) )
+
+	install_descriptor = env.Install( install_dirs.lib+'/pkgconfig', lib_descriptor )
+
+	install_tgt = env.Alias( 'install_' + name, [install_headers, install_lib, install_descriptor] )
+
+	runtime_lib = env.Install( install_dirs.lib, lib )
+	runtime_soname = env.SonameLink( install_dirs.lib + '/' + soname, runtime_lib )
+
+	env.Alias( 'install_'+name+'_runtime', [runtime_lib, runtime_soname] )
+
+	static_lib = env.Library( 'clam_'+name, source_files )
+	install_static = env.Install( install_dirs.lib, static_lib )
+
+	dev_linkername =  env.LinkerNameLink( install_dirs.lib+'/'+linker_name, install_dirs.lib+'/'+soname) 
+	env.Alias( 'install_'+name+'_dev', [install_headers,dev_linkername, install_descriptor, install_static] )
+	
+	return tgt, install_tgt
+
+def win32_lib_rules( name, version, headers, source_files, install_dirs, env ) :
+	static_lib = env.Library( 'clam_' + name, source_files )
+	tgt = env.Alias(name, static_lib)
+	lib_descriptor = env.File( 'clam_'+name+'.pc' )
+	install_static = env.Install( install_dirs.lib, static_lib )
+	install_descriptor = env.Install( install_dirs.lib+'/pkgconfig', lib_descriptor )
+	install_headers = env.Install( install_dirs.inc+'/CLAM', headers )
+	env.Alias('install-'+name, [install_headers,install_static,install_descriptor])
+	return tgt, install_static
+	
+	
