c16ceacf062e3b4891c9d6135530ae69f3d1c664
David García Garzón <vokimon@users.noreply.github.com>
2005-11-22 04:23:26 +0000

    Talking about Onsets and Offsets instead of BoundsEnd and BoundsBegin Picking Onset as well as Offsets Refactoring extract common code for PickOnset and PickOffset -> PickPosition Functions to read the onsets and offsets lists and the selections

--- CLAM_Annotator/test/Segmentation.hxx
+++ CLAM_Annotator/test/Segmentation.hxx
@@ -16,12 +16,12 @@ namespace CLAM_Annotator
 			public:
 			const char * what() const throw () { return "Segmentation point inserted out of limits";}
 		};
-		typedef std::vector<double> Bounds;
+		typedef std::vector<double> TimePositions;
 	public:
 		Segmentation(double maxLength)
 		{
-			_beginBounds.push_back(0);
-			_endBounds.push_back(maxLength);
+			_onsets.push_back(0);
+			_offsets.push_back(maxLength);
 			_selection.push_back(false);
 
 		}
@@ -31,13 +31,13 @@ namespace CLAM_Annotator
 		unsigned insert(double timePosition)
 		{
 			if (timePosition<=0.0) throw InsertedOutOfBounds();
-			Bounds::iterator insertPoint = 
-				std::lower_bound(_endBounds.begin(), _endBounds.end(), timePosition);
-			if (insertPoint == _endBounds.end()) throw InsertedOutOfBounds();
+			TimePositions::iterator insertPoint = 
+				std::lower_bound(_offsets.begin(), _offsets.end(), timePosition);
+			if (insertPoint == _offsets.end()) throw InsertedOutOfBounds();
 			// 'position' must be computed before the insertion to not invalidate iterators.
-			int position = insertPoint - _endBounds.begin() +1;
-			_endBounds.insert(insertPoint, timePosition);
-			_beginBounds.insert(_beginBounds.begin()+position, _endBounds[position-1]);
+			int position = insertPoint - _offsets.begin() +1;
+			_offsets.insert(insertPoint, timePosition);
+			_onsets.insert(_onsets.begin()+position, _offsets[position-1]);
 			_selection.insert(_selection.begin()+position, false);
 			return position;
 		}
@@ -49,69 +49,61 @@ namespace CLAM_Annotator
 		 */
 		void remove(unsigned segment)
 		{
-			if (_endBounds.size()==1) return;
-			unsigned endBoundToRemove = segment? segment-1 : 0;
-			_endBounds.erase(_endBounds.begin()+endBoundToRemove);
-			_beginBounds.erase(_beginBounds.begin()+segment);
+			if (_offsets.size()==1) return;
+			unsigned offsetToRemove = segment? segment-1 : 0;
+			_offsets.erase(_offsets.begin()+offsetToRemove);
+			_onsets.erase(_onsets.begin()+segment);
 			_selection.erase(_selection.begin()+segment);
-			if (segment==0) _beginBounds[0]=0;
+			if (segment==0) _onsets[0]=0;
 		}
 		/**
-		 * Returns the index of the segment which is nearest to the
+		 * Returns the index of the segment offset which is nearest to the
 		 * given time position, and within the tolerance.
 		 * If no end of segment within the tolerance range an invalid
-		 * segment is returned (> nSegments)
+		 * segment is returned (nSegments)
 		 */
-		unsigned pickEndBound(double timePosition, double tolerance) const
+		unsigned pickOffset(double timePosition, double tolerance) const
 		{
-			Bounds::const_iterator lowerBound = 
-				std::lower_bound(_endBounds.begin(), _endBounds.end(), timePosition-tolerance);
-			Bounds::const_iterator upperBound = 
-				std::upper_bound(lowerBound, _endBounds.end(), timePosition+tolerance);
-
-			if (lowerBound==upperBound) return _endBounds.size(); // None found
-	
-			// Pick the closest in range
-			unsigned lowerSegment = lowerBound - _endBounds.begin();
-			unsigned upperSegment = upperBound - _endBounds.begin();
-			double lastDifference = std::fabs(timePosition-_endBounds[lowerSegment]);
-			for (unsigned i=lowerSegment; i<upperSegment; i++)
-			{
-				double newDifference = std::fabs(timePosition-_endBounds[i]);
-				if (newDifference>lastDifference) break;
-				lastDifference = newDifference;
-				lowerSegment = i;
-			}
-			return lowerSegment;
+			return pickPosition(_offsets, timePosition, tolerance);
+		}
+		/**
+		 * Returns the index of the segment onset which is nearest to the
+		 * given time position, and within the tolerance.
+		 * If no end of segment within the tolerance range an invalid
+		 * segment is returned (nSegments)
+		 */
+		unsigned pickOnset(double timePosition, double tolerance) const
+		{
+			return pickPosition(_onsets, timePosition, tolerance);
 		}
 		/**
 		 * Returns the index of the segment which body is on timePosition.
 		 */
 		unsigned pickSegmentBody(double timePosition) const
 		{
-			if (timePosition<0) return _endBounds.size();
-			Bounds::const_iterator lowerBound =
-				std::lower_bound(_endBounds.begin(), _endBounds.end(), timePosition);
-			return lowerBound - _endBounds.begin();
+			if (timePosition<0) return _offsets.size();
+			TimePositions::const_iterator lowerBound =
+				std::lower_bound(_offsets.begin(), _offsets.end(), timePosition);
+			return lowerBound - _offsets.begin();
 		}
-		void dragEndBound(unsigned segment, double newTimePosition)
+		void dragOffset(unsigned segment, double newTimePosition)
 		{
-			if (segment==_endBounds.size()) return;
-			if (segment+1==_endBounds.size()) return;
+			if (segment==_offsets.size()) return;
+			if (segment+1==_offsets.size()) return;
 
-			double leftBound = _beginBounds[segment];
+			double leftBound = _onsets[segment];
 			if (newTimePosition<leftBound)
 				newTimePosition = leftBound;
-			if (newTimePosition>_endBounds[segment+1])
-				newTimePosition = _endBounds[segment+1];
+			if (newTimePosition>_offsets[segment+1])
+				newTimePosition = _offsets[segment+1];
 
-			_endBounds[segment]=newTimePosition;
-			_beginBounds[segment+1]=newTimePosition;
+			_offsets[segment]=newTimePosition;
+			_onsets[segment+1]=newTimePosition;
 		}
-		void dragBeginBound(unsigned segment, double newTimePosition)
+		void dragOnset(unsigned segment, double newTimePosition)
 		{
 			if (segment==0) return;
-			dragEndBound(segment-1, newTimePosition);
+			dragOffset(segment-1, newTimePosition);
 		}
 		void select(unsigned segment)
 		{
@@ -129,17 +121,59 @@ namespace CLAM_Annotator
 		std::string boundsAsString() const
 		{
 			std::ostringstream os;
-			for (unsigned i=0; i<_endBounds.size(); i++)
+			for (unsigned i=0; i<_offsets.size(); i++)
 			{
 				if (_selection[i]) os << "+";
-				os << "(" << _beginBounds[i] << "," << _endBounds[i] << ") ";
+				os << "(" << _onsets[i] << "," << _offsets[i] << ") ";
 			}
 			return os.str();
 		}
+
+		const TimePositions & onsets() const
+		{
+			return _onsets;
+		}
+		const TimePositions & offsets() const
+		{
+			return _offsets;
+		}
+		const std::vector<bool> & selections() const
+		{
+			return _selection;
+		}
 	private:
-		Bounds _endBounds;
-		Bounds _beginBounds;
+		TimePositions _onsets;
+		TimePositions _offsets;
 		std::vector<bool> _selection;
+	private:
+		/**
+		 * Returns the index of the position which is nearest to the
+		 * given time position and within the tolerance.
+		 * If no end of segment within the tolerance range an invalid
+		 * index is returned (nPositions)
+		 */
+		unsigned pickPosition(const TimePositions & positions, double timePosition, double tolerance) const
+		{
+			TimePositions::const_iterator lowerBound = 
+				std::lower_bound(positions.begin(), positions.end(), timePosition-tolerance);
+			TimePositions::const_iterator upperBound = 
+				std::upper_bound(lowerBound, positions.end(), timePosition+tolerance);
+
+			if (lowerBound==upperBound) return positions.size(); // None found
+	
+			// Pick the closest in range
+			unsigned lowerSegment = lowerBound - positions.begin();
+			unsigned upperSegment = upperBound - positions.begin();
+			double lastDifference = std::fabs(timePosition-positions[lowerSegment]);
+			for (unsigned i=lowerSegment; i<upperSegment; i++)
+			{
+				double newDifference = std::fabs(timePosition-positions[i]);
+				if (newDifference>lastDifference) break;
+				lastDifference = newDifference;
+				lowerSegment = i;
+			}
+			return lowerSegment;
+		}
 	};
 
 }
