ad0cd73355196d2021a1ab2f692c36a01cb0ee89
imosquera <imosquera@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2005-11-23 12:53:49 +0000

    SegmentEditorExample first revision.

---
+++ CLAM/examples/QtPlots/SegmentEditorExample/SegmentEditorExample.cxx
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include "audio_file_utils.hxx"
+#include "QtAppWrapper.hxx"
+#include "QtAudioPlot.hxx"
+#include "Segmentation.hxx"
+
+int main()
+{
+	CLAM::Audio audio;
+	if(qtvm_examples_utils::load_audio("../data/birthday.wav",audio))
+	{
+	    printf("File Error: \'birthday.wav\' audio file not found!\n");
+	    exit(1);
+	}
+
+	CLAM::Segmentation* seg = new CLAM::Segmentation(audio.GetSize());
+	double step = double(audio.GetSize())/4.0;
+	// make some divisions
+	for(int i=1; i <= 3; i++)
+	{
+		seg->insert(double(i)*step);
+	}
+
+	CLAM::VM::QtAppWrapper::Init();
+
+	CLAM::VM::QtAudioPlot plot;
+	plot.Label("Audio");
+	plot.Geometry(100,100,700,350);
+	plot.SetData(audio);
+	plot.SetSegmentation(seg);
+	plot.SetForegroundColor(CLAM::VM::VMColor::Blue());
+	plot.Show();
+	
+	return CLAM::VM::QtAppWrapper::Run();
+
+
+
+}
+
+// END
+
