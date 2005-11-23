3852a1d8f072082578ac1fc20f9ca05a78ec4b1f
imosquera <imosquera@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2005-11-23 12:49:58 +0000

    SegmentRenderer class. First revision.

---
+++ trunk/CLAM/src/Visualization/QTVM/Renderers/SegmentRenderer.hxx
@@ -0,0 +1,32 @@
+#ifndef __SEGMENTRENDERER__
+#define __SEGMENTRENDERER__
+
+#include "Renderer.hxx"
+#include "Segmentation.hxx"
+
+namespace CLAM
+{
+	namespace VM
+	{
+		class SegmentRenderer : public Renderer
+		{
+		public:
+			SegmentRenderer();
+			~SegmentRenderer();
+
+			void SetData(const Segmentation& s);
+			void Render();
+		
+		private:
+			Segmentation mSegmentation;
+			
+			enum { NORMAL=0, CURRENT, SELECTED };
+
+			void Rect(double left, double right, double top);
+			bool IsVisible(double left, double right, double lBound, double rBound);
+			void DrawSegment(double left, double right, double top, int type=NORMAL);
+		};
+	}
+}
+
+#endif
