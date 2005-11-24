d1686dced54c0f746fa08ac2292ea3814adc996f
imosquera <imosquera@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2005-11-24 09:49:33 +0000

    add some methods to test mouse move events.

--- CLAM/src/Visualization/QTVM/Controllers/SegmentEditor/SegmentEditor.cxx
+++ CLAM/src/Visualization/QTVM/Controllers/SegmentEditor/SegmentEditor.cxx
@@ -7,7 +7,19 @@ namespace CLAM
 	{
 		SegmentEditor::SegmentEditor()
 			: mStrategy(0)
+			, mMinX(0.0)
+			, mMaxX(100.0)
+			, mMinY(0.0)
+			, mMaxY(100.0)
+			, mLeftBound(0.0)
+			, mRightBound(100.0)
+			, mBottomBound(0.0)
+			, mTopBound(100.0)
+			, mScreenWidth(0)
+			, mScreenHeight(100)
 			, mMustProcessData(false)
+			, mMousePressed(false)
+			, mHit(false)
 		{
 		}
 
@@ -24,12 +36,16 @@ namespace CLAM
 
 		void SegmentEditor::SetHBounds(double left, double right)
 		{
-			mRenderer.SetHBounds(left,right);
+			mLeftBound = left;
+			mRightBound = right;
+			mRenderer.SetHBounds(mLeftBound,mRightBound);
 		}
 
 		void SegmentEditor::SetVBounds(double bottom, double top)
 		{
-			mRenderer.SetVBounds(bottom,top);
+			mBottomBound = bottom;
+			mTopBound = top;
+			mRenderer.SetVBounds(mBottomBound,mTopBound);
 		}
 
 		void SegmentEditor::Draw()
@@ -39,11 +55,122 @@ namespace CLAM
 			mRenderer.Render();
 		}
 
+		void SegmentEditor::ScreenArea(int w, int h)
+		{
+			mScreenWidth = w;
+			mScreenHeight = h;
+		}
+
+		void SegmentEditor::SetXRange(double xmin, double xmax)
+		{
+			mMinX = xmin;
+			mMaxX = xmax;
+		}
+
+		void SegmentEditor::SetYRange(double ymin, double ymax)
+		{
+			mMinY = ymin;
+			mMaxY = ymax;
+		}
+
+		void SegmentEditor::MousePos(double x, double y)
+		{
+			// TODO
+/*
+			unsigned size = mStrategy->onsets().size();
+			// check for begin
+			unsigned index = mStrategy->pickOnset(x,3.0);
+			if(index != size) 
+			{
+				printf("begin found at index %d\n",index);
+				emit cursorChanged(QCursor(Qt::SizeHorCursor));
+				return;
+			}
+			// check for end
+			index = mStrategy->pickOffset(x,3.0);
+			if(index != size) 
+			{
+				printf("end found at index %d\n",index);
+				emit cursorChanged(QCursor(Qt::SizeHorCursor));
+				return;
+			}
+			emit cursorChanged(QCursor(Qt::ArrowCursor));
+*/
+
+			std::pair<unsigned,unsigned> pickInfo = PickHBound(x);
+			switch(pickInfo.second)
+			{
+				case BEGIN:
+					printf("begin found at index %d\n",pickInfo.first);
+					emit cursorChanged(QCursor(Qt::SizeHorCursor));
+					break;
+				case END:
+					printf("end found at index %d\n",pickInfo.first);
+					emit cursorChanged(QCursor(Qt::SizeHorCursor));
+					break;
+				default:
+					emit cursorChanged(QCursor(Qt::ArrowCursor));
+					break;
+			}
+
+		}
+
+		void SegmentEditor::MousePressed(double xpos, double ypos)
+		{
+			mMousePressed = true;
+			// TODO
+		}
+
+		void SegmentEditor::MouseReleased(double xpos, double ypos)
+		{
+			mMousePressed = false;
+			// TODO
+		}
+
 		void SegmentEditor::ProcessData()
 		{
 			mRenderer.SetData(*mStrategy);
 			mMustProcessData = false;
 		}
+
+		std::pair<unsigned, unsigned> SegmentEditor::PickHBound(double x)
+		{
+			unsigned selected_pixel=GetPixel(x);
+			Segmentation::TimePositions begin = mStrategy->onsets();
+			Segmentation::TimePositions end = mStrategy->offsets();
+			unsigned nSegments = begin.size();
+			unsigned index = nSegments;
+			unsigned type = NONE;
+			for(unsigned i=0; i < nSegments; i++)
+			{
+				unsigned owned_pixel=GetPixel(begin[i]);
+				if(abs(int(selected_pixel-owned_pixel)) <= TOLERANCE)
+				{
+					index = i;
+					type = BEGIN;
+					break;
+				}
+				owned_pixel=GetPixel(end[i]);
+				if(abs(int(selected_pixel-owned_pixel)) <= TOLERANCE)
+				{
+					index = i;
+					type = END;
+					break;
+				}
+			}
+			return std::make_pair(index,type);
+		}
+
+		unsigned SegmentEditor::GetPixel(double x) const
+		{
+			double w = double(mScreenWidth);
+			double left = mLeftBound;
+			double right = mRightBound;
+			double xcoord = x-left;
+			double pixel = xcoord*w/(right-left);
+			return unsigned(pixel);
+		}
+
 	}
 }
 
--- CLAM/src/Visualization/QTVM/Controllers/SegmentEditor/SegmentEditor.hxx
+++ CLAM/src/Visualization/QTVM/Controllers/SegmentEditor/SegmentEditor.hxx
@@ -2,6 +2,7 @@
 #define __SEGMENTEDITOR__
 
 #include <qobject.h>
+#include <qcursor.h>
 #include "SegmentRenderer.hxx"
 
 namespace CLAM
@@ -22,12 +23,40 @@ namespace CLAM
 			void SetVBounds(double bottom, double top);
 			void Draw();
 
+			void MousePos(double x, double y);
+			void MousePressed(double xpos, double ypos);
+			void MouseReleased(double xpos, double ypos);
+
+			void ScreenArea(int w, int h);
+			void SetXRange(double xmin, double xmax);
+			void SetYRange(double ymin, double ymax);
+			
+		signals:
+			void cursorChanged(QCursor);
+
 		private:
 			Segmentation*   mStrategy;
 			SegmentRenderer mRenderer;
+			double          mMinX;
+			double          mMaxX;
+			double          mMinY;
+			double          mMaxY;
+			double          mLeftBound;
+			double          mRightBound;
+			double          mBottomBound;
+			double          mTopBound;
+			int             mScreenWidth;
+			int             mScreenHeight;
 			bool            mMustProcessData;
+			bool            mMousePressed;
+			bool            mHit;
+
+			enum { BEGIN=0, END=1, NONE=2, TOLERANCE=1 };
 
 			void ProcessData();
+			std::pair<unsigned, unsigned> PickHBound(double x); // firs=index second=type[BEGIN/END/NONE]
+			unsigned GetPixel(double x) const;
+			
 		};
 	}
 }
