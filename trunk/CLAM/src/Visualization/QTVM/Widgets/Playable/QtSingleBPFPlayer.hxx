c0b9a37afa0081bd5d6d4ef7af3e7aa5bd0304b2
imosquera <imosquera@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2005-12-10 18:38:20 +0000

    qt player for a single bpf; first revision

---
+++ trunk/CLAM/src/Visualization/QTVM/Widgets/Playable/QtSingleBPFPlayer.hxx
@@ -0,0 +1,47 @@
+#ifndef __QTSINGLEBPFPLAYER__
+#define __QTSINGLEBPFPLAYER__
+
+#include "Thread.hxx"
+#include "BPF.hxx"
+#include "QtSinglePlayer.hxx"
+
+namespace CLAM
+{
+	class Audio;
+
+	namespace VM
+	{
+		class QtSingleBPFPlayer : public QtSinglePlayer
+		{
+			Q_OBJECT
+		public:
+			QtSingleBPFPlayer(QWidget* parent=0);
+			~QtSingleBPFPlayer();
+
+			void SetData(const BPF& bpf);
+			void SetAudioPtr(const Audio* audio);
+			void SetDuration(TData duration);
+			void Update(TIndex index, TData value);
+
+		public slots:
+			void play();
+
+		private:
+			double mYMin;
+			double mYMax;
+			bool   mMustDoMapping;
+			bool   mWorking;
+			bool   mCancelThread;
+
+			BPF    mBPF;
+			Thread mThread;
+			
+			void AdaptIncomingData();
+			void thread_code();
+			void StopThread();
+			std::pair<double,double> GetMinMaxY(const BPF& bpf);
+		};
+	}
+}
+
+#endif
