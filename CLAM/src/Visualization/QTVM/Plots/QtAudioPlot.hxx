15be37a6cfc8e0e2430cf568164bb7b806755b7c
imosquera <imosquera@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2005-12-12 23:32:46 +0000

    constructor: added bool parameter to deactivate player

--- CLAM/src/Visualization/QTVM/Plots/QtAudioPlot.hxx
+++ CLAM/src/Visualization/QTVM/Plots/QtAudioPlot.hxx
@@ -48,7 +48,7 @@ namespace CLAM
 			Q_OBJECT
 
 		public:
-			QtAudioPlot(QWidget* parent=0, const char * name = 0, WFlags f = 0 );
+			QtAudioPlot(QWidget* parent=0, const char * name = 0, WFlags f = 0, bool hasPlayer=true );
 			~QtAudioPlot();
 
 			void SetData(const Audio& audio, bool to_controller=true);
@@ -89,6 +89,8 @@ namespace CLAM
 			Slotv1<TData>           mSlotStopPlayingReceived;
 
 			QFrame *lefthole, *righthole;
+
+			bool mHasPlayer;
 
 			void InitAudioPlot();
 
