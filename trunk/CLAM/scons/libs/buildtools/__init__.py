b6fb285d531a749d4f5949f3deb690981ba6e3e7
xoliver <xoliver@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2005-12-16 10:43:09 +0000

    Upgraded the library link creation function to create two links: libAAA.dylib --> libAAA.X.dylib --> libAAA.X.Y.dylib

--- trunk/CLAM/scons/libs/buildtools/__init__.py
+++ trunk/CLAM/scons/libs/buildtools/__init__.py
@@ -12,19 +12,24 @@ def make_lib_names( source, target, env ) :
 	target_file = os.path.basename( str(target[0]) )
 	cwd = os.getcwd()
 	os.chdir( target_dir )
+
+	pieces=target_file.split('.')
+	
 	if sys.platform == 'linux2' :
 		os.system( "ldconfig -n ." )
 		linkername = '.'.join(target_file.split('.')[0:2])
 		soname = '.'.join( target_file.split('.')[0:3])
 	else : #darwin
-		soname = target_file
-		pieces = soname.split('.')
-		linkername = pieces[0]+"."+pieces[-1]
+		soname = target_file					# libAAA.X.Y.dylib
+		linkername = pieces[0]+"."+pieces[1]+"."+pieces[-1]	# libAAA.X.dylib
+		biglinkername = pieces[0]+"."+pieces[-1]		# libAAA.dylib
 
 	os.system( "ln -sf %s %s"%(soname,linkername) )
+	os.system( "ln -sf %s %s"%(linkername,biglinkername) )
 	os.system( "chmod 755 %s"%target_file )
 	os.system( "chmod 755 %s"%soname )
 	os.system( "chmod 755 %s"%linkername )
+	os.system( "chmod 755 %s"%biglinkername )
 	os.chdir( cwd )
 
 def make_lib_names_message( source, target, env ) :
