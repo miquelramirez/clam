409f1c7bcc6105356c2c1d44af609a1a6b527a19
xoliver <xoliver@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2005-12-16 10:09:04 +0000

    Upgraded the naming method to libname.X.Y-extrathings.dylib or libname.so.X.Y-extrathings in mac and linux

--- trunk/CLAM/scons/libs/buildtools/rulesets.py
+++ trunk/CLAM/scons/libs/buildtools/rulesets.py
@@ -1,5 +1,6 @@
 from SCons.Action import *
 from buildtools import *
+import sys
 
 def handle_preinclude ( env ):
 	if sys.platform != 'win32' :
@@ -11,25 +12,34 @@ def handle_preinclude ( env ):
 def posix_lib_rules( name, version, headers, source_files, install_dirs, env) :
 	lib_descriptor = env.File( 'clam_'+name+'.pc' )
 
+	# We expect a version like " X.Y-possibleextrachars "
 	versionnumbers = version.split('.')
+
+	if len(versionnumbers) > 2:
+		print " ERROR in buildtools.posix_lib_rules: version name does not follow CLAM standard "
+		print "   Check the variable 'version' in the main SConstruct"
+		sys.exit(1)
+
 	if sys.platform == 'linux2' :
-		soname = 'libclam_'+name+'.so.%s' % (versionnumbers[0])
-		#soname = 'libclam_'+name+'.so.%s.%s' % (versionnumbers[0], versionnumbers[1])
+		soname = 'libclam_'+name+'.so.%s' % versionnumbers[0]
 		linker_name = 'libclam_'+name+'.so'
 		env.Append(SHLINKFLAGS=['-Wl,-soname,%s'%soname ] )
 		lib = env.SharedLibrary( 'clam_' + name, source_files, SHLIBSUFFIX='.so.%s'%version )
-		soname_lib = env.SonameLink( soname, lib )			# lib***.X.so -> lib***.X.Y.dylib
-		print('soname: ' + soname)
-		linkername_lib = env.LinkerNameLink( linker_name, soname_lib)	# lib***.so -> lib***.X.so
+		#NOT needed now
+		#soname_lib = env.SonameLink( soname, lib )			# lib***.so.X -> lib***.so.X.Y
+		#linkername_lib = env.LinkerNameLink( linker_name, soname_lib)	# lib***.so -> lib***.so.X
+		linkername_lib = env.SoNameLink( soname, lib )			# lib***.so.X -> lib***.so
 	else : #darwin
-		soname = 'libclam_'+name+'.%s.%s.dylib' % (versionnumbers[0], versionnumbers[1])
+		soname = 'libclam_'+name+'.%s.dylib' % versionnumbers[0]
+		#soname = 'libclam_'+name+'.%s.dylib' % (versionnumbers[0], versionnumbers[1])
 		linker_name = 'libclam_'+name+'.dylib'
 		env.Append( CCFLAGS=['-fno-common'] )
 		env.Append( SHLINKFLAGS=['-dynamic',
 				'-Wl,-install_name,%s'%(install_dirs.lib + '/' + 'libclam_' + name + '.%s.dylib'%(version))] )
 		lib = env.SharedLibrary( 'clam_' + name, source_files, SHLIBSUFFIX='.%s.dylib'%version )
-		soname_lib = env.LinkerNameLink( soname, lib )			# lib***.X.dylib -> lib***.X.Y.dylib
-		linkername_lib = env.LinkerNameLink( linker_name, soname_lib)	# lib***.dylib -> lib***.X.Y.dylib
+		#soname_lib = env.LinkerNameLink( soname, lib )			# lib***.X.dylib -> lib***.X.Y.dylib
+		#linkername_lib = env.LinkerNameLink( linker_name, soname_lib)	# lib***.dylib -> lib***.X.Y.dylib
+		linkername_lib = env.LinkerNameLink( soname, lib)		# lib***.X.dylib -> lib***.dylib
 	tgt = env.Alias( name, linkername_lib )
 
 	install_headers = env.Install( install_dirs.inc+'/CLAM', headers )
