2019b339d7e37a23a6f994b70d429586d0973402
xoliver <xoliver@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2005-12-16 16:29:18 +0000

    Corrected library naming control

--- trunk/CLAM/scons/libs/buildtools/rulesets.py
+++ trunk/CLAM/scons/libs/buildtools/rulesets.py
@@ -15,27 +15,31 @@ def posix_lib_rules( name, version, headers, source_files, install_dirs, env) :
 	# We expect a version like " X.Y-possibleextrachars "
 	versionnumbers = version.split('.')
 
-	if len(versionnumbers) > 2:
+	if len(versionnumbers) != 3:
 		print " ERROR in buildtools.posix_lib_rules: version name does not follow CLAM standard "
 		print "   Check the variable 'version' in the main SConstruct"
 		sys.exit(1)
 
 	if sys.platform == 'linux2' :
 		soname = 'libclam_'+name+'.so.%s' % versionnumbers[0]
+		middle_linker_name = 'libclam_'+name+'.so.%s.%s' % (versionnumbers[0], versionnumbers[1])
 		linker_name = 'libclam_'+name+'.so'
 		env.Append(SHLINKFLAGS=['-Wl,-soname,%s'%soname ] )
 		lib = env.SharedLibrary( 'clam_' + name, source_files, SHLIBSUFFIX='.so.%s'%version )
-		soname_lib = env.SonameLink( soname, lib )			# lib***.so.X -> lib***.so.X.Y
-		linkername_lib = env.LinkerNameLink( linker_name, soname_lib)	# lib***.so -> lib***.so.X
+		soname_lib = env.SonameLink( middle_linker_name, lib )				# lib***.so.X.Y -> lib***.so.X.Y.Z
+		middlelinkername_lib = env.LinkerNameLink( soname, soname_lib )			# lib***.so.X -> lib***.so.X.Y
+		linkername_lib = env.LinkerNameLink( linker_name, middlelinkername_lib)		# lib***.so -> lib***.so.X
 	else : #darwin
 		soname = 'libclam_'+name+'.%s.dylib' % versionnumbers[0]
+		middle_linker_name = 'libclam_'+name+'.%s.%s.dylib' % (versionnumbers[0], versionnumbers[1])
 		linker_name = 'libclam_'+name+'.dylib'
 		env.Append( CCFLAGS=['-fno-common'] )
 		env.Append( SHLINKFLAGS=['-dynamic',
 				'-Wl,-install_name,%s'%(install_dirs.lib + '/' + 'libclam_' + name + '.%s.dylib'%(version))] )
 		lib = env.SharedLibrary( 'clam_' + name, source_files, SHLIBSUFFIX='.%s.dylib'%version )
-		soname_lib = env.LinkerNameLink( soname, lib )			# lib***.X.dylib -> lib***.X.Y.dylib
-		linkername_lib = env.LinkerNameLink( linker_name, soname_lib)	# lib***.dylib -> lib***.X.Y.dylib
+		soname_lib = env.LinkerNameLink( middle_linker_name, lib )		# lib***.X.Y.dylib -> lib***.X.Y.Z.dylib
+		middlelinkername_lib = env.LinkerNameLink( soname, soname_lib )		# lib***.so.X -> lib***.so.X.Y
+		linkername_lib = env.LinkerNameLink( linker_name, middlelinkername_lib)		# lib***.dylib -> lib***.X.dylib
 	tgt = env.Alias( name, linkername_lib )
 
 	install_headers = env.Install( install_dirs.inc+'/CLAM', headers )
