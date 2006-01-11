3c6084592e726eb52d7a85458d3ea4e2b005e847
imosquera <imosquera@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2006-01-11 17:01:07 +0000

    pick peaks to show values as toottip

--- CLAM_Annotator/vmqt/render/vm_peaks_renderer.cxx
+++ CLAM_Annotator/vmqt/render/vm_peaks_renderer.cxx
@@ -71,11 +71,13 @@ namespace CLAM
 			if(peak_index != -1)
 			{
 				emit working(rd_key,true);
+				emit cursorChanged(QCursor(Qt::CrossCursor));
 				QString ttip = "freq="+QString::number(rd_cached_peaks[peak_index].freq,'f',0)+"Hz "+"mag="+QString::number(rd_cached_peaks[peak_index].mag,'f',0)+"dB";
 				emit toolTip(ttip);
 				return;
 			}
 			emit working(rd_key,false);
+			emit cursorChanged(QCursor(Qt::ArrowCursor));
 			emit toolTip("");
 		}
 		
@@ -83,8 +85,31 @@ namespace CLAM
 		{
 			int index = -1;
 			double xtolerance = double(TOLERANCE)*(rd_view.right-rd_view.left)/double(rd_viewport.w);
-			double ytolerence = double(TOLERANCE)*(rd_view.top-rd_view.bottom)/double(rd_viewport.h);
-			return index;
+			double ytolerance = double(TOLERANCE)*(rd_view.top-rd_view.bottom)/double(rd_viewport.h);
+			int left_index = 0;
+			int right_index = rd_cached_peaks.Size()-1;
+			while(left_index <= right_index)
+			{
+				const int current_index = (left_index+right_index)/2;
+			
+				if(rd_cached_peaks[current_index].freq >= (x-xtolerance) && 
+				   rd_cached_peaks[current_index].freq <= (x+xtolerance))
+				{
+					index = current_index;
+					break;
+				}
+				if(x < rd_cached_peaks[current_index].freq)
+				{
+					right_index = current_index-1;
+				}
+				else if(x > rd_cached_peaks[current_index].freq)
+				{
+					left_index = current_index+1;
+				}
+			}
+			if(index==-1) return -1;
+			if(rd_cached_peaks[index].mag >= (y-ytolerance)) return index;
+			return -1;
 		}
 	}
 }
