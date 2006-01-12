db30d624790f7a0620373861b1c77407f2883778
imosquera <imosquera@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2006-01-12 12:36:31 +0000

    audio plot example; first revision.

---
+++ trunk/CLAM_Annotator/vmqt/examples/src/audio_plot_example.cxx
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <QApplication>
+#include <QFileDialog>
+#include "vm_audio_plot.hxx"
+#include "vm_contiguous_segmentation.hxx"
+#include "audio_file_utils.hxx"
+
+int main(int argc, char** argv)
+{
+	QApplication app( argc, argv );
+	QString fn = QFileDialog::getOpenFileName(0,"Choose a mono audio file",".","Audio (*.wav *.ogg)");
+	if(fn.isEmpty())
+	{
+		printf("No file was choosen.\n");
+		exit(1);
+	}
+	CLAM::Audio audio;
+	printf("loading audio...\n");
+	qtvm_examples_utils::load_audio(fn.toAscii().data(),audio);
+	printf("done\n");
+	double xmax = audio.GetDuration()/1000.0;
+	CLAM::Segmentation* seg = new CLAM::ContiguousSegmentation(xmax);
+	unsigned nSegments = 15;
+	double step = xmax/double(nSegments);
+	// make some divisions
+	for(unsigned i=1; i < nSegments; i++)
+	{
+		seg->insert(double(i)*step);
+	}
+	seg->current(2);
+	
+	CLAM::VM::AudioPlot plot;
+	plot.set_title("Contiguous Segmentation");
+	plot.set_geometry(100,50,600,300);
+	plot.set_data(audio);
+	plot.set_segmentation(seg);
+	plot.show();
+
+// uncomment the following line to see the example on background black mode
+//	plot.backgroundBlack();
+
+	app.connect(&app,SIGNAL(lastWindowClosed()),&app,SLOT(quit()));
+	return app.exec();
+}
+
+// END
+
+
+
