82194593cb31086c2a7611318512a4b306fab09e
imosquera <imosquera@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2006-01-12 12:34:36 +0000

    fundamental plot; first revision.

---
+++ CLAM_Annotator/vmqt/plot/vm_fundamental_plot.hxx
@@ -0,0 +1,39 @@
+#ifndef __VMQT_FUNDAMENTAL_PLOT_H__
+#define __VMQT_FUNDAMENTAL_PLOT_H__
+
+#include "Segment.hxx"
+#include "vm_segmentation_plot.hxx"
+
+namespace CLAM
+{
+	namespace VM
+	{
+		class FundamentalPlot : public SegmentationPlot
+		{
+		public:
+			FundamentalPlot(QWidget* parent=0);
+			virtual ~FundamentalPlot();
+
+			void set_data(const CLAM::Segment& segment, bool update=false);
+
+		public slots:
+			virtual void backgroundWhite();
+			virtual void backgroundBlack();
+
+		private:
+			CLAM::DataArray wp_fund_data;
+
+			void init_fundamental_plot();
+			std::pair<int,int> get_zoom_steps(CLAM::TSize size, CLAM::TData yrange);
+
+			void set_xrange(double xmin, double xmax);
+			void set_yrange(double ymin, double ymax);
+			void set_zoom_steps(int hsteps, int vsteps);
+
+			CLAM::DataArray get_fund_data(const CLAM::Segment& segment);
+		};
+	}
+}
+
+#endif
+
