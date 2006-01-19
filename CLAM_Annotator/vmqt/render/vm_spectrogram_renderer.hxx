fab5c8dbf2a6839177f24e600d2fc4d9f656ed2a
imosquera <imosquera@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2006-01-19 12:44:29 +0000

    spectrogram renderer, first revision

---
+++ CLAM_Annotator/vmqt/render/vm_spectrogram_renderer.hxx
@@ -0,0 +1,51 @@
+#ifndef __VMQT_SPECTROGRAM_RENDERER_H__
+#define __VMQT_SPECTROGRAM_RENDERER_H__
+
+#include <vector>
+#include "Spectrum.hxx"
+#include "vm_renderer2d.hxx"
+
+namespace CLAM
+{
+	namespace VM
+	{
+		class SpectrogramRenderer : public CLAM::VM::Renderer2D
+		{
+			Q_OBJECT
+		public:
+			SpectrogramRenderer();
+			~SpectrogramRenderer();
+
+			void set_data(const CLAM::Array<CLAM::Spectrum>& specMtx);
+			
+		public slots:
+			void colorSonogram();
+			void blackWhiteSonogram();
+
+		protected:
+			void render();
+			void set_hbounds(double left, double right);
+			void set_vbounds(double bottom, double top);
+
+		private:
+			int rd_glColorList;	
+			int rd_glBlackWhiteList;
+			int rd_colormap;
+
+			std::vector<std::vector<float> >           rd_computed_data;
+			std::vector<std::vector<CLAM::VM::Color> > rd_color_data;
+			std::vector<std::vector<CLAM::VM::Color> > rd_blackwhite_data;
+
+			CLAM::VM::GLView      rd_local_view;
+
+			enum { COLOR_SONOGRAM=0, BW_SONOGRAM=1 };
+
+			void draw_color_data();
+			void draw_blackwhite_data();
+
+			int get_glList();
+		};
+	}
+}
+
+#endif
