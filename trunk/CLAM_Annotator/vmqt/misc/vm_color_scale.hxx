f1bb851dab73307fa5a786d37d98f849aa5dc0b6
imosquera <imosquera@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2006-01-19 12:42:05 +0000

    color scale first revision

---
+++ trunk/CLAM_Annotator/vmqt/misc/vm_color_scale.hxx
@@ -0,0 +1,38 @@
+#ifndef __VMQT_COLORSCALE_H__
+#define __VMQT_COLORSCALE_H__
+
+#include <vector>
+#include <QWidget>
+#include "vm_data_types.hxx"
+
+namespace CLAM
+{
+    namespace VM
+    {
+		class ColorScale : public QWidget
+		{
+			Q_OBJECT
+		public:
+			ColorScale(QWidget* parent=0);
+			~ColorScale();
+
+			void set_scale(const std::vector<CLAM::VM::Color>& scale);
+
+		signals:
+			void widthChanged(int);
+	    
+		protected:
+			void paintEvent(QPaintEvent* e);
+			void resizeEvent(QResizeEvent* e);
+
+		private:
+			std::vector<CLAM::VM::Color> cs_scale;
+
+			void init_color_scale();
+
+		};
+    }
+}
+
+#endif
+
