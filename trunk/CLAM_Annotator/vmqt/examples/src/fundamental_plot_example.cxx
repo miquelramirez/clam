8fc1c552c80eeaf2bbe213c0e9c91ab981f52fa5
imosquera <imosquera@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2006-01-22 12:34:20 +0000

    trivial change

--- trunk/CLAM_Annotator/vmqt/examples/src/fundamental_plot_example.cxx
+++ trunk/CLAM_Annotator/vmqt/examples/src/fundamental_plot_example.cxx
@@ -1,30 +1,21 @@
-#include <stdlib.h>
 #include <QApplication>
-#include <QFileDialog>
 #include "audio_file_utils.hxx"
 #include "analysis_utils.hxx"
 #include "vm_fundamental_plot.hxx"
 
 int main(int argc, char** argv)
 {
-	QApplication app( argc, argv );
-
-	QString fn = QFileDialog::getOpenFileName(0,"Choose a mono audio file",".","Audio (*.wav *.ogg)");
-	if(fn.isEmpty())
-	{
-		printf("No file was choosen.\n");
-		exit(1);
-	}
-
 	CLAM::Audio audio;
 	CLAM::Segment segment;
 
 	printf("loading audio...\n");
-	qtvm_examples_utils::load_audio(fn.toAscii().data(),audio);
+	qtvm_examples_utils::load_audio("../../data/birthday.wav",audio);
 	printf("analysing");
 	qtvm_examples_utils::analyze(audio,segment);
 	printf("\ndone!\n");
-	
+
+	QApplication app( argc, argv );
+
 	CLAM::VM::FundamentalPlot plot;
 	plot.set_title("Fundamental");
 	plot.set_geometry(100,100,600,300);
--- trunk/CLAM_Annotator/vmqt/examples/src/sintracks_plot_example.cxx
+++ trunk/CLAM_Annotator/vmqt/examples/src/sintracks_plot_example.cxx
@@ -1,29 +1,20 @@
-#include <stdlib.h>
 #include <QApplication>
-#include <QFileDialog>
 #include "audio_file_utils.hxx"
 #include "analysis_utils.hxx"
 #include "vm_sintracks_plot.hxx"
 
 int main(int argc, char** argv)
 {
-	QApplication app( argc, argv );
-
-	QString fn = QFileDialog::getOpenFileName(0,"Choose a mono audio file",".","Audio (*.wav *.ogg)");
-	if(fn.isEmpty())
-	{
-		printf("No file was choosen.\n");
-		exit(1);
-	}
-
 	CLAM::Audio audio;
 	CLAM::Segment segment;
 
 	printf("loading audio...\n");
-	qtvm_examples_utils::load_audio(fn.toAscii().data(),audio);
+	qtvm_examples_utils::load_audio("../../data/birthday.wav",audio);
 	printf("analysing");
 	qtvm_examples_utils::analyze(audio,segment);
 	printf("\ndone!\n");
+
+	QApplication app( argc, argv );
 
 	CLAM::VM::SinTracksPlot plot;
 	plot.set_title("Sinusoidal tracks");
