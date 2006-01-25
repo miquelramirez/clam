55c5265f5a9f8d60c896e2467806c47b6265fc7d
imosquera <imosquera@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2006-01-25 12:07:40 +0000

    grid; first revision

---
+++ trunk/CLAM_Annotator/vmqt/render/vm_grid.hxx
@@ -0,0 +1,42 @@
+#ifndef __VMQT_GRID_H__
+#define __VMQT_GRID_H__
+
+#include "vm_renderer2d.hxx"
+
+namespace CLAM
+{
+	namespace VM
+	{
+		class Grid : public CLAM::VM::Renderer2D
+		{
+		public:
+			Grid();
+			~Grid();
+
+			void show_grid(bool show);
+			void snap_to_grid(bool snap);
+			void set_grid_steps(double xstep, double ystep);
+		    void set_grid_color(const CLAM::VM::Color& color);
+
+			bool show_grid() const;
+			bool snap_to_grid() const;
+
+			double xstep() const;
+			double ystep() const;
+
+		protected:
+			void render();
+
+		private:
+			bool   rd_show_grid;
+			bool   rd_snap_to_grid;
+			double rd_grid_xstep;
+			double rd_grid_ystep;
+			
+			CLAM::VM::Color rd_grid_color;
+		};
+	}
+}
+
+#endif
+
