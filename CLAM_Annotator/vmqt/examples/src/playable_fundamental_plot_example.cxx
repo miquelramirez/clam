e1a2ba9e377832589cd7c9950ea963b4525ba2d3
imosquera <imosquera@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2006-01-30 10:22:55 +0000

    playable fundamental plot example; first revision

---
+++ CLAM_Annotator/vmqt/examples/src/playable_fundamental_plot_example.cxx
@@ -0,0 +1,35 @@
+#include <QApplication>
+#include "audio_file_utils.hxx"
+#include "analysis_utils.hxx"
+#include "vm_playable_fundamental_plot.hxx"
+
+int main(int argc, char** argv)
+{
+	CLAM::Audio audio;
+	CLAM::Segment segment;
+
+	printf("loading audio...\n");
+	qtvm_examples_utils::load_audio("../../data/birthday.wav",audio);
+	printf("analysing");
+	qtvm_examples_utils::analyze(audio,segment);
+	printf("\ndone!\n");
+   
+	QApplication app( argc, argv );
+
+	CLAM::VM::PlayableFundamentalPlot plot;
+	plot.set_title("Fundamental");
+	plot.set_geometry(100,100,600,300);
+	plot.set_data(segment);
+	plot.show();
+
+// uncomment the following line to see the example on background black mode
+//	plot.backgroundBlack();
+	
+	app.connect(&app,SIGNAL(lastWindowClosed()),&app,SLOT(quit()));
+	return app.exec();
+}
+
+// END
+
+
+
