8d47d2b4a117b29023ff256ea8b5a56d488ae6ad
imosquera <imosquera@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2006-01-30 10:22:14 +0000

    playable audio plot example; first revision

---
+++ trunk/CLAM_Annotator/vmqt/examples/src/playable_audio_plot_example.cxx
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <QApplication>
+#include <QFileDialog>
+#include "vm_playable_audio_plot.hxx"
+#include "audio_file_utils.hxx"
+
+int main(int argc, char** argv)
+{
+	QApplication app( argc, argv );
+	QString fn = QFileDialog::getOpenFileName(0,"Choose a mono audio file",".","Audio (*.wav *.ogg)");
+	if(fn.isEmpty())
+	{
+		printf("No file was choosen.\n");
+		exit(1);
+	}
+	CLAM::Audio audio;
+	printf("loading audio...\n");
+	qtvm_examples_utils::load_audio(fn.toAscii().data(),audio);
+	printf("done\n");
+   
+	CLAM::VM::PlayableAudioPlot plot;
+	plot.set_title("Audio");
+	plot.set_geometry(100,50,600,300);
+	plot.set_data(audio);
+	plot.show();
+
+// uncomment the following line to see the example on background black mode
+//	plot.backgroundBlack();
+
+	app.connect(&app,SIGNAL(lastWindowClosed()),&app,SLOT(quit()));
+	return app.exec();
+}
+
+// END
+
+
+
