f529d40a0d40414c72edf2f59c56ab9a00278318
imosquera <imosquera@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2006-01-30 10:19:27 +0000

    playable audio stereo plot; first revision

---
+++ CLAM_Annotator/vmqt/plot/vm_playable_audio_stereo_plot.hxx
@@ -0,0 +1,47 @@
+#ifndef __VMQT_PLAYABLE_AUDIO_STEREO_PLOT_H__
+#define __VMQT_PLAYABLE_AUDIO_STEREO_PLOT_H__
+
+#include "vm_audio_stereo_plot.hxx"
+
+class QComboBox;
+class QCheckBox;
+
+namespace CLAM
+{
+	namespace VM
+	{
+		class CLAM::VM::WPlayer;
+		class CLAM::VM::AudioPlayer;
+
+		class PlayableAudioStereoPlot : public CLAM::VM::AudioStereoPlot
+		{
+			Q_OBJECT
+		public:
+			PlayableAudioStereoPlot(QWidget* parent=0);
+			~PlayableAudioStereoPlot();
+
+			void set_data(const CLAM::Audio& chn0, const CLAM::Audio& chn1, bool update=false);
+
+		public slots:
+			void backgroundWhite();
+			void backgroundBlack();
+
+		private slots:
+			void setColorSchema(int);
+			void muteChannel0(int);
+			void muteChannel1(int);
+
+		private:
+			QComboBox* wp_combo_box;
+			QCheckBox* wp_mute_chn0;
+			QCheckBox* wp_mute_chn1;
+
+			CLAM::VM::WPlayer*     wp_wplayer;
+			CLAM::VM::AudioPlayer* wp_audio_player;
+
+			void init_playable_audio_stereo_plot();
+		};
+	}
+}
+
+#endif
