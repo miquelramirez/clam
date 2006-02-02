cd76894a40c75eb9f0eb3b25d5634f077702971a
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2006-02-02 20:15:22 +0000

    Promoted as function to be used by other scripts

--- trunk/CLAM_Annotator/scripts/Lab2Pools.py
+++ trunk/CLAM_Annotator/scripts/Lab2Pools.py
@@ -3,64 +3,74 @@
 import sys
 import re
 
-# Default parameters
-filter = '.*'
-childScope = None
-labelAttribute = None
+def Lab2Plot(labfile, output, attributeName, filter, childScope, labelAttribute):
+	print >> output, '<?xml version="1.0" encoding="UTF-8" standalone="no" ?>'
+	print >> output, '<DescriptorsPool>'
+	print >> output, '\t<ScopePool name="Song" size="1">'
+	tokens = list()
+	labelFilter = re.compile(filter)
+	for line in labfile:
+		segment = line.split(" ",3)
+		if len(segment)!=3:
+			print >> sys.stderr, "Found a line with %s tokens, 3 expected"%len(segment)
+			continue
+		segment[2]=segment[2].rstrip().lstrip()
+		if not labelFilter.match(segment[2]): continue
+		tokens.append(segment)
 
-if len(sys.argv) == 3:
-	(labfilename,attributeName) = sys.argv[1:3]
-elif len(sys.argv) == 4:
-	(labfilename,attributeName,filter) = sys.argv[1:4]
-elif len(sys.argv) == 5:
-	(labfilename,attributeName,childScope,labelAttribute) = sys.argv[1:5]
-elif len(sys.argv) == 6:
-	(labfilename,attributeName,filter,childScope,labelAttribute) = sys.argv[1:6]
-else:
-	print >> sys.stderr, "Lab2Pools v1.0: Wavesurfer Lab file to CLAM Annotator pools converter"
-	print >> sys.stderr
-	print >> sys.stderr, "Usage: " + sys.argv[0] + " <labFile> <attributeName> [<labelFilter>] [<childScope> <labelAttribute>]"
-	print >> sys.stderr
-	print >> sys.stderr, " <labFile>        An annotation file from wave surfer"
-	print >> sys.stderr, " <attributeName>  Attribute name for the segmentation"
-	print >> sys.stderr, " <labelFilter>    A regular expresion to filter segments by label (default: all segments)"
-	print >> sys.stderr, " <childScope>     If defined the child scope that should contain the label attribute (by default it is discarded)"
-	print >> sys.stderr, " <labelAttribute> The attribute that holds the label"
-	sys.exit()
+	sampleRate=44100
 
-
-labfile=file(labfilename)
-
-print '<?xml version="1.0" encoding="UTF-8" standalone="no" ?>'
-print '<DescriptorsPool>'
-print '<ScopePool name="Song" size="1">'
-tokens = list()
-labelFilter = re.compile(filter)
-for line in labfile:
-	segment = line.split(" ",3)
-	if len(segment)!=3:
-		print >> sys.stderr, "Found a line with %s tokens, 3 expected"%len(segment)
-		continue
-	if not labelFilter.match(segment[2]): continue
-	tokens.append(segment)
-
-sampleRate=44100
-
-print '<AttributePool name="%s" size="%s">'%(attributeName, len(tokens)) ,
-for segment in tokens :
-	print float(segment[0])*sampleRate, float(segment[1])*sampleRate,
-print '</AttributePool>'
-print '</ScopePool>'
-
-if childScope!=None:
-	print '<ScopePool name="%s size="%s">\n' % (childScope, len(tokens))
-	print '<AttributePool name="%s">' % (labelAttribute)
-
+	print >> output, '\t\t<AttributePool name="%s" size="%s">'%(attributeName, len(tokens)) ,
 	for segment in tokens :
-		print '\t<Enumerated>%s</Enumerated>' % ( segment[2].rstrip().lstrip() ) 
-	print '</AttributePool>'
-	print '</ScopePool>'
+		print >> output, float(segment[0])*sampleRate, float(segment[1])*sampleRate,
+	print >> output, '</AttributePool>'
+	print >> output, '\t</ScopePool>'
 
-print '</DescriptorsPool>'
+	if childScope!=None:
+		print >> output, '\t<ScopePool name="%s" size="%s">' % (childScope, len(tokens))
+		print >> output, '\t\t<AttributePool name="%s">' % (labelAttribute)
+
+		for segment in tokens :
+			print >> output, '\t\t\t<Enumerated>%s</Enumerated>' % ( segment[2] ) 
+		print >> output, '\t\t</AttributePool>'
+		print >> output, '\t</ScopePool>'
+	print >> output, '</DescriptorsPool>'
+	return dict.fromkeys([ segment[2] for segment in tokens ]).keys() # unique labels
+
+
+if __name__ == "__main__":
+	# Default parameters
+	filter = '.*'
+	childScope = None
+	labelAttribute = None
+
+	if len(sys.argv) == 3:
+		(labfilename,attributeName) = sys.argv[1:3]
+	elif len(sys.argv) == 4:
+		(labfilename,attributeName,filter) = sys.argv[1:4]
+	elif len(sys.argv) == 5:
+		(labfilename,attributeName,childScope,labelAttribute) = sys.argv[1:5]
+	elif len(sys.argv) == 6:
+		(labfilename,attributeName,filter,childScope,labelAttribute) = sys.argv[1:6]
+	else:
+		print >> sys.stderr, "Lab2Pools v1.0: Wavesurfer Lab file to CLAM Annotator pools converter"
+		print >> sys.stderr
+		print >> sys.stderr, "Usage: " + sys.argv[0] + " <labFile> <attributeName> [<labelFilter>] [<childScope> <labelAttribute>]"
+		print >> sys.stderr
+		print >> sys.stderr, " <labFile>        An annotation file from wave surfer"
+		print >> sys.stderr, " <attributeName>  Attribute name for the segmentation"
+		print >> sys.stderr, " <labelFilter>    A regular expresion to filter segments by label (default: all segments)"
+		print >> sys.stderr, " <childScope>     If defined the child scope that should contain the label attribute (by default it is discarded)"
+		print >> sys.stderr, " <labelAttribute> The attribute that holds the label"
+		sys.exit()
+
+	print Lab2Plot( 
+			file(labfilename), 
+			file(labfilename+".pool","w"), 
+			attributeName, 
+			filter, 
+			childScope, labelAttribute)
+
+
 
 
