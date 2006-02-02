c0b6506ae2afa59e4dba124fe4fd3906f5f003c9
imosquera <imosquera@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2006-02-02 23:02:42 +0000

    trivial change

--- trunk/CLAM_Annotator/vmqt/examples/src/playable_fundamental_plot_example.cxx
+++ trunk/CLAM_Annotator/vmqt/examples/src/playable_fundamental_plot_example.cxx
@@ -3,8 +3,6 @@
 #include "analysis_utils.hxx"
 #include "vm_playable_fundamental_plot.hxx"
 
-#include "vm_playable_audio_plot.hxx"
-
 int main(int argc, char** argv)
 {
 	CLAM::Audio audio;
@@ -23,12 +21,6 @@ int main(int argc, char** argv)
 	plot.set_geometry(100,100,600,300);
 	plot.set_data(segment);
 	plot.show();
-
-	CLAM::VM::PlayableAudioPlot aplot;
-	aplot.set_title("Audio");
-	aplot.set_geometry(100,200,600,300);
-	aplot.set_data(audio);
-	aplot.show();
 
 	app.connect(&app,SIGNAL(lastWindowClosed()),&app,SLOT(quit()));
 	return app.exec();
