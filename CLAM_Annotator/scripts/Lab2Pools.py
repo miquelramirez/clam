0f68b9b53297657aaabe58367faac406bd12356d
David García Garzón <vokimon@users.noreply.github.com>
2006-02-03 18:22:28 +0000

    Batch generation and fixes on the format

--- CLAM_Annotator/scripts/Lab2Pools.py
+++ CLAM_Annotator/scripts/Lab2Pools.py
@@ -3,7 +3,7 @@
 import sys
 import re
 
-def Lab2Plot(labfile, output, attributeName, filter, childScope, labelAttribute):
+def Lab2Plot(labfile, output, segmentationAttribute, filter, childScope, labelAttribute):
 	print >> output, '<?xml version="1.0" encoding="UTF-8" standalone="no" ?>'
 	print >> output, '<DescriptorsPool>'
 	print >> output, '\t<ScopePool name="Song" size="1">'
@@ -20,9 +20,9 @@ def Lab2Plot(labfile, output, attributeName, filter, childScope, labelAttribute)
 
 	sampleRate=44100
 
-	print >> output, '\t\t<AttributePool name="%s" size="%s">'%(attributeName, len(tokens)) ,
+	print >> output, '\t\t<AttributePool name="%s" size="%s">'%(segmentationAttribute, len(tokens)*2) ,
 	for segment in tokens :
-		print >> output, float(segment[0])*sampleRate, float(segment[1])*sampleRate,
+		print >> output, int(round(float(segment[0])*sampleRate)), int(round(float(segment[1])*sampleRate)),
 	print >> output, '</AttributePool>'
 	print >> output, '\t</ScopePool>'
 
@@ -37,6 +37,24 @@ def Lab2Plot(labfile, output, attributeName, filter, childScope, labelAttribute)
 	print >> output, '</DescriptorsPool>'
 	return dict.fromkeys([ segment[2] for segment in tokens ]).keys() # unique labels
 
+def generateSchema(mainScope, segmentationAttribute, childScope, labelAttribute, values):
+	result = ''
+	result += '<?xml version="1.0" encoding="UTF-8" standalone="no" ?>\n'
+	result += '<DescriptionScheme>\n'
+	result += '\t<Attributes>\n'
+	result += '\t\t<Attribute scope="%s" name="%s" type="Segmentation">\n' % (mainScope, segmentationAttribute)
+	result += '\t\t\t<ChildScope>%s</ChildScope>\n' %(childScope)
+	result += '\t\t\t<SegmentationPolicy>%s</SegmentationPolicy>\n' %("Discontinuous")
+	result += '\t\t</Attribute>\n'
+	result += '\t\t<Attribute scope="%s" name="%s" type="Enumerated">\n' % (childScope, labelAttribute)
+	result += '\t\t\t<EnumerationValues>'
+	result += " ".join(values)
+	result += '</EnumerationValues>\n'
+	result += '\t\t</Attribute>\n'
+	result += '\t</Attributes>\n'
+	result += '</DescriptionScheme>\n'
+	return result
+	
 
 if __name__ == "__main__":
 	# Default parameters
@@ -45,32 +63,40 @@ if __name__ == "__main__":
 	labelAttribute = None
 
 	if len(sys.argv) == 3:
-		(labfilename,attributeName) = sys.argv[1:3]
+		(labFileList,segmentationAttribute) = sys.argv[1:3]
 	elif len(sys.argv) == 4:
-		(labfilename,attributeName,filter) = sys.argv[1:4]
+		(labFileList,segmentationAttribute,filter) = sys.argv[1:4]
 	elif len(sys.argv) == 5:
-		(labfilename,attributeName,childScope,labelAttribute) = sys.argv[1:5]
+		(labFileList,segmentationAttribute,childScope,labelAttribute) = sys.argv[1:5]
 	elif len(sys.argv) == 6:
-		(labfilename,attributeName,filter,childScope,labelAttribute) = sys.argv[1:6]
+		(labFileList,segmentationAttribute,filter,childScope,labelAttribute) = sys.argv[1:6]
 	else:
 		print >> sys.stderr, "Lab2Pools v1.0: Wavesurfer Lab file to CLAM Annotator pools converter"
 		print >> sys.stderr
-		print >> sys.stderr, "Usage: " + sys.argv[0] + " <labFile> <attributeName> [<labelFilter>] [<childScope> <labelAttribute>]"
+		print >> sys.stderr, "Usage: " + sys.argv[0] + " <labFile> <segmentationAttr> [<labelFilter>] [<childScope> <labelAttribute>]"
 		print >> sys.stderr
-		print >> sys.stderr, " <labFile>        An annotation file from wave surfer"
-		print >> sys.stderr, " <attributeName>  Attribute name for the segmentation"
-		print >> sys.stderr, " <labelFilter>    A regular expresion to filter segments by label (default: all segments)"
-		print >> sys.stderr, " <childScope>     If defined the child scope that should contain the label attribute (by default it is discarded)"
-		print >> sys.stderr, " <labelAttribute> The attribute that holds the label"
+		print >> sys.stderr, " <labFileList>       A list of annotation files from wave surfer ('-' means stdin)"
+		print >> sys.stderr, " <segmentationAttr>  Attribute name for the segmentation"
+		print >> sys.stderr, " <labelFilter>       A regular expresion to filter segments by label (default: all segments)"
+		print >> sys.stderr, " <childScope>        If defined the child scope that should contain the label attribute (by default it is discarded)"
+		print >> sys.stderr, " <labelAttribute>    The attribute that holds the label"
 		sys.exit()
 
-	print Lab2Plot( 
-			file(labfilename), 
-			file(labfilename+".pool","w"), 
-			attributeName, 
-			filter, 
-			childScope, labelAttribute)
+	if labFileList == '-' : 
+		labFileList = sys.stdin
 
-
+	labelValues = [];
+	for labfilename in labFileList :
+		labfilename = labfilename.lstrip().rstrip()
+		values = Lab2Plot( 
+				file(labfilename), 
+				file(labfilename+".pool","w"), 
+				segmentationAttribute, 
+				filter, 
+				childScope, labelAttribute)
+		labelValues += values
+	labelValues = dict.fromkeys(labelValues).keys() # unique
+	labelValues.sort()
+	print generateSchema("Song", segmentationAttribute, childScope, labelAttribute, labelValues)
 
 
