dcf123f8a1a64dd8033a80cc0075a35f05303917
imosquera <imosquera@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2006-02-06 10:22:03 +0000

    OpenGL implementation

--- CLAM_Annotator/vmqt/widget/vm_ruler.hxx
+++ CLAM_Annotator/vmqt/widget/vm_ruler.hxx
@@ -1,26 +1,32 @@
 #ifndef __VMQT_RULER_H__
 #define __VMQT_RULER_H__
 
-#include <QWidget>
+#include <vector>
+#include <QGLWidget>
 #include "vm_data_types.hxx"
 
 namespace CLAM
 {
 	namespace VM
 	{
-		class Ruler : public QWidget
+		class Ruler : public QGLWidget
 		{
 			Q_OBJECT
 		public:
 			Ruler(QWidget* parent, ERulerPos pos, ERulerScale scale=CLAM::VM::eLinearScale);
 			~Ruler();
 
+			void set_range(double min, double max);
+			void set_step(double step);
 			void set_scale(ERulerScale scale);
 			ERulerScale get_scale() const;
 	    
 			void set_bg_color(const Color& c);
 			void set_fg_color(const Color& c);
 	
+			void show_floats();
+			void show_integers();
+
 			const QFont& get_font() const;
 
 		signals:
@@ -30,30 +36,45 @@ namespace CLAM
 			void updateRange(double, double);
 
 		protected:
-			void paintEvent(QPaintEvent* pe);
+			void paintGL(); 
+			void resizeEvent(QResizeEvent* e);
 
 		private:
-			int   rw_ticks;
-			int   rw_label_width;
-			int   rw_label_height;
-			QFont rw_font;
+			int    rw_label_height;
+			bool   rw_do_resize;
+			bool   rw_show_floats;
+			double rw_step;
+			QFont  rw_font;
 	
 			ERulerPos   rw_position;
 			ERulerScale rw_scale;
-			Range       rw_range;
+			GLViewport  rw_viewport;
+			Range       rw_total_range;
+			Range       rw_current_range;
+			Color       rw_bg_color;
 			Color       rw_fg_color;
-			
-			enum { MINTICKS=3, MAXTICKS=17 };
 
+			std::vector<double> rw_values_to_draw;
+
+			enum { MINTICKS=3 };
+
+			void rebuild();
+
+			void draw();
 			void draw_left();
 			void draw_right();
 			void draw_bottom();
 			void draw_top();
+			void draw_labels();
 
 			int get_ticks();
 			int test_ticks_for_span();
-			void compute_max_label_width();
-			QString get_label(int tick);
+			int get_max_label_width();
+			bool is_visible(double value);
+			double round(double x);
+			double ceil(double x);
+			QString get_label(double value);
+			std::pair<double,double> get_label_coords(double value);
 		};
     }
 }
