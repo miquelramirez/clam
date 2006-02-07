77345e586572953c2f288ae69631daab6cac769b
xoliver <xoliver@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2006-02-07 16:53:42 +0000

    Added generic error management with a custom exception to allow usage of this class through the GUI or command-line style

--- trunk/CLAM_Annotator/SimacServicesClient/Tasker.py
+++ trunk/CLAM_Annotator/SimacServicesClient/Tasker.py
@@ -7,16 +7,6 @@ from os import path
 #  --> al final ho tindra el mateix source de metadata, no problem
 #TODO CANVIS QUE CAL FER:
 #	~~separar be en metodes tot aixo
-if __name__ == "__main__" :
-	if len(sys.argv) != 3:
-		print "CLIENT - Usage: ./Client <TASKFILE> <PROJECTNAME>"
-		sys.exit(0)
-
-	client=Client( sys.argv[1], sys.argv[2] )
-	client.processTask()
-	#client.runAnnotator()
-	os.system("touch I\ Lost\ You.mp3")
-	client.uploadChanges()
 
 
 clamAnnotatorProjectSkeleton = """<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
@@ -32,43 +22,57 @@ clamAnnotatorProjectSongSkeleton = """    <Song>
     </Song>
 """
 
+class ClientError(Exception):
+	def __init__(self, message):
+		self.message=message
+
+	def __str__(self):
+		return self.message
+
 class Client:
-	def __init__(self, taskfile, projectname):
+	def __init__(self, printfunction=sys.stdout.write):
+		self.songlisting = {}
+		self.printfunction=printfunction
+
+	def setParameters(self, taskfile, projectname ):
 		self.retrieveTask( taskfile)
 		self.projectname = projectname
-		self.songlisting = {}
 
 	def retrieveTask(self, location):
 		#TODO in the future it should be an XML file (cool yeah)
 		try:
 			self.task = open( location, 'r').read()
 		except IOError:
-			self.quitError("ERROR: reading file <%s>. Maybe it does not exist"%location)
+			raise "Task file error\nReading file <%s>. Maybe it does not exist."%location
 
 	def processTask(self):
 		lines = self.task.splitlines()
 		if not self.correctParameters( lines ):
-			self.quitError("TaskFile error: malformed file, some field is wrong or missing!")
+			raise "Task file error\nMalformed file, some field is wrong or missing!"
 	
 		ids, descriptors, contentlocatoruri, metadataprovideruri = self.extractParameters(lines)
 		self.contentlocator = ServiceStub.ContentLocator(contentlocatoruri)
 		self.metadataprovider = ServiceStub.MetadataProvider(metadataprovideruri)
 	
 		projectsonglisting = ""
-		print "\nRetrieving data"
+		self.printfunction("\nRetrieving data")
 	
 		for id in ids:
 			#Grab audio file
-			locations = self.contentlocator.LocateId( id )
+			try:
+				locations = self.contentlocator.LocateId( id )
+			except:
+				raise ClientError("Access error\nError accessing content locator server.")
+
 			if locations == "NotFound":
-				self.quitError("Service error: file with id=%s not found on the server" % id)
+				raise "Service error\nFile with id=%s not found on the server." % id
 			elif locations == "ServerError":
-				self.quitError("Service error: the server is not working properly, a ServerError was received")
+				raise "Service error\nThe server is not working properly, a ServerError was received."
 			
 			url = self.downloadSong( locations)
 	
 			if url == None:
-				self.quitError("ERROR: could not download the song from the given source urls")
+				raise "Download error\nCould not download the song from the given source urls."
 
 			#File name
 			audiofilename = urllib.unquote( urlparse.urlparse(url)[2].split('/')[-1] )
@@ -77,7 +81,11 @@ class Client:
 			#Store modification file for each pool to keep track of the ones upgraded
 			self.songlisting[audiofilename] = os.path.getmtime(audiofilename)
 
-			pool = self.metadataprovider.QueryDescriptors( id, descriptors )	
+			try:
+				pool = self.metadataprovider.QueryDescriptors( id, descriptors )
+			except:
+				raise ClientError("Access error\nError accessing metadata provider server.")
+			
 			self.createFile( audiofilename, '.pool', pool )
 	
 		self.createFile(self.projectname, '.pro', clamAnnotatorProjectSkeleton % ( self.projectname, projectsonglisting ) )
@@ -86,19 +94,17 @@ class Client:
 		self.createFile( self.projectname, '.sc', schema )
 	
 		#Print log
-		print "\n\tTASK PROCESSING FINISHED"
-		print "-Downloaded audio files"
-		print "-Created files %s and %s\n" % ( self.projectname+".sc", self.projectname+".pro" )
-		print "LOG:"
-		print self.songlisting
+		self.printfunction("\n\tTASK PROCESSING FINISHED")
+		self.printfunction("-Downloaded audio files")
+		self.printfunction("-Created files %s and %s\n" % ( self.projectname+".sc", self.projectname+".pro" ))
 
 	def uploadChanges(self):
-		print "LOOKING FOR MODIFIED FILES:"
+		self.printfunction("LOOKING FOR MODIFIED FILES:")
 		for song in self.songlisting.keys():
 			if self.songlisting[song] != os.path.getmtime(song):
-				print " > File '%s' modified"  % song
+				self.printfunction(" > File '%s' modified"  % song)
 			else:
-				print " > File '%s' NOT modified"  % song
+				self.printfunction(" > File '%s' NOT modified"  % song)
 
 	def correctParameters(self, lines):
 		return ( lines[0].split('=')[0] == 'id' ) and ( lines[1].split('=')[0] == 'descriptors' )\
@@ -108,17 +114,18 @@ class Client:
 		#EP: quan es fagi servir el ContentProvider, el nom d'arxiu sera un problemet, diria -> caldria mirar header pel nom d'arxiu?
 		for url in locations.splitlines():
 			try:
-				print "Trying "+url+" ..."
+				self.printfunction("Trying "+url)
 				stream = urllib.urlopen(url)
 				file = open( "temp", 'w')
 				file.write( stream.read() )
 				file.close()
 				ok = True
-				print "\tOK"
+				self.printfunction("..OK")
 				return url
 			except:
-				print "\tERROR"
+				self.printfunction("..ERROR")
 				continue
+				
 		return None
 	
 	def extractParameters(self, lines):
@@ -128,10 +135,6 @@ class Client:
 		metadataprovider = lines[3].split('=')[1]
 		return ids, descriptors, contentlocator, metadataprovider
 
-	def quitError(self, message):
-		print message
-		sys.exit(-1)
-
 	def createFile(self, name, extension, content):
 		file = open( name+extension, 'w' )
 		file.write( content )
@@ -139,4 +142,17 @@ class Client:
 
 	def runAnnotator(self):
 		result = os.system("Annotator %s.pro"%self.projectname)
-		print "RESULT = %d"%result
+		self.printfunction("RESULT = %d"%result)
+
+
+if __name__ == "__main__" :
+	if len(sys.argv) != 3:
+		self.printfunction("CLIENT - Usage: ./Client <TASKFILE> <PROJECTNAME>")
+		sys.exit(0)
+
+	client=Client( sys.argv[1], sys.argv[2] )
+	client.processTask()
+	#client.runAnnotator()
+	os.system("touch I\ Lost\ You.mp3")
+	client.uploadChanges()
+
