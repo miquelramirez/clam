1c4c97a4b20e56dfc758e9ac5aef537488db5f05
xoliver <xoliver@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2006-02-08 11:23:49 +0000

    Corrected some layout issues

--- CLAM_Annotator/SimacServicesClient/GUI.py
+++ CLAM_Annotator/SimacServicesClient/GUI.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'GUI.ui'
 #
-# Created: Tue Feb 7 12:18:03 2006
+# Created: Tue Feb 7 18:09:56 2006
 #      by: The PyQt User Interface Compiler (pyuic) 3.14.1
 #
 # WARNING! All changes made in this file will be lost!
@@ -19,15 +19,11 @@ class GUI(QDialog):
             self.setName("GUI")
 
         self.setSizePolicy(QSizePolicy(QSizePolicy.Fixed,QSizePolicy.Fixed,0,0,self.sizePolicy().hasHeightForWidth()))
-        self.setMinimumSize(QSize(470,660))
+        self.setMinimumSize(QSize(470,670))
         self.setMaximumSize(QSize(470,660))
+        f = QFont(self.font())
+        self.setFont(f)
 
-
-        self.goButton = QPushButton(self,"goButton")
-        self.goButton.setGeometry(QRect(20,600,200,30))
-
-        self.uploadButton = QPushButton(self,"uploadButton")
-        self.uploadButton.setGeometry(QRect(240,600,200,30))
 
         LayoutWidget = QWidget(self,"layout4")
         LayoutWidget.setGeometry(QRect(10,13,450,580))
@@ -55,13 +51,34 @@ class GUI(QDialog):
         layout4.addLayout(layout3)
 
         self.logEdit = QTextEdit(LayoutWidget,"logEdit")
+        logEdit_font = QFont(self.logEdit.font())
+        logEdit_font.setFamily("FreeMono")
+        logEdit_font.setPointSize(9)
+        self.logEdit.setFont(logEdit_font)
+        self.logEdit.setHScrollBarMode(QTextEdit.Auto)
+        self.logEdit.setTextFormat(QTextEdit.PlainText)
         self.logEdit.setWordWrap(QTextEdit.WidgetWidth)
+        self.logEdit.setWrapPolicy(QTextEdit.Anywhere)
         layout4.addWidget(self.logEdit)
+
+        self.exitButton = QPushButton(self,"exitButton")
+        self.exitButton.setGeometry(QRect(330,620,120,30))
+
+        self.groupBox1 = QGroupBox(self,"groupBox1")
+        self.groupBox1.setGeometry(QRect(10,600,270,60))
+
+        self.uploadButton = QPushButton(self.groupBox1,"uploadButton")
+        self.uploadButton.setGeometry(QRect(140,20,120,30))
+
+        self.goButton = QPushButton(self.groupBox1,"goButton")
+        self.goButton.setGeometry(QRect(10,20,120,30))
 
         self.languageChange()
 
-        self.resize(QSize(470,660).expandedTo(self.minimumSizeHint()))
+        self.resize(QSize(470,670).expandedTo(self.minimumSizeHint()))
         self.clearWState(Qt.WState_Polished)
+
+        self.connect(self.exitButton,SIGNAL("clicked()"),self.close)
 
         self.setTabOrder(self.projectEdit,self.taskEdit)
         self.setTabOrder(self.taskEdit,self.taskButton)
@@ -71,15 +88,17 @@ class GUI(QDialog):
 
 
     def languageChange(self):
-        self.setCaption(self.__tr("Annotator task handler"))
-        self.goButton.setText(self.__tr("GO!"))
-        self.uploadButton.setText(self.__tr("Upload!"))
+        self.setCaption(self.__tr("Annotator Task Handler"))
         self.projectLabel.setText(self.__tr("Project Name"))
         self.projectEdit.setText(QString.null)
         self.taskLabel.setText(self.__tr("Task file"))
         self.taskEdit.setText(QString.null)
         self.taskButton.setText(self.__tr("Select file"))
         self.logEdit.setText(self.__tr("Press Go! when ready"))
+        self.exitButton.setText(self.__tr("Exit"))
+        self.groupBox1.setTitle(self.__tr("Task processing"))
+        self.uploadButton.setText(self.__tr("Upload!"))
+        self.goButton.setText(self.__tr("GO!"))
 
 
     def __tr(self,s,c = None):
