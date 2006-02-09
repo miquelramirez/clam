35ad135cf7fec7fdf0f38a4617c3a8ab2fcf589c
xoliver <xoliver@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2006-02-09 11:46:11 +0000

    Added 'destination directory' methods, and others..

--- trunk/CLAM_Annotator/SimacServicesClient/Tasker.py
+++ trunk/CLAM_Annotator/SimacServicesClient/Tasker.py
@@ -1,15 +1,11 @@
 import ServiceStub
 import os, sys
 import urlparse, urllib
-from os import path
 
 from xml.dom.ext.reader.Sax2 import FromXmlStream
 
-#TODO El projecte incloura la descripcio d'aquest (el que ha generat la tasca)
-#  --> al final ho tindra el mateix source de metadata, no problem
 #TODO CANVIS QUE CAL FER:
-#	~~separar be en metodes tot aixo
-
+#	separar be en metodes tot aixo (bastant fet)
 
 clamAnnotatorProjectSkeleton = """<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
 <Project>
@@ -24,97 +20,110 @@ clamAnnotatorProjectSongSkeleton = """    <Song>
     </Song>
 """
 
-class ClientError(Exception):
+class TaskerError(Exception):
 	def __init__(self, message):
 		self.message=message
 
 	def __str__(self):
 		return self.message
 
-class Client:
+class Tasker:
 	def __init__(self, printfunction=sys.stdout.write):
 		self.songlisting = {}
-		self.printfunction=printfunction
+		self.printfunction = printfunction
+		self.path = os.getcwd() + os.sep
 
-	def setParameters(self, taskfile, projectname ):
+	def setParameters(self, taskfile, projectname, path=os.getcwd() ):
 		self.retrieveTask( taskfile)
 		self.projectname = projectname
+		self.path = path + os.sep
 
 	def retrieveTask(self, location):
 		try:
 			self.task = FromXmlStream(location)
 		except:
-			raise ClientError("Task file error\nReading file '%s'. It doesn't exist or it is a malformed XML file." % location)
+			raise TaskerError("Task file error\nReading file '%s'. It doesn't exist or it is a malformed XML file." % location)
 
 	def processTask(self):
 		ids, descriptors, contentlocatoruri, metadataprovideruri = self.extractParameters()
+
 		self.contentlocator = ServiceStub.ContentLocator(contentlocatoruri)
 		self.metadataprovider = ServiceStub.MetadataProvider(metadataprovideruri)
 	
 		projectsonglisting = ""
 		self.printfunction(u"\n  == Retrieving data ==\n")
+		
+		schema = self.metadataprovider.QuerySchema( descriptors )
+		self.createFile( self.projectname, '.sc', schema )
 	
 		for id in ids:
 			#Grab audio file
 			try:
 				locations = self.contentlocator.LocateId( id )
 			except:
-				raise ClientError("Access error\nError accessing content locator server.")
+				raise TaskerError("Access error\nError accessing content locator server.")
 
-			if locations == "NotFound":
-				raise ClientError("Service error\nFile with id=%s not found on the server." % id)
-			elif locations == "ServerError":
-				raise ClientError("Service error\nThe server is not working properly, a ServerError was received.")
+			if locations == "ServerError": #Stop execution
+				raise TaskerError("Service error\nThe server is not working properly, a ServerError was received.")
+			elif locations == "NotFound": #Ignore song
+				self.printfunction(u"\n  WARNING: File with id=%s not found on the server. Id will be ignored.\n" % id)
+				continue
 			
 			url = self.downloadSong( locations)
 	
 			if url == None:
-				raise ClientError("Download error\nCould not download the song from the given source urls.")
+				self.printfunction(u"\n  WARNING: it was impossible to download the file with id=%s from the given source urls.\n" % id)
+				continue
 
 			#File name
 			audiofilename = urllib.unquote( urlparse.urlparse(url)[2].split('/')[-1] )
 			projectsonglisting += clamAnnotatorProjectSongSkeleton % audiofilename
-			os.rename('temp',audiofilename)
+			os.rename( self.path+'temp', self.path+audiofilename )
+
 			#Store modification file for each pool to keep track of the ones upgraded
 			self.songlisting[audiofilename] = os.path.getmtime(audiofilename)
 
 			try:
 				pool = self.metadataprovider.QueryDescriptors( id, descriptors )
 			except:
-				raise ClientError("Access error\nError accessing metadata provider server.")
+				raise TaskerError("Access error\nError accessing metadata provider server.")
 			
 			self.createFile( audiofilename, '.pool', pool )
-	
+
+		if projectsonglisting == "":
+			raise TaskerError("Access error\nNo file from the task could be downloaded. Check the task file for correct ids.")
+
 		self.createFile(self.projectname, '.pro', clamAnnotatorProjectSkeleton % ( self.projectname, projectsonglisting ) )
 	
-		schema = self.metadataprovider.QuerySchema( descriptors )
-		self.createFile( self.projectname, '.sc', schema )
 	
 		#Print log
 		self.printfunction(u"\n  == Task processing finished ==\n")
-		self.printfunction(u" - Downloaded audio files\n")
-		self.printfunction(u" - Created files %s and %s\n" % ( self.projectname+".sc", self.projectname+".pro" ))
+		
+		self.printfunction(u" - Created files %s and %s\n" % ( self.projectname+".pro", self.projectname+".sc" ))
+		self.printfunction(u" - Downloaded %d audio file(s) and generated the corresponding pools\n" % len(self.songlisting) )
 
-	def downloadSong( self, locations):
+	def downloadSong(self, locations):
 		#EP: quan es fagi servir el ContentProvider, el nom d'arxiu sera un problemet, diria -> caldria mirar header pel nom d'arxiu?
+		if len(locations) == 0:
+			return None
+
 		for url in locations.splitlines():
 			try:
 				self.printfunction(u" - Trying '%s'" % url)
 				stream = urllib.urlopen(url)
-				file = open( "temp", 'w')
+				file = open( self.path+"temp", 'w')
 				file.write( stream.read() )
 				file.close()
 				ok = True
 				self.printfunction(u"     (OK)\n")
 				return url
 			except:
-				self.printfunction("     (ERROR)\n")
+				self.printfunction(u"     (ERROR)\n")
 				continue
 				
 		return None
 	
 	def extractParameters(self):
-		dataerror = ClientError("Task file error\nMalformed file, some field is wrong or missing!")
 		ids = []
 		descriptors = []
 
@@ -126,10 +135,17 @@ class Client:
 			desc.normalize()
 			descriptors.append(desc.firstChild.data)
 
-		contentlocator = self.task.getElementsByTagName("ContentLocator")[0].firstChild.data
-		metadataprovider = self.task.getElementsByTagName("MetadataProvider")[0].firstChild.data
+		contentlocator = self.task.getElementsByTagName("ContentLocator")
+		metadataprovider = self.task.getElementsByTagName("MetadataProvider")
 
-		return ids, descriptors, contentlocator, metadataprovider
+		if ( len(ids)<1 ) or ( len(descriptors)<1 ) \
+			or len(contentlocator)!=1 or len(metadataprovider)!=1 :
+			raise TaskerError("Task file error\nMalformed file, some field is wrong or missing!")
+		else:
+			metadataprovider = metadataprovider[0].firstChild.data
+			contentlocator = contentlocator[0].firstChild.data
+
+		return ids, descriptors, contentlocator.strip(), metadataprovider.strip()
 
 	def uploadChanges(self):
 		self.printfunction(u"\n  == Looking for modified files ==\n")
@@ -140,11 +156,13 @@ class Client:
 			else:
 				self.printfunction(u" - File '%s' NOT modified\n"  % songpool)
 
-
 	def createFile(self, name, extension, content):
-		file = open( name+extension, 'w' )
-		file.write( content )
-		file.close()
+		try:
+			file = open( self.path+name+extension, 'w' )
+			file.write( content )
+			file.close()
+		except:
+			raise TaskerError("File write error\nError writing the files, maybe the directory does not exist!")
 
 	def runAnnotator(self):
 		result = os.system( "Annotator %s.pro" % self.projectname )
@@ -153,12 +171,12 @@ class Client:
 
 if __name__ == "__main__" :
 	if len(sys.argv) != 3:
-		self.printfunction("CLIENT - Usage: ./Client <TASKFILE> <PROJECTNAME>")
+		self.printfunction("CLIENT - Usage: ./Tasker <TASKFILE> <PROJECTNAME>")
 		sys.exit(0)
 
-	client=Client()
-	client.setParameters( sys.argv[1], sys.argv[2] )
-	client.processTask()
-	#client.runAnnotator()
-	os.system("touch I\ Lost\ You.mp3")
-	client.uploadChanges()
+	tasker=Tasker()
+	tasker.setParameters( sys.argv[1], sys.argv[2] )
+	tasker.processTask()
+	#tasker.runAnnotator()
+	os.system("touch I\ Lost\ You.mp3.pool")
+	tasker.uploadChanges()
