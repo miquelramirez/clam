07db37ab0e54c9ce1da6e2d26b5f68cdeca671a0
xoliver <xoliver@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2006-02-09 11:47:56 +0000

    Added 'source directory'-related methos and minor changes

--- trunk/CLAM_Annotator/SimacServicesClient/SimacAnnoTask.py
+++ trunk/CLAM_Annotator/SimacServicesClient/SimacAnnoTask.py
@@ -2,13 +2,13 @@
 
 from qt import *
 from GUI import GUI
-from Tasker import *
-import sys
+from Tasker import Tasker, TaskerError
+import sys, os
 
 form=None
 app=None
 outputfunction=None
-client=None
+tasker=None
 
 def selectFile():
 	s = QFileDialog.getOpenFileName(
@@ -18,43 +18,61 @@ def selectFile():
 		"open file dialog"
 		"Choose a file" )
 	global form
-	form.taskEdit.setText(s)
+	if len(s) > 0:
+		form.taskEdit.setText(s)
+
+def selectDirectory():
+	s = QFileDialog.getExistingDirectory(
+		"",
+		None,
+		None,
+		"Choose a directory" )
+	global form
+	if len(s) > 0:
+		form.pathEdit.setText(s)
 
 def startProcess():
-	global form,client
-	if form.taskEdit.text()=="" or form.projectEdit.text()=="":
+	global form,tasker
+	if form.taskEdit.text()=="" or form.projectEdit.text()=="" \
+		or form.pathEdit.text()=="":
 		QMessageBox.warning( None, "Data missing",
-			"You must specify the task file and the project name\n",
+			"You must specify the task file, the project name and the destination directory\n",
 			"I see")
 	else:
 		try:
-			client.setParameters( str(form.taskEdit.text()), str(form.projectEdit.text()) )
-			client.processTask()	
+			tasker.setParameters( str(form.taskEdit.text()), str(form.projectEdit.text()), str(form.pathEdit.text()) )
+			tasker.processTask()	
 			printLog(u"\n  == CLAM-Annotator ==\n")
-			printLog(u" - Launched\n")
+			printLog(u" - Launching...\n")
 			form.hide()
-			#client.runAnnotator()
+			#tasker.runAnnotator()
 			form.show()
 			printLog(u" - Finalized\n")
-			client.uploadChanges()
-	
-		except ClientError, x:
-			QMessageBox.warning( None, "%s" % str(x).split('\n')[0],
-				"%s\n" % str(x).split('\n')[1],
-				"I see")
+			tasker.uploadChanges()
+		except TaskerError, x:
+			title=str(x).split('\n')[0]
+			message=str(x).split('\n')[1:]
+			QMessageBox.warning( None, "%s" % title,
+				"%s\n" % ('\n').join(message),
+				"I see" )
 		else:
 			form.goButton.setEnabled(False)
 			form.uploadButton.setEnabled(True)
 			form.taskEdit.setEnabled(False)
-			form.projectEdit.setEnabled(False)
 			form.taskButton.setEnabled(False)
+			form.projectEdit.setEnabled(False)
+			form.pathEdit.setEnabled(False)
+			form.pathButton.setEnabled(False)
 			form.uploadButton.setFocus()
 
 def uploadChanges():
 	global form
-	printLog("Uploaded (fake)\n")
 	
 	form.uploadButton.setEnabled(False)
+
+	#your code here
+
+	printLog(u"Uploaded (fake)\n")
 	form.exitButton.setFocus()
 
 def printLog( message ):
@@ -69,6 +87,7 @@ def quit():
 def createConnections():
 	global form, app
 	app.connect( form.taskButton, SIGNAL( "clicked()" ), selectFile )
+	app.connect( form.pathButton, SIGNAL( "clicked()" ), selectDirectory )
 	app.connect( form.goButton, SIGNAL( "clicked()" ), startProcess )
 	app.connect( form.uploadButton, SIGNAL( "clicked()" ), uploadChanges )
 	app.connect( form.exitButton, SIGNAL( "clicked()" ), quit )
@@ -79,9 +98,10 @@ def initWidgets():
 	form.uploadButton.setEnabled( False )
 	form.logEdit.setReadOnly( True )
 	outputfunction=form.logEdit.append
+	form.pathEdit.setText( os.getcwd() )
 
 def main( args ):
-	global form,app,client
+	global form,app,tasker
 
 	app=QApplication( args )
 	form=GUI()
@@ -93,7 +113,7 @@ def main( args ):
 		form.taskEdit.setText( args[1] )
 	createConnections()
 
-	client=Client( form.logEdit.append )
+	tasker=Tasker( form.logEdit.append )
 
 	app.exec_loop()
 
