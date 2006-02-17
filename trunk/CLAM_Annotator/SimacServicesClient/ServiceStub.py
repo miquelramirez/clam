af7e9ce6e98b436824077c4c0ee7ee9e10806ec6
xoliver <xoliver@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2006-02-17 11:21:51 +0000

    Converted to form encoding method with a dictionary

--- trunk/CLAM_Annotator/SimacServicesClient/ServiceStub.py
+++ trunk/CLAM_Annotator/SimacServicesClient/ServiceStub.py
@@ -1,8 +1,11 @@
-import urllib
+import urllib, urllib2
 import urlparse
+import HttpFormPost
 
 Proxies = { 'http' : 'http://proxy.upf.edu:8080', 'ftp' : 'http://proxy.upf.edu:8080' }
 NoProxiesFor = ["localhost", "127.0.0.1"]
+useragent = 'simac-annotator-tasker'
+
 
 class ServiceStub :
 	def __init__(self, serviceLocation, proxies=Proxies) :
@@ -10,33 +13,45 @@ class ServiceStub :
 		self.proxies = proxies
 		pass
 
-	def remoteCall(self, serviceName, **kwargs) :
-		try :
-			params=urllib.urlencode(kwargs)
-			if urlparse.urlparse ( self.serviceUrl )[1] in NoProxiesFor :
-				serviceResult=urllib.urlopen(self.serviceUrl+"/"+serviceName, params, {}).read();
-			else :
-				serviceResult=urllib.urlopen(self.serviceUrl+"/"+serviceName, params, Proxies).read();
-			return serviceResult
-		except:# IOError:
+	def remoteCall(self, serviceName, **fields):
+		if urlparse.urlparse (self.serviceUrl)[1] in NoProxiesFor:
+			proxy_support = urllib2.ProxyHandler({})
+		else:
+			proxy_support = urllib2.ProxyHandler(Proxies )
+
+		opener = urllib2.build_opener( proxy_support )
+		urllib2.install_opener(opener)
+
+		try:
+			content_type, body = HttpFormPost.encode_multipart_formdata_dictionary(fields)
+			headers = { 'User-Agent': useragent, 'Content-Type': content_type }
+			req=urllib2.Request(self.serviceUrl+"/"+serviceName, body, headers)
+			result= urllib2.urlopen(req).read()
+			return result
+		except:
 			raise "ERROR GETTING DATA FROM SERVICE"
+
 
 class ContentLocator(ServiceStub) :
 	def LocateId(self, id) :
-		return self.remoteCall("LocateId", id=id)
+		return self.remoteCall( "LocateId", id=id )
 
 	def IdentifyUrl(self, url) :
-		return self.remoteCall("IdentifyUrl", url=url)
+		return self.remoteCall( "IdentifyUrl", url=url )
 
 	def AddUrl(self, url) :
-		return self.remoteCall("AddUrl", url=url)
+		return self.remoteCall( "AddUrl", url=url )
+
 
 class MetadataProvider(ServiceStub) :
 	def QueryIdByUrl(self, url) :
-		return self.remoteCall("QueryIdByUrl", url=url)
+		return self.remoteCall( "QueryIdByUrl", url=url )
 
 	def QuerySchema(self, descriptors) :
-		return self.remoteCall("QuerySchema", descriptors=descriptors)
+		return self.remoteCall( "QuerySchema", descriptors=descriptors )
 
 	def QueryDescriptors(self, id, descriptors) :
-		return self.remoteCall("QueryDescriptors", id=id, descriptors=descriptors)
+		return self.remoteCall( "QueryDescriptors", id=id, descriptors=descriptors)
+
+	def UploadPackedDescriptors(self, file) :
+		return self.remoteCall( "UploadPackedDescriptors", packedpoolfile=open(file, 'rb') )
