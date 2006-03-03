108ea661edb10dbb934a313638cd10e610294c90
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2006-03-03 06:39:29 +0000

    Traces removed Delete on close

--- trunk/CLAM_Annotator/src/TaskRunner.hxx
+++ trunk/CLAM_Annotator/src/TaskRunner.hxx
@@ -18,35 +18,32 @@ public:
 		mOutputDisplay = new QTextEdit(this);
 //		QVBoxLayout * layout = new QVBoxLayout(this);
 		layout()->addWidget(mOutputDisplay);
+		setAttribute(Qt::WA_DeleteOnClose, true);
 	}
 	virtual ~TaskRunner();
 	bool run(QString command, QStringList & arguments, QString workingDir)
 	{
-		std::cout << "yop" << std::endl;
+		mOutputDisplay->setHtml("<div style='color: blue;'>Executing "+ command +"</div>");
 		mProcess = new QProcess(this);
 		mProcess->setWorkingDirectory(workingDir);
 		connect(mProcess, SIGNAL(readyReadStandardError()), this, SLOT(dumpError()));
 		connect(mProcess, SIGNAL(readyReadStandardOutput()), this, SLOT(dumpOutput()));
 		connect(mProcess, SIGNAL(finished(int,QProcess::ExitStatus)), this, SLOT(finished()));
-		mOutputDisplay->setHtml("<div style='color: blue;'>Executing "+ command +"</div>");
 		mProcess->start(command, arguments);
 		return mProcess->waitForStarted();
 	}
 private slots:
 	void finished()
 	{
-		std::cout << "finished" << std::endl;
 		mOutputDisplay->append("<div style='color: blue;'>Done.</div>");
 		QTimer::singleShot(5000, this, SLOT(close()));
 	}
 	void dumpError()
 	{
-		std::cout << "Weo" << std::endl;
 		mOutputDisplay->append("<pre style='color: red;'>"+  mProcess->readAllStandardError() +"</pre>");
 	}
 	void dumpOutput()
 	{
-		std::cout << "douuu" << std::endl;
 		mOutputDisplay->append("<pre style='color: green;'>"+  mProcess->readAllStandardOutput() +"</pre>");
 	}
 private:
