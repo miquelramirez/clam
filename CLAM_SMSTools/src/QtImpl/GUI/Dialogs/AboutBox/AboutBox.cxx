ff912a41cf5921a41f4f82497f2b5ca60e25735d
David García Garzón <vokimon@users.noreply.github.com>
2006-05-19 00:26:39 +0000

    New about box Icon for the main window Injected versions from the code

--- CLAM_SMSTools/src/QtImpl/GUI/Dialogs/AboutBox/AboutBox.cxx
+++ CLAM_SMSTools/src/QtImpl/GUI/Dialogs/AboutBox/AboutBox.cxx
@@ -1,5 +1,3 @@
-#include <qlabel.h>
-#include <qpixmap.h>
 #include "AboutBox.hxx"
 
 namespace QtSMS
@@ -7,8 +5,6 @@ namespace QtSMS
 	AboutBox::AboutBox(QWidget* parent)
 		: AboutBoxBase(parent)
 	{
-		mMtgXpmLbl->setPixmap(QPixmap("rc/mtg3.xpm"));
-		mIuaXpmLbl->setPixmap(QPixmap("rc/iua2.xpm"));
 	}
 		
 	AboutBox::~AboutBox()
