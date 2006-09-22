d1a23945d850a18964b80eb1fb9c5b1666734849
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2006-09-22 12:21:06 +0000

    added IsCallbackBased into NetworkPlayer Extern* are no longer marked as SyncSource

--- CLAM/src/Flow/Networks/PANetworkPlayer.hxx
+++ CLAM/src/Flow/Networks/PANetworkPlayer.hxx
@@ -46,6 +46,7 @@ public:
 	void DoInPorts(TData** input, unsigned long nframes);
 	void DoOutPorts(TData** output, unsigned long nframes);
 	
+	virtual bool IsCallbackBased() const { return true; }
 	virtual void Start();
 	virtual void Stop();
 
