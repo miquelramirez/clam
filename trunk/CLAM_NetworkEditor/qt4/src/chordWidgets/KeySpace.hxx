536e56f15f0fcce0e5ebe555591782650062b562
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2006-10-03 18:30:20 +0000

    Added chord views with dummy data

---
+++ trunk/CLAM_NetworkEditor/qt4/src/chordWidgets/KeySpace.hxx
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2001-2006 MUSIC TECHNOLOGY GROUP (MTG)
+ *                         UNIVERSITAT POMPEU FABRA
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#ifndef _KEYSPACE_
+#define _KEISPACE_
+
+#include "Tonnetz.hxx"
+namespace CLAM {
+namespace VM {
+
+class KeySpace : public Tonnetz
+{
+protected:
+	float x_res, y_res;
+
+public:
+	KeySpace(QWidget * parent);
+
+	virtual void initializeGL();
+	virtual void resizeGL(int width, int height);
+	virtual void paintGL();
+private:
+	void DrawTiles();
+	void DrawLabels();
+	double wdist(double x1,double x2)
+	{
+		if (x2 > x1+.5) return 1. - (x2-x1);
+		if (x2 < x1-.5) return 1. - (x1-x2);
+		if (x2 >= x1)   return x2-x1;
+		else            return x1-x2;
+	}
+
+	float centroidx_,centroidy_;
+	float pKeySpaceValue_[24];
+	float pRColor[201],pGColor[201],pBColor[201];
+	int ColorsIndex[6];
+};
+
+}
+}
+
+#endif
---
+++ trunk/CLAM_NetworkEditor/qt4/src/chordWidgets/Tonnetz.hxx
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2001-2006 MUSIC TECHNOLOGY GROUP (MTG)
+ *                         UNIVERSITAT POMPEU FABRA
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#ifndef Tonnetz_hxx
+#define Tonnetz_hxx
+
+#include <QtOpenGL/QGLWidget>
+#undef GetClassName
+#include "FloatArrayDataSource.hxx"
+
+#include <vector>
+
+
+namespace CLAM_Annotator { class FrameDivision; }
+
+namespace CLAM
+{
+namespace VM
+{
+
+	class Tonnetz : public QGLWidget
+	{
+		Q_OBJECT
+
+		public:
+			Tonnetz(QWidget * parent);
+			~Tonnetz();
+			virtual void initializeGL();
+			virtual void resizeGL(int width, int height);
+			virtual void paintGL();
+		private:
+			unsigned BinAtPosition(int x, int y);
+			void Draw();
+			void DrawTile(int x, int y);
+			void DrawLabel(int x, int y);
+			void DrawChordsShapes();
+		protected:
+			const double *frameData() const
+			{
+				if (not _dataSource) return 0;
+				return _dataSource->frameData();
+			}
+		public:
+			void updateIfNeeded();
+			void setSource( const FloatArrayDataSource & dataSource );
+			void clearData();
+		protected:
+			int _updatePending;
+			double _maxValue;
+			unsigned _nBins;
+			const FloatArrayDataSource * _dataSource;
+		private:
+			QFont _font;
+	};
+
+
+}
+}
+
+
+
+#endif// Tonnetz_hxx
