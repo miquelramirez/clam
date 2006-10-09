f4b25abac9fd0f2f2568812b978f7b760085f8c2
David García Garzón <vokimon@users.noreply.github.com>
2006-10-09 07:52:07 +0000

    Spectral peaks into the prototyper

--- CLAM_NetworkEditor/qt4/src/PeakView.cxx
+++ CLAM_NetworkEditor/qt4/src/PeakView.cxx
@@ -1,16 +1,7 @@
 
-#include <CLAM/PortMonitor.hxx>
 #include <CLAM/Factory.hxx>
-#include <CLAM/SpectralPeakArray.hxx>
+#include "PeakView.hxx"
 
-
-//TODO move to a clam lib
-
-class PeakView : public CLAM::PortMonitor<CLAM::SpectralPeakArray>
-{
-	const char* GetClassName() const { return "PeakView"; };
-};
-
-static CLAM::Factory<CLAM::Processing>::Registrator<PeakView> registrator("PeakView");
+static CLAM::Factory<CLAM::Processing>::Registrator<PeakViewMonitor> registrator("PeakView");
 
 
