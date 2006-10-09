ef44cdb41d47156a5e4809a14a9741c9ba6a47f7
David García Garzón <vokimon@users.noreply.github.com>
2006-10-09 01:32:06 +0000

    Fix: vumeter didn't update because an extra return in the timer callback Vumeter canbe loaded as a plugin Oscilloscope line color property Vumeter is now a led vumeter

--- CLAM_NetworkEditor/qt4/src/Vumeter.hxx
+++ CLAM_NetworkEditor/qt4/src/Vumeter.hxx
@@ -9,32 +9,64 @@ class VumeterMonitor : public OscilloscopeMonitor
 
 #include <QtOpenGL/QGLWidget>
 #undef GetClassName
-#include <QtGui/QLabel>
-#include <CLAM/Processing.hxx>
 #include <CLAM/DataTypes.hxx>
+#include <QtDesigner/QDesignerExportWidget>
 
-
-class Vumeter : public QWidget
+class QDESIGNER_WIDGET_EXPORT Vumeter : public QWidget
 {
+	Q_OBJECT
 	enum Dimensions {
-		margin=4
+		margin=4,
+		ledHeight=10
 	};
 public:
+	Vumeter(QWidget * parent=0)
+		: QWidget(parent)
+		, _dataSource( 0)
+		, _energy(0)
+	{
+		if (!_dataSource) _dataSource = new OscilloscopeDummySource;
+		startTimer(50);
+	}
 	Vumeter(CLAM::VM::FloatArrayDataSource * dataSource, QWidget * parent=0)
 		: QWidget(parent)
 		, _dataSource( dataSource)
 		, _energy(0)
 	{
+		if (!_dataSource) _dataSource = new OscilloscopeDummySource;
 		startTimer(50);
 	}
 	void paintEvent(QPaintEvent * event)
 	{
+		unsigned nLeds=height()/ledHeight;
+		double ledStep = 1.0/nLeds;
+		unsigned actualLedHeight = height()/nLeds;
+		
+		double logEnergy = energy()*10;
 		QPainter painter(this);
-		painter.setBrush(Qt::red);
 		painter.setPen(Qt::black);
-		int vumeterHeight = int(height()*(energy()*10));
-//		std::cout << "painting.."  << vumeterHeight << "\tenergy: "<<_energy << std::endl;
-		painter.drawRect(margin,height()-vumeterHeight,width()-2*margin,height());
+		painter.setBrush(Qt::red);
+		for (unsigned i=0; i<nLeds; i++)
+		{
+			QColor color = Qt::green;
+			if (i>3*nLeds/8) color=Qt::yellow;
+			if (i>5*nLeds/8) color=QColor(0xff,0x77,0x00);
+			if (i>=7*nLeds/8) color=Qt::red;
+			if (logEnergy<=ledStep*i)
+				painter.setBrush(color.dark());
+			else
+				painter.setBrush(color);
+			painter.drawRect(margin,height()-actualLedHeight*i-margin,width()-2*margin,-actualLedHeight+margin);
+		}
+
+	}
+	QSize minimumSizeHint() const
+	{
+		return QSize(2*margin+10,2*margin+2*ledHeight);
+	}
+	void setDataSource(CLAM::VM::FloatArrayDataSource * source)
+	{
+		_dataSource = source;
 	}
 	double energy()
 	{
@@ -62,7 +94,6 @@ public:
 	{
 		if ( !_dataSource) return;
 		if ( !_dataSource->isEnabled()) return;
-			return;
 		update();
 	}
 private:
