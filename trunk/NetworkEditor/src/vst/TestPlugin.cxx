31c8812c4eefe6836988c6c547923400a98f1450
testfarm <testfarm@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2006-12-14 19:47:02 +0000

    little app to test clam vst plugins with console for debug

---
+++ trunk/NetworkEditor/src/vst/TestPlugin.cxx
@@ -0,0 +1,17 @@
+#include "CLAMVstPlugin.hpp"
+#include <iostream>
+int main()
+{
+	std::cout << "== Testing CLAM vst plugin" << std::endl;
+	audioMasterCallback audioMaster=0;
+	CLAMTest* effect = new CLAMTest (audioMaster);
+	float inbuf[] = {1,1,1,1,1};
+	float outbuf[] = {0,0,0,0,0};
+	float* firstInBuf = &inbuf[0];
+	float* firstOutBuf = &outbuf[0];
+	float **ins = &firstInBuf;
+	float **outs = &firstOutBuf;
+	effect->process(ins, outs, 5);
+	delete effect;
+	return 0;
+}
