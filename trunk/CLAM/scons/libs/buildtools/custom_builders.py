d3cbe141a6677d5fb32a2b9963ee2cafc4ad0571
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2007-02-08 19:13:07 +0000

    Scons simplification

--- trunk/CLAM/scons/libs/buildtools/custom_builders.py
+++ trunk/CLAM/scons/libs/buildtools/custom_builders.py
@@ -31,14 +31,8 @@ def generate_so_name_message( target, source, env ) :
 	return "generating %s with /sbin/ldconfig -n"%target[0]
 
 def generate_linker_name( target, source, env ) :
-	source_dir = os.path.dirname( str(source[0]) )
 	source_file = os.path.basename( str(source[0]) )
-	target_file = os.path.basename( str(target[0]) )
-	target_dir = os.path.dirname( str(target[0]) )
-	cwd = os.getcwd()
-	os.chdir( target_dir )
-	os.system( "ln -sf %s %s"%(source_file,target_file) )
-	os.chdir(cwd)
+	os.system( "ln -sf %s %s"%(source_file,str(target[0]) ))
 	return None
 
 def generate_linker_name_message( target, source, env ) :
