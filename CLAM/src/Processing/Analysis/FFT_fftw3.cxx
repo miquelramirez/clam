d6d7f8c2dc2231b1f4fbc1ccbba7972a304f9040
David García Garzón <vokimon@users.noreply.github.com>
2007-02-15 06:15:30 +0000

    FFT_fftw3 added

---
+++ CLAM/src/Processing/Analysis/FFT_fftw3.cxx
@@ -0,0 +1,147 @@
+/*
+ * Copyright (c) 2001-2004 MUSIC TECHNOLOGY GROUP (MTG)
+ *                         UNIVERSITAT POMPEU FABRA
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+
+#include "FFT_fftw3.hxx"
+
+#include "Assert.hxx"
+#include "Audio.hxx"
+#include "Spectrum.hxx"
+#include "SpectrumConfig.hxx"
+#include "CLAM_Math.hxx"
+#include "Factory.hxx"
+
+namespace CLAM {
+
+ 
+	namespace detail
+	{
+		static Factory<Processing>::Registrator<FFT_fftw3> regtFFT_fftw3( "FFT_fftw3" );
+	}
+
+bool FFT_fftw3::ConcreteConfigure(const ProcessingConfig& c)
+{
+	int oldSize=mSize;
+	FFT_base::ConcreteConfigure(c);
+	if (mSize<=0) 
+	{
+		AddConfigErrorMessage("Invalid zero or negative input size");
+		return false;
+	}
+	if (mSize == oldSize) return true;
+	SetupMemory();
+	return true;
+}
+
+void FFT_fftw3::ReleaseMemory() {
+	if (_plan)
+	{
+		fftw_destroy_plan(*_plan);
+		delete _plan;
+		_plan=0;
+	}
+	if (_realInput)
+	{
+		fftw_free(_realInput);
+		_realInput=0;
+	}
+	if (_complexOutput)
+	{
+		fftw_free(_complexOutput);
+		_complexOutput=0;
+	}
+}
+
+void FFT_fftw3::SetupMemory() {
+	ReleaseMemory();
+	// Special malloc which aligns to SIMD segment boundaries
+	_realInput = (double*) fftw_malloc(sizeof(double) * mSize);
+	_complexOutput = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * mSize);
+	_plan = new fftw_plan;
+	*_plan = fftw_plan_dft_r2c_1d(mSize, _realInput, _complexOutput, FFTW_ESTIMATE);
+	}
+
+FFT_fftw3::FFT_fftw3()
+	: _realInput(0)
+	, _complexOutput(0)
+	, _plan(0)
+{
+	Configure(FFTConfig());
+}
+
+FFT_fftw3::FFT_fftw3(const FFTConfig &c) throw(ErrDynamicType)
+	: _realInput(0)
+	, _complexOutput(0)
+	, _plan(0)
+{ 
+	Configure(c);
+}
+
+FFT_fftw3::~FFT_fftw3()
+{
+	ReleaseMemory();
+}
+
+bool FFT_fftw3::Do() 
+{
+	mOutput.GetData().SetSize( mInput.GetSize()/2+1);
+	bool toReturn = Do(mInput.GetAudio(), mOutput.GetData());
+	mInput.Consume();
+	mOutput.Produce();
+	return toReturn;
+}
+
+bool FFT_fftw3::Do(const Audio& in, Spectrum &out)
+{
+	CLAM_DEBUG_ASSERT(GetExecState() == Running,
+		"FFT_fftw3: Do(): Not in execution mode");
+	CLAM_DEBUG_ASSERT(isPowerOfTwo(mSize),
+		"FFT_fftw3: Do(): Not a power of two");
+
+	out.SetSpectralRange(in.GetSampleRate()/2);
+	if (mState==sOther) CheckTypes(in,out);
+	TData * inbuffer = in.GetBuffer().GetPtr();
+	for (int i=0; i<mSize; i++)
+		_realInput[i] = inbuffer[i];
+	fftw_execute(*_plan);
+	if (mState!=sOther)
+		ToComplex(out);
+	else
+		ToOther(out);
+	if (mState==sComplexSync)
+		out.SynchronizeTo(mComplexflags);
+
+	return true;
+}
+
+
+void FFT_fftw3::ToComplex(Spectrum &out)
+{
+	Array<Complex>& outbuffer = out.GetComplexArray();
+	outbuffer.Resize(mSize/2+1); // TODO: Any sense?
+	outbuffer.SetSize(mSize/2+1); // TODO: Any sense?
+	for (int i=0; i<mSize/2+1; i++) {
+		outbuffer[i].SetReal(_complexOutput[i][0]);
+		outbuffer[i].SetImag(_complexOutput[i][1]);
+	}
+}
+
+}; // CLAM
---
+++ CLAM/src/Processing/Analysis/FFT_fftw3.hxx
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2001-2004 MUSIC TECHNOLOGY GROUP (MTG)
+ *                         UNIVERSITAT POMPEU FABRA
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+
+#ifndef _FFT_fftw3
+#define _FFT_fftw3
+
+#ifndef USE_FFTW3
+#error This header should not be used without the USE_FFTW3 macro defined
+#endif
+
+#include "FFT_base.hxx"
+#include "DataTypes.hxx"
+#include "SpecTypeFlags.hxx"
+#include "ErrDynamicType.hxx"
+#include <fftw3.h>
+
+namespace CLAM {
+
+	struct FFTConfig;
+	class Spectrum;
+	class Audio;
+	class ProcessingConfig;
+	class IFFT_fftw3;
+
+	/** Implementation of the FFT using the Fastest Fourier Transform of the West version 3
+	 */
+	class FFT_fftw3: public FFT_base
+	{
+		double * _realInput;
+		fftw_complex * _complexOutput;
+		fftw_plan * _plan;
+
+//		bool FFTConfigure();
+
+		/** Configuration change method
+		 */
+		bool ConcreteConfigure(const ProcessingConfig&);
+
+		// Memory Management (for work areas and stuff)
+		void ReleaseMemory();
+		void SetupMemory();
+
+		// Output conversions
+
+		void ToComplex(Spectrum &out);
+
+	public:
+
+		FFT_fftw3();
+		FFT_fftw3(const FFTConfig &c) throw(ErrDynamicType);
+		~FFT_fftw3();
+		const char * GetClassName() const {return "FFT_fftw3";}
+
+		// Execution methods
+		bool Do();
+		bool Do(const Audio& in, Spectrum &out);
+		bool MayDisableExecution() const {return true;}
+
+	};
+
+};//namespace CLAM
+
+#endif // _FFT_numrec_
