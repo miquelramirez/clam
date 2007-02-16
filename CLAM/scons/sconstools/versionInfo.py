00afbf053f56c2d5092a0d6198ac47f64fe82c89
David García Garzón <vokimon@users.noreply.github.com>
2007-02-16 11:41:35 +0000

    Taking revision from keyword in CHANGES

--- CLAM/scons/sconstools/versionInfo.py
+++ CLAM/scons/sconstools/versionInfo.py
@@ -4,15 +4,15 @@ repositoryBase = "http://iua-share.upf.edu/svn/clam/trunk/"
 def takeFromChangeLog(changelogFile, product='CLAM') :
 	import re
 	import datetime
-	versionExtractor = re.compile(r'[0-9?]+-[0-9?]+-[0-9?]+ %s (?P<Major>[0-9]+)\.(?P<Minor>[0-9]+)\.(?P<Patch>[0-9]+)(?P<SVN>.*SVN.*)?'%product)
+	versionExtractor = re.compile(r'[0-9?]+-[0-9?]+-[0-9?]+ %s (?P<Major>[0-9]+)\.(?P<Minor>[0-9]+)\.(?P<Patch>[0-9]+)(?P<SVN>.*SVN[^0-9]*(?P<Revision>[0-9]+))?'%product)
 	for line in file(changelogFile) :
 		match = versionExtractor.match(line)
 		if match is None: continue
-		major, minor, patch, isSvn = [ match.group(tag) for tag in ('Major', 'Minor', 'Patch', 'SVN') ]
+		major, minor, patch, isSvn, revision = [ match.group(tag) for tag in ('Major', 'Minor', 'Patch', 'SVN', 'Revision') ]
 		versionString = "%s.%s.%s"%(major,minor,patch)
 		today = datetime.date.today()
 		if not isSvn : return versionString, versionString
-		return versionString, "%s~svn%05i"%(versionString, int(svnRevision()))
+		return versionString, "%s~svn%05i"%(versionString, int(revision))
 
 def svnRevision():
 	import re
