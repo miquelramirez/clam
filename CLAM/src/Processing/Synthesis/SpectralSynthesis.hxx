9fbb49d6bac46b7ebbc616b3bd3a1f32b37e1c3c
David García Garzón <vokimon@users.noreply.github.com>
2007-02-16 01:28:49 +0000

    SpectralAnalysis and SpectralAnalysis uses the default FFT

--- CLAM/src/Processing/Synthesis/SpectralSynthesis.hxx
+++ CLAM/src/Processing/Synthesis/SpectralSynthesis.hxx
@@ -34,13 +34,6 @@
 #include "Err.hxx"
 #include "IFFT.hxx"
 
-
-#ifdef CLAM_OOURA
-	#include "IFFT_ooura.hxx"
-#else
-	#include "IFFT_rfftw.hxx"
-#endif
-
 #include "Frame.hxx"
 #include "Segment.hxx"
 #include "InControl.hxx"
@@ -92,11 +85,7 @@ private:
 		//internal Processing Objects
 		WindowGenerator			mPO_AnalWindowGen;
 		WindowGenerator			mPO_SynthWindowGen;
-#ifdef CLAM_OOURA
-		IFFT_ooura				mPO_IFFT;
-#else
-		IFFT_rfftw				mPO_IFFT;
-#endif
+		IFFT				mPO_IFFT;
 		AudioMultiplier            mPO_AudioProduct;
 		CircularShift			mPO_CircularShift;
 			
