b02256c4ecea987524e301c9f45362cbbd078eef
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2007-03-07 20:07:03 +0000

    * added new program (binary) in NetworkEditor: OfflinePlayer.    Usage OfflinePlayer <networkfile> <in.wav> <out.wav>  * NetworkEditor/SConstruct updated to compile OfflinePlayer  * NetworkEditor/INSTALL added tip about using QTDIR  * PrototypeLoader: remove code that explicitely choosed NaiveFlowControl.    now takes the default FlowControl of the network

---
+++ NetworkEditor/src/OfflinePlayerMain.cxx
@@ -0,0 +1,39 @@
+#include <CLAM/FreewheelingNetworkPlayer.hxx>
+#include <CLAM/XMLStorage.hxx>
+
+int usage(const std::string & program)
+{
+		std::cout << " Usage: " << program
+			<< " <networkfile> <infile.wav> <outfile.wav>\n"
+			<< std::endl;
+		return -1;
+}
+
+int main( int argc, char *argv[] )
+{
+
+#ifdef WIN32
+	CLAM::ProcessingModule::init();
+	CLAM::AudioIOModule::init();
+#endif
+	if (argc != 4)
+		return usage(argv[0]);
+
+	std::string networkFile = argv[1];
+	std::string inputFile = argv[2];
+	std::string outputFile = argv[3];
+	std::cout << "\nUsing the following files. Network: "<< networkFile << " Input: "<< inputFile
+		<< " Output: " << outputFile << std::endl;
+
+	CLAM::Network net;
+ 	CLAM::XMLStorage::Restore(net, networkFile);
+	CLAM::FreewheelingNetworkPlayer * player =  new CLAM::FreewheelingNetworkPlayer;
+	net.SetPlayer( player ); // network owns the player memory
+	player->InputFile(inputFile);
+	player->OutputFile(outputFile);
+	net.Start();
+	net.Stop();
+	std::cout << "\nDone!" << std::endl;
+
+	return 0;
+}
