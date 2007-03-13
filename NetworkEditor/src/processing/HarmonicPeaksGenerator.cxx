4ca9af6e7454c06baffb542d9adf35a04cac9e45
David García Garzón <vokimon@users.noreply.github.com>
2007-03-13 01:46:44 +0000

    * New processing HarmonicPeaksGenerator which generates a set of    harmonic peaks with a given fundamental frequency and a given slope.

---
+++ NetworkEditor/src/processing/HarmonicPeaksGenerator.cxx
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2001-2004 MUSIC TECHNOLOGY GROUP (MTG)
+ *                         UNIVERSITAT POMPEU FABRA
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#include "HarmonicPeaksGenerator.hxx"
+#include <CLAM/Factory.hxx>
+#include <cmath>
+
+namespace CLAM
+{
+
+bool HarmonicPeaksGenerator::Do(SpectralPeakArray& out)
+{
+	out.AddAll();
+	out.UpdateData();
+	out.InitIndices();
+	float spectralRange = 22050;
+	TData fundamental=mFundamental.GetLastValue();
+	unsigned nHarmonics = std::min(20u,unsigned(spectralRange/fundamental));
+	out.SetnMaxPeaks(nHarmonics);
+	out.SetnPeaks(0);
+	out.SetScale(EScale::eLog);
+
+	IndexArray& outIndexBuffer=out.GetIndexArray();
+	DataArray& outMagBuffer=out.GetMagBuffer();
+	DataArray& outFreqBuffer=out.GetFreqBuffer();
+	DataArray& outPhaseBuffer=out.GetPhaseBuffer();
+	DataArray& outBinPosBuffer=out.GetBinPosBuffer();
+	DataArray& outBinWidthBuffer=out.GetBinWidthBuffer();
+	unsigned firstBin = fundamental/22050;
+	static unsigned t = 0;
+	for (unsigned i=1; i<nHarmonics; i++)
+	{
+		outIndexBuffer.AddElem(i-1);
+		outFreqBuffer.AddElem(i*fundamental);
+		outMagBuffer.AddElem(-20-2.*i);
+		outPhaseBuffer.AddElem(i*fundamental*t*512*2*M_PI/44100);
+		outBinPosBuffer.AddElem(i*firstBin);
+		outBinWidthBuffer.AddElem(firstBin/2);
+	}
+	t++;
+	out.SetIsIndexUpToDate(true);
+	return true;
+}
+
+typedef Factory<Processing> ProcessingFactory;
+
+static ProcessingFactory::Registrator<HarmonicPeaksGenerator> regtFreqShift( "HarmonicPeaksGenerator" );
+
+}
---
+++ NetworkEditor/src/processing/HarmonicPeaksGenerator.hxx
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2001-2004 MUSIC TECHNOLOGY GROUP (MTG)
+ *                         UNIVERSITAT POMPEU FABRA
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+
+#ifndef _HarmonicPeaksGenerator_
+#define _HarmonicPeaksGenerator_
+
+#include <CLAM/InPort.hxx>
+#include <CLAM/OutPort.hxx>
+#include <CLAM/InControl.hxx>
+#include <CLAM/SpectralPeakArray.hxx>
+#include <CLAM/NullProcessingConfig.hxx>
+#include <CLAM/Processing.hxx>
+#include <CLAM/Frame.hxx>
+
+namespace CLAM{
+
+	class HarmonicPeaksGenerator: public Processing
+	{
+		OutPort<SpectralPeakArray> mOut;
+		InControl mFundamental;
+		TData mTime;
+
+	public:
+		typedef NullProcessingConfig Config;
+		const char* GetClassName() const
+		{
+			return "HarmonicPeaksGenerator";
+		}
+
+		HarmonicPeaksGenerator() 
+			: mOut("Out SpectralPeaks", this) 
+			, mFundamental("Fundamental", this)
+		{
+			Configure( Config() );
+		}
+
+ 		~HarmonicPeaksGenerator() {}
+		virtual bool ConcreteConfigure(const CLAM::ProcessingConfig&)
+		{
+			return true;
+		}
+		const ProcessingConfig& GetConfig() const
+		{
+			static NullProcessingConfig config;
+			return config;
+		}
+		
+		bool Do(SpectralPeakArray& out);
+
+		bool Do()
+		{
+			bool result = Do(mOut.GetData()); 
+			mOut.Produce();
+			return result;
+		}
+	
+	};		
+};//namespace CLAM
+
+#endif // _HarmonicPeaksGenerator_
