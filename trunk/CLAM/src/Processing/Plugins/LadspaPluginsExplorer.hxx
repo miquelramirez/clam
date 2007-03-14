261245fcad9d083b39819a542068a41a1c6cfc3a
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2007-03-14 19:27:42 +0000

    * ProcessingTree : extracted Ladspa exploration to new LadspaPluginsExplorer class  * new LadspaPluginsExplorer class with a static method that returns plugins information  * new test to LadspaFactoryTest

---
+++ trunk/CLAM/src/Processing/Plugins/LadspaPluginsExplorer.hxx
@@ -0,0 +1,27 @@
+#ifndef __LADSPAPLUGINSEXPLORER_HXX__
+#define __LADSPAPLUGINSEXPLORER_HXX__
+
+#include <string>
+#include <list>
+
+namespace CLAM 
+{
+
+struct LadspaPlugin
+{
+	std::string libraryFileName;
+	unsigned index;
+	std::string description;
+	std::string factoryID;
+};
+
+typedef std::list<LadspaPlugin> LadspaPlugins;
+
+class LadspaPluginsExplorer {
+	public:
+		static LadspaPlugins GetList();
+};
+
+} // namespace CLAM
+#endif //__LADSPAPLUGINSEXPLORER_HXX__
+
