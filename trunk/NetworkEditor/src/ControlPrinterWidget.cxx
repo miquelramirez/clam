7816ba7c3935c72eaf5d4f006a4322bffc0248eb
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2007-04-08 00:18:14 +0000

    * Zach control-printer patch  "he attached patch provides a new ControlPrinterWidget class that displays the  input control value inside the processing widget.  Its design allows it to be  embedded into any Processing widget (e.g. for debugging purposes), and it can  handle any number of input controls.  This will allow it to be re-used  without changes in a ControlPrinterArray class, or something similar. "

---
+++ trunk/NetworkEditor/src/ControlPrinterWidget.cxx
@@ -0,0 +1,52 @@
+#include "ControlPrinterWidget.hxx"
+#include <CLAM/InControl.hxx>
+#include <QtGui/QGridLayout>
+#include <QtGui/QLabel>
+
+ControlPrinterWidget::ControlPrinterWidget(CLAM::Processing * processing)
+	: _processing(processing)
+{
+	QGridLayout *grid = new QGridLayout();
+	setLayout(grid);
+
+	CLAM::InControlRegistry &cr = _processing->GetInControls();
+	int nLabels = cr.Size();
+	for (int i = 0; i < nLabels; i++) {
+		CLAM::InControl *control = &cr.GetByNumber(i);
+		QString name(control->GetName().c_str());
+
+		QLabel *label = new QLabel(name);
+		label->setMinimumHeight(label->sizeHint().height());
+		label->setMinimumWidth(label->sizeHint().width());
+		grid->addWidget(label, i, 0, Qt::AlignRight);
+
+		label = new QLabel("100.00");
+		label->setMinimumHeight(label->sizeHint().height());
+		label->setMinimumWidth(label->sizeHint().width());
+		label->setText("");
+		grid->addWidget(label, i, 1);
+
+		_labels.push_back(label);
+	}
+
+	startTimer(50);
+}
+
+ControlPrinterWidget::~ControlPrinterWidget()
+{
+}
+
+void ControlPrinterWidget::timerEvent(QTimerEvent *event)
+{
+	CLAM::InControlRegistry &cr = _processing->GetInControls();
+	int nLabels = cr.Size();
+	for (int i = 0; i < nLabels; i++)
+	{
+		CLAM::InControl *control = &cr.GetByNumber(i);
+		CLAM::TControlData value = control->GetLastValue();
+
+		QString str("%1");
+		_labels[i]->setText(str.arg(value, 0, 'f', 2));
+	}
+}
+
---
+++ trunk/NetworkEditor/src/ControlPrinterWidget.hxx
@@ -0,0 +1,27 @@
+#ifndef ControlPrinterWidget_hxx
+#define ControlPrinterWidget_hxx
+
+#include <CLAM/Processing.hxx>
+#include <QtGui/QWidget>
+#include <vector>
+
+class QLabel;
+
+class ControlPrinterWidget : public QWidget
+{
+	Q_OBJECT
+public:
+	ControlPrinterWidget(CLAM::Processing *processing);
+	~ControlPrinterWidget();
+
+protected:
+	void timerEvent(QTimerEvent *event);
+
+private:
+	CLAM::Processing *_processing;
+	std::vector<QLabel *> _labels;
+};
+
+#endif //ControlPrinterWidget_hxx
+
+
