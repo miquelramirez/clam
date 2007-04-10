015b6793cbe9c153b9efeaecc5f401d8d599ff7b
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2007-04-10 14:08:43 +0000

    * Zach's patch for multiple controls in ControlPrinter   "I went ahead and added a NumberOfInputs attribute to ControlPrinterConfig,   with the default of 1.  With the default of one input, the control preserves   the exists behavior; however, it now supports printing a list of values.  The   previous widget patch should transparently handle this change.  Altogether,   this obviates the need for a separate ControlPrinterArray class."

--- CLAM/src/Processing/Controls/ControlPrinter.cxx
+++ CLAM/src/Processing/Controls/ControlPrinter.cxx
@@ -1,6 +1,7 @@
 #include "ControlPrinter.hxx"
 #include "Factory.hxx"
 #include <iostream>
+#include <sstream>
 
 typedef CLAM::Factory<CLAM::Processing> ProcessingFactory;
 
@@ -16,35 +17,64 @@ namespace CLAM
 		AddAll();
 		UpdateData();
 		SetIdentifier( "ControlPrinter" );
+		SetNumberOfInputs(1.);
 	}
 
 	ControlPrinter::ControlPrinter()
-	: mInControl( "Control In", this )
 	{
 		Configure( mConfig );	
 	}
 	
 	ControlPrinter::ControlPrinter( const ControlPrinterConfig& cfg )
-	: mInControl( "Control In", this )
 	{ 
 		Configure( cfg );
+	}
+	ControlPrinter::~ControlPrinter()
+	{
+		RemoveOldControls();
 	}
 		
 	bool ControlPrinter::ConcreteConfigure( const ProcessingConfig& cfg )
 	{ 
+		RemoveOldControls();
 		CopyAsConcreteConfig( mConfig, cfg );
+		if (!mConfig.HasNumberOfInputs())
+			mConfig.SetNumberOfInputs(1.);
+		int nInputs = int(mConfig.GetNumberOfInputs());
+		if (nInputs < 1) nInputs = 1;
+		if (nInputs == 1) 
+		{
+			// preserve old port name 
+			std::list<std::string> names;
+			names.push_back("In Control");
+			mInControls.Resize(1, names, this);
+		} 
+		else 
+		{
+			// multi-port names share user-configured identifier
+			mInControls.Resize(nInputs,
+				mConfig.GetIdentifier(), this);
+		}
 		return true; 
 	}
 	
 	bool ControlPrinter::Do()
 	{
-		std::cout 	<< mConfig.GetIdentifier() 
-					<< ": " 
-					<< mInControl.GetLastValue()
-					<< std::endl;
+		std::string separator = "";
+		std::stringstream values;
+		for (int i = 0; i < mInControls.Size(); i++) 
+		{
+			values << separator << mInControls[i].GetLastValue();
+			separator = ", ";
+		}
+		std::cout << mConfig.GetIdentifier() << ": " 
+				<< values.str() << std::endl;
 		return true;
 	}
-		
 
-		
+	void ControlPrinter::RemoveOldControls()
+	{
+		mInControls.Clear();
+		GetInControls().Clear();
+	}
 }
