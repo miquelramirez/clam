cfaf0ec23bad231f7c6bdb510b5319d7f2bd3b9a
David García Garzón <vokimon@users.noreply.github.com>
2007-04-23 07:39:31 +0000

    * Doxygen for Filename and subclasses with examples

--- CLAM/src/Standard/Filename.hxx
+++ CLAM/src/Standard/Filename.hxx
@@ -32,15 +32,18 @@
 namespace CLAM {
 
 /**
- * This class represents a filename. 
- * You can do with it nearly whatever you can do with a string
- * but it has filename semantics in order to be diferentiated by
- * by type discriminators.
- * @todo Build a filename specific interface
+ * This class is a base class for configuration fields representing file names.
+ * By deriving from Text you have most of the std::string operations available
+ * and input serialization supporting spaces on reading.
+ * 
+ * Use InFilename and OutFilename subclasses to discriminate the serialization
+ * direction
+ * and subclass them to specify concrete filters and file type family names.
  */
 class Filename : public Text
 {
 public:
+		/// Item of a Filename filter definition
 		struct Filter {
 			const char * description;
 			const char * wildcard;
@@ -49,7 +52,14 @@ public:
 		Filename(const std::string & s) : Text(s) {} 
 		Filename(const char * s) : Text(s) {}
 		virtual ~Filename() {}
+
+		/// Rewrite this method to set a name for the type family name.
+		/// It is used to build messages such as 'Open the audio file'
+		/// where 'audio' is the type family.
 		virtual const char * TypeFamily() const { return ""; }
+		/// Rewrite this method to define the set of filters that subclasses
+		/// bounds to. See the examples on how to return it.
+		/// Last item should be {0,0} for class users to stop iterating.
 		virtual const Filter * Filters() const {
 			static const Filter filters[] = {
 				{0, 0}
@@ -58,6 +68,36 @@ public:
 		}
 };
 
+/**
+ * Input file name configuration field.
+ *
+ * Common operations on std::string are available to this field.
+ * Specialize this class for concrete input file formats
+ * by redefining Filename::Filter and Filename::TypeFamily.
+ *
+@code
+class InImageFilename : public InFilename
+{
+public:
+	InImageFilename(const std::string & s="") : InFilename(s) {}
+	InImageFilename(const char * s) : InFilename(s) {}
+	virtual const char * TypeFamily() const { return "Image"; }
+	virtual const Filter * Filters() const
+	{
+		static const Filter filters[] = {
+			{"Portable Network Graphics", "*.png"},
+			{"JPEG Image", "*.jpg"},
+			{"Scalable Vector Graphics", "*.svg"},
+			{0,0}
+		};
+		return filters;
+	}
+};
+CLAM_TYPEINFOGROUP(BasicCTypeInfo, InImageFilename);
+@endcode
+@see OutFilename
+ */
+
 class InFilename : public Filename
 {
 public:
@@ -65,13 +105,41 @@ public:
 		InFilename(const char * s) : Filename(s) {}
 };
 
+/**
+ * Output file name configuration field.
+ *
+ * Common operations on std::string are available to this field.
+ * Specialize this class for concrete input file formats
+ * by redefining Filename::Filter and Filename::TypeFamily.
+ *
+@code
+class OutImageFilename : public OutFilename
+{
+public:
+	OutImageFilename(const std::string & s="") : OutFilename(s) {}
+	OutImageFilename(const char * s) : OutFilename(s) {}
+	virtual const char * TypeFamily() const { return "Image"; }
+	virtual const Filter * Filters() const
+	{
+		static const Filter filters[] = {
+			{"Portable Network Graphics", "*.png"},
+			{"JPEG Image", "*.jpg"},
+			{0,0}
+		};
+		return filters;
+	}
+};
+CLAM_TYPEINFOGROUP(BasicCTypeOutfo, OutImageFilename);
+@endcode
+@see InFilename
+ */
+
 class OutFilename : public Filename
 {
 public:
 		OutFilename(const std::string & s="") : Filename(s) {} 
 		OutFilename(const char * s) : Filename(s) {}
 };
-
 
 CLAM_TYPEINFOGROUP(BasicCTypeInfo, Filename);
 CLAM_TYPEINFOGROUP(BasicCTypeInfo, InFilename);
