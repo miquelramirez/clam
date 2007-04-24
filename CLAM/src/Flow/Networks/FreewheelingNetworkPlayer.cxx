da92f1c3d5116269474cecf0b2cce78c9a7403a7
David García Garzón <vokimon@users.noreply.github.com>
2007-04-24 09:25:28 +0000

    * FreewheelingNetworkPlayer: Arbitrary numbers of files assigned first to inputs then to outputs  * OfflinePlayer: Arbitrary numbers of files assigned first to inputs then to outputs

--- CLAM/src/Flow/Networks/FreewheelingNetworkPlayer.cxx
+++ CLAM/src/Flow/Networks/FreewheelingNetworkPlayer.cxx
@@ -10,11 +10,15 @@ FreewheelingNetworkPlayer::FreewheelingNetworkPlayer()
 }
 bool FreewheelingNetworkPlayer::IsWorking() const
 {
-	return true;
+	return mFilenames.size()!=GetAudioSinks().size()+GetAudioSources().size();
 }
 std::string FreewheelingNetworkPlayer::NonWorkingReason() const
 {
-	return "Still not implemented";
+	std::stringstream ss;
+	ss << GetAudioSources().size() << " inputs and " 
+		<< GetAudioSinks().size() << " outputs needed but just " 
+		<< mFilenames.size() << " files provided" << std::ends;
+	return ss.str();
 }
 void FreewheelingNetworkPlayer::Start()
 {
@@ -31,48 +35,65 @@ void FreewheelingNetworkPlayer::Start()
 		std::cerr << "Warning: FreewheelingNetworkPlayer is going to use only one AudioSink. "
 		<< "Network have " << GetAudioSinks().size() << std::endl;
 	}
-	AudioSource& source = *GetAudioSources().front();
-	AudioSink& sink = *GetAudioSinks().front();
-
-	// configure file reader and writer
-	int frameSize = 512;
-
-	MonoAudioFileReader fileReader;
-	fileReader.GetOutPort("Samples Read").SetSize( frameSize );
-	fileReader.GetOutPort("Samples Read").SetHop( frameSize );
+	const int frameSize = 512;
+	unsigned filename=0;
+	std::vector<Audio> audioBuffers(mFilenames.size());
+	std::vector<MonoAudioFileReader*> readers;
 	MonoAudioFileReaderConfig readercfg;
-	readercfg.SetSourceFile(mInFilename);
-	fileReader.Configure( readercfg );
-
-	MonoAudioFileWriterConfig writercfg;
-	writercfg.SetTargetFile(mOutFilename);
-	writercfg.SetSampleRate(fileReader.GetAudioFile().GetHeader().GetSampleRate());
-	MonoAudioFileWriter fileWriter;
-	fileWriter.Configure( writercfg );
-	
-	// start processings. do now worry about network processings since network is calling us.
-	fileReader.Start();
-	fileWriter.Start();
-	
-	// start freewheeling!
-	Audio incomingAudio;
-	Audio outgoingAudio;
-	//incomingAudio.SetSampleRate( file.GetHeader().GetSampleRate() );
-	incomingAudio.SetSize( frameSize );
-	outgoingAudio.SetSize( frameSize );
-	
-	source.SetExternalBuffer( &(incomingAudio.GetBuffer()[0]) ,frameSize);
-	sink.SetExternalBuffer( &(outgoingAudio.GetBuffer()[0]) ,frameSize); 
-	while ( fileReader.Do(incomingAudio) )
+	for (unsigned i=0; i<GetAudioSources().size(); i++)
 	{
-		GetNetwork().Do();
-		fileWriter.Do( outgoingAudio );
+		CLAM_ASSERT(filename<mFilenames.size(),
+			"Not all the network inputs could be fullfiled. Have you checked the IsWorking() method?");
+		AudioSource& source = *GetAudioSources()[i];
+		readercfg.SetSourceFile(mFilenames[filename]);
+		MonoAudioFileReader * fileReader = new MonoAudioFileReader(readercfg);
+		fileReader->GetOutPort("Samples Read").SetSize( frameSize );
+		fileReader->GetOutPort("Samples Read").SetHop( frameSize );
+		readers.push_back(fileReader);
+		audioBuffers[filename].SetSize( frameSize );
+		source.SetExternalBuffer( &(audioBuffers[filename].GetBuffer()[0]) ,frameSize);
+		std::cout << " In: " << mFilenames[filename] << std::endl;
+		filename++;
+	}
+	std::vector<MonoAudioFileWriter*> writers;
+	MonoAudioFileWriterConfig writercfg;
+	for (unsigned i=0; i<GetAudioSinks().size(); i++)
+	{
+		if (filename>=mFilenames.size())
+		{
+			std::cerr <<
+			"Not all the network outputs could be fullfiled.";
+			break;
+		}
+		AudioSink& sink = *GetAudioSinks()[i];
+		writercfg.SetTargetFile(mFilenames[filename]);
+		MonoAudioFileWriter * fileWriter = new MonoAudioFileWriter(writercfg);
+		writers.push_back(fileWriter);
+		audioBuffers[filename].SetSize( frameSize );
+		sink.SetExternalBuffer( &(audioBuffers[filename].GetBuffer()[0]) ,frameSize);
+		std::cout << " Out: " << mFilenames[filename] << std::endl;
+		filename++;
 	}
 	
-	// close files (stop processings)
-	fileReader.Stop();
-	fileWriter.Stop();
-	
+	for (unsigned i = 0; i<readers.size(); i++)
+		readers[i]->Start();
+	for (unsigned i = 0; i<writers.size(); i++)
+		writers[i]->Start();
+	while ( true )
+	{
+		unsigned audioIndex =0;
+		bool end=false;
+		for (unsigned i = 0; i<readers.size(); i++)
+			end |= !readers[i]->Do(audioBuffers[audioIndex++]);
+		if (end) break;
+		GetNetwork().Do();
+		for (unsigned i = 0; i<writers.size(); i++)
+			writers[i]->Do(audioBuffers[audioIndex++]);
+	}
+	for (unsigned i = 0; i<readers.size(); i++)
+		readers[i]->Stop();
+	for (unsigned i = 0; i<writers.size(); i++)
+		writers[i]->Stop();
 }
 void FreewheelingNetworkPlayer::Stop()
 {
@@ -89,10 +110,12 @@ void FreewheelingNetworkPlayer::ProcessInputFile()
 void FreewheelingNetworkPlayer::InputFile( const std::string& filename )
 {
 	mInFilename = filename;
+	mFilenames.push_back(filename);
 }
 void FreewheelingNetworkPlayer::OutputFile( const std::string& filename )
 {
 	mOutFilename = filename;
+	mFilenames.push_back(filename);
 }
 
 } //namespace
