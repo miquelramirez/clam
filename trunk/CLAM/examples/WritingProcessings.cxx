f8451fcee66d377c21a6bd0cb97eb868858acd72
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2007-05-16 12:11:03 +0000

    * Changes in Processing base class  ** Implement virtual ProcessingConfig& GetConfigure() const at the     base class, returning a static NullProcessingConfig. Before it was     virtual pure.  ** Added Processing::Config typedef defined as NullProcessingConfig     so processing writers can use Configure( Config() ); in their ctr.  ** Added GetExecStateString() method for debugging  * examples/WritingProcessings.cxx shows an example of the most simple    processing

--- trunk/CLAM/examples/WritingProcessings.cxx
+++ trunk/CLAM/examples/WritingProcessings.cxx
@@ -1,37 +1,63 @@
-#include <iostream>
-#include "Processing.hxx"
 
-// TODOS
-// Do() is void
-// GetConfig not mandatory
-// ConcreteConfig not mandatoru
+#ifndef MyProcessing_hxx
+#define MyProcessing_hxx
 
-class SimpleProcessing : public CLAM::Processing
-{
-	bool Do() 
+#include <CLAM/InPort.hxx>
+#include <CLAM/OutPort.hxx>
+#include <CLAM/Processing.hxx>
+#include <CLAM/NullProcessingConfig.hxx>
+
+// Those two configuration types could be the ones you choose.
+// Your own ones or any type already in CLAM.
+//#include "MyInputDataType.hxx"
+//#include "MyOutputDataType.hxx"
+typedef float MyInputDataType;
+typedef float MyOutputDataType;
+
+class MyProcessing : public CLAM::Processing
+{ 
+	CLAM::InPort<MyInputDataType> mIn;
+	CLAM::OutPort<MyOutputDataType> mOut;
+public:
+	const char* GetClassName() const { return "MyProcessing"; }
+	MyProcessing() 
+		: mIn("My Input", this)
+		, mOut("My Output", this) 
 	{
-		return true;
+		Configure( Config() );
 	}
-	const char* GetClassName() const
+ 
+	~MyProcessing() {}
+
+	// Configuration to the minimal expression
+	bool ConcreteConfigure(const CLAM::ProcessingConfig&)
 	{
-		return "--";
+		return true; // I got proper configuration
 	}
-	const CLAM::ProcessingConfig& GetConfig() const
+	
+	bool Do()
 	{
-		CLAM_ASSERT(false, "");
-	}
-	bool ConcreteConfigure (const CLAM::ProcessingConfig& )
-	{
-		return true;
+		bool result = Do(mIn.GetData(), mOut.GetData()); 
+		// Tell the ports this is done
+		mIn.Consume();
+		mOut.Produce();
+		return result;
 	}
 
+	bool Do(const MyInputDataType& in, MyOutputDataType& out)
+	{
+		// Your implementation
+	}
 };
 
 int main()
 {
-	SimpleProcessing simple;
-
+//	CLAM_DEBUG_ASSERT(false, "debug---");
+	MyProcessing proc;
+	std::cout << "State: "<< proc.GetExecStateString() << std::endl;
+	std::cout << "Config error message: " << proc.GetConfigErrorMessage() << std::endl;
+//	proc.Do();
 	std::cout << "Done." << std::endl;
 	return 0;
 }
-
+#endif // MyProcessing_hxx
