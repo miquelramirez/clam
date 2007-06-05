25b3505c85cc789fd91f305b781b235375f790a1
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2007-06-05 17:45:44 +0000

    * Factory refactoring step 1 of 2  * Factory now have a second registry based on Creator (base) classes instead of methods  * Factory now have duplicated interface to test this second registry  * FactoryTests uses both registries  * Pairprogrammed with Andreas

--- CLAM/test/UnitTests/FactoryTest/FactoryTest.cxx
+++ CLAM/test/UnitTests/FactoryTest/FactoryTest.cxx
@@ -18,11 +18,17 @@ class FactoryTest : public CppUnit::TestFixture
 {
 	CPPUNIT_TEST_SUITE( FactoryTest );
 	CPPUNIT_TEST( testCreateFooReturnsAFoo );
+	CPPUNIT_TEST( NewtestCreateFooReturnsAFoo );
 	CPPUNIT_TEST( testCreate_ReturnsAFoo );
+	CPPUNIT_TEST( NewtestCreate_ReturnsAFoo );
 	CPPUNIT_TEST( testCreateSafe_WithABadKey );
+	CPPUNIT_TEST( NewtestCreateSafe_WithABadKey );
 	CPPUNIT_TEST( testAddCreator_WithRepeatedKey );
+	CPPUNIT_TEST( NewtestAddCreator_WithRepeatedKey );
 	CPPUNIT_TEST( testAddCreatorSafe_WithRepeatedKey );
+	CPPUNIT_TEST( NewtestAddCreatorSafe_WithRepeatedKey );
 	CPPUNIT_TEST( testGetRegisteredNames_WithNoKeys );
+	CPPUNIT_TEST( NewtestGetRegisteredNames_WithNoKeys );
 
 	CPPUNIT_TEST_SUITE_END();
 
@@ -49,12 +55,37 @@ public:
 	MyFactoryType::CreatorMethod BarCreator() {
 		return MyFactoryType::Registrator<DummyProductBar>::Create;
 	}
+
+	class NewFooCreator : public MyFactoryType::Creator
+	{
+	public:
+		DummyProduct *Create()
+		{	
+			return new DummyProductFoo();
+		}
+
+	};
+	class NewBarCreator : public MyFactoryType::Creator
+	{
+	public:
+		DummyProduct *Create()
+		{	
+			return new DummyProductBar();
+		}
+
+	};
 	// Tests definition :
 protected:
 	void testCreateFooReturnsAFoo()
 	{
 		DummyProduct* returned = FooCreator()();
 
+		CLAMTEST_ASSERT_EQUAL_RTTYPES( DummyProductFoo, *returned );
+		delete returned;
+	}
+	void NewtestCreateFooReturnsAFoo()
+	{
+		DummyProduct* returned = NewFooCreator().Create();
 		CLAMTEST_ASSERT_EQUAL_RTTYPES( DummyProductFoo, *returned );
 		delete returned;
 	}
@@ -71,11 +102,29 @@ protected:
 		mTheFactory->Clear();
 
 	}
+	void NewtestCreate_ReturnsAFoo()
+	{
+		mTheFactory->NewAddCreator( "DummyProductFoo", new NewFooCreator() );
+
+		DummyProduct* returned = mTheFactory->NewCreate("DummyProductFoo");
+		CLAMTEST_ASSERT_EQUAL_RTTYPES( DummyProductFoo, *returned );
+
+		// tear down:
+		delete returned;
+		mTheFactory->Clear();
+	}
 
 	void testCreateSafe_WithABadKey()
 	{
 		try{
 			mTheFactory->CreateSafe("DummyProductFoo");
+			CPPUNIT_FAIL("Should throw an exception");
+		} catch ( CLAM::ErrFactory& ) {}
+	}
+	void NewtestCreateSafe_WithABadKey()
+	{
+		try{
+			mTheFactory->NewCreateSafe("XXX");
 			CPPUNIT_FAIL("Should throw an exception");
 		} catch ( CLAM::ErrFactory& ) {}
 	}
@@ -86,6 +135,15 @@ protected:
 		mTheFactory->AddCreator("DummyProductFoo", FooCreator() );
 		try{
 			mTheFactory->AddCreator("DummyProductFoo", FooCreator());
+			CPPUNIT_FAIL("an assertion should happen");
+		} catch ( CLAM::ErrAssertionFailed& )
+		{}
+	}
+	void NewtestAddCreator_WithRepeatedKey()
+	{
+		mTheFactory->NewAddCreator("DummyProductFoo", new NewFooCreator() );
+		try{
+			mTheFactory->NewAddCreator("DummyProductFoo", new NewFooCreator());
 			CPPUNIT_FAIL("an assertion should happen");
 		} catch ( CLAM::ErrAssertionFailed& )
 		{}
@@ -101,8 +159,26 @@ protected:
 
 		}
 	}
+	void NewtestAddCreatorSafe_WithRepeatedKey()
+	{
+		mTheFactory->NewAddCreator("DummyProductFoo", new NewBarCreator() );
+		try{
+			mTheFactory->NewAddCreatorSafe("DummyProductFoo", new NewFooCreator());
+			CPPUNIT_FAIL("an ErrFactory should be raised");
+		} catch (CLAM::ErrFactory&) {
+
+		}
+	}
 
 	void testGetRegisteredNames_WithNoKeys()
+	{
+		std::list<std::string> registeredNames;
+
+		mTheFactory->GetRegisteredNames( registeredNames );
+
+		CPPUNIT_ASSERT_EQUAL( true, registeredNames.empty() );
+	}
+	void NewtestGetRegisteredNames_WithNoKeys()
 	{
 		std::list<std::string> registeredNames;
 
@@ -125,8 +201,11 @@ class FactorySingletonTest : public FactoryTest
 {
 	CPPUNIT_TEST_SUITE( FactorySingletonTest );
 	CPPUNIT_TEST( testCreateFooReturnsAFoo );
+	CPPUNIT_TEST( NewtestCreateFooReturnsAFoo );
 	CPPUNIT_TEST( testCreate_ReturnsAFoo );
+	CPPUNIT_TEST( NewtestCreate_ReturnsAFoo );
 	CPPUNIT_TEST( testCreateSafe_WithABadKey );
+	CPPUNIT_TEST( NewtestCreateSafe_WithABadKey );
 	CPPUNIT_TEST( testFactoryIsSingleton );
 
 	CPPUNIT_TEST_SUITE_END();
