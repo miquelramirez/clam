3908356c958d59f6f8767d06e5b7259b1670716c
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2007-06-18 11:44:59 +0000

    * added doxygen comment on Audio::SetSize  * OfflineAudioConvolution example almost finished

--- trunk/CLAM/examples/OfflineAudioConvolution.cxx
+++ trunk/CLAM/examples/OfflineAudioConvolution.cxx
@@ -1,29 +1,79 @@
 #include <iostream>
 #include <CLAM/Audio.hxx>
 #include <CLAM/MonoAudioFileReader.hxx>
+#include <CLAM/MonoAudioFileWriter.hxx>
+int error(const std::string & msg)
+{
+	std::cerr << msg << std::endl;
+	return -1;
+}
+
 int main()
 {
-	std::string impulseResponseFile = "/home/parumi/acustica-bm/Carnel_3P.wav";
-	CLAM::MonoAudioFileReaderConfig config;
-	config.SetSourceFile(impulseResponseFile);
+	const std::string impulseResponseFile = "/home/parumi/acustica-bm/Carnel_3P.wav";
+	const std::string inputFile = "/home/parumi/acustica-bm/InLanguageAnecoic-cut.wav";
+	const std::string reverberedFile = "out.wav"; // the output file
 
-	CLAM::MonoAudioFileReader reader(config);
-	if (!reader.IsConfigured()) 
-	{
-		std::cout << reader.GetConfigErrorMessage() << std::endl;
-		return -1;
-	}
+	CLAM::MonoAudioFileReaderConfig readerConfig;
+	readerConfig.SetSourceFile(impulseResponseFile);
+
+	CLAM::MonoAudioFileReader reader(readerConfig);
+	if (!reader.IsConfigured()) return error(reader.GetConfigErrorMessage());
 	reader.Start();
-
-	long int nSamples = reader.GetHeader().GetSamples();
-	std::cout << "samples of file: "<< impulseResponseFile << " " << nSamples << std::endl;
-
+	long impulseSamples = reader.GetHeader().GetSamples();
 	CLAM::Audio impulseResponse;
+	impulseResponse.SetSize(impulseSamples);
+	reader.Do( impulseResponse );
+	reader.Stop();
+
+	readerConfig.SetSourceFile(inputFile);
+	reader.Configure(readerConfig);
+	if (!reader.IsConfigured()) return error(reader.GetConfigErrorMessage());
+	reader.Start();
+	long inputSamples = reader.GetHeader().GetSamples();
 	CLAM::Audio input;
+	input.SetSize(inputSamples);
+	reader.Do( input );
+	reader.Stop();
+
+
+
+//	long int inputSamples = reader.GetHeader().GetSamples();
+
+	long reverberedSize = impulseResponse.GetSize()+input.GetSize();
 	CLAM::Audio reverbered;
-	impulseResponse.SetSize(1000);
-	input.SetSize(400000);
-	reverbered.SetSize( impulseResponse.GetSize()+input.GetSize() );
-	std::cout << "hello" << std::endl;
+	reverbered.SetSize( reverberedSize );
+	
+	CLAM::TData * inputArray = &input.GetBuffer()[0];
+	CLAM::TData * impulseArray = &impulseResponse.GetBuffer()[0];
+	CLAM::TData * reverberedArray = &reverbered.GetBuffer()[0];
+	unsigned frameSamples=0;
+	for (long i=0; i<inputSamples; i++)
+	{
+		frameSamples++;
+		frameSamples%=1000;
+		if (!frameSamples)
+			std::cout << "." << std::flush;
+		for (long j=0; j<impulseSamples; j++)
+			reverberedArray[i+j] += inputArray[i]*impulseArray[j];
+	}
+	std::cout << std::endl << "Done." << std::endl;
+/*
+	for (long i=0; i<impulseSamples; i++)
+		std::cout << impulseResponse.GetBuffer()[i] << " ";
+	for (long i=0; i<inputSamples; i++)
+		std::cout << input.GetBuffer()[i] << " ";
+	for (long i=0; i<reverberedSize; i++)
+		std::cout << reverbered.GetBuffer()[i] << " ";
+*/
+	CLAM::MonoAudioFileWriterConfig writerConfig;
+	writerConfig.SetTargetFile( reverberedFile );
+	CLAM::MonoAudioFileWriter writer(writerConfig);
+	if (!writer.IsConfigured()) return error(writer.GetConfigErrorMessage());
+	writer.Start();
+	writer.Do( reverbered );
+	writer.Stop();
+
+
 	return 0;
 }
