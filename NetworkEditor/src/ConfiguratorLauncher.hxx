9fb67ced959a645b71bac2ad3cd12bcaf633c1ac
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2007-06-20 14:24:22 +0000

    * ConfiguratorLaunchers now have its own Factory subclass  * ConfiguratorLauncher file refactoring  * Factory.hxx have been documented with example code

--- NetworkEditor/src/ConfiguratorLauncher.hxx
+++ NetworkEditor/src/ConfiguratorLauncher.hxx
@@ -1,9 +1,31 @@
+/*
+ * Copyright (c) 2001-2003 MUSIC TECHNOLOGY GROUP (MTG)
+ *                         UNIVERSITAT POMPEU FABRA
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
 #ifndef ConfiguratorLauncher_hxx
 #define ConfiguratorLauncher_hxx
 
 #include "Qt4Configurator.hxx"
 #include <CLAM/Processing.hxx>
 #include <QtCore/QString>
+#include <CLAM/Factory.hxx>
 
 class ConfiguratorLauncher : public CLAM::Qt4Configurator // Inherits just to access protected
 {
@@ -58,6 +80,21 @@ public:
 	
 };
 
+
+
+class ConfiguratorLauncherFactory : public CLAM::Factory<ConfiguratorLauncher>
+{
+public:
+	static ConfiguratorLauncherFactory& GetInstance();
+};
+
+// Convenient macros (Not to use widely)
+#define STANDARD_PROCESSING_CONFIG_REGISTER(configName) \
+   	static CLAM::FactoryRegistrator<ConfiguratorLauncherFactory, TypedConfiguratorLauncher< CLAM::configName > > \
+		 reg##configName(#configName)
+#define SPECIAL_PROCESSING_CONFIG_REGISTER(configName, configurator) \
+   	static CLAM::FactoryRegistrator<ConfiguratorLauncherFactory, WidgetTypedConfiguratorLauncher<configurator> > \
+		 regt##configName( #configName)
 
 
 
