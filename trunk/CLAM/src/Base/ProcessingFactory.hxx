cfbc31bcfaf9542804ab93fdfd75a35f8a8ee38d
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2007-06-27 16:04:51 +0000

    * First steps to enrich ProcessingFactory  ** PF::GetListOfKeys  ** pair-programmed with Andreas

--- trunk/CLAM/src/Base/ProcessingFactory.hxx
+++ trunk/CLAM/src/Base/ProcessingFactory.hxx
@@ -10,7 +10,12 @@ namespace CLAM
 class ProcessingFactory : public Factory<Processing>
 {
 public:
+	typedef RegistryKey Key;
+	typedef std::list<Key> Keys;
+	typedef std::list<std::string> Values;
 	static ProcessingFactory& GetInstance();
+	Keys GetListOfKeys(const std::string& attribute, const std::string& value);
+	Values GetValuesFor(const std::string&); //TODO Unique? Different values?
 };
 
 } //namespace CLAM
