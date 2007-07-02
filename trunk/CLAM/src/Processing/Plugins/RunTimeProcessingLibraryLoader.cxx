9e0bb98c96b47f87bc2011c0f6f88a283816df39
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2007-07-02 14:47:59 +0000

    * CLAM Plugins  ** Looking at a set of default paths (should look at the prefix instead)  ** Looking at $HOME/.clam/lib  ** Looking at colon(posix)/semicolon(windows) separated paths in CLAM_PLUGIN_PATH  ** Added some windows code (not even compiled)

--- trunk/CLAM/src/Processing/Plugins/RunTimeProcessingLibraryLoader.cxx
+++ trunk/CLAM/src/Processing/Plugins/RunTimeProcessingLibraryLoader.cxx
@@ -2,34 +2,113 @@
 #include <dirent.h>
 #include <iostream>
 #include <string>
+#include <vector>
 
 class RunTimeProcessingLibraryLoader
 {
-public:
-	RunTimeProcessingLibraryLoader(const std::string & directory="libs")
+
+	static const char * pathSeparator;
+
+	void * FullyLoadLibrary(const std::string & libraryPath)
 	{
-		DIR* dir = opendir(directory.c_str());
+#ifdef WIN32
+		SetErrorMode( SEM_FAILCRITICALERRORS | SEM_NOALIGNMENTFAULTEXCEPT |
+			SEM_NOGPFAULTERRORBOX | SEM_NOOPENFILEERRORBOX );
+		return LoadLibrary(libraryPath.c_str());
+#else
+		return dlopen( libraryPath.c_str(), RTLD_NOW);	
+#endif
+	}
+	std::string LibraryLoadError()
+	{
+#ifdef WIN32
+		LPVOID lpMsgBuf;
+		FormatMessage(
+			FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+			NULL,
+			GetLastError(),
+			MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+			(LPTSTR) &lpMsgBuf,
+			0, NULL );
+		std::string message(lpMsgBuf);
+		LocalFree(lpMsgBuf);
+#else
+		return dlerror();
+#endif
+	}
+
+	void loadLibrariesFromPath(const std::string & path)
+	{
+		std::cerr << "[Plugins] Looking at path '" << path << "'" << std::endl;
+		DIR* dir = opendir(path.c_str());
 		if (!dir)
 		{
-			std::cout << "[Plugins] warning: could not open CLAM plugins dir: " << directory << std::endl;
+			std::cerr << "[Plugins] warning: could not open CLAM plugins dir: " << path << std::endl;
 			return;
 		}
-		struct dirent * dirEntry;
-		while ( (dirEntry = readdir(dir)) )
+		while ( struct dirent * dirEntry = readdir(dir) )
 		{
 			std::string pluginFilename(dirEntry->d_name);
 			if(pluginFilename == "." || pluginFilename == "..")
 				continue;
-			std::string pluginFullFilename(directory + std::string("/") + pluginFilename);
-			std::cout << "[Plugins] Loading CLAM plugin: " << pluginFullFilename << std::endl;
-			void* handle = dlopen( pluginFullFilename.c_str(), RTLD_NOW);
-			if (handle) continue; // Ok
-			std::cout << "[Plugins] warning: failed to load CLAM plugin: " << pluginFullFilename << std::endl;
-			
+			std::string pluginFullFilename(path + std::string("/") + pluginFilename);
+			std::cout << "[Plugins] Loading: " << pluginFullFilename << std::endl;
+			void* handle = FullyLoadLibrary( pluginFullFilename );
+			if (!handle)
+			{
+				std::cerr << "[Plugins] Error: " << LibraryLoadError() << std::endl;
+				continue;
+			}
 		}
+	}
+public:
+	std::vector<std::string> splitPathVariable(const std::string & pathVariable)
+	{
+		std::string content=pathVariable;
+		std::vector<std::string> result;
+		while (!content.empty())
+		{
+			std::cout << "Splitting " << content << std::endl;
+			size_t separatorPos = content.find(pathSeparator);
+			if (separatorPos == std::string::npos)
+			{
+				result.push_back(content);
+				break;
+			} 
+			result.push_back(content.substr(0, separatorPos));
+			content = content.substr(separatorPos+1);
+		}
+		return result;
+	}
+	RunTimeProcessingLibraryLoader()
+	{
+		const char * standardPaths[] = 
+		{
+			"/usr/local/lib/clam",
+			"/usr/lib/clam",
+			"/opt/lib/clam",
+			0
+		};
+		const char * envPath = getenv("CLAM_PLUGIN_PATH");
+		const char * envHome = getenv("HOME");
+		std::string path = envPath ? envPath : "";
+		if (envHome) 
+			path += std::string(path.empty()? "":pathSeparator) + envHome + "/.ladspa";
+		for (const char ** standardPath=standardPaths; *standardPath; standardPath++)
+			path += std::string(path.empty()? "":pathSeparator) + *standardPath;
+		std::vector <std::string> environmentPaths = splitPathVariable(path);
+		for (unsigned i=0; i<environmentPaths.size(); i++)
+			loadLibrariesFromPath(environmentPaths[i]);
 	}
 };
 
-static RunTimeProcessingLibraryLoader dllLoader("/usr/lib/clam/");
+const char * RunTimeProcessingLibraryLoader::pathSeparator = 
+	#ifdef WIN32
+		";";
+	#else
+		":";
+	#endif
+
+static RunTimeProcessingLibraryLoader dllLoader;
 
 
