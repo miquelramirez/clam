ceccd9c8527a6d002b7d8f1444bd3c624db281bb
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2007-07-06 15:14:26 +0000

    * Now: reverted ProcessingFactory.*xx to rev 10326 to apply andreas patch

--- CLAM/src/Base/ProcessingFactory.cxx
+++ CLAM/src/Base/ProcessingFactory.cxx
@@ -15,57 +15,13 @@ ProcessingFactory& ProcessingFactory::GetInstance()
 ProcessingFactory::Keys ProcessingFactory::GetListOfKeys(const std::string& attribute, const std::string& value)
 {
 	Keys result;
-	ProcessingAttributes::const_iterator it;
-	for(it = mProcessingAttributes.begin(); it != mProcessingAttributes.end(); it++)
-	{
-		Values pAttributes = it->second;
-		Values::const_iterator itAtt;
-		for(itAtt = pAttributes.begin(); itAtt != pAttributes.end(); itAtt++)
-		{
-			std::string _attribute, _value;
-			if((*itAtt).find(":") != std::string::npos) // check for integrity of string
-			{
-				_attribute = 	(*itAtt).substr(0, (*itAtt).find(":"));
-				_value = (*itAtt).substr((*itAtt).find(":")+1);
-				if( (_attribute == attribute) && (_value == value))
-				{
-					result.push_back(it->first);
-				}
-			}
-		}
-	}
+	result.push_back("lala");
 	return result;
 }
-ProcessingFactory::Values ProcessingFactory::GetValuesFor(const std::string& key)
+ProcessingFactory::Values ProcessingFactory::GetValuesFor(const std::string&)
 {
-	Values values;
-	ProcessingAttributes::const_iterator it = mProcessingAttributes.find(key);
-	if(it!=mProcessingAttributes.end())
-	{
-		values = it->second;
-	}
-	return values;
-}
-void ProcessingFactory::AddAttribute(const std::string& key, const std::string& attribute, const std::string& value)
-{
-	Values mValue;
-	ProcessingAttributes::const_iterator it = mProcessingAttributes.find(key);
-	// TODO check if the factory has the key
-	if(it == mProcessingAttributes.end()) // first check if it does not exist
-	{
-		mValue.push_back(std::string(attribute + ":" + value));
-		mProcessingAttributes.insert( ProcessingAttributes::value_type( key, mValue ) );
-	} // if exists, add a new attribute
-}
-
-void ProcessingFactory::ProcessingClear()
-{
-	mProcessingAttributes.clear();
-}
-
-std::size_t ProcessingFactory::ProcessingCount()
-{
-	return mProcessingAttributes.size();
+	Values result;
+	return result;
 }
 } //namespace CLAM
 
--- CLAM/src/Base/ProcessingFactory.hxx
+++ CLAM/src/Base/ProcessingFactory.hxx
@@ -12,15 +12,10 @@ class ProcessingFactory : public Factory<Processing>
 public:
 	typedef RegistryKey Key;
 	typedef std::list<Key> Keys;
-	typedef std::list<std::string> Values; //pau: TODO poposal: std::list<Pair> Pairs; where Pair is a (attr,value) struct
-	typedef std::map<Key, Values> ProcessingAttributes;
-	ProcessingAttributes mProcessingAttributes; //pau: TODO don't like the name. how about mMetadata?
+	typedef std::list<std::string> Values;
 	static ProcessingFactory& GetInstance();
-	void AddAttribute(const std::string& key, const std::string& attribute, const std::string& value);
 	Keys GetListOfKeys(const std::string& attribute, const std::string& value);
-	Values GetValuesFor(const std::string& key); //TODO name: UniqueValues? DifferentValues? SetOfValues. pau: I like GetSetOfValues()
-	void ProcessingClear(); //pau: TODO remove it till need it
-	std::size_t ProcessingCount(); //pau: idem
+	Values GetValuesFor(const std::string&); //TODO Unique? Different values?
 };
 
 } //namespace CLAM
