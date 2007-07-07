be21d7228e95a3731538aefef2ab2f9501e61f63
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2007-07-07 21:02:51 +0000

    * New Andreas patch to fix my spoiled last commit

--- CLAM/test/UnitTests/FactoryTest/ProcessingFactoryTest.cxx
+++ CLAM/test/UnitTests/FactoryTest/ProcessingFactoryTest.cxx
@@ -17,12 +17,10 @@ class ProcessingFactoryTest : public CppUnit::TestFixture
 	CPPUNIT_TEST( testGetListOfKeys_twoElements );
 	CPPUNIT_TEST( testGetValuesFor_empty );
 	CPPUNIT_TEST( testGetValuesFor_twoElements );
-	CPPUNIT_TEST( testAddAttribute_nonExistingKey );
-	CPPUNIT_TEST( testGetValueFor_nonExistingKey );
-	CPPUNIT_TEST( testGetValueFor_nonExistingAttribute );
-	CPPUNIT_TEST( testGetValueFor_existingAttribute );
-
-
+	CPPUNIT_TEST( testGetValuesFrom_empty );
+	CPPUNIT_TEST( testGetValuesFrom_twoElements );
+	CPPUNIT_TEST( testGetValuesOf_empty );
+	CPPUNIT_TEST( testGetValuesOf_twoElements );
 	CPPUNIT_TEST_SUITE_END();
 
 protected:
@@ -34,12 +32,14 @@ protected:
 		}
 	};
 
+	CLAM::ProcessingFactory factory;
 public:
 	void setUp()
 	{
 	}
 	void tearDown()
 	{
+		factory.Clear();
 	}
 
 	// Tests definition :
@@ -47,91 +47,62 @@ protected:
 
 	void testGetListOfKeys_empty()
 	{
-		CLAM::ProcessingFactory factory;
 		CLAM::ProcessingFactory::Keys result = factory.GetListOfKeys("category","");
 		CPPUNIT_ASSERT_EQUAL(size_t(0), result.size() );
 	}
 
 	void testGetListOfKeys_twoElements()
 	{
-		CLAM::ProcessingFactory factory;
-		factory.AddCreator("first", new DummyCreator() );
-		factory.AddCreator("second", new DummyCreator() );
+		//factory.AddCreator("the key", new DummyCreator());
 		factory.AddAttribute("first", "category", "spectral");
 		factory.AddAttribute("second", "category", "time-domain");
 		CLAM::ProcessingFactory::Keys result = factory.GetListOfKeys("category", "spectral");
 		CPPUNIT_ASSERT_EQUAL(size_t(1), result.size() );
 		CPPUNIT_ASSERT_EQUAL(std::string("first"), result.front() );
 	}
-
 	void testGetValuesFor_empty()
 	{
-		CLAM::ProcessingFactory factory;
 		CLAM::ProcessingFactory::Values result = factory.GetValuesFor("non-existing");
 		CPPUNIT_ASSERT_EQUAL(size_t(0), result.size());
 	}
-
 	void testGetValuesFor_twoElements()
 	{
-		CLAM::ProcessingFactory factory;
-		factory.AddCreator("first", new DummyCreator() );
-		factory.AddCreator("second", new DummyCreator() );
 		factory.AddAttribute("first", "category", "spectral");
 		factory.AddAttribute("second", "category", "time-domain");
-		CLAM::ProcessingFactory::Values values = factory.GetValuesFor("category");
-		CPPUNIT_ASSERT_EQUAL(size_t(2), values.size());
-		CPPUNIT_ASSERT_EQUAL(std::string("spectral"), *values.begin());
-		CPPUNIT_ASSERT_EQUAL(std::string("time-domain"), *values.end());
-	}	
-
-/*	void testAddAttribute_nonExistingKey()
-	{
-		CLAM::ProcessingFactory factory;
-		try
-		{
-			factory.AddAttribute("non-existing-key", "attr");
-		} 
-		catch (CLAM::AssertFailedErr& e)
-		{
-			CPPUNIT_ASSERT_EQUAL(
-				std::string("ProcessingFactory::AddAttribute: Adding an attribute with an non existing key "), 
-				std::string(e.what())
-				);
-		}
+		CLAM::ProcessingFactory::Values result = factory.GetValuesFor("category");
+		CPPUNIT_ASSERT_EQUAL(size_t(2), result.size());
+		CPPUNIT_ASSERT_EQUAL(std::string("spectral"), result.front());
+		CPPUNIT_ASSERT_EQUAL(std::string("time-domain"), result.back());
 	}
-	void testGetValueFor_nonExistingKey()
+	void testGetValuesFrom_empty()
 	{
-		CLAM::ProcessingFactory factory;
-		try
-		{
-			factory.GetValueFor("non-existing-key", "attr");
-		} 
-		catch (CLAM::AssertFailedErr& e)
-		{
-			CPPUNIT_ASSERT_EQUAL(
-				std::string("ProcessingFactory::GetValueFor: Retrieving a value of an non existing key "), 
-				std::string(e.what())
-				);
-		}
+		CLAM::ProcessingFactory::Values result = factory.GetValuesFrom("key","category");
+		CPPUNIT_ASSERT_EQUAL(size_t(0), result.size());
 	}
-
-	void testGetValueFor_nonExistingAttribute()
+	void testGetValuesFrom_twoElements()
 	{
-		CLAM::ProcessingFactory factory;
-		factory.AddCreator("first", new DummyCreator());
 		factory.AddAttribute("first", "category", "spectral");
-		CPPUNIT_ASSERT_EQUAL( std::string("--undefined--"), factory.GetValueFor("first", "non existing attr") );
+		factory.AddAttribute("first", "category", "time-domain");
+		factory.AddAttribute("first", "description", "short description");
+		CLAM::ProcessingFactory::Values result = factory.GetValuesFrom("first","category");
+		CPPUNIT_ASSERT_EQUAL(size_t(2), result.size());
+		CPPUNIT_ASSERT_EQUAL(std::string("spectral"), result.front());
+		CPPUNIT_ASSERT_EQUAL(std::string("time-domain"), result.back());
 	}
-
-	void testGetValueFor_existingAttribute()
+	void testGetValuesOf_empty()
 	{
-		CLAM::ProcessingFactory factory;
-		factory.AddCreator("first", new DummyCreator());
-		factory.AddAttribute("first", "category", "spectral");
-		CPPUNIT_ASSERT_EQUAL( std::string("spectral"), factory.GetValueFor("category") );
-
+		CLAM::ProcessingFactory::Attributes result = factory.GetValuesOf("key");
+		CPPUNIT_ASSERT_EQUAL(size_t(0), result.size());
 	}
-*/
+	void testGetValuesOf_twoElements()
+	{
+		factory.AddAttribute("first", "category", "spectral");
+		factory.AddAttribute("first", "category", "time-domain");
+		CLAM::ProcessingFactory::Attributes result = factory.GetValuesOf("first");
+		CPPUNIT_ASSERT_EQUAL(size_t(2), result.size());
+		CPPUNIT_ASSERT_EQUAL(std::string("spectral"), result.front().value);
+		CPPUNIT_ASSERT_EQUAL(std::string("time-domain"), result.back().value);
+	}
 };
 
 
