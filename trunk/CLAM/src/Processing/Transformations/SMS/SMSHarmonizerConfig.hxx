9facc5f48f290f4d51c2368974ef8867a59414b7
hordiales <hordiales@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2007-07-11 20:17:17 +0000

    * Forgot added this two new files (SMSHarmonizerConfig) in my new https repo

---
+++ trunk/CLAM/src/Processing/Transformations/SMS/SMSHarmonizerConfig.hxx
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2004 MUSIC TECHNOLOGY GROUP (MTG)
+ *                         UNIVERSITAT POMPEU FABRA
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#ifndef __SMSHarmonizerConfig__
+#define __SMSHarmonizerConfig__
+
+#include "ProcessingConfig.hxx"
+
+namespace CLAM {
+
+/**
+* The Harmonizer configuration object.
+*/
+class SMSHarmonizerConfig : public ProcessingConfig
+{
+public:
+	DYNAMIC_TYPE_USING_INTERFACE( SMSHarmonizerConfig, 2, ProcessingConfig );
+	DYN_ATTRIBUTE (0, public, bool, IgnoreResidual);
+	DYN_ATTRIBUTE( 1, public, int, VoicesSize ); // FIXME TSize
+
+public:
+// 	void SetVoicesSize(TSize f);
+// 	TSize GetVoicesSize();
+// 	void SetIgnoreResidual(bool value);
+
+	void DefaultValues();
+
+protected:
+	void DefaultInit();
+};
+
+} // namespace CLAM
+
+#endif
+
