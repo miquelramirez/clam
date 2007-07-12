bacdba057c538203c54001f4c6fac36be4beb9c8
David García Garzón <vokimon@users.noreply.github.com>
2007-07-12 19:21:37 +0000

    * Welcome to our friend ComplexSpectrum

---
+++ CLAM/examples/PluginExample/ComplexSpectrum.hxx
@@ -0,0 +1,22 @@
+#ifndef ComplexSpectrum_hxx
+#define ComplexSpectrum_hxx
+
+#include <vector>
+#include <complex>
+
+namespace CLAM
+{
+
+/**
+ * Experimental light-weight spectrum
+ * Linear scale is implicit.
+ */
+struct ComplexSpectrum 
+{
+	std::vector<std::complex<TData> > bins;
+	TData spectralRange;
+};
+
+} // namespace CLAM
+
+#endif // ComplexSpectrum_hxx
