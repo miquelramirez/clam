203787421810f5308c9b136f2ac2da7723487560
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2007-07-12 19:59:09 +0000

    * Spike on simpler FFT based on simplified spectrums:  ** Renamed from FFT_fftw3 to MyFFT while the experiment is on  ** Not inheriting from FFT_base  ** Implementation is an inner class instead of a class in Hidden namespace  ** Inputs are audio buffers instead of an audio stream  ** Output are ComplexSpectrum instead regular Spectrum

--- trunk/CLAM/examples/PluginExample/MyFFT.cxx
+++ trunk/CLAM/examples/PluginExample/MyFFT.cxx
@@ -20,14 +20,10 @@
  */
 
 
-#include "FFT_fftw3.hxx"
+#include "MyFFT.hxx"
 
-#include "Assert.hxx"
-#include "Audio.hxx"
-#include "Spectrum.hxx"
-#include "SpectrumConfig.hxx"
-#include "CLAM_Math.hxx"
-#include "ProcessingFactory.hxx"
+#include <CLAM/CLAM_Math.hxx>
+#include <CLAM/ProcessingFactory.hxx>
 #include <fftw3.h>
 
 namespace CLAM 
@@ -35,115 +31,111 @@ namespace CLAM
 
 namespace Hidden
 {
-	static FactoryRegistrator<ProcessingFactory, FFT_fftw3> regFFT_fftw3("FFT_fftw3");
+	static FactoryRegistrator<ProcessingFactory, MyFFT> regMyFFT("MyFFT");
 }
 
-namespace Hidden
+struct MyFFT::Implementation
 {
-	struct FFT_fftw3_Implementation
+	Implementation(unsigned size)
 	{
-		FFT_fftw3_Implementation(unsigned size)
-		{
-			// Special malloc which aligns to SIMD segment boundaries
-			realInput = (double*) fftw_malloc(sizeof(double) * size);
-			complexOutput = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * size);
-			fftw_import_system_wisdom();
-			plan = fftw_plan_dft_r2c_1d(size, realInput, complexOutput, FFTW_ESTIMATE);
-		}
-		~FFT_fftw3_Implementation()
-		{
-			fftw_destroy_plan(plan);
-			fftw_free(realInput);
-			fftw_free(complexOutput);
-			fftw_forget_wisdom();	
-		}
-		double * realInput;
-		fftw_complex * complexOutput;
-		fftw_plan plan;
-	};
-}
+		// Special malloc which aligns to SIMD segment boundaries
+		realInput = (double*) fftw_malloc(sizeof(double) * size);
+		complexOutput = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * size);
+		fftw_import_system_wisdom();
+		plan = fftw_plan_dft_r2c_1d(size, realInput, complexOutput, FFTW_ESTIMATE);
+	}
+	~Implementation()
+	{
+		fftw_destroy_plan(plan);
+		fftw_free(realInput);
+		fftw_free(complexOutput);
+		fftw_forget_wisdom();	
+	}
+	double * realInput;
+	fftw_complex * complexOutput;
+	fftw_plan plan;
+};
 
-
-bool FFT_fftw3::ConcreteConfigure(const ProcessingConfig& c)
+bool MyFFT::ConcreteConfigure(const ProcessingConfig& c)
 {
-	int oldSize=mSize;
-	FFT_base::ConcreteConfigure(c);
-	if (mSize<=0) 
+	CopyAsConcreteConfig(mConfig,c);
+	unsigned int configSize = mConfig.GetAudioSize();
+	if (configSize<=0) 
 	{
 		AddConfigErrorMessage("Invalid zero or negative input size");
 		return false;
 	}
-	if (mSize == oldSize) return true;
+	if (!isPowerOfTwo(configSize))
+	{
+		AddConfigErrorMessage("Size is not a power of two");
+		return false;
+	}
+	if (mSize == configSize) return true;
+	mSize=configSize;
 	SetupMemory();
 	return true;
 }
+const ProcessingConfig & MyFFT::GetConfig() const
+{
+	return mConfig;
+}
 
-void FFT_fftw3::ReleaseMemory()
+void MyFFT::ReleaseMemory()
 {
 	if (_fftw3) delete _fftw3;
 }
 
-void FFT_fftw3::SetupMemory()
+void MyFFT::SetupMemory()
 {
 	ReleaseMemory();
-	_fftw3 = new Hidden::FFT_fftw3_Implementation(mSize);
+	_fftw3 = new MyFFT::Implementation(mSize);
 }
 
-FFT_fftw3::FFT_fftw3(const FFTConfig &c)
-	: _fftw3(0)
+MyFFT::MyFFT(const FFTConfig &c)
+	: mInput("Audio Buffer", this)
+	, mOutput("Complex Spectrum", this)
+	, mSize(0)
+	, _fftw3(0)
 { 
 	Configure(c);
 }
 
-FFT_fftw3::~FFT_fftw3()
+MyFFT::~MyFFT()
 {
 	ReleaseMemory();
 }
 
-bool FFT_fftw3::Do() 
+bool MyFFT::Do() 
 {
-	mOutput.GetData().SetSize( mInput.GetSize()/2+1);
-	bool toReturn = Do(mInput.GetAudio(), mOutput.GetData());
+	mOutput.GetData().bins.resize( mInput.GetData().GetSize()/2+1);
+	bool toReturn = Do(mInput.GetData(), mOutput.GetData());
 	mInput.Consume();
 	mOutput.Produce();
 	return toReturn;
 }
 
-bool FFT_fftw3::Do(const Audio& in, Spectrum &out)
+bool MyFFT::Do(const Audio& in, ComplexSpectrum &out)
 {
 	CLAM_DEBUG_ASSERT(IsRunning(),
-		"FFT_fftw3: Do(): Not in execution mode");
-	CLAM_DEBUG_ASSERT(isPowerOfTwo(mSize),
-		"FFT_fftw3: Do(): Not a power of two");
+		"MyFFT: Do(): Not in execution mode");
 
-	out.SetSpectralRange(in.GetSampleRate()/2);
-	if (mState==sOther) CheckTypes(in,out);
 	TData * inbuffer = in.GetBuffer().GetPtr();
 	for (int i=0; i<mSize; i++)
 		_fftw3->realInput[i] = inbuffer[i];
-	fftw_execute(_fftw3->plan);
-	if (mState!=sOther)
-		ToComplex(out);
-	else
-		ToOther(out);
-	if (mState==sComplexSync)
-		out.SynchronizeTo(mComplexflags);
 
+	fftw_execute(_fftw3->plan);
+
+	const unsigned spectrumSize = mSize/2+1;
+	out.bins.resize(spectrumSize);
+	for (int i=0; i<spectrumSize; i++)
+	{
+		out.bins[i]=std::complex<TData>(
+			_fftw3->complexOutput[i][0],
+			_fftw3->complexOutput[i][1]);
+	}
 	return true;
 }
 
-
-void FFT_fftw3::ToComplex(Spectrum &out)
-{
-	Array<Complex>& outbuffer = out.GetComplexArray();
-	outbuffer.Resize(mSize/2+1); // TODO: Any sense?
-	outbuffer.SetSize(mSize/2+1); // TODO: Any sense?
-	for (int i=0; i<mSize/2+1; i++)
-	{
-		outbuffer[i].SetReal(_fftw3->complexOutput[i][0]);
-		outbuffer[i].SetImag(_fftw3->complexOutput[i][1]);
-	}
-}
 
 }; // CLAM
 
