b71622b6434e613783ed013b68839bb3f54d1dda
David García Garzón <vokimon@users.noreply.github.com>
2007-07-16 17:24:31 +0000

    Complex2MagPhaseSpectrum: Removed a Spectrum product class

--- CLAM/examples/PluginExample/Complex2MagPhaseSpectrum.hxx
+++ CLAM/examples/PluginExample/Complex2MagPhaseSpectrum.hxx
@@ -46,51 +46,6 @@ public:
 	}
 };
 
-class MagPhaseSpectrumProduct : public Processing
-{ 
-	InPort<MagPhaseSpectrum> mFactor1;
-	InPort<MagPhaseSpectrum> mFactor2;
-	OutPort<MagPhaseSpectrum> mProduct;
-public:
-	const char* GetClassName() const { return "MagPhaseSpectrumProduct"; }
-	MagPhaseSpectrumProduct(const Config& config = Config()) 
-		: mFactor1("Factor1", this)
-		, mFactor2("Factor2", this)
-		, mProduct("Product", this) 
-	{
-		Configure( config );
-	}
- 
-	bool Do()
-	{
-		const MagPhaseSpectrum & factor1 = mFactor1.GetData();
-		const MagPhaseSpectrum & factor2 = mFactor2.GetData();
-		MagPhaseSpectrum & product = mProduct.GetData();
-	
-		CLAM_ASSERT(factor1.magnitudes.size()==factor2.magnitudes.size(), "Factors should be of equal size");
-		CLAM_ASSERT(factor1.spectralRange==factor2.spectralRange, "SpectralRanges should be equal");
-
-		product.spectralRange = factor1.spectralRange;
-		const unsigned nBins = factor1.magnitudes.size(); 
-//		product.magnitudes.reserve( nBins );
-//		product.phases.reserve( nBins );
-		product.magnitudes.resize( nBins );
-		product.phases.resize( nBins );
-		
-		for (unsigned i=0; i<nBins; i++)
-		{
-			product.magnitudes[i] = factor1.magnitudes[i] * factor2.magnitudes[i];
-			product.phases[i] = factor1.phases[i] + factor2.phases[i];
-		}
-
-		// Tell the ports this is done
-		mFactor1.Consume();
-		mFactor2.Consume();
-		mProduct.Produce();
-		return true;
-	}
-};
-
 } // namespace CLAM
 
 #endif 
