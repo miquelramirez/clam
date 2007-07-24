44ea8a22cf64b0e177fe70b8a4e1cbaf469d1045
David García Garzón <vokimon@users.noreply.github.com>
2007-07-24 15:37:43 +0000

    * Added ComplexSpectrumSum at image of ComplexSpectrumProduct

---
+++ CLAM/examples/PluginExample/ComplexSpectrumSum.cxx
@@ -0,0 +1,15 @@
+#include "ComplexSpectrumSum.hxx"
+#include <CLAM/ProcessingFactory.hxx>
+namespace CLAM
+{
+namespace Hidden
+{
+
+static CLAM::FactoryRegistrator<CLAM::ProcessingFactory, ComplexSpectrumSum>
+	regComplexSpectrumSum("ComplexSpectrumSum");
+
+
+
+}
+
+}
