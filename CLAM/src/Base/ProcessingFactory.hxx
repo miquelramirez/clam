a2c18d6798f2c10a3fce54a05a67448d373ef8ac
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2007-07-27 15:07:31 +0000

    * ProcessingFactory: added doxygen documentation to metadata interface

--- CLAM/src/Base/ProcessingFactory.hxx
+++ CLAM/src/Base/ProcessingFactory.hxx
@@ -24,11 +24,21 @@ public:
 	typedef std::map<Key, Pairs> Metadata;
 //TODO use Attribute and Value
 	void AddAttribute(const std::string& key, const std::string& attribute, const std::string& value);
+	/// Get all keys that have attribute==value in its metadata.
 	Keys GetKeys(const std::string& attribute, const std::string& value);
+	/// Get all keys in the factory
 	Keys GetKeys();
+	/// Get the set of all values present for a given metadata attribute.
 	Values GetSetOfValues(const std::string& attribute); 
+	/// Return all the metadata available for a product key
 	Pairs GetPairsFromKey(const std::string& key); 
+	/// Return the list of values for a metadata attribute for a product key.
 	Values GetValuesFromAttribute(const std::string& key, const std::string& attribute);
+	/// Return the value for a metadata attribute of product key.
+	Value GetValueFromAttribute(const std::string& key, const std::string& attribute)
+	{
+		return GetValuesFromAttribute(key,attribute).front();
+	}
 
 private:
 	Metadata _metadata;
