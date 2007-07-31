281e1801d2ccb4af8ea6c899594339e23580b000
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2007-07-31 07:21:53 +0000

    * Removed references to DummyStaticData (removed)  * ImpulseResponseLoader -> LowLatencyConvolution communicates via ports

--- trunk/CLAM/examples/PluginExample/ImpulseResponseLoader.hxx
+++ trunk/CLAM/examples/PluginExample/ImpulseResponseLoader.hxx
@@ -1,5 +1,5 @@
-#ifndef ImpulseResponseLoaderAndInterpolator_hxx
-#define ImpulseResponseLoaderAndInterpolator_hxx
+#ifndef ImpulseResponseLoader_hxx
+#define ImpulseResponseLoader_hxx
 
 #include <CLAM/InPort.hxx>
 #include <CLAM/OutPort.hxx>
@@ -15,15 +15,14 @@
 namespace CLAM
 {
 
-class ImpulseResponseLoaderAndInterpolator : public Processing
+class ImpulseResponseLoader : public Processing
 { 
 public:
 	class Config : public ProcessingConfig
 	{
-	    DYNAMIC_TYPE_USING_INTERFACE( Config, 3, ProcessingConfig );
+	    DYNAMIC_TYPE_USING_INTERFACE( Config, 2, ProcessingConfig );
 	    DYN_ATTRIBUTE( 0, public, int, FrameSize);
-	    DYN_ATTRIBUTE( 1, public, AudioInFilename, OriginImpulseResponse);
-	    DYN_ATTRIBUTE( 2, public, AudioInFilename, FinalImpulseResponse);
+	    DYN_ATTRIBUTE( 1, public, AudioInFilename, ImpulseResponse);
 
 	protected:
 	    void DefaultInit()
@@ -36,69 +35,30 @@ public:
 
 private:
 	Config _config;
-	InControl _position;
 	OutPort< std::vector<ComplexSpectrum>* > _impulseResponse;
 	std::vector<ComplexSpectrum> _responseSpectrums;
-
-	double _lastPosition;
-	std::vector<ComplexSpectrum> _originResponseSpectrums;
 	std::vector<ComplexSpectrum> _finalResponseSpectrums;
 
 
 public:
-	const char* GetClassName() const { return "ImpulseResponseLoaderAndInterpolator"; }
-	ImpulseResponseLoaderAndInterpolator(const Config& config = Config()) 
-		: _position("Position", this)
-		, _impulseResponse("ImpulseResponse", this)
+	const char* GetClassName() const { return "ImpulseResponseLoader"; }
+	ImpulseResponseLoader(const Config& config = Config()) 
+		: _impulseResponse("ImpulseResponse", this)
 	{
 		Configure( config );
-		_position.SetBounds(0,1);
 	}
 	bool ConcreteConfigure(const ProcessingConfig & config)
 	{
 		CopyAsConcreteConfig(_config, config);
 
-		if (!ComputeResponseSpectrums( _config.GetOriginImpulseResponse(), _originResponseSpectrums )) return false;
-		if (!ComputeResponseSpectrums( _config.GetFinalImpulseResponse(), _finalResponseSpectrums )) return false;
-
-		ConfigureInterpolatedResponseSpectrums();
-		_position.DoControl(_position.DefaultValue());
-		InterpolateResponseSpectrums();
+		if (!ComputeResponseSpectrums( _config.GetImpulseResponse(), _responseSpectrums )) return false;
 
 		return true;
 	}
 	const ProcessingConfig & GetConfig() const { return _config; }
 
-	void ConfigureInterpolatedResponseSpectrums()
-	{
-		const unsigned nBlocks = std::min(_originResponseSpectrums.size(), _finalResponseSpectrums.size());
-		_responseSpectrums.resize(nBlocks);
-		for (unsigned i=0; i<nBlocks; i++)
-		{
-			ComplexSpectrum & spectrum = _responseSpectrums[i];
-			spectrum.spectralRange=_finalResponseSpectrums[0].spectralRange;
-			spectrum.bins.assign(_finalResponseSpectrums[0].bins.size(),std::complex<CLAM::TData>());
-		}
-	}
-	void InterpolateResponseSpectrums()
-	{
-		if (std::abs( _position.GetLastValue() - _lastPosition) < 0.001 )
-			return;
-		std::cout << "ImpulseResponseLoaderAndInterpolator: position changed"<<std::endl;
-		_lastPosition = _position.GetLastValue();
-		ComplexSpectrumMixer mixer;
-		mixer.GetInControl("Gain1").DoControl(_lastPosition);
-		mixer.GetInControl("Gain2").DoControl(1-_lastPosition);
-		unsigned nBlocks = _responseSpectrums.size();
-		for (unsigned i=0; i<nBlocks; i++) //TODO paramter to limit the nblocks to interpolate
-		{
-			mixer.Do( _originResponseSpectrums[i], _finalResponseSpectrums[i], _responseSpectrums[i]);
-		}
-	}
-
 	bool Do()
 	{
-		InterpolateResponseSpectrums();
 		_impulseResponse.GetData()= &_responseSpectrums;
 		_impulseResponse.Produce();
 		return true;
@@ -167,5 +127,5 @@ private:
 
 } // namespace CLAM
 
-#endif // ImpulseResponseLoaderAndInterpolator_hxx
+#endif // ImpulseResponseLoader_hxx
 
