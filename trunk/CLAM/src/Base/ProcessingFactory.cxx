3223331db673845e7203af3378483060a1317598
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2007-08-09 16:24:39 +0000

    * All ProcessingFactory metadata has been pushed to Factory base class so now we can use the registrator to put metadata  * New registrator tests  * ppd from a andreas patch

--- trunk/CLAM/src/Base/ProcessingFactory.cxx
+++ trunk/CLAM/src/Base/ProcessingFactory.cxx
@@ -2,123 +2,13 @@
 #include <string>
 #include <set>
 
-//#include <iostream> //TODO only when debugging
 namespace CLAM
 {
 
 ProcessingFactory& ProcessingFactory::GetInstance()	
 {
 	static ProcessingFactory theInstance;
-//	std::cout << "ProcessingFactory Singleton accessed: "<< (void *) &theInstance << std::endl;
 	return theInstance;
-}
-
-ProcessingFactory::Keys ProcessingFactory::GetKeys()
-{
-	return GetKeys("","");
-}
-ProcessingFactory::Keys ProcessingFactory::GetKeys(const std::string& attribute, const std::string& value)
-{
-	Keys result;
-	Metadata::const_iterator it;
-	for(it = _metadata.begin(); it != _metadata.end(); it++)
-	{
-		if( (attribute == "") )
-		{
-			result.push_back(it->first);
-			continue;
-		}
-		Pairs attributes = it->second;
-		Pairs::const_iterator itAtt;
-		for(itAtt = attributes.begin(); itAtt != attributes.end(); itAtt++)
-		{
-			if( ((*itAtt).attribute == attribute) && ((*itAtt).value == value) )
-			{
-				result.push_back(it->first);
-			}
-		}
-	}
-	return result;
-}
-ProcessingFactory::Pairs ProcessingFactory::GetPairsFromKey(const std::string& key)
-{
-	Pairs attributes;
-	Metadata::const_iterator it = _metadata.find(key);
-	if(it!=_metadata.end())
-	{
-		attributes = it->second;
-	}
-	return attributes;
-}
-/// Example GetSetOfValues("category") could return ["modulators","generators","reverbs"] without repeated items.
-ProcessingFactory::Values ProcessingFactory::GetSetOfValues(const std::string& attribute)
-{
-	std::set<Value> AttributeSet;
-	std::set<Value>::const_iterator itSet;
-	Values values;
-	Metadata::const_iterator it;
-	for(it = _metadata.begin(); it != _metadata.end(); it++)
-	{
-		Pairs attributes = it->second;
-		Pairs::const_iterator itAtt;
-		for(itAtt = attributes.begin(); itAtt != attributes.end(); itAtt++)
-		{
-			if((*itAtt).attribute == attribute)
-			{
-				itSet = AttributeSet.find((*itAtt).value);
-				if(itSet == AttributeSet.end())
-				{
-					AttributeSet.insert((*itAtt).value);
-				}
-			}
-		}
-	}
-	// keep using the ProcessingFactory::Values
-	for(itSet = AttributeSet.begin(); itSet != AttributeSet.end(); itSet++)
-	{
-		values.push_back(*itSet);
-	}
-	return values;
-}
-
-ProcessingFactory::Values ProcessingFactory::GetValuesFromAttribute(const std::string& key, const std::string& attribute)
-{
-	Values values;
-	Metadata::const_iterator it = _metadata.find(key);
-	if(it != _metadata.end())
-	{
-		Pairs::const_iterator itAtt;
-		for(itAtt = it->second.begin(); itAtt != it->second.end(); itAtt++)
-		{
-			if((*itAtt).attribute == attribute)
-			{
-				values.push_back((*itAtt).value);
-			}
-		}
-	}
-	return values;
-}
-void ProcessingFactory::AddAttribute(const std::string& key, const std::string& attribute, const std::string& value)
-{
-	Pairs mValue;
-	Metadata::const_iterator it = _metadata.find(key);
-	Pair pair;
-	pair.attribute = attribute;
-	pair.value = value;
-	if(!ExistsKey(key))
-	{
-		std::cout << "[ProcessingFactory] tryind to add metadata to a non-existing key \"" << key << "\"" << std::endl; 
-//		return;  //pau: debugging: add metadata anyway. maybe factory registrator is about to be instantiated.
-	}
-	if(it == _metadata.end()) // first check if it does not exist
-	{
-		mValue.push_back(pair);
-		_metadata.insert( Metadata::value_type( key, mValue ) );
-	} else { // TODO if exists, add a new attribute
-		mValue = it->second;
-		mValue.push_back(pair);
-		_metadata[key] = mValue;
-	}
 }
 
 } //namespace CLAM
--- trunk/CLAM/src/Base/ProcessingFactory.hxx
+++ trunk/CLAM/src/Base/ProcessingFactory.hxx
@@ -10,44 +10,10 @@ namespace CLAM
 class ProcessingFactory : public Factory<Processing>
 {
 public:
-	typedef std::string Attribute;
-	typedef std::string Value;
-	struct Pair 
-	{
-		Attribute attribute;
-		Value value;
-	};
-	typedef RegistryKey Key;
-	typedef std::list<Key> Keys;
-	typedef std::list<std::string> Values;
-	typedef std::list<Pair> Pairs; 
-	typedef std::map<Key, Pairs> Metadata;
-//TODO use Attribute and Value
-	void AddAttribute(const std::string& key, const std::string& attribute, const std::string& value);
-	/// Get all keys that have attribute==value in its metadata.
-	Keys GetKeys(const std::string& attribute, const std::string& value);
-	/// Get all keys in the factory
-	Keys GetKeys();
-	/// Get the set of all values present for a given metadata attribute.
-	Values GetSetOfValues(const std::string& attribute); 
-	/// Return all the metadata available for a product key
-	Pairs GetPairsFromKey(const std::string& key); 
-	/// Return the list of values for a metadata attribute for a product key.
-	Values GetValuesFromAttribute(const std::string& key, const std::string& attribute);
-	/// Return the value for a metadata attribute of product key.
-	Value GetValueFromAttribute(const std::string& key, const std::string& attribute)
-	{
-		return GetValuesFromAttribute(key,attribute).front();
-	}
-
-private:
-	Metadata _metadata;
-
-
-public:
 	static ProcessingFactory& GetInstance();
 };
 
 } //namespace CLAM
+
 #endif
 
