ded9cb5695a48da3149067f16ed8d361dc26b819
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2007-08-09 16:23:02 +0000

    * Removed LadspaPluginsExplorer class  * Ladspa loading is done in RunTimeProcessingLibraryLoader  ** TODO: refactor: extract two different subclasses in different cxx one for clam plugins other for ladspas  * ppd with andreas

--- trunk/CLAM/src/Processing/Plugins/RunTimeProcessingLibraryLoader.cxx
+++ trunk/CLAM/src/Processing/Plugins/RunTimeProcessingLibraryLoader.cxx
@@ -8,6 +8,11 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#ifdef USE_LADSPA
+	#include <CLAM/ProcessingFactory.hxx>
+	#include <CLAM/LadspaWrapperCreator.hxx>
+	#include <ladspa.h>
+#endif
 
 class RunTimeProcessingLibraryLoader
 {
@@ -64,6 +69,48 @@ class RunTimeProcessingLibraryLoader
 		}
 		closedir(dir);
 	}
+	
+	void loadLibrariesFromPathLADSPA(const std::string & path)
+	{
+		std::cerr << "[LADSPA] Looking at path '" << path << "'" << std::endl;
+		DIR* dir = opendir(path.c_str());
+		if (!dir)
+		{
+			std::cerr << "[LADSPA] warning: could not open CLAM plugins dir: " << path << std::endl;
+			return;
+		}
+		while ( struct dirent * dirEntry = readdir(dir) )
+		{
+			std::string pluginFilename(dirEntry->d_name);
+			if(pluginFilename == "." || pluginFilename == "..")
+				continue;
+			LADSPA_Descriptor_Function descriptorTable = 0;
+			std::string pluginFullFilename(path + std::string("/") + pluginFilename);
+			void* handle = FullyLoadLibrary(pluginFullFilename);
+			descriptorTable = (LADSPA_Descriptor_Function)dlsym(handle, "ladspa_descriptor");
+			if (!descriptorTable)
+			{
+				std::cout << "[LADSPA] warning: trying to open non ladspa plugin: " << pluginFullFilename << std::endl;
+				continue;
+			}
+			//std::cout << "[LADSPA] \topened plugin: " << pluginFullFilename << std::endl;
+			CLAM::ProcessingFactory& factory = CLAM::ProcessingFactory::GetInstance();
+			for (unsigned long i=0; descriptorTable(i); i++)
+			{
+				LADSPA_Descriptor* descriptor = (LADSPA_Descriptor*)descriptorTable(i);
+				std::ostringstream oss;
+				oss << descriptor->Label << i;
+				factory.AddCreatorWarningRepetitions(oss.str(), 
+						new CLAM::LadspaWrapperCreator(pluginFullFilename, 
+							i, 
+							oss.str()));
+				factory.AddAttribute(oss.str(), "category", "LADSPA");
+				factory.AddAttribute(oss.str(), "description", descriptor->Name);
+				//std::cout << "[LADSPA] added \"" << plugin.factoryID << "\" to the Factory" << std::endl;
+			}
+		}
+		closedir(dir);	
+	}
 public:
 	std::vector<std::string> splitPathVariable(const std::string & pathVariable)
 	{
@@ -91,19 +138,39 @@ public:
 			"/opt/lib/clam",
 			0
 		};
+		const char * standardPathsLADSPA[] = 
+		{
+			"/usr/local/lib/ladspa",
+			"/usr/lib/ladspa",
+			0
+		};
 		const char * envPath = getenv("CLAM_PLUGIN_PATH");
+		const char * envPathLADSPA = getenv("LADSPA_PATH");
 		const char * envHome = getenv("HOME");
 		std::string path = envPath ? envPath : "";
-		if (envHome) 
+		std::string pathLADSPA = envPathLADSPA ? envPathLADSPA : "";
+		if (envHome)
+		{	
 			path += std::string(path.empty()? "":pathSeparator) + envHome + "/.clam/plugins";
+			pathLADSPA += std::string(pathLADSPA.empty()? "":pathSeparator) + envHome + "/.ladspa";
+		}
+		// add standardPath to the env path string (i.e. path1:path2)
 		for (const char ** standardPath=standardPaths; *standardPath; standardPath++)
 			path += std::string(path.empty()? "":pathSeparator) + *standardPath;
-
+		// for each path, load libraries
 		std::vector <std::string> environmentPaths = splitPathVariable(path);
 		for (unsigned i=0; i<environmentPaths.size(); i++)
 			loadLibrariesFromPath(environmentPaths[i]);
+
+		//idem for ladspa
+		for (const char ** standardPathLADSPA=standardPathsLADSPA; *standardPathLADSPA; standardPathLADSPA++)
+			pathLADSPA += std::string(pathLADSPA.empty()? "":pathSeparator) + *standardPathLADSPA;
+		std::vector <std::string> environmentPathsLADSPA = splitPathVariable(pathLADSPA);
+		for (unsigned i=0; i<environmentPathsLADSPA.size(); i++)
+			loadLibrariesFromPathLADSPA(environmentPathsLADSPA[i]);
 	}
 };
+
 
 const char * RunTimeProcessingLibraryLoader::pathSeparator = 
 	#ifdef WIN32
