ab9ac3a431661804b1f57654570e4b87df988940
acalvo <acalvo@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2007-08-13 17:23:14 +0000

    * VectorBasedArrayPanning: comment metadata

--- trunk/NetworkEditor/src/processing/VectorBasedArrayPanning.cxx
+++ trunk/NetworkEditor/src/processing/VectorBasedArrayPanning.cxx
@@ -5,8 +5,8 @@ namespace detail
 {
 	static const char * metadata[] = {
 		"key", "VectorBasedArrayPanning",
-		"category", "CLAM",
-		"description", "VectorBasedArrayPanning",
+	//	"category", "CLAM",
+	//	"description", "VectorBasedArrayPanning",
 		0
 	};
 	//static CLAM::FactoryRegistrator<CLAM::ProcessingFactory, VectorBasedArrayPanning> regVectorBasedArrayPanning("VectorBasedArrayPanning");
