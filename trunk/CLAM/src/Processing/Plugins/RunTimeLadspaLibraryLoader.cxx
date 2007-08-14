885d4ade94ef57901838af1fe955249d698f42d3
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2007-08-14 18:10:52 +0000

    * RunTimeProcessingLibraryLoader refactoring  ** New super class RunTimeLibraryLoader with subclasses for Ladspa and Processings  ** Superclass implements template method (aka hollywood) pattern in the Load() method  ** Automatic static registration is done in a different class that creates object and calls Load()

---
+++ trunk/CLAM/src/Processing/Plugins/RunTimeLadspaLibraryLoader.cxx
@@ -0,0 +1,62 @@
+#ifdef WIN32
+#include <windows.h>
+#undef GetClassName
+#else
+#include <dlfcn.h>
+#endif
+#include <dirent.h>
+#include "RunTimeLibraryLoader.hxx"
+
+#include "ProcessingFactory.hxx"
+#include "LadspaWrapperCreator.hxx"
+#include <ladspa.h>
+
+
+class RunTimeLadspaLibraryLoader : public RunTimeLibraryLoader
+{
+
+protected:
+
+	void SetupLibrary(void* handle, const std::string & pluginFullFilename) const
+	{
+		LADSPA_Descriptor_Function descriptorTable = 0;
+		descriptorTable = (LADSPA_Descriptor_Function)dlsym(handle, "ladspa_descriptor");
+		if (!descriptorTable)
+		{
+			std::cout << "[LADSPA Plugin] Warning: trying to open non ladspa plugin: " << pluginFullFilename << std::endl;
+			return;
+		}
+		//std::cout << "[LADSPA] \topened plugin: " << pluginFullFilename << std::endl;
+		CLAM::ProcessingFactory& factory = CLAM::ProcessingFactory::GetInstance();
+		for (unsigned long i=0; descriptorTable(i); i++)
+		{
+			LADSPA_Descriptor* descriptor = (LADSPA_Descriptor*)descriptorTable(i);
+			std::ostringstream oss;
+			oss << descriptor->Label << i;
+			factory.AddCreatorWarningRepetitions(oss.str(), 
+					new CLAM::LadspaWrapperCreator(pluginFullFilename, 
+						i, 
+						oss.str()));
+			factory.AddAttribute(oss.str(), "category", "LADSPA");
+			factory.AddAttribute(oss.str(), "description", descriptor->Name);
+			//std::cout << "[LADSPA] added \"" << plugin.factoryID << "\" to the Factory" << std::endl;
+		}
+	}
+
+	const char ** standardPaths() const
+	{ 
+		static const char * result[] = 
+		{
+			"/usr/local/lib/ladspa",
+			"/usr/lib/ladspa",
+			0
+		};
+		return result;
+	}
+
+	const char * homePath() const { return  "/.ladspa"; }
+	const char * pathEnvironmentVar() const { return  "LADSPA_PATH"; }
+	const char * libraryType() const { return  "LADSPA"; }
+};
+
+
---
+++ trunk/CLAM/src/Processing/Plugins/RunTimeLibraryLoader.hxx
@@ -0,0 +1,128 @@
+#ifndef RunTimeLibraryLoader_hxx
+#define RunTimeLibraryLoader_hxx
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include <dirent.h>
+#ifdef WIN32
+#	include <windows.h>
+#	undef GetClassName
+#else
+#	include <dlfcn.h>
+#endif
+
+
+class RunTimeLibraryLoader
+{
+public:
+	RunTimeLibraryLoader()
+	{
+	}
+	void Load() const
+	{
+		const char * envPath = getenv(pathEnvironmentVar());
+		const char * envHome = getenv("HOME");
+		std::string path = envPath ? envPath : "";
+		if (envHome)
+		{	
+			path += std::string(path.empty()? "":pathSeparator()) + envHome + homePath();
+		}
+		// add standardPath to the env path string (i.e. path1:path2)
+		for (const char ** standardPath=standardPaths(); *standardPath; standardPath++)
+			path += std::string(path.empty()? "":pathSeparator()) + *standardPath;
+		// for each path, load libraries
+		std::vector <std::string> environmentPaths = SplitPathVariable(path);
+		for (unsigned i=0; i<environmentPaths.size(); i++)
+			LoadLibrariesFromPath(environmentPaths[i]);
+	}
+	virtual ~RunTimeLibraryLoader() {}
+
+protected:
+	void LoadLibrariesFromPath(const std::string & path) const
+	{
+		std::cerr << "[" << libraryType() << " Plugins] Looking at path '" << path << "'" << std::endl;
+		DIR* dir = opendir(path.c_str());
+		if (!dir)
+		{
+			std::cerr << "[" << libraryType() << " Plugins]  Warning: could not open plugins dir: " << path << std::endl;
+			return;
+		}
+		while ( struct dirent * dirEntry = readdir(dir) )
+		{
+			std::string pluginFilename(dirEntry->d_name);
+			if(pluginFilename == "." || pluginFilename == "..")
+				continue;
+			std::string pluginFullFilename(path + std::string("/") + pluginFilename);
+			void* handle = FullyLoadLibrary(pluginFullFilename);
+			SetupLibrary( handle, pluginFullFilename );
+		}
+		closedir(dir);
+	}
+	virtual const char ** standardPaths() const = 0;
+	virtual const char * homePath() const = 0;
+	virtual const char * pathEnvironmentVar() const = 0;
+	virtual const char * libraryType() const = 0;
+	virtual void SetupLibrary( void* handle, const std::string& pluginFullFilename ) const 
+	{
+	}
+
+	void * FullyLoadLibrary(const std::string & libraryPath) const
+	{
+	#ifdef WIN32
+//		SetErrorMode( SEM_FAILCRITICALERRORS | SEM_NOALIGNMENTFAULTEXCEPT |
+//			SEM_NOGPFAULTERRORBOX | SEM_NOOPENFILEERRORBOX );
+		return LoadLibrary(libraryPath.c_str());
+	#else
+		return dlopen( libraryPath.c_str(), RTLD_NOW);	
+	#endif
+	}
+
+	std::string LibraryLoadError() const
+	{
+	#ifdef WIN32
+		LPVOID lpMsgBuf;
+		FormatMessage(
+			FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+			NULL,
+			GetLastError(),
+			MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+			(LPTSTR) &lpMsgBuf,
+			0, NULL );
+		std::string message((char*)lpMsgBuf);
+		LocalFree(lpMsgBuf);
+		return message;
+	#else
+		return dlerror();
+	#endif
+	}
+	std::vector<std::string> SplitPathVariable(const std::string & pathVariable) const
+	{
+		std::string content=pathVariable;
+		std::vector<std::string> result;
+		while (!content.empty())
+		{
+			size_t separatorPos = content.find(pathSeparator());
+			if (separatorPos == std::string::npos)
+			{
+				result.push_back(content);
+				break;
+			} 
+			result.push_back(content.substr(0, separatorPos));
+			content = content.substr(separatorPos+1);
+		}
+		return result;
+	}
+	const char * pathSeparator() const
+	{
+		return 
+		#ifdef WIN32
+			";";
+		#else
+			":";
+		#endif
+	}
+};
+
+#endif //RunTimeLibraryLoader_hxx
+
