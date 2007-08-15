4729931efdccc65db56f3974577758b4c0f33c2d
bkolasinski <bkolasinski@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2007-08-15 00:11:11 +0000

    add FrameDescriptorsPane

--- Annotator/src/Annotator.cxx
+++ Annotator/src/Annotator.cxx
@@ -75,57 +75,7 @@
 
 #include <QtGui/QSplitter>
 #include "SegmentationPane.hxx"
-
-class FrameDescriptorsPane : public QSplitter
-{
-	Q_OBJECT
-	CLAM::VM::BPFPlot *mBPFEditor;
-	QListWidget *mFrameLevelAttributeList;
- 
-public:
-	FrameDescriptorsPane(QWidget * parent)
-		: QSplitter(parent)
-	{
-		setOrientation(Qt::Horizontal);
-		mBPFEditor = new CLAM::VM::BPFPlot(this);
-		QSizePolicy sizePolicy3(QSizePolicy::MinimumExpanding, QSizePolicy::MinimumExpanding);
-		sizePolicy3.setHorizontalStretch(2);
-		sizePolicy3.setVerticalStretch(0);
-		sizePolicy3.setHeightForWidth(mBPFEditor->sizePolicy().hasHeightForWidth());
-		mBPFEditor->setSizePolicy(sizePolicy3);
-		mBPFEditor->setMinimumSize(QSize(700, 200));
-		mBPFEditor->setFocusPolicy(Qt::WheelFocus);
-		mBPFEditor->setFrameShape(QFrame::StyledPanel);
-		mBPFEditor->setFrameShadow(QFrame::Raised);
-		addWidget(mBPFEditor);
-		mFrameLevelAttributeList = new QListWidget(this);
-		mFrameLevelAttributeList->setMinimumSize(QSize(150, 0));
-		mFrameLevelAttributeList->setAlternatingRowColors(true);
-		mFrameLevelAttributeList->setResizeMode(QListView::Adjust);
-		addWidget(mFrameLevelAttributeList);
-// TODO: move elsewhere!
-//		mVSplit->addWidget(this);
-
-		mBPFEditor->setWhatsThis(QApplication::translate("Annotator", "<p>The <b>frame level descriptors editor</b> allows editing\n"
-			"			 floating point descriptors that has an occurrence for each frame.</p>\n"
-			"			 <ul>\n"
-			"			 <li>Dragging Points: Moving them.</li>\n"
-			"			 <li>Page up/down: Set focus prev/next point</li>\n"
-			"			 <li>Hover point: Set focus to point</li>\n"
-			"			 <li>Dragging play positon: Moves it.</li>\n"
-			"			 <li>'r'+Click: Set the current play position.</li>\n"
-			"			 <li>Arrows: Change focused point values</li>\n"
-			"			 </ul>", 0, QApplication::UnicodeUTF8));
-
-		mBPFEditor->SetFlags(CLAM::VM::eAllowVerEdition);//|CLAM::VM::eHasVerticalScroll); // QTPORT: What about this flag
-		mBPFEditor->SetZoomSteps(5,5);
-		mBPFEditor->SetXRange(0.0,2.0);
-
-		mBPFEditor->setAutoFillBackground(true);
-	}
-
-
-};
+#include "FrameDescriptorsPane.hxx"
 
 void Annotator::abortLoader()
 {
@@ -226,6 +176,7 @@ Annotator::Annotator(const std::string & nameProject = "")
 
 	setupUi(this);
 	mGlobalDescriptors = new CLAM_Annotator::DescriptorTableController(mDescriptorsTable, mProject);
+	_frameDescriptorsPane = new FrameDescriptorsPane(this);
 	addNewSegmentationPane();
 	mAbout = new QDialog(this);
 	Ui::About aboutUi;
---
+++ Annotator/src/FrameDescriptorsPane.hxx
@@ -0,0 +1,60 @@
+#ifndef FrameDescriptorsPane_hxx
+#define FrameDescriptorsPane_hxx
+
+#include <QtGui/QSplitter>
+#include <QtGui/QListWidget>
+#include "vmBPFPlot.hxx"
+
+class FrameDescriptorsPane : public QSplitter
+{
+	Q_OBJECT
+
+	CLAM::VM::BPFPlot *mBPFEditor;
+	QListWidget *mFrameLevelAttributeList;
+ 
+public:
+	FrameDescriptorsPane(QWidget * parent)
+		: QSplitter(parent)
+	{
+		setOrientation(Qt::Horizontal);
+		mBPFEditor = new CLAM::VM::BPFPlot(this);
+		QSizePolicy sizePolicy3(QSizePolicy::MinimumExpanding, QSizePolicy::MinimumExpanding);
+		sizePolicy3.setHorizontalStretch(2);
+		sizePolicy3.setVerticalStretch(0);
+		sizePolicy3.setHeightForWidth(mBPFEditor->sizePolicy().hasHeightForWidth());
+		mBPFEditor->setSizePolicy(sizePolicy3);
+		mBPFEditor->setMinimumSize(QSize(700, 200));
+		mBPFEditor->setFocusPolicy(Qt::WheelFocus);
+		mBPFEditor->setFrameShape(QFrame::StyledPanel);
+		mBPFEditor->setFrameShadow(QFrame::Raised);
+		addWidget(mBPFEditor);
+		mFrameLevelAttributeList = new QListWidget(this);
+		mFrameLevelAttributeList->setMinimumSize(QSize(150, 0));
+		mFrameLevelAttributeList->setAlternatingRowColors(true);
+		mFrameLevelAttributeList->setResizeMode(QListView::Adjust);
+		addWidget(mFrameLevelAttributeList);
+// TODO: move elsewhere!
+//		mVSplit->addWidget(this);
+
+		mBPFEditor->setWhatsThis(
+			tr("Annotator", "<p>The <b>frame level descriptors editor</b> allows editing\n"
+			"			 floating point descriptors that has an occurrence for each frame.</p>\n"
+			"			 <ul>\n"
+			"			 <li>Dragging Points: Moving them.</li>\n"
+			"			 <li>Page up/down: Set focus prev/next point</li>\n"
+			"			 <li>Hover point: Set focus to point</li>\n"
+			"			 <li>Dragging play positon: Moves it.</li>\n"
+			"			 <li>'r'+Click: Set the current play position.</li>\n"
+			"			 <li>Arrows: Change focused point values</li>\n"
+			"			 </ul>")
+		);
+
+		mBPFEditor->SetFlags(CLAM::VM::eAllowVerEdition);//|CLAM::VM::eHasVerticalScroll); // QTPORT: What about this flag
+		mBPFEditor->SetZoomSteps(5,5);
+		mBPFEditor->SetXRange(0.0,2.0);
+
+		mBPFEditor->setAutoFillBackground(true);
+	}
+};
+
+#endif
