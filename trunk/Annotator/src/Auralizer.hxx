7897631316b3d0575cae4993ffae792e434c9ecc
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2007-08-16 11:15:33 +0000

    * Auralizer: inline CLAM::VM::Player declaration

--- trunk/Annotator/src/Auralizer.hxx
+++ trunk/Annotator/src/Auralizer.hxx
@@ -29,12 +29,57 @@
 #include <CLAM/Segmentation.hxx>
 #include <CLAM/Oscillator.hxx>
 #include "EquidistantPointsFunction.hxx"
-#include "vmPlayer.hxx"
 
-#include "AudioIO.hxx"
-#include "AudioManager.hxx"
-#include "AudioOut.hxx"
-#include "SimpleOscillator.hxx"
+#include <CLAM/AudioIO.hxx>
+#include <CLAM/AudioManager.hxx>
+#include <CLAM/AudioOut.hxx>
+#include <CLAM/SimpleOscillator.hxx>
+
+
+#include <QtCore/QThread>
+#include "vmDataTypes.hxx"
+
+namespace CLAM
+{
+	namespace VM
+	{
+		class Player : public QThread
+		{
+			Q_OBJECT
+		public:
+			enum PlayStatus {Stoped, Paused, Playing};
+			Player(QObject* parent=0);
+			virtual ~Player();
+
+			void SetDuration(double dur);
+			void SetSamplingRate(double sr);
+			void SetPlayingFlags(int flags);
+
+			bool IsPlaying() const;
+
+		signals:
+			void playingTime(double);
+			void stopTime(double, bool);
+
+		public slots:
+			void play();
+			void pause();
+			void stop();
+
+			void timeBounds(double, double);
+
+		protected:
+			int           mPlayingFlags;
+			volatile PlayStatus    mPlayStatus;
+			double        mSamplingRate;
+			double        mBeginTime;
+			Range         mTimeBounds;
+
+			virtual void run()=0; // thread code here
+		};
+	}
+}
+
 
 class BPFPlayer : public CLAM::VM::Player
 {
