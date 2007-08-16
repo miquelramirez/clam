4e3bd6be4901763eeb97dbc64ebc24ba4ee7b467
bkolasinski <bkolasinski@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2007-08-16 05:14:05 +0000

    add refreshEnvelopes and changeFrameLevelDescriptor to FrameDescriptors pane

--- Annotator/src/FrameDescriptorsPane.hxx
+++ Annotator/src/FrameDescriptorsPane.hxx
@@ -3,9 +3,12 @@
 
 #include <QtGui/QSplitter>
 #include <QtGui/QListWidget>
+#include "FrameDivision.hxx"
 #include "vmBPFPlot.hxx"
 #include "Project.hxx"
 #include "EquidistantPointsFunction.hxx"
+#include "Audio.hxx"
+#include <CLAM/Pool.hxx>
 
 class FrameDescriptorsPane : public QSplitter
 {
@@ -17,6 +20,8 @@ class FrameDescriptorsPane : public QSplitter
 private:
 	CLAM_Annotator::Project& _mProject;
 	std::vector<CLAM::EquidistantPointsFunction> _mEPFs;
+	const CLAM::Audio * _audio;
+	CLAM::DescriptionDataPool * _pool;
 
 public:
 	FrameDescriptorsPane(QWidget * parent,
@@ -26,6 +31,8 @@ public:
 		: QSplitter(parent)
 		, _mProject(mProject)
 		, _mEPFs(mEPFs)
+		, _audio(0)
+		, _pool(0)
 	{
 		setOrientation(Qt::Horizontal);
 		mBPFEditor = new CLAM::VM::BPFPlot(this);
@@ -66,6 +73,14 @@ public:
 
 		mBPFEditor->setAutoFillBackground(true);
 	}
+	
+	void setData(CLAM::DescriptionDataPool * pool, const CLAM::Audio & audio)
+	{
+		_audio = & audio;
+		_pool=pool;
+		refreshEnvelopes();
+	}
+	
 	void adaptEnvelopesToCurrentSchema()
 	{
 		mFrameLevelAttributeList->clear();
@@ -78,6 +93,64 @@ public:
 			mFrameLevelAttributeList->addItem(name->c_str());
 		}
 	}
+	
+	void refreshEnvelopes()
+	{
+		std::cout<<"Loading frame level data..."<< std::endl;
+
+		// TODO: Not all the things should be done here
+		_mEPFs.clear();
+		mBPFEditor->SetXRange(0.0,double(_audio->GetDuration())/1000.0);	
+
+		if (!_pool) return;
+
+		const std::list<std::string>& divisionNames = _mProject.GetNamesByScopeAndType("Song", "FrameDivision");
+
+		std::list<std::string>::const_iterator divisionName;
+		for(divisionName = divisionNames.begin();divisionName != divisionNames.end(); divisionName++)
+		{
+			const CLAM_Annotator::FrameDivision & division = _pool->GetReadPool<CLAM_Annotator::FrameDivision>("Song",*divisionName)[0];
+
+			const std::string & frameDivisionChildScope = _mProject.GetAttributeScheme("Song", *divisionName).GetChildScope();
+			const std::list<std::string>& descriptorsNames = _mProject.GetNamesByScopeAndType(frameDivisionChildScope, "Float");
+			std::list<std::string>::const_iterator it;
+			for(it = descriptorsNames.begin();it != descriptorsNames.end(); it++)
+			{
+				_mEPFs.push_back(CLAM::EquidistantPointsFunction());
+				CLAM::TData firstCenter = division.GetFirstCenter();
+				CLAM::TData interCenterGap = division.GetInterCenterGap();
+				const CLAM::TData* values = _pool->GetReadPool<CLAM::TData>(frameDivisionChildScope,*it);
+				CLAM::TData sr = _audio->GetSampleRate();
+				int nFrames = _pool->GetNumberOfContexts(frameDivisionChildScope);
+				_mEPFs.back().setDivision(firstCenter/sr, interCenterGap/sr, "s");
+				_mEPFs.back().setValues(values, nFrames, "");
+			}
+		}
+		changeFrameLevelDescriptor(mFrameLevelAttributeList->currentRow());
+	}
+
+	void changeFrameLevelDescriptor(int current)
+	{
+		unsigned index = mFrameLevelAttributeList->currentRow();
+		if (index >= _mEPFs.size()) return; // No valid descriptor
+		double minValue = _mEPFs[index].getMin();
+		double maxValue = _mEPFs[index].getMax();
+		// TODO: Move this margin to the widget
+		double span = maxValue-minValue;
+		minValue -= span*0.1;
+		maxValue += span*0.1;
+		mBPFEditor->SetData(&_mEPFs[index].GetBPF());
+		CLAM::VM::ERulerScale scale = CLAM::VM::eLinearScale;
+		if (fabs(minValue) > 9999.99) scale=CLAM::VM::eLogScale;
+		if (fabs(maxValue) > 9999.99) scale=CLAM::VM::eLogScale;
+		if (maxValue-minValue < CLAM::TData(5E-2)) scale=CLAM::VM::eLogScale;
+		mBPFEditor->SetYRange(minValue, maxValue, scale);
+
+		//mPlayer->SetData(mEPFs[index].GetBPF());
+		//mPlayer->SetPitchBounds(minValue, maxValue);
+		mBPFEditor->show();
+	}
+
 };
 
 #endif
