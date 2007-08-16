8ade61cb1ac1fc0ebe26c17a5b9fd0cc0956fb0d
David García Garzón <vokimon@users.noreply.github.com>
2007-08-16 09:15:31 +0000

    * Auralizer:  ** Annotator::changeFrameLevelDescriptor delegates auralization on Auralizer::setLLD  ** Annotator::setAudio refresh the audio on the player

--- Annotator/src/Auralizer.hxx
+++ Annotator/src/Auralizer.hxx
@@ -8,10 +8,10 @@ class Auralizer
 public:
 	CLAM::Audio mClick; ///< A vector of audios to produce click
 	CLAM::Audio mOnsetAuralizationAudio; ///< Current audio with segmentation marks inserted
-	const CLAM::Audio & mCurrentAudio;
+	const CLAM::Audio * _currentAudio;
 	CLAM::VM::BPFPlayer* mPlayer;
-	Auralizer(QWidget * parent, const CLAM::Audio & audio)
-		: mCurrentAudio(audio)
+	Auralizer(QWidget * parent)
+		: _currentAudio(0)
 	{
 		mPlayer = new CLAM::VM::BPFPlayer(parent);
 		initClick();
@@ -24,30 +24,50 @@ public:
 	{
 		mPlayer->timeBounds(miliseconds, endMiliseconds);
 	}
+	void setOptions(bool playOnsets, bool playLLDs)
+	{
+		CLAM_ASSERT(_currentAudio, "Auralizer::setOptions without audio");
+//		unsigned int LEFT_CHANNEL = 1;
+		unsigned int RIGHT_CHANNEL = 2;
+		mPlayer->SetAudioPtr(_currentAudio);
+		if (playOnsets)
+			mPlayer->SetAudioPtr(&(mOnsetAuralizationAudio), RIGHT_CHANNEL);
+		mPlayer->SetPlayingFlags( CLAM::VM::eAudio | (playLLDs?CLAM::VM::eUseOscillator:0));
+	}
+	void setAudio(const CLAM::Audio & audio)
+	{
+		_currentAudio= & audio;
+		mPlayer->SetAudioPtr(_currentAudio);
+		mPlayer->SetDuration(double(audio.GetDuration())/1000.0);
+	}
 	void setSegmentation(const CLAM::Segmentation * segmentation)
 	{
 		if (!segmentation) return;
+		CLAM_ASSERT(_currentAudio, "Auralizer::setOptions without audio");
 		const std::vector<double> & marks = segmentation->onsets();
 		int nMarks = marks.size();
 		mOnsetAuralizationAudio.SetSize(0);
-		mOnsetAuralizationAudio.SetSize(mCurrentAudio.GetSize());
-		mOnsetAuralizationAudio.SetSampleRate(mCurrentAudio.GetSampleRate());
+		mOnsetAuralizationAudio.SetSize(_currentAudio->GetSize());
+		mOnsetAuralizationAudio.SetSampleRate(_currentAudio->GetSampleRate());
 		int size = mOnsetAuralizationAudio.GetSize();
 		for (int i=0; i<nMarks; i++)
 		{
-			int samplePosition = Round(marks[i]*mCurrentAudio.GetSampleRate());
+			int samplePosition = Round(marks[i]*_currentAudio->GetSampleRate());
 			if(samplePosition<size)
 				mOnsetAuralizationAudio.SetAudioChunk(samplePosition,mClick);
 		}
 	}
-	void setOptions(bool playOnsets, bool playLLDs)
+	void setLLD(const CLAM::EquidistantPointsFunction & epf)
 	{
-//		unsigned int LEFT_CHANNEL = 1;
-		unsigned int RIGHT_CHANNEL = 2;
-		mPlayer->SetAudioPtr(&mCurrentAudio);
-		if (playOnsets)
-			mPlayer->SetAudioPtr(&(mOnsetAuralizationAudio), RIGHT_CHANNEL);
-		mPlayer->SetPlayingFlags( CLAM::VM::eAudio | (playLLDs?CLAM::VM::eUseOscillator:0));
+		mPlayer->SetData(epf.GetBPF());
+
+		double minValue = epf.getMin();
+		double maxValue = epf.getMax();
+		// TODO: Move this margin to the widget
+		double span = maxValue-minValue;
+		minValue -= span*0.1;
+		maxValue += span*0.1;
+		mPlayer->SetPitchBounds(minValue, maxValue);
 	}
 private:
 	void initClick()
