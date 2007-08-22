7adf154698d0e195d23852d00acaceaa3cd63872
David García Garzón <vokimon@users.noreply.github.com>
2007-08-22 03:49:23 +0000

    * Reverted last Bennett commit  * Rationale: No need to do all that propagation. If first propagation generates    a second one, the code is very hard to follow. I think the work can be done    locally at the inner widgets without tainting the Panes and the Annotator.    When we set the range we can just recompute the scrollbar positions and    the inner zoom status to reflect such limits, and propagate it but just to    the scrollbar and zoom buttons.  * Let me try that local solution and if it doesn't work then redoing this patch.

--- Annotator/src/FrameDescriptorsPane.hxx
+++ Annotator/src/FrameDescriptorsPane.hxx
@@ -66,9 +66,6 @@ public:
 		// zoom change
 		connect(mBPFEditor, SIGNAL(visibleXRangeChanged(double,double)),
 			this, SIGNAL(visibleXRangeChanged(double,double)));
-		// scroll value change
-		connect(mBPFEditor, SIGNAL(xScrollValueChanged(int)),
-			this, SIGNAL(xScrollValueChanged(int)));
 
 		mBPFEditor->setWhatsThis(
 			tr("Annotator", "<p>The <b>frame level descriptors editor</b> allows editing\n"
@@ -161,11 +158,6 @@ public:
 		mBPFEditor->setVisibleXRange(xmin, xmax);
 	}
 	
-	void setXScrollValue(int val)
-	{
-		mBPFEditor->setXScrollValue(val);
-	}
-	
 	void updateEnvelopesData()
 	{
 		// TODO: Any child scope of any FrameDivision in Song not just Frame, which may not even exist
@@ -241,7 +233,6 @@ signals:
 	void frameDescriptorsRegionChanged(double startMiliseconds, double endMiliseconds);
 	void frameDescriptorsSelectionChanged();
 	void visibleXRangeChanged(double min, double max);
-	void xScrollValueChanged(int val);
 };
 
 #endif
