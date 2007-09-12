1a91257ec6e7ab2e457d0d33121b8ca128a8c0e9
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2007-09-12 14:48:07 +0000

    * spacialization: extracted helper function.  * new example to do back-to-back tests

--- trunk/CLAM/plugins/spacialization/ImpulseResponseLoader.hxx
+++ trunk/CLAM/plugins/spacialization/ImpulseResponseLoader.hxx
@@ -4,13 +4,9 @@
 #include <CLAM/InPort.hxx>
 #include <CLAM/OutPort.hxx>
 #include <CLAM/Processing.hxx>
-#include <CLAM/MonoAudioFileReader.hxx>
-#include <CLAM/AudioWindowing.hxx>
-#include "MyFFT.hxx"
 #include "ComplexSpectrum.hxx"
-#include "ComplexSpectrumMixer.hxx"
-#include <algorithm>
-#include <cmath>
+#include "LoadImpulseResponse.hxx"
+#include <CLAM/AudioInFilename.hxx>
 
 namespace CLAM
 {
@@ -50,8 +46,12 @@ public:
 	bool ConcreteConfigure(const ProcessingConfig & config)
 	{
 		CopyAsConcreteConfig(_config, config);
-
-		if (!ComputeResponseSpectrums( _config.GetImpulseResponse(), _responseSpectrums )) return false;
+		std::string errorMsg;
+		if (!ComputeResponseSpectrums( _config.GetImpulseResponse(), _responseSpectrums, _config.GetFrameSize(), errorMsg ))
+		{
+			AddConfigErrorMessage(errorMsg);
+			return false;
+		}
 
 		return true;
 	}
@@ -63,66 +63,6 @@ public:
 		_impulseResponse.Produce();
 		return true;
 	}
-private:
-	bool ComputeResponseSpectrums(const std::string & wavfile, std::vector<ComplexSpectrum> & responseSpectrums)
-	{
-		MonoAudioFileReaderConfig readerConfig;
-		readerConfig.SetSourceFile(wavfile);
-		MonoAudioFileReader reader(readerConfig);
-		if (!reader.IsConfigured())
-		{
-			AddConfigErrorMessage("Configuring the inner AudioFileReader:\n");
-			AddConfigErrorMessage(reader.GetConfigErrorMessage());
-			return false;
-		}
-		const unsigned nSamples = reader.GetHeader().GetSamples();
-		std::cout << "NSamples: " << nSamples << std::endl;
-
-		AudioWindowingConfig windowerConfig;
-		windowerConfig.SetSamplingRate(44100); // TODO: Take it from the file
-		windowerConfig.SetHopSize(_config.GetFrameSize());
-		windowerConfig.SetWindowSize(_config.GetFrameSize()+1);
-		windowerConfig.SetFFTSize(_config.GetFrameSize()*2);
-		windowerConfig.SetDoHalfWindowShift(false);
-		windowerConfig.SetWindowType(EWindowType::eNone);
-		AudioWindowing windower(windowerConfig);
-		if (!windower.IsConfigured())
-		{
-			AddConfigErrorMessage("Configuring the inner AudioWindowing:\n");
-			AddConfigErrorMessage(windower.GetConfigErrorMessage());
-			return false;
-		}
-		FFTConfig fftConfig; 
-		fftConfig.SetAudioSize(_config.GetFrameSize()*2);
-		MyFFT fft(fftConfig);
-
-		ConnectPorts(reader,0,windower,0);
-		ConnectPorts(windower,0,fft,0);
-		InPort<ComplexSpectrum> fetcher;
-		fft.GetOutPorts().GetByNumber(0).ConnectToIn(fetcher);
-
-		responseSpectrums.clear();
-		reader.Start();
-		windower.Start();
-		fft.Start();
-		
-		for (bool samplesAvailable=true; samplesAvailable; )
-		{
-			samplesAvailable = reader.Do();
-			if (!windower.CanConsumeAndProduce()) continue;
-			windower.Do();
-			fft.Do();
-			responseSpectrums.push_back(fetcher.GetData());
-//			fetcher.GetData().dump(std::cout);
-			fetcher.Consume();
-		}
-
-		reader.Stop();
-		windower.Stop();
-		fft.Stop();
-		return true;
-	}
-
 };
 
 } // namespace CLAM
---
+++ trunk/CLAM/plugins/spacialization/LoadImpulseResponse.cxx
@@ -0,0 +1,69 @@
+#include "LoadImpulseResponse.hxx"
+#include <CLAM/MonoAudioFileReader.hxx>
+#include <CLAM/AudioWindowing.hxx>
+#include "MyFFT.hxx"
+
+namespace CLAM
+{
+
+bool ComputeResponseSpectrums(const std::string & wavfile, std::vector<ComplexSpectrum> & responseSpectrums, unsigned framesize, std::string & errorMsg)
+{
+	MonoAudioFileReaderConfig readerConfig;
+	readerConfig.SetSourceFile(wavfile);
+	MonoAudioFileReader reader(readerConfig);
+	if (!reader.IsConfigured())
+	{
+		errorMsg += "Configuring the inner AudioFileReader:\n";
+		errorMsg += reader.GetConfigErrorMessage();
+		return false;
+	}
+	const unsigned nSamples = reader.GetHeader().GetSamples();
+	std::cout << "NSamples: " << nSamples << std::endl;
+
+	AudioWindowingConfig windowerConfig;
+	windowerConfig.SetSamplingRate(44100); // TODO: Take it from the file
+	windowerConfig.SetHopSize(framesize);
+	windowerConfig.SetWindowSize(framesize+1);
+	windowerConfig.SetFFTSize(framesize*2);
+	windowerConfig.SetDoHalfWindowShift(false);
+	windowerConfig.SetWindowType(EWindowType::eNone);
+	AudioWindowing windower(windowerConfig);
+	if (!windower.IsConfigured())
+	{
+		errorMsg += "Configuring the inner AudioWindowing:\n";
+		errorMsg += windower.GetConfigErrorMessage();
+		return false;
+	}
+	FFTConfig fftConfig; 
+	fftConfig.SetAudioSize(framesize*2);
+	MyFFT fft(fftConfig);
+
+	ConnectPorts(reader,0,windower,0);
+	ConnectPorts(windower,0,fft,0);
+	InPort<ComplexSpectrum> fetcher;
+	fft.GetOutPorts().GetByNumber(0).ConnectToIn(fetcher);
+
+	responseSpectrums.clear();
+	reader.Start();
+	windower.Start();
+	fft.Start();
+	
+	for (bool samplesAvailable=true; samplesAvailable; )
+	{
+		samplesAvailable = reader.Do();
+		if (!windower.CanConsumeAndProduce()) continue;
+		windower.Do();
+		fft.Do();
+		responseSpectrums.push_back(fetcher.GetData());
+//			fetcher.GetData().dump(std::cout);
+		fetcher.Consume();
+	}
+
+	reader.Stop();
+	windower.Stop();
+	fft.Stop();
+	return true;
+}
+
+} //namespace CLAM
+
