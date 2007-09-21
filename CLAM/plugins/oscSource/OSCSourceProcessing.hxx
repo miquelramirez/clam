dcb04ce0a8c98fda092e195380499ad42eb2b212
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2007-09-21 18:15:04 +0000

    * plugins/oscSource added copyright and GPL notice

--- CLAM/plugins/oscSource/OSCSourceProcessing.hxx
+++ CLAM/plugins/oscSource/OSCSourceProcessing.hxx
@@ -1,6 +1,5 @@
 /*
- * Copyright (c) 2001-2004 MUSIC TECHNOLOGY GROUP (MTG)
- *                         UNIVERSITAT POMPEU FABRA
+ * Copyright (c) 2007 UNIVERSITAT POMPEU FABRA
  *
  *
  * This program is free software; you can redistribute it and/or modify
