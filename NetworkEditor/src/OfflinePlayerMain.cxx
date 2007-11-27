4ff8d51cf1d1d517b16dcc54e50e485bf3f6020a
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2007-11-27 16:07:13 +0000

    * OfflinePlayer app now lists the sources and sinks when given a clamnetwork

--- NetworkEditor/src/OfflinePlayerMain.cxx
+++ NetworkEditor/src/OfflinePlayerMain.cxx
@@ -1,6 +1,6 @@
 #include <CLAM/FreewheelingNetworkPlayer.hxx>
 #include <CLAM/XMLStorage.hxx>
-
+#include <fstream>
 #ifdef _MSVC_VER
 #include <CLAM/InitAudioIO.hxx>
 #include <CLAM/InitProcessing.hxx>
@@ -21,7 +21,7 @@ int main( int argc, char *argv[] )
 	CLAM::ProcessingModule::init();
 	CLAM::AudioIOModule::init();
 #endif
-	if (argc < 3)
+	if (argc < 2)
 		return usage(argv[0]);
 
 	std::string networkFile = argv[1];
@@ -29,6 +29,9 @@ int main( int argc, char *argv[] )
  	CLAM::XMLStorage::Restore(net, networkFile);
 	CLAM::FreewheelingNetworkPlayer * player =  new CLAM::FreewheelingNetworkPlayer;
 	net.SetPlayer( player ); // network owns the player memory
+	std::cout << "Sources and Sinks list:\n" << player->SourcesAndSinksToString() << std::endl;
+	if (argc==2)
+		return 0;
 	for (int i=2; i<argc; i++)
 	{
 		player->AddInputFile(argv[i]);
