88f9c03820bf3ebcc7f12196e0da096138efdcbb
David García Garzón <vokimon@users.noreply.github.com>
2007-12-22 01:59:14 +0000

    * nsis.py, updated to be able to use it in a crosscompiling environment

--- CLAM/scons/sconstools/nsis.py
+++ CLAM/scons/sconstools/nsis.py
@@ -1,4 +1,4 @@
-import re
+import re, os
 import SCons.Util
 nsisFiles_re = re.compile(r'^\s*File "([^"]*)"', re.M)
 
@@ -16,9 +16,12 @@ def generate(env) :
 
 	env['NSIS_MAKENSIS'] = 'makensis'
 	env['NSIS_OPTIONS'] = ''
+	def winToLocalReformat(path) :
+		return os.path.join(*path.split("\\"))
 	def scanNsisContent(node, env, path, arg):
 		contents = node.get_contents()
 		includes = nsisFiles_re.findall(contents)
+		includes = [ winToLocalReformat(include) for include in includes ]
 		return filter(lambda x: x.rfind('*')==-1, includes)
 	nsisscanner = env.Scanner(name = 'nsisfile',
 		function = scanNsisContent,
