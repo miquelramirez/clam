a49f9e6e1577fe2ba3c9c5f566f9f24bd393a338
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2008-01-22 18:11:12 +0000

    * diff now uses dBs. so it works both for int samples and float samples

--- CLAM/scripts/diff_audio_files.py
+++ CLAM/scripts/diff_audio_files.py
@@ -12,6 +12,7 @@ def run(command) :
 		print line,
 		sys.stdout.flush()
 	return "".join(lines)
+
 def silentrun(command) :
 	lines = []
 	for line in os.popen(command) :
@@ -23,22 +24,22 @@ def norun(command) :
 	print "\033[31mXX ", command, "\033[0m"
 
 
+threshold = -25.0 # dB
+
 def diff_files(expected, result) :
-
 	silentrun('sox -m -v 1 %s -v -1 %s diff.wav 2>&1 '%(expected, result))
-	max_sample = float(silentrun("sndfile-info diff.wav | awk '/Signal/ {print $4}'"))
-	print "max", max_sample
-
-	threshold = 0.00001
-
-	if max_sample > threshold :
+	max_dbs = float(silentrun("sndfile-info diff.wav | awk '/Signal/ {print $5}'")[1:])
+	if max_dbs > threshold :
 		silentrun('wav2png --input diff.wav --width 700 --linecolor ff0088 --backgroundcolor dddddd --zerocolor 000000')
 		print "Files are different with threshold", threshold
-		print "Maxi diff is :", max_sample
+		print "Max diff is :", max_dbs, "dB"
 		print "expected:",expected
 		print "result:",result
 		return False
 	return True
 
 equals = diff_files(sys.argv[1], sys.argv[2])
-print "equals?", equals
+if equals:
+	print "\033[32m Equals\033[0m"
+else:
+	print "\033[31m Non equals\033[0m"
