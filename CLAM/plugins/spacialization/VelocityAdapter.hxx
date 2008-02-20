bb81a0d0d13214bdcbe83416fb68ec33aa657541
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2008-02-20 17:35:45 +0000

    * VelocityAdapter: updated angle after change on how we export paths  * ImpulseResponseCalculatedOnTheFly fixed. Some missing TODOs

--- CLAM/plugins/spacialization/VelocityAdapter.hxx
+++ CLAM/plugins/spacialization/VelocityAdapter.hxx
@@ -43,7 +43,7 @@ public:
 
 		}
 		float beta = _betaIn.GetLastValue();
-		_betaOut.SendControl( -beta );
+		_betaOut.SendControl( beta+90. );
 		// Tell the ports this is done
 		_vxIn.Consume();
 		_vyIn.Consume();
