1f02825e43a78fd9ab834387ae80b88e2767a4b6
David García Garzón <vokimon@users.noreply.github.com>
2008-02-26 01:41:29 +0000

    * NetworkCanvas: Refactoring out general abstract class    * created the abstrac class    * non-model attributes moved up    * basic methods moved up

--- NetworkEditor/src/NetworkCanvas.cxx
+++ NetworkEditor/src/NetworkCanvas.cxx
@@ -5,25 +5,6 @@ NetworkCanvas::~NetworkCanvas()
 	clear();
 }
 
-void NetworkCanvas::clear()
-{
-	clearWires();
-	for (unsigned i = 0; i<_processings.size(); i++)
-		delete _processings[i];
-	_processings.clear();
-	update();
-}
-
-void NetworkCanvas::clearWires()
-{
-	for (unsigned i = 0; i<_portWires.size(); i++)
-		delete _portWires[i];
-	_portWires.clear();
-	for (unsigned i = 0; i<_controlWires.size(); i++)
-		delete _controlWires[i];
-	_controlWires.clear();
-}
-
 
 
 
