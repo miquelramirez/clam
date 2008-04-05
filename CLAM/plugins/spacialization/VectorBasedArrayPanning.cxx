72e0fd7e8df8dad0640ea2107d0d8db45fe4ae10
David García Garzón <vokimon@users.noreply.github.com>
2008-04-05 06:05:02 +0000

    * VectorBasedArrayPanning: proper metadata

--- CLAM/plugins/spacialization/VectorBasedArrayPanning.cxx
+++ CLAM/plugins/spacialization/VectorBasedArrayPanning.cxx
@@ -5,8 +5,8 @@ namespace detail
 {
 	static const char * metadata[] = {
 		"key", "VectorBasedArrayPanning",
-	//	"category", "CLAM",
-	//	"description", "VectorBasedArrayPanning",
+		"category", "[plugin] 3D Audio",
+		"description", "VectorBasedArrayPanning",
 		0
 	};
 	//static CLAM::FactoryRegistrator<CLAM::ProcessingFactory, VectorBasedArrayPanning> regVectorBasedArrayPanning("VectorBasedArrayPanning");
