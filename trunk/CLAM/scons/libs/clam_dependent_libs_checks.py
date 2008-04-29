be114a443d67e8c4a12ceb31a3efff34cd02eead
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2008-04-29 06:42:57 +0000

    * clam_dependent_libs_checks: id3 has nothing to do with c++  * clam_dependent_libs_checks: removed the code that set the LINKOPTION for id3 as it is used just in MSVC

--- trunk/CLAM/scons/libs/clam_dependent_libs_checks.py
+++ trunk/CLAM/scons/libs/clam_dependent_libs_checks.py
@@ -104,26 +104,18 @@ def test_mad( env, conf ) :
 def test_id3lib( env, conf ) :
 	crosscompiling=env.has_key('crossmingw') and env['crossmingw']
 
-	libName = 'id3'
-	env.Append( LIBS=[libName] )
-	#TODO: Checking whether those options are needed at all
-	"""
-	if sys.platform == 'win32' :
-		if crosscompiling :
-			env.Append( CPPFLAGS=['-DID3LIB_LINKOPTION=3'] )
-		else :
-			env.Append( CPPFLAGS=['-DID3LIB_LINKOPTION=1'] )
-	"""
 	zlib = 'z'
 	if not conf.CheckLib(zlib, 'uncompress') :
 		return config_error( "Could not link zlib. Please, check your zlib/id3lib installation" )
 
-	if not conf.CheckCXXHeader( 'id3.h' ) :
+	libName = 'id3'
+	env.Append( LIBS=[libName] )
+	if not conf.CheckCHeader( 'id3.h' ) :
 		return config_error( "Could not find id3lib headers! Please check your id3lib installation" )
 
-	if not conf.CheckLibWithHeader( libName, 'id3/tag.h', 'cxx', call='ID3_Tag myTag;' ) :
+	if not conf.CheckLibWithHeader( libName, 'id3/tag.h', 'c', call='ID3_Tag myTag;' ) :
 		return config_error( "Could not find id3lib binaries! Please check your id3lib installation" )
-	if not conf.CheckLibrarySample(libName, 'c++', None, id3lib_test_code) :
+	if not conf.CheckLibrarySample(libName, 'c', None, id3lib_test_code) :
 		return config_error( "id3lib compile/link/run tests failed!" )
 	return True
 
