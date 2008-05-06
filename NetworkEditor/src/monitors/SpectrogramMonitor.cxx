f1142c0b409b4578aa124e67a1abbd2f5d91f468
David García Garzón <vokimon@users.noreply.github.com>
2008-05-06 21:38:07 +0000

    * Spectrogram had monitor but not in the metadata, fixed.

--- NetworkEditor/src/monitors/SpectrogramMonitor.cxx
+++ NetworkEditor/src/monitors/SpectrogramMonitor.cxx
@@ -29,7 +29,7 @@ namespace Hidden
 		"category", "Graphical Monitors",
 		"description", "Spectrogram",
 		"port_monitor_type", typeid(std::vector<CLAM::TData>).name(),
-		"icon", "none.svg",
+		"icon", "spectrogram.svg",
 		0
 	};
 	static CLAM::FactoryRegistrator<CLAM::ProcessingFactory, SpectrogramMonitor> reg = metadata;
