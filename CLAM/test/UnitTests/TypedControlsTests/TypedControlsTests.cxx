e9c07245965fc4d0a97ab2867e234579515a6fcc
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2008-05-15 17:34:11 +0000

    * Francisco Trufo patch with TypedControls connection interface and tests. Thanks!

--- CLAM/test/UnitTests/TypedControlsTests/TypedControlsTests.cxx
+++ CLAM/test/UnitTests/TypedControlsTests/TypedControlsTests.cxx
@@ -14,8 +14,27 @@ namespace CLAMTest {
 		// testing TypedInControl and TypedOutControl
 		 CPPUNIT_TEST( testTypedInControl_DoControl_ChangesInternalState );
 		 CPPUNIT_TEST( testLinkAndSendControl_ChangesTypedInControlInternalState );
-		 CPPUNIT_TEST_SUITE_END();
 		
+		// tests for IsConnected / IsConnectedTo
+		CPPUNIT_TEST( testIsConnected_WithTypedOutControl_AfterConnection );
+		CPPUNIT_TEST( testIsConnected_WithTypedOutControl_WithoutConnection );
+		CPPUNIT_TEST( testIsConnectedTo_WithTypedOutControl_WhenControlsAreConnected );
+		CPPUNIT_TEST( testIsConnectedTo_WithTypedOutControl_WhenControlsAreNotConnected );
+		CPPUNIT_TEST( testTypedInControlIsConnected_byDefault );
+		CPPUNIT_TEST( testTypedInControlIsConnected_whenConnected );
+		CPPUNIT_TEST( testTypedInControlIsConnected_whenDisconnected );
+		
+		// Destructor disconnections
+		CPPUNIT_TEST( testTypedInControlDestructor_disconnectsOutControl );
+		CPPUNIT_TEST( testTypedOutControlDestructor_disconnectsInControl );
+
+		// 1 Out - Many Ins Test
+		CPPUNIT_TEST( testOneTypedOutControlManyTypedInControls_DoControl_ChangesInternalState );
+
+		
+		CPPUNIT_TEST_SUITE_END();
+		
+		// testing TypedInControl and TypedOutControl
 		void testTypedInControl_DoControl_ChangesInternalState()
 		{
 			CLAM::TypedInControl<int> in("IntInControl");
@@ -32,6 +51,88 @@ namespace CLAMTest {
 			CPPUNIT_ASSERT_EQUAL( 1 , in.GetLastValue() );
 		}
 
-	};
+		// tests for IsConnected / IsConnectedTo
+		void testIsConnected_WithTypedOutControl_AfterConnection()
+		{	
+			CLAM::TypedInControl<int> in("IntInControl");
+			CLAM::TypedOutControl<int> out("IntOutControl");
+			out.AddLink(in);
+			CPPUNIT_ASSERT_EQUAL( true, out.IsConnected() );
+		}
+
+		void testIsConnected_WithTypedOutControl_WithoutConnection()
+		{	
+			CLAM::TypedOutControl<int> out("IntOutControl");
+			CPPUNIT_ASSERT_EQUAL(  false, out.IsConnected() );		
+		}
+
+		void testIsConnectedTo_WithTypedOutControl_WhenControlsAreConnected()
+		{
+			CLAM::TypedInControl<int> in("IntInControl");
+			CLAM::TypedOutControl<int> out("IntOutControl");
+			out.AddLink(in);
+			CPPUNIT_ASSERT_EQUAL( true, out.IsConnectedTo(in) );
+		}
+
+		void testIsConnectedTo_WithTypedOutControl_WhenControlsAreNotConnected()
+		{
+			CLAM::TypedInControl<int> in("IntInControl");
+			CLAM::TypedOutControl<int> out("IntOutControl");
+			CPPUNIT_ASSERT_EQUAL( false, out.IsConnectedTo(in) );
+		}
+
+		void testTypedInControlIsConnected_byDefault()
+		{
+			CLAM::TypedInControl<int> in("IntInControl");
+			CPPUNIT_ASSERT_EQUAL(false, in.IsConnected());
+		}
+		void testTypedInControlIsConnected_whenConnected()
+		{
+			CLAM::TypedInControl<int> in("IntInControl");
+			CLAM::TypedOutControl<int> out("IntOutControl");
+			out.AddLink(in);
+			CPPUNIT_ASSERT_EQUAL(true, in.IsConnected());
+		}
+		void testTypedInControlIsConnected_whenDisconnected()
+		{
+			CLAM::TypedInControl<int> in("IntInControl");
+			CLAM::TypedOutControl<int> out("IntOutControl");
+			out.AddLink(in);
+			out.RemoveLink(in);
+			CPPUNIT_ASSERT_EQUAL(false, in.IsConnected());
+		}
 		
+		// Destructor disconnections
+		void testTypedInControlDestructor_disconnectsOutControl()
+		{
+			CLAM::TypedOutControl<int> out("IntOutControl");
+			{
+				CLAM::TypedInControl<int> in("IntInControl");
+				out.AddLink(in);
+			}
+			CPPUNIT_ASSERT_EQUAL(false, out.IsConnected());
+		}
+		void testTypedOutControlDestructor_disconnectsInControl()
+		{
+			CLAM::TypedInControl<int> in("IntInControl");
+			{
+				CLAM::TypedOutControl<int> out("IntOutControl");
+				out.AddLink(in);
+			}
+			CPPUNIT_ASSERT_EQUAL(false, in.IsConnected());
+		}
+		
+		// 1 Out - Many Ins Test
+		void testOneTypedOutControlManyTypedInControls_DoControl_ChangesInternalState()
+		{
+			CLAM::TypedOutControl<int> out("IntTypedOutControl");
+			CLAM::TypedInControl<int> in1("IntTypedInControl 1");
+			CLAM::TypedInControl<int> in2("IntTypedInControl 2");
+			out.AddLink(in1);
+			out.AddLink(in2);
+			out.SendControl(1);
+			CPPUNIT_ASSERT_EQUAL( 1 , in1.GetLastValue() );
+			CPPUNIT_ASSERT_EQUAL( 1 , in2.GetLastValue() );
+		}
+	};
 } // namespace
