75e1631ba0d149064e64056aec5b73e289f3ae36
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2008-05-20 20:18:17 +0000

    * ChoreoSequencer:  ** Added scene size config parameters  ** On bad file (and not empty filename), config error  ** Added a new output with the relative azimut and elevation from the target to the source

--- trunk/CLAM/plugins/spacialization/src/ChoreoSequencer.hxx
+++ trunk/CLAM/plugins/spacialization/src/ChoreoSequencer.hxx
@@ -13,42 +13,34 @@
 namespace CLAM
 {
 
-/*
-// Don't know how to make this class work. So use string instad --Pau
-
-class DataInFilename : public InFilename
-{
-public:
-	DataInFilename(const std::string & s="") : InFilename(s) {}
-	DataInFilename(const char * s) : InFilename(s) {}
-	const char* TypeFamily() const;
-	const Filter * Filters() const;
-};
-CLAM_TYPEINFOGROUP(BasicCTypeInfo, CLAM::DataInFilename);
-*/
-
-
 class ChoreoSequencer : public CLAM::Processing
 {
 	class Config : public CLAM::ProcessingConfig
 	{ 
-	    DYNAMIC_TYPE_USING_INTERFACE( Config, 5, ProcessingConfig );
-	    DYN_ATTRIBUTE( 0, public, InFilename, Filename);
-	    DYN_ATTRIBUTE( 1, public, unsigned, SourceIndex); // first is 0
-	    DYN_ATTRIBUTE( 2, public, unsigned, FrameSize);
-	    DYN_ATTRIBUTE( 3, public, unsigned, SampleRate);
-	    DYN_ATTRIBUTE( 4, public, unsigned, ControlsPerSecond);
+		DYNAMIC_TYPE_USING_INTERFACE( Config, 8, ProcessingConfig );
+		DYN_ATTRIBUTE( 0, public, InFilename, Filename);
+		DYN_ATTRIBUTE( 1, public, unsigned, SourceIndex); // first is 0
+		DYN_ATTRIBUTE( 2, public, unsigned, FrameSize);
+		DYN_ATTRIBUTE( 3, public, unsigned, SampleRate);
+		DYN_ATTRIBUTE( 4, public, unsigned, ControlsPerSecond);
+		DYN_ATTRIBUTE( 5, public, TData, SizeX);
+		DYN_ATTRIBUTE( 6, public, TData, SizeY);
+		DYN_ATTRIBUTE( 7, public, TData, SizeZ);
 	protected:
-	    void DefaultInit()
-	    {
-		  AddAll();
-		  UpdateData();
-		  SetFilename("");
-		  SetSourceIndex(0);
-		  SetFrameSize(512);
-		  SetSampleRate(48000);
-		  SetControlsPerSecond(15);
-	    };
+		void DefaultInit()
+		{
+			AddAll();
+			UpdateData();
+			SetFilename("");
+			SetSourceIndex(0);
+			SetFrameSize(512);
+			SetSampleRate(48000);
+			SetControlsPerSecond(15);
+			SetSizeX(1);
+			SetSizeY(1);
+			SetSizeZ(1);
+		
+		};
 	};
 
 	Config _config;
@@ -60,6 +52,8 @@ class ChoreoSequencer : public CLAM::Processing
 	OutControl _sourceX;
 	OutControl _sourceY;
 	OutControl _sourceZ;
+	OutControl _sourceAzimuth;
+	OutControl _sourceElevation;
 	AudioInPort _syncIn;
 
 	unsigned _samplesPerControl;
@@ -77,6 +71,9 @@ class ChoreoSequencer : public CLAM::Processing
 	unsigned _indexSourceX;
 	unsigned _indexSourceY;
 	unsigned _indexSourceZ;
+	double _sizeX;
+	double _sizeY;
+	double _sizeZ;
 
 public:
 	ChoreoSequencer(const Config& config = Config()) 
@@ -88,6 +85,8 @@ public:
 		, _sourceX("source X", this)
 		, _sourceY("source Y", this)
 		, _sourceZ("source Z", this)
+		, _sourceAzimuth("source azimuth", this)
+		, _sourceElevation("source elevation", this)
 		, _syncIn("sync", this)
 	{
 		Configure( config );
@@ -105,15 +104,21 @@ public:
 		if (_sampleCount>=_samplesPerControl)
 		{
 			std::cout << "("<<_sequenceIndex << "/" <<_controlSequence.size() << ")" << std::flush;
-			//TODO check that _indexTargetX,Y,Z < _controlSequence[_sequenceIndex].size()
-			_targetX.SendControl( _controlSequence[_sequenceIndex][_indexTargetX] );
-			_targetY.SendControl( _controlSequence[_sequenceIndex][_indexTargetY] );
-			_targetZ.SendControl( _controlSequence[_sequenceIndex][_indexTargetZ] );
-			_targetAzimuth.SendControl( _controlSequence[_sequenceIndex][_indexTargetAzimuth] );
-			_targetElevation.SendControl( _controlSequence[_sequenceIndex][_indexTargetElevation] );
-			_sourceX.SendControl( _controlSequence[_sequenceIndex][_indexSourceX+3*sourceIndex] );
-			_sourceY.SendControl( _controlSequence[_sequenceIndex][_indexSourceY+3*sourceIndex] );
-			_sourceZ.SendControl( _controlSequence[_sequenceIndex][_indexSourceZ+3*sourceIndex] );
+			const Row & row = _controlSequence[_sequenceIndex];
+			//TODO check that _indexTargetX,Y,Z < row.size()
+			_targetX.SendControl( row[_indexTargetX] );
+			_targetY.SendControl( row[_indexTargetY] );
+			_targetZ.SendControl( row[_indexTargetZ] );
+			_targetAzimuth.SendControl( row[_indexTargetAzimuth] );
+			_targetElevation.SendControl( row[_indexTargetElevation] );
+			_sourceX.SendControl( row[_indexSourceX+3*sourceIndex] );
+			_sourceY.SendControl( row[_indexSourceY+3*sourceIndex] );
+			_sourceZ.SendControl( row[_indexSourceZ+3*sourceIndex] );
+			double dx = _sizeX * (row[_indexSourceX+3*sourceIndex] - row[_indexTargetX]);
+			double dy = _sizeY * (row[_indexSourceY+3*sourceIndex] - row[_indexTargetY]);
+			double dz = _sizeZ * (row[_indexSourceZ+3*sourceIndex] - row[_indexTargetZ]);
+			_sourceAzimuth.SendControl( std::atan2(dx,dy) - row[_indexTargetAzimuth] );
+			_sourceElevation.SendControl( std::asin(dz) - row[_indexTargetElevation] );
 			_sampleCount -= _samplesPerControl;
 			_sequenceIndex++;
 			if (_sequenceIndex >= _controlSequence.size())
@@ -139,6 +144,9 @@ protected:
 		_sampleCount=0;
 		_frameSize=_config.GetFrameSize();
 		_sequenceIndex=0;
+		_sizeX = _config.HasSizeX()? _config.GetSizeX(): 1.;
+		_sizeY = _config.HasSizeY()? _config.GetSizeY(): 1.;
+		_sizeZ = _config.HasSizeZ()? _config.GetSizeZ(): 1.;
 
 		_controlSequence.clear();
 
@@ -184,6 +192,11 @@ protected:
 		_indexSourceZ=9; // y orig 1
 		// Load table from file
 		std::ifstream file( _config.GetFilename().c_str() );
+		if (!file)
+		{
+			AddConfigErrorMessage("Unable to open the file "+_config.GetFilename());
+			return false;
+		}
 		while (file)
 		{
 			std::string line;
