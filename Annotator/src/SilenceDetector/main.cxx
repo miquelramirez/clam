1a180edab265c5c5eb99d908dd24f022e3b959a3
David García Garzón <vokimon@users.noreply.github.com>
2008-05-22 11:36:54 +0000

    * Wang Jun patch: First skeleton for a simple SilenceDetector

---
+++ Annotator/src/SilenceDetector/main.cxx
@@ -0,0 +1,16 @@
+int main(int argc, char* argv[])			// access command line arguments
+{
+	return 0;
+}
+int main(int argc, char* argv[])			// access command line arguments
+{
+	return 0;
+}
+int main(int argc, char* argv[])			// access command line arguments
+{
+	return 0;
+}
+int main(int argc, char* argv[])			// access command line arguments
+{
+	return 0;
+}
