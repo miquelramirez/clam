ed78f1240c7fd3209970c983df2a2ac9a07801ad
David García Garzón <vokimon@users.noreply.github.com>
2008-05-23 09:22:42 +0000

    * Silence detector: i applied too many times the patch ¬¬

--- Annotator/src/SilenceDetector/main.cxx
+++ Annotator/src/SilenceDetector/main.cxx
@@ -2,15 +2,3 @@ int main(int argc, char* argv[])			// access command line arguments
 {
 	return 0;
 }
-int main(int argc, char* argv[])			// access command line arguments
-{
-	return 0;
-}
-int main(int argc, char* argv[])			// access command line arguments
-{
-	return 0;
-}
-int main(int argc, char* argv[])			// access command line arguments
-{
-	return 0;
-}
