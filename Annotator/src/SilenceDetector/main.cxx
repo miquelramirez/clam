7b2ec726dd4abd0f8cc4ecf13842574b9c2071f4
David García Garzón <vokimon@users.noreply.github.com>
2008-05-23 15:24:25 +0000

    * Wang Jun patch: Created an empty schema and dumped to std out as xml

--- Annotator/src/SilenceDetector/main.cxx
+++ Annotator/src/SilenceDetector/main.cxx
@@ -1,4 +1,11 @@
+#include <CLAM/XMLStorage.hxx>
+#include "../Schema.hxx"
+
+
 int main(int argc, char* argv[])			// access command line arguments
 {
+	CLAM_Annotator::Schema schema;
+	CLAM::XMLStorage::Dump(schema, "Schema", std::cout);
+
 	return 0;
 }
