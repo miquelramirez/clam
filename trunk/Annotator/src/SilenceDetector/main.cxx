b4ee8027c71dab772d23539945beb0a6806a541d
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2008-05-27 12:55:02 +0000

    * Patch from Wang Jun. Thanks!  ** SilenceDetector: Added attribute documentation  * SilenceDetector: Minor changes on the layout of the code

--- trunk/Annotator/src/SilenceDetector/main.cxx
+++ trunk/Annotator/src/SilenceDetector/main.cxx
@@ -12,6 +12,13 @@ int main(int argc, char* argv[])			// access command line arguments
 
 	schema.SetUri("descriptionScheme:www.iua.upf.edu:clam:JunJunSilenceDetector"); // the Uri is used to identify the kind of document.
 
+	//The documentation is the text you can see on the schema browser in Annotator
+ 	schema.AttributeDocumentation("Song", "Silences",
+		"<p>The <em>Silences</em> is a discontinuous segmentation attribute"
+		" that indicates which part of the audio is the silence</p>\n"
+		"<p><b>Data for this data is not the one in Simac but randomly generated.</b></p>\n"
+		);
+
 	CLAM::XMLStorage::Dump(schema, "Schema", std::cout);
 
 	return 0;
