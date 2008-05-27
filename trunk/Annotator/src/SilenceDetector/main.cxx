b695cfa4d9f6e16ddc1918eda280a9fd8920369e
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2008-05-27 12:41:01 +0000

    * Wang Jun patch:  ** SilenceDetector: added a segmentation attribute

--- trunk/Annotator/src/SilenceDetector/main.cxx
+++ trunk/Annotator/src/SilenceDetector/main.cxx
@@ -1,10 +1,15 @@
+#include <iostream>
 #include <CLAM/XMLStorage.hxx>
 #include "../Schema.hxx"
 
 
 int main(int argc, char* argv[])			// access command line arguments
 {
-	CLAM_Annotator::Schema schema;
+
+    CLAM_Annotator::Schema schema;
+    
+    schema.AddSegmentation("Song","Silences", CLAM_Annotator::SegmentationPolicy::eDiscontinuous, "");
+
 	CLAM::XMLStorage::Dump(schema, "Schema", std::cout);
 
 	return 0;
