685d30b860fcc546ae0ab7c7d32afd79ab412676
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2008-06-04 23:49:39 +0000

    * AudioFileMemoryLoader: This plugin is temporary placed there, it should be moved into clam when done

---
+++ trunk/CLAM/plugins/AudioFileMemoryLoader/AudioFileMemoryLoader.cxx
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2001-2004 MUSIC TECHNOLOGY GROUP (MTG)
+ *                         UNIVERSITAT POMPEU FABRA
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#include "AudioFileMemoryLoader.hxx"
+#include <CLAM/ProcessingFactory.hxx>
+
+
+namespace CLAM
+{
+
+namespace Hidden
+{
+	static const char * metadata[] = {
+		"key", "AudioFileMemoryLoader",
+		"category", "Audio File I/O",
+		"description", "AudioFileMemoryLoader",
+		0
+	};
+	static FactoryRegistrator<ProcessingFactory, AudioFileMemoryLoader> reg = metadata;
+}
+	
+	
+	AudioFileMemoryLoader::AudioFileMemoryLoader( const ProcessingConfig& cfg )
+		: _Output( "Samples Read", this ),
+		  _TimeOutput( "Current Time Position", this)
+	{
+		Configure( cfg );
+	}
+
+	AudioFileMemoryLoader::~AudioFileMemoryLoader()
+	{
+	}
+	
+	// TODO: move it to the header
+	const char* AudioFileMemoryLoader::GetClassName() const
+	{
+		return "AudioFileMemoryLoader";
+	}
+
+	const ProcessingConfig& AudioFileMemoryLoader::GetConfig() const
+	{
+		return _Config;
+	}
+
+	bool AudioFileMemoryLoader::ConcreteConfigure( const ProcessingConfig& cfgObject )
+	{
+		CopyAsConcreteConfig( _Config, cfgObject );
+
+		/*if ( !mConfig.HasSourceFile() )
+		{
+			AddConfigErrorMessage("The provided config object lacked the field 'SourceFile'");
+			return false;
+		}*/
+		
+		return true;
+	}
+
+	bool AudioFileMemoryLoader::ConcreteStart()
+	{
+		return true;
+	}
+
+	bool AudioFileMemoryLoader::Do()
+	{
+		bool result = Do( _Output.GetAudio() );
+		_Output.Produce();
+
+		return result;
+	}
+
+
+	
+	bool AudioFileMemoryLoader::Do( Audio & outputSamples )		
+	{
+		return true;
+	}
+	
+}
+
---
+++ trunk/CLAM/plugins/AudioFileMemoryLoader/AudioFileMemoryLoader.hxx
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2001-2004 MUSIC TECHNOLOGY GROUP (MTG)
+ *                         UNIVERSITAT POMPEU FABRA
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#ifndef AudioFileMemoryLoader_hxx
+#define AudioFileMemoryLoader_hxx
+
+#include <CLAM/Processing.hxx>
+#include <CLAM/MonoAudioFileReader.hxx>
+#include <CLAM/MonoAudioFileReaderConfig.hxx>
+#include <CLAM/AudioOutPort.hxx>
+#include <CLAM/OutControl.hxx>
+
+namespace CLAM
+{
+
+	class AudioFileMemoryLoader : public Processing
+	{
+	public:
+		
+		typedef MonoAudioFileReaderConfig Config;
+		
+		AudioFileMemoryLoader( const ProcessingConfig& cfg = Config() );
+   
+		virtual const char* GetClassName() const;
+		virtual const ProcessingConfig& GetConfig() const;
+		bool Do( Audio & outputSamples );
+		virtual bool Do();	
+		virtual ~AudioFileMemoryLoader();
+
+	protected: // methods
+
+		virtual bool ConcreteConfigure( const ProcessingConfig& cfgObject );
+		virtual bool ConcreteStart();
+
+	protected: // attributes
+		
+		Config _Config;
+		AudioOutPort	          _Output;
+		OutControl                _TimeOutput;
+	};
+}
+
+#endif // AudioFileMemoryLoader.hxx
