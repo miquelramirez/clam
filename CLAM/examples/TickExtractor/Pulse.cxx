a662d9ab5ae29f8d5a939303506ee5ea9b0bff5b
David García Garzón <vokimon@users.noreply.github.com>
2008-06-10 02:26:31 +0000

    * Pulse taken from clam-draft/cuidado

---
+++ CLAM/examples/TickExtractor/Pulse.cxx
@@ -0,0 +1,39 @@
+/*
+ * Author: fabien gouyon 
+ * http://www.iua.upf.es/~fgouyon
+ * Description:
+ *
+ * Syntax: C++
+ *
+ * Copyright (c) 2001-2002 MUSIC TECHNOLOGY GROUP (MTG)
+ *                         UNIVERSITAT POMPEU FABRA
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#include "Pulse.hxx"
+
+
+namespace CLAM
+{
+
+	void Pulse::DefaultInit()
+	{
+		AddAll();
+		UpdateData();
+	}
+}
