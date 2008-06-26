a79c4381a1620107fe3493f0cc3aa39ad70cdf3e
David García Garzón <vokimon@users.noreply.github.com>
2008-06-26 02:27:52 +0000

    * UpdateAnnotatorData: Locating segmentation attributes in the schema

--- Annotator/scripts/UpdateAnnotatorData.py
+++ Annotator/scripts/UpdateAnnotatorData.py
@@ -5,9 +5,15 @@ import sys
 schema = sys.argv[1]
 data = sys.argv[2:]
 
-
 print "Schema:", schema
 print "Data:", data
+
+from xml.etree import ElementTree
+schemaDoc = ElementTree.parse(file(schema))
+for line in schemaDoc.findall("//Attribute"):
+	if line.attrib['type'] != 'Segmentation' : continue
+	print line.attrib['scope'], line.attrib['name']
+	print dir(line)
 
 
 
