0ec200731700ff667c02b6ee30aa1e6527cd7d4c
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2008-07-08 16:07:00 +0000

    * Ferran's patch with first version of file player plugin. Thanks!

---
+++ CLAM/plugins/sndfile/SndfilePlayer.cxx
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2008 Fundació Barcelona Media Universitat Pompeu Fabra
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#include "SndfilePlayer.hxx"
+#include <CLAM/ProcessingFactory.hxx>
+
+static const char * metadata[] = {
+	"key", "SndfilePlayer",
+	"category", "XXMyProcessing",
+	0
+	};
+
+static CLAM::FactoryRegistrator<CLAM::ProcessingFactory, CLAM::SndfilePlayer> registratorSndfile(metadata);
+
+
+
+/*
+ * Copyright (c) 2008 Fundació Barcelona Media Universitat Pompeu Fabra
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#include "SndfilePlayer.hxx"
+#include <CLAM/ProcessingFactory.hxx>
+
+static const char * metadata[] = {
+	"key", "SndfilePlayer",
+	"category", "XXMyProcessing",
+	0
+	};
+
+static CLAM::FactoryRegistrator<CLAM::ProcessingFactory, CLAM::SndfilePlayer> registratorSndfile(metadata);
+
+
+
