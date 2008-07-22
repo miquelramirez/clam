544b26e2582975909d739bf657ee5832192dee29
junjun <junjun@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2008-07-22 04:33:41 +0000

    * Reformat the sources and map lists  * Change the absolute path into relative path

--- Annotator/scripts/AggregationExtractor.py
+++ Annotator/scripts/AggregationExtractor.py
@@ -6,19 +6,31 @@ import sys
 import os
 from optparse import OptionParser
 
-# location of the first extractor's fixed schema file and pool files
-path1="D:/devel/Annotator/example-data/fileExample/"
-# location of the second extractor's fixed schema file and pool files
-path2="D:/devel/Annotator/example-data/fileChord/"
-# location of the output aggregated pool files
-path3="D:/devel/Annotator/example-data/"
 
-sources = [("example", FileMetadataSource(path1, extractor="D:/devel/Annotator/ClamExtractorExample.exe")),
-("chord", FileMetadataSource(path2, extractor="D:/devel/Annotator/ChordExtractor.exe")),]
+sources = [("example", FileMetadataSource(path="./",
+schemaTitle="CLAMDescriptors.sc",
+poolSuffix=".pool",
+extractor="D:/devel/Annotator/ClamExtractorExample.exe")),
+("chord", FileMetadataSource(path="./",
+schemaTitle="Chords.sc",
+poolSuffix=".chords",
+extractor="D:/devel/Annotator/ChordExtractor.exe")),]
 
-map = [("Track::Player", "example", "Song::Artist"),("Track::Harte", "chord", "Song::Chords_Harte"),]
+map = [
+("Song::Frames", "example", "Song::Frames"),
+("Song::Danceable", "example", "Song::Danceability"),
+#("TrackFrame::ClamMean", "example", "Frame::Mean"),  # there is a bug or something.
+("Frame::Energy", "chord", "Frame::Energy"),
+("Frame::ChordHartePcp", "chord", "Frame::HartePcp"),
+("Frame::HarteChordCorrelation", "chord", "Frame::HarteChordCorrelation"),
+("Song::Harte", "chord", "Song::Chords_Harte"),
+# How to remain the childscope relationship while changing the scope names?
+("ExtractedChord::ChordRoot", "chord", "ExtractedChord::Root"),
+("ExtractedChord::ChordMode", "chord", "ExtractedChord::Mode"),]
+
 
 provider = MetadataSourceAggregator(sources, map)
+
 
 parser = OptionParser(
 	usage="%prog [options] audiofile1 audiofile2 ...",
@@ -38,14 +50,14 @@ parser.add_option("-f",# "--suffix",
 
 
 if options.printSchema :
-	provider.QuerySchema(provider.AvailableDescriptors()).Dump(sys.stdout)
+	target= open("./Aggregation.sc",'w')
+	provider.QuerySchema(provider.AvailableDescriptors()).Dump(target)
 	sys.exit()
 
 
 import sys
 for audiofile in args:
-	target = open(path3+audiofile+options.suffix,'w')
+	target = open(audiofile+options.suffix,'w')
 	provider.QueryDescriptors(audiofile, provider.AvailableDescriptors()).Dump(target)
 	target.close()
-
 
