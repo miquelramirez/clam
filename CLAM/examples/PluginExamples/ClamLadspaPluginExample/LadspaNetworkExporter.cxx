c1aac4a9068ddc17176b8e5041507f596433267f
David García Garzón <vokimon@users.noreply.github.com>
2008-07-29 17:52:13 +0000

    * LadspaNetworkExporter: CLAM_EXTERNAL_FILE_DATA macro moved to the header

--- CLAM/examples/PluginExamples/ClamLadspaPluginExample/LadspaNetworkExporter.cxx
+++ CLAM/examples/PluginExamples/ClamLadspaPluginExample/LadspaNetworkExporter.cxx
@@ -1,20 +1,5 @@
 #include "LadspaNetworkExporter.hxx"
 
-/// Include a given file as binary data
-/// @arg varname is the variable that will hold the var (const char[])
-/// @arg file is an absolute path or a relative path from the build point (not the source file!!)
-/// @todo Try to make CLAM_EXTERNAL_FILE_DATA work with path relatives to the source that uses the macro.
-#define CLAM_EXTERNAL_FILE_DATA(varname, file) \
-asm ( \
-"  .section \".rodata\"\n" \
-"   .type " #varname ", @object\n" \
-" " #varname ":\n" \
-"   .incbin \"" file "\"\n" \
-"   .byte 0\n" \
-"   .size " #varname ", .-" #varname "\n" \
-); \
-extern const char varname[]; \
-
 CLAM_EXTERNAL_FILE_DATA(embededNetwork, "PluginExamples/ClamLadspaPluginExample/genderChange.clamnetwork")
 
 // Ladspa Callbacks
--- CLAM/examples/PluginExamples/ClamLadspaPluginExample/LadspaNetworkExporter.hxx
+++ CLAM/examples/PluginExamples/ClamLadspaPluginExample/LadspaNetworkExporter.hxx
@@ -15,6 +15,21 @@
 #include "ControlSink.hxx"
 #include "LadspaLibrary.hxx"
 
+/// Include a given file as binary data
+/// @arg varname is the variable that will hold the var (const char[])
+/// @arg file is an absolute path or a relative path from the build point (not the source file!!)
+/// @todo Try to make CLAM_EXTERNAL_FILE_DATA work with path relatives to the source that uses the macro.
+#define CLAM_EXTERNAL_FILE_DATA(varname, file) \
+asm ( \
+"  .section \".rodata\"\n" \
+"   .type " #varname ", @object\n" \
+" " #varname ":\n" \
+"   .incbin \"" file "\"\n" \
+"   .byte 0\n" \
+"   .size " #varname ", .-" #varname "\n" \
+); \
+extern const char varname[]; \
+
 namespace CLAM
 {
 
