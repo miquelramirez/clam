1eebaad0bad6bee47daff39ca9f8e4f02381b600
hordiales <hordiales@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2008-08-07 06:33:41 +0000

    * ProcessingCodeGenerator ** +/- buttons to add/remove ports and controls ** AutoDefault to 'Generate' button ** keepAspectRatio on the clam logo ** shortcuts + tooltip added

--- trunk/CLAM/scripts/TemplatedPluginsGenerator/ProcessingCodeGenerator.py
+++ trunk/CLAM/scripts/TemplatedPluginsGenerator/ProcessingCodeGenerator.py
@@ -23,8 +23,48 @@ import TemplateGenerator
 import TemplatedPluginsGenerator
 
 (Ui_MainWindow, Ui_MainWindow_base) = uic.loadUiType('main.ui')
-
 class MainWindow(Ui_MainWindow_base, Ui_MainWindow):
+
+	class RowItemsManager():
+		""" Items/rows manager for a table """
+
+		#Table fields names
+		FPORTS_INOUT, FPORTS_TYPE, FPORTS_NAME = range(3)
+		FCONTROLS_INOUT, FCONTROLS_MIN, FCONTROLS_MAX, FCONTROLS_NAME = range(4)
+
+		def __init__(self, tableWidget, rowsWidth=[100, 200, 450]):
+			self.tableWidget = tableWidget
+
+			for i in range(len(rowsWidth)):
+				self.tableWidget.setColumnWidth(i, rowsWidth[i])
+		#__init__
+
+		def getCount(self):
+			return self.tableWidget.rowCount()
+		#getCount()
+
+		def add(self):
+			rowCount = self.tableWidget.rowCount()
+			self.tableWidget.insertRow( rowCount )
+			comboBox_InOut = QComboBox()
+			comboBox_InOut.addItem("In")
+			comboBox_InOut.addItem("Out")
+			self.tableWidget.setCellWidget(rowCount, self.FPORTS_INOUT, comboBox_InOut);
+		#add()
+
+		def remove(self):
+			self.tableWidget.removeRow( self.tableWidget.rowCount()-1 )
+		#remove()
+
+		def getItem(self, row, column):
+			return self.tableWidget.item(row, column)
+		#getItem()
+
+		def getWidget(self, row, column):
+			return self.tableWidget.cellWidget(row, column)
+		#getWidget()
+	#class RowItemsManager
+
 
 	def __init__(self, parent=None):
 		QWidget.__init__(self, parent)
@@ -35,11 +75,26 @@ class MainWindow(Ui_MainWindow_base, Ui_MainWindow):
 		self.setWindowIcon( QIcon(clam_logo_svg) )
 		self.clamLogo.setPixmap( QPixmap(clam_logo_svg) )
 
+		#Ports table setup:
+		self.ports_manager = MainWindow.RowItemsManager( self.tableWidget_Ports, rowsWidth=[100, 200, 400] )
+		self.connect( self.pushButton_Ports_Plus, SIGNAL("clicked()"), self.ports_manager.add )
+		self.connect( self.pushButton_Ports_Minus, SIGNAL("clicked()"), self.ports_manager.remove )
+
+		#Controls table setup:
+		self.controls_manager = MainWindow.RowItemsManager( self.tableWidget_Controls, rowsWidth=[100, 100, 100, 400])
+		self.connect( self.pushButton_Controls_Plus, SIGNAL("clicked()"), self.controls_manager.add )
+		self.connect( self.pushButton_Controls_Minus, SIGNAL("clicked()"), self.controls_manager.remove )
+
+		#Generate button:
 		self.connect( self.pushButton_Generate, SIGNAL("clicked()"), self.generate )
 	#__init__()
 
 	def generate(self):
-		""" Generates plugin base code """
+		""" Generates processing plugin base code """
+
+		if self.lineEdit_PluginName.text()=="":
+			self.statusBar.showMessage("Name required.")
+			return
 
 		definitions_dict = {}
 		definitions_dict["member_style"] = "_" # Member name convention. Option: "m" 
@@ -52,22 +107,30 @@ class MainWindow(Ui_MainWindow_base, Ui_MainWindow):
 
 		definitions_dict["inputs"] = list()
 		definitions_dict["outputs"] = list()
-		for index in range(1,6):
-			in_out = str( getattr(self, "comboBox_Port%i"%(index)).currentText() )
-			port_type = str( getattr(self, "lineEdit_Port%i_Type"%(index)).text() )
-			port_name = str( getattr(self, "lineEdit_Port%i_Name"%(index)).text() )
-			if port_type=="Type" or port_name=="Name": continue
+		Ports = self.ports_manager
+		for index in range(Ports.getCount()):
+			try:
+				in_out = str( Ports.getWidget(index, Ports.FPORTS_INOUT ).currentText() )
+				port_type = str( Ports.getItem(index, Ports.FPORTS_TYPE).text() )
+				port_name = str( Ports.getItem(index, Ports.FPORTS_NAME).text() )
+			except AttributeError:
+				self.statusBar.showMessage("Fields description error.")
+				return
 			if in_out=="In": definitions_dict["inputs"].append( (port_type,port_name) )
 			elif in_out=="Out": definitions_dict["outputs"].append( (port_type,port_name) )
 
 		definitions_dict["incontrols"] = list()
 		definitions_dict["outcontrols"] = list()
-		for index in range(1,6):
-			in_out = str( getattr(self, "comboBox_Control%i"%(index)).currentText() )
-			cmin = str( getattr(self, "lineEdit_Control%i_Min"%(index)).text() )
-			cmax = str( getattr(self, "lineEdit_Control%i_Max"%(index)).text() )
-			control_name = str( getattr(self, "lineEdit_Control%i_Name"%(index)).text() )
-			if control_name=="Name" or cmin=="Min" or cmax=="Max": continue
+		Controls = self.controls_manager
+		for index in range(Controls.getCount()):
+			try:
+				in_out = str( Controls.getWidget(index, Controls.FCONTROLS_INOUT ).currentText() )
+				cmin = str( Controls.getItem(index, Controls.FCONTROLS_MIN).text() )
+				cmax = str( Controls.getItem(index, Controls.FCONTROLS_MAX).text() )
+				control_name = str( Controls.getItem(index, Controls.FCONTROLS_NAME).text() )
+			except AttributeError:
+				self.statusBar.showMessage("Fields description error.")
+				return
 			if in_out=="In": definitions_dict["incontrols"].append( (cmin,cmax,control_name) )
 			elif in_out=="Out": definitions_dict["outcontrols"].append( (cmin,cmax,control_name) )
 
@@ -88,7 +151,7 @@ class MainWindow(Ui_MainWindow_base, Ui_MainWindow):
 		TemplatedPluginsGenerator.make_file( definitions_dict, standard_name, "SConstruct", "SConstruct", True )
 		TemplatedPluginsGenerator.copy_file( definitions_dict, "README" )
 
-		print "Done"; self.statusBar().showMessage("Done")
+		print "Done"; self.statusBar.showMessage("Done")
 	#generate()
 
 #class MainWindow
@@ -100,6 +163,4 @@ def main(args):
 	sys.exit( app.exec_() )
 
 if __name__ == '__main__':
-    main(sys.argv)
-
-
+	main(sys.argv)
