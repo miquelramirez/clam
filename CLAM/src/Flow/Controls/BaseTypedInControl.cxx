c61f902c35ccb4ac449aa511753d17251ab5f705
nictuku <nictuku@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2008-08-09 17:23:02 +0000

    * TYPEDCONTROLS REFACTORING: Added functions to handle bounding stuff with BaseTypedInControl, and its tests.

--- CLAM/src/Flow/Controls/BaseTypedInControl.cxx
+++ CLAM/src/Flow/Controls/BaseTypedInControl.cxx
@@ -1,10 +1,15 @@
 #include <CLAM/BaseTypedInControl.hxx>
 #include <CLAM/Processing.hxx>
 
-namespace CLAM{
+namespace CLAM
+{
 	BaseTypedInControl::BaseTypedInControl(const std::string &name, Processing * proc)
 		: mName(name)
 		, mProcessing(proc)
+		, mUpperBound(1)
+		, mLowerBound(0)
+		, mBounded(false)
+		, mHasDefaultValue(false)
 	{
 		if (proc) proc->RegisterTypedInControl(this);
 	}
@@ -13,5 +18,33 @@ namespace CLAM{
 	{
 		if (mProcessing)
 			mProcessing->GetTypedInControls().ProcessingInterface_Unregister(this);
+	}
+	bool BaseTypedInControl::IsBounded() const
+	{
+		return mBounded;
+	}
+	float BaseTypedInControl::UpperBound() const
+	{
+		return mUpperBound;
+	}
+	float BaseTypedInControl::LowerBound() const
+	{
+		return mLowerBound;
+	}
+	void BaseTypedInControl::SetBounds( float lower, float upper )
+	{
+		mLowerBound = lower;
+		mUpperBound = upper;
+		mBounded = true;
+	}
+	void BaseTypedInControl::SetDefaultValue( float val )
+	{
+		mDefaultValue = val;
+		mHasDefaultValue = true;
+	}
+	float BaseTypedInControl::DefaultValue() const
+	{
+		if (mHasDefaultValue) return mDefaultValue;
+		return (mUpperBound+mLowerBound)/2.f;
 	}
 }
