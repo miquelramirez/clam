d916c85292d1e2624e1f07b6b60203ba2bf3ddc2
nolaiz <nolaiz@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2008-08-11 01:40:32 +0000

    * RunTimeLibraryLoader: deleted static CurrentLibrary()

--- CLAM/src/Processing/Plugins/RunTimeLibraryLoader.hxx
+++ CLAM/src/Processing/Plugins/RunTimeLibraryLoader.hxx
@@ -18,13 +18,6 @@ public:
 	static void * LazyLoadLibrary(const std::string & libraryPath);
 	static bool ReleaseLibraryHandler(void * handle, const std::string pluginFullFilename="");
 	static std::string LibraryLoadError();
-
-	static std::string & CurrentLibrary()
-	{
-		static std::string sCurrentLibrary;
-		return sCurrentLibrary;
-	}
-
 	static std::string FileOfSymbol (void * symbolAddress);
 protected:
 	void LoadLibrariesFromPath(const std::string & path) const;
