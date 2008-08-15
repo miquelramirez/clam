848ed4dc5f6e152d072f5feb0542925237c571ae
nolaiz <nolaiz@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2008-08-15 04:56:00 +0000

    * LibloSource: shutting down the server on remove processing. More stylished spaces -> tabs

--- trunk/CLAM/plugins/osc/LibloSource.cxx
+++ trunk/CLAM/plugins/osc/LibloSource.cxx
@@ -11,8 +11,8 @@ static CLAM::FactoryRegistrator<CLAM::ProcessingFactory, CLAM::LibloSource> regi
 
 void CLAM::LibloSource::error(int num, const char *msg, const char *path)
 {
-    printf("liblo server error %d in path %s: %s\n", num, path, msg);
-    fflush(stdout);
+	printf("liblo server error %d in path %s: %s\n", num, path, msg);
+	fflush(stdout);
 }
 
 
@@ -21,43 +21,42 @@ void CLAM::LibloSource::error(int num, const char *msg, const char *path)
 int CLAM::LibloSource::generic_handler(const char *path, const char *types, lo_arg **argv,
 		    int argc, void *data, void *user_data)
 {
-    int i;
-
-    printf("path: <%s>\n", path);
-    for (i=0; i<argc; i++) {
-	printf("arg %d '%c' ", i, types[i]);
-	lo_arg_pp((lo_type)types[i], argv[i]);
+	int i;
+	printf("path: <%s>\n", path);
+	for (i=0; i<argc; i++) 
+	{
+		printf("arg %d '%c' ", i, types[i]);
+		lo_arg_pp((lo_type)types[i], argv[i]);
+		printf("\n");
+	}
 	printf("\n");
-    }
-    printf("\n");
-    fflush(stdout);
-    return 1;
+	fflush(stdout);
+	return 1;
 }
 
 int CLAM::LibloSource::controls_handler(const char *path, const char *types, lo_arg **argv, int argc,
 		 void *data, void *user_data)
 {
-    CLAM::LibloSource & self = *((CLAM::LibloSource*)user_data);
-    /* example showing pulling the argument values out of the argv array */
-    printf("%s <- f:",path);
-    for (int i=0;i<argc;i++)
-    {
-	if(i>0) 
-		printf(", ");
-	printf ("%f", argv[i]->f);
-	self._outControls[i].SendControl( argv[i]->f );
-    }
-    printf("\n\n");
-    fflush(stdout);
-    return 0;
+	CLAM::LibloSource & self = *((CLAM::LibloSource*)user_data);
+	/* example showing pulling the argument values out of the argv array */
+	printf("%s <- f:",path);
+	for (int i=0;i<argc;i++)
+	{
+		if(i>0) 
+			printf(", ");
+		printf ("%f", argv[i]->f);
+		self._outControls[i].SendControl( argv[i]->f );
+	}
+	printf("\n\n");
+	fflush(stdout);
+	return 0;
 }
 
 int CLAM::LibloSource::quit_handler(const char *path, const char *types, lo_arg **argv, int argc,
 		 void *data, void *user_data)
 {
-    printf("quiting\n\n");
-    fflush(stdout);
-
-    return 0;
+	printf("quiting\n\n");
+	fflush(stdout);
+	return 0;
 }
 
