3769dadf0ce6d2528a5f10179d2292bbeed92347
nolaiz <nolaiz@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2008-08-17 05:31:09 +0000

    * network_scene_exporter: some clean up

--- trunk/CLAM/plugins/spacialization/blender/network_scene_exporter.py
+++ trunk/CLAM/plugins/spacialization/blender/network_scene_exporter.py
@@ -5,39 +5,37 @@ Blender: 244
 Group: 'Object'
 """
 header="""<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
-<network id="Blender_exported_scene">
-"""
+<network id="Blender_exported_scene">"""
 
-controlPrinter=""" <processing id="NameOfControlPrinter" position="XPosition,YPosition" size="291,285" type="ControlPrinter">
+controlPrinter="""
+ <processing id="%(printerName)s" position="%(XLPosition)f,%(YPPosition)f" size="291,285" type="ControlPrinter">
     <Identifier>ControlPrinter</Identifier>
     <NumberOfInputs>3</NumberOfInputs>
     <GuiOnly>1</GuiOnly>
-  </processing>
-"""
-
-libloSource="""<processing id="NameOfMultiLibloSource" position="XPosition,YPosition" size="130,65" type="MultiLibloSource">
-    <OscPath>/SpatDIF/SUBPATH/xyz</OscPath>
+ </processing>"""
+libloSource="""
+ <processing id="%(libloName)s" position="%(XLPosition)f,%(YLPosition)f" size="130,65" type="MultiLibloSource">
+    <OscPath>/SpatDIF/%(objectType)s/%(objectNumber)s/xyz</OscPath>
     <ServerPort>7000</ServerPort>
     <NumberOfArguments>3</NumberOfArguments>
-  </processing>
-"""
+ </processing>"""
 connection="""
-  <control_connection>
-    <out>NameOfMultiLibloSource_xyz_0</out>
-    <in>NameOfControlPrinter.ControlPrinter_0</in>
-  </control_connection>
+ <control_connection>
+   <out>%(libloName)s._SpatDIF_%(objectType)s_%(objectNumber)s_xyz_0</out>
+   <in>printer_%(libloName)s.ControlPrinter_0</in>
+ </control_connection>
 
-  <control_connection>
-    <out>NameOfMultiLibloSource_xyz_1</out>
-    <in>NameOfControlPrinter.ControlPrinter_1</in>
-  </control_connection>
+ <control_connection>
+   <out>%(libloName)s._SpatDIF_%(objectType)s_%(objectNumber)s_xyz_1</out>
+   <in>printer_%(libloName)s.ControlPrinter_1</in>
+ </control_connection>
 
-  <control_connection>
-    <out>NameOfMultiLibloSource_xyz_2</out>
-    <in>NameOfControlPrinter.ControlPrinter_2</in>
-  </control_connection>
-"""
-endHeader="""</network>"""
+ <control_connection>
+   <out>%(libloName)s._SpatDIF_%(objectType)s_%(objectNumber)s_xyz_2</out>
+   <in>printer_%(libloName)s.ControlPrinter_2</in>
+ </control_connection>"""
+endHeader="""
+</network>"""
 
 
 # use bpy module of Blender:
@@ -53,26 +51,32 @@ def write(filename):
 	printers=""
 	#print header
 	i=1
-	XPosition=100
-	YPosition=100
+	XLPosition=100
+	YLPosition=100
+	YPPosition=300
+	objectType='sources'
 	for source in data.groups['AudioSources'].objects:
-		#print source.name
 		libloName="source_"+source.name
 		printerName="printer_source_"+source.name
-		liblos+=libloSource.replace("NameOfMultiLibloSource",libloName).replace("SUBPATH","sources/"+str(i)).replace("XPosition",str(XPosition)).replace("YPosition",str(YPosition))
-		printers+=controlPrinter.replace("NameOfControlPrinter",printerName).replace("XPosition",str(XPosition)).replace("YPosition",str(YPosition+200))
-		connections+=connection.replace("NameOfMultiLibloSource",libloName+"._SpatDIF_sources_"+str(i)).replace("NameOfControlPrinter","printer_"+libloName)
+		objectNumber=str(i)
+		liblos+=libloSource % vars()
+		printers+=controlPrinter % vars()
+		connections+=connection % vars()
 		i+=1
-		XPosition+=300
+		XLPosition+=300
+
+	objectType='sinks'
 	i=1
 	for sink in data.groups['AudioSinks'].objects:
 		libloName="sink_"+sink.name
 		printerName="printer_sink_"+sink.name
-		liblos+=libloSource.replace("NameOfMultiLibloSource",libloName).replace("SUBPATH","sinks/"+str(i)).replace("XPosition",str(XPosition)).replace("YPosition",str(YPosition))
-		printers+=controlPrinter.replace("NameOfControlPrinter",printerName).replace("XPosition",str(XPosition)).replace("YPosition",str(YPosition+200))
-		connections+=connection.replace("NameOfMultiLibloSource",libloName+"._SpatDIF_sinks_"+str(i)).replace("NameOfControlPrinter","printer_"+libloName)		
+		objectNumber=str(i)
+		liblos+=libloSource % vars()
+		printers+=controlPrinter % vars()
+		connections+=connection % vars()
 		i+=1
-		XPosition+=300
+		XLPosition+=300
+
 	f=open(filename,'w')
 	f.write(header+liblos+printers+connections+endHeader)
 	f.close()
