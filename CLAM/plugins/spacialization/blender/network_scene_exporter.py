937db5ff70aa8411ba5f07d68dc28e7368e1d6b6
nolaiz <nolaiz@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2008-08-21 04:50:15 +0000

    * scene_maker: imports choreofile_exporter_with_network as internal text in the generated blender scene  * network_scene_exporter: general refactoring, added new processings (for choreofile) and port connections  * choreofile_exporter_with_network: uses network_scene_exporter for generate the network. Uses rotation angles of the source as azimuth/elevation

--- CLAM/plugins/spacialization/blender/network_scene_exporter.py
+++ CLAM/plugins/spacialization/blender/network_scene_exporter.py
@@ -20,41 +20,44 @@ def write(filename):
 	xPosition=50
 	yPosition=150
 	path="/SpatDIF/sources/%(objectNumber)s/xyz"
+	port=7000
 	objectsList=list(data.groups['AudioSources'].objects)
 	for source in data.groups['AudioSources'].objects:
 		libloName="source_"+source.name
 		printerName="printer_source_"+source.name
 		objectNumber=str(objectsList.index(source))
-		liblos+=makeLibloSource(libloName,path % vars(),xPosition,yPosition,3,7000)
-		printers+=makeControlPrinter(printerName,xPosition,yPosition+150,3)
-
+		liblos+=makeLibloSource(libloName,path % vars(),(xPosition,yPosition),3,port)
+		printers+=makeControlPrinter(printerName,(xPosition,yPosition+150),3)
 		for o in range(3):
 			sufix="_"+str(o)
-			connections+=makeConnection(libloName,(path%vars()).replace("/","_")+sufix,printerName,"ControlPrinter"+sufix)
+			connections+=makeControlConnection(libloName,(path%vars()).replace("/","_")+sufix,printerName,"ControlPrinter"+sufix)
 		xPosition+=300
 
+	xPosition=50
+	yPosition=600
 	objectsList=list(data.groups['AudioSinks'].objects)
 	path="/SpatDIF/sinks/%(objectNumber)s/xyz"
 	for sink in data.groups['AudioSinks'].objects:
 		libloName="sink_"+sink.name
 		printerName="printer_sink_"+sink.name
 		objectNumber=str(objectsList.index(sink))
-		liblos+=makeLibloSource(libloName,path % vars(),xPosition,yPosition,3,7000)
-		printers+=makeControlPrinter(printerName,xPosition,yPosition+150,3)
+		liblos+=makeLibloSource(libloName,path % vars(),(xPosition,yPosition),3,port)
+		printers+=makeControlPrinter(printerName,(xPosition,yPosition+150),3)
 		for o in range(3):
 			sufix="_"+str(o)
-			connections+=makeConnection(libloName,(path%vars()).replace("/","_")+sufix,printerName,"ControlPrinter"+sufix)
+			connections+=makeControlConnection(libloName,(path%vars()).replace("/","_")+sufix,printerName,"ControlPrinter"+sufix)
 		xPosition+=300
 
 	path="/SpatDIF/sync/FrameChanged"
 	libloName="sync_Framechanged"
 	printerName="printer_sync_FrameChanged"
-	liblos+=makeLibloSource(libloName,path,50,0,1,7000)
-	printers+=makeControlPrinter(printerName,300,0,1,291,70)
-	connections+=makeConnection(libloName,path.replace("/","_")+"_0",printerName,"In Control")
-
+	liblos+=makeLibloSource(libloName,path,(50,0),1,port)
+	printers+=makeControlPrinter(printerName,(300,0),1,(291,70))
+	connections+=makeControlConnection(libloName,path.replace("/","_")+"_0",printerName,"In Control")
 	f=open(filename,'w')
-	f.write(header+liblos+printers+connections+endHeader)
+	networkId="Exported_Blender_scene_receiver_network"
+	header=Header % vars()
+	f.write(header+liblos+printers+connections+Tail)
 	f.close()
 	print "OSC receivers CLAM Network exported as %(filename)s" % vars()
 
@@ -62,35 +65,76 @@ def write(filename):
 if __name__ == '__main__':
 	main()
 
-def makeLibloSource(libloName,path,xPosition,yPosition,argumentsNumber,port=7000):
-	return libloSource % vars()
-def makeControlPrinter(printerName,xPosition,yPosition,argumentsNumber,width=300,height=285):
-	return controlPrinter % vars()
+def makeAudioSource(processingName,position,size=(130,65)):
+	processingPosition="%i,%i" % (position[0],position[1])
+	processingSize="%i,%i" % (size[0],size[1])	
+	return AudioSource % vars()
 
-def makeConnection(sourceName,outName,targetName,inName):
-	outDescriptor="%(sourceName)s.%(outName)s" % vars()
-	inDescriptor="%(targetName)s.%(inName)s" % vars()
-	return connection % vars()
+def makeChoreoSequencer(processingName,choreoFilename,fps,position,size=(194,65)):
+	processingPosition="%i,%i" % (position[0],position[1])
+	processingSize="%i,%i" % (size[0],size[1])
+	return ChoreoSequencer % vars()
 
-header="""<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
-<network id="Blender_exported_scene">"""
-endHeader="""
-</network>"""
+def makeLibloSource(processingName,path,position,argumentsNumber,port=7000,size=(130,65)):
+	processingPosition="%i,%i" % (position[0],position[1])
+	processingSize="%i,%i" % (size[0],size[1])
+	return LibloSource % vars()
+	
+def makeControlPrinter(processingName,position,argumentsNumber,size=(300,285)):
+	processingPosition="%i,%i" % (position[0],position[1])
+	processingSize="%i,%i" % (size[0],size[1])
+	printerId="ControlPrinter"
+	return ControlPrinter % vars()
 
-controlPrinter="""
- <processing id="%(printerName)s" position="%(xPosition)i,%(yPosition)i" size="%(width)i,%(height)i" type="ControlPrinter">
-    <Identifier>ControlPrinter</Identifier>
+def makeControlConnection(sourceName,outName,targetName,inName):
+	return ControlConnection % vars()
+
+def makePortConnection(sourceName,outName,targetName,inName):
+	return PortConnection % vars()
+
+Header="""<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
+<network id="%(networkId)s">"""
+Tail="""
+</network>
+"""
+ControlPrinter="""
+  <processing id="%(processingName)s" position="%(processingPosition)s" size="%(processingSize)s" type="ControlPrinter">
+    <Identifier>%(printerId)s</Identifier>
     <NumberOfInputs>%(argumentsNumber)s</NumberOfInputs>
     <GuiOnly>1</GuiOnly>
- </processing>"""
-libloSource="""
- <processing id="%(libloName)s" position="%(xPosition)i,%(yPosition)i" size="130,65" type="MultiLibloSource">
+  </processing>
+  """
+LibloSource="""
+  <processing id="%(processingName)s" position="%(processingPosition)s" size="%(processingSize)s" type="MultiLibloSource">
     <OscPath>%(path)s</OscPath>
     <ServerPort>%(port)s</ServerPort>
     <NumberOfArguments>%(argumentsNumber)s</NumberOfArguments>
- </processing>"""
-connection="""
- <control_connection>
-   <out>%(outDescriptor)s</out>
-   <in>%(inDescriptor)s</in>
- </control_connection>"""
+  </processing>
+  """
+ControlConnection="""
+  <control_connection>
+    <out>%(sourceName)s.%(outName)s</out>
+    <in>%(targetName)s.%(inName)s</in>
+  </control_connection>
+  """
+PortConnection="""
+  <port_connection>
+    <out>%(sourceName)s.%(outName)s</out>
+    <in>%(targetName)s.%(inName)s</in>
+  </port_connection>
+  """
+ChoreoSequencer="""
+  <processing id="%(processingName)s" position="%(processingPosition)s" size="%(processingSize)s" type="ChoreoSequencer">
+    <Filename>%(choreoFilename)s</Filename>
+    <SourceIndex>0</SourceIndex>
+    <FrameSize>512</FrameSize>
+    <SampleRate>48000</SampleRate>
+    <ControlsPerSecond>%(fps)i</ControlsPerSecond>
+    <SizeX>1</SizeX>
+    <SizeY>1</SizeY>
+    <SizeZ>1</SizeZ>
+  </processing>
+  """
+AudioSource="""
+  <processing id="%(processingName)s" position="%(processingPosition)s" size="%(processingSize)s" type="AudioSource"/>
+  """
