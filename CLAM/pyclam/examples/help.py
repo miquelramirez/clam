eaf586be6e3c1f262fc5288d9129859ec3f7dcff
hordiales <hordiales@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2008-09-19 03:26:15 +0000

    pyclam: * Doxygen documentation extractor script fixed and improved. Now most embedded documentation at C++ sources are accessible from python through the help() command or the .__doc__ member. * help.py example added

---
+++ CLAM/pyclam/examples/help.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import clam
+
+print
+print "== SendFloatToInControl() method doxygen documentation ==\n"
+print clam.SendFloatToInControl.__doc__
+print "\n\n"
+
+raw_input( "Press Enter to see \'help(clam.SendFloatToInControl)\' output." ) 
+help(clam.SendFloatToInControl)
+
+print
+print "== InControl.GetName() method doxygen documentation ==\n"
+print clam.InControl.GetName.__doc__
+print "\n\n"
+
+raw_input( "Press Enter to see \'help(clam.FFT_ooura)\' output." ) 
+help(clam.FFT_ooura)
+
