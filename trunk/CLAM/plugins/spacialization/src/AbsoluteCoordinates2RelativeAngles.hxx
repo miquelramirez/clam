8133dd2f6bc037f2027f13c3ab90a9f1796a4673
forriols <forriols@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2008-10-09 12:10:49 +0000

    **AbsoluteCoordinates2RelativeAngles.hxx This processing has the input and output control for the Roll value. Now it doesn't process it, but it will. Changes the name of the elevation for zenit but only the variable targetZenith and its cos and sin.

--- trunk/CLAM/plugins/spacialization/src/AbsoluteCoordinates2RelativeAngles.hxx
+++ trunk/CLAM/plugins/spacialization/src/AbsoluteCoordinates2RelativeAngles.hxx
@@ -18,27 +18,31 @@ class AbsoluteCoordinates2RelativeAngles : public CLAM::Processing
 	CLAM::InControl _targetX;
 	CLAM::InControl _targetY;
 	CLAM::InControl _targetZ;
-	CLAM::InControl _targetAzimuth;
+	CLAM::InControl _targetRoll;
 	CLAM::InControl _targetElevation;
+	CLAM::InControl _targetAzimuth;
+
 	CLAM::InControl _sourceX;
 	CLAM::InControl _sourceY;
 	CLAM::InControl _sourceZ;
-
-	CLAM::OutControl _sourceAzimuth;
+	CLAM::OutControl _sourceRoll;
 	CLAM::OutControl _sourceElevation;
+	CLAM::OutControl _sourceAzimuth;
 public:
 	const char* GetClassName() const { return "AbsoluteCoordinates2RelativeAngles"; }
 	AbsoluteCoordinates2RelativeAngles(const Config& config = Config()) 
 		: _targetX("target X", this)
 		, _targetY("target Y", this)
 		, _targetZ("target Z", this)		
-		, _targetAzimuth("target azimuth", this)
+		, _targetRoll("target roll", this)
 		, _targetElevation("target elevation", this)
+		, _targetAzimuth("target azimuth", this)
 		, _sourceX("source X", this)
 		, _sourceY("source Y", this)
 		, _sourceZ("source Z", this)		
-		, _sourceAzimuth("source azimuth", this)
+		, _sourceRoll("source roll", this)
 		, _sourceElevation("source elevation", this)
+		, _sourceAzimuth("source azimuth", this)
 	{
 		Configure( config );
 	}
@@ -46,50 +50,38 @@ public:
 	bool Do()
 	{	
 
-/*		const Row & row = _controlSequence[_sequenceIndex];
-		//TODO check that _indexTargetX,Y,Z < row.size()
-		_targetX.SendControl( row[TargetXColumn] );
-		_targetY.SendControl( row[TargetYColumn] );
-		_targetZ.SendControl( row[TargetZColumn] );*/
-//		_targetAzimuth.SendControl( row[TargetAzimutColumn] );
-//		_targetElevation.SendControl( row[TargetElevationColumn] );
-/*		_sourceX.SendControl( row[SourceXColumn+3*sourceIndex] );
-		_sourceY.SendControl( row[SourceYColumn+3*sourceIndex] );
-		_sourceZ.SendControl( row[SourceZColumn+3*sourceIndex] );*/
-
 		float sourceX = _sourceX.GetLastValue();
 		float sourceY = _sourceY.GetLastValue();
 		float sourceZ = _sourceZ.GetLastValue();
 		float targetX = _targetX.GetLastValue();
 		float targetY = _targetY.GetLastValue();
 		float targetZ = _targetZ.GetLastValue();
+		double targetRoll = _targetRoll.GetLastValue();
+		//We sum pi/2 because the matrix is for zenith and no elevation. TODO change zenith for elevation
+		double targetZenith = _targetElevation.GetLastValue()+PI/2; 
 		double targetAzimuth = _targetAzimuth.GetLastValue();
-		double targetElevation = _targetElevation.GetLastValue()+PI/2; 
-//		double targetRoll = 0;
 		double dx = (sourceX - targetX);
 		double dy = (sourceY - targetY);
 		double dz = (sourceZ - targetZ);
 		double cosAzimuth = std::cos(targetAzimuth);
 		double sinAzimuth = std::sin(targetAzimuth);
-		double cosElevation = std::cos(targetElevation);
-		double sinElevation = std::sin(targetElevation);
+		double cosZenith = std::cos(targetZenith);
+		double sinZenith = std::sin(targetZenith);
 //		double cosRoll = std::cos(targetRoll);
 //		double sinRoll = std::sin(targetRoll);
 
-//		double rotatedX = + cosAzimuth*sinElevation * dx + sinAzimuth * dy 	+ cosAzimuth*cosElevation * dz;
-//		double rotatedY = - sinAzimuth*sinElevation * dx + cosAzimuth * dy 	- sinAzimuth*cosElevation * dz;
-//		double rotatedZ = - cosElevation * dx 		 + /* 0 * vy[i] */  	+ sinElevation  * dz;
-
-		double rotatedX = + cosAzimuth*sinElevation * dx + sinAzimuth * sinElevation * dy+ cosElevation * dz;
+		double rotatedX = + cosAzimuth*sinZenith * dx + sinAzimuth * sinZenith * dy+ cosZenith * dz;
 		double rotatedY = - sinAzimuth * dx + cosAzimuth * dy - 0 * dz;
-		double rotatedZ = - cosElevation * cosAzimuth *dx + -cosElevation*sinAzimuth*dy 	+ sinElevation  * dz;
+		double rotatedZ = - cosZenith * cosAzimuth *dx + -cosZenith*sinAzimuth*dy 	+ sinZenith  * dz;
 
 		// TODO: Test that with target elevation and azimut
 		double dazimut = 180./M_PI*std::atan2(rotatedY,rotatedX);
 		double delevation = 180./M_PI*std::asin(rotatedZ/std::sqrt(rotatedX*rotatedX+rotatedY*rotatedY+rotatedZ*rotatedZ));
-		_sourceAzimuth.SendControl( dazimut );
+
+		//TODO calculate the roll relative between the listener and the source
+		_sourceRoll.SendControl( targetRoll );
 		_sourceElevation.SendControl( delevation );
-		
+		_sourceAzimuth.SendControl( dazimut );
 		return true;
 	}
 
