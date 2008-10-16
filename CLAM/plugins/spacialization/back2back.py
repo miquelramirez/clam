9639e3ac190d65e7708e92a1e534b7eedab6dbb7
David García Garzón <vokimon@users.noreply.github.com>
2008-10-16 12:49:52 +0000

    * spacialization/back2back: more tests

--- CLAM/plugins/spacialization/back2back.py
+++ CLAM/plugins/spacialization/back2back.py
@@ -4,15 +4,29 @@ import sys
 sys.path.append('../../../CLAM/scripts/')
 from audiob2b import runBack2BackProgram
 
-# Pau, quan tinguem un directori pels fitxers b2b_spatialization -> b2b/spatialization
-
 back2BackTests = [
-	("mono2bformat", "OfflinePlayer example-data/mono2bformat.clamnetwork jaume.wav -o -c 4 output.wav", [
-			("b2b_spacialization_mono2bformat_output", "output.wav"),
+	("mono2bformat",
+		"OfflinePlayer example-data/mono2bformat.clamnetwork jaume.wav -o -c 4 bformat.wav"
+		, [
+			"bformat.wav",
+		]),
+	("bformat2binaural",
+		"OfflinePlayer example-data/mono2bformat.clamnetwork jaume.wav -o -c 4 bformat.wav;"
+		"OfflinePlayer example-data/bformat2binaural.clamnetwork bformat.wav -o -c 2 binaural.wav;"
+		, [
+			"bformat.wav",
+			"binaural.wav",
+		]),
+	("bformat2surround",
+		"OfflinePlayer example-data/mono2bformat.clamnetwork jaume.wav -o -c 4 bformat.wav;"
+		"OfflinePlayer example-data/bformat2surround.clamnetwork bformat.wav -o -c 2 surround.wav;"
+		, [
+			"bformat.wav",
+			"surround.wav",
 		]),
 ]
 
-runBack2BackProgram(sys.argv, back2BackTests)
+runBack2BackProgram("b2b", sys.argv, back2BackTests)
 
 
 
