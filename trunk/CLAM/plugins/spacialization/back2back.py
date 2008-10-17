98d4013aa88daa62f3406e58d32f406b62de88b5
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2008-10-17 18:42:05 +0000

    * back2back.py fixed input file

--- trunk/CLAM/plugins/spacialization/back2back.py
+++ trunk/CLAM/plugins/spacialization/back2back.py
@@ -3,6 +3,7 @@
 """
 Data for this B2B test can be found at the clam-test-data repository.
 Create a link to clam-test-data/b2b to this directory.
+Create links to acustica/bformat2binaural/{Ew,Ex,Ey,Ez} to this directory.
 """
 
 
@@ -14,18 +15,18 @@ data_path="b2b/spacialization"
 
 back2BackTests = [
 	("mono2binaural",
-		"OfflinePlayer example-data/mono2binaural.clamnetwork jaume.wav -o -c 2 binaural.wav"
+		"OfflinePlayer example-data/mono2binaural.clamnetwork b2b/spacialization/inputs/jaume.wav -o -c 2 binaural.wav"
 		, [
 			"binaural.wav",
 		]),
 	("mono2bformat",
-		"OfflinePlayer example-data/mono2bformat.clamnetwork jaume.wav -o -c 4 bformat.wav"
+		"OfflinePlayer example-data/mono2bformat.clamnetwork b2b/spacialization/inputs/jaume.wav -o -c 4 bformat.wav"
 		, [
 			"bformat.wav",
 		]),
 	# TODO: take an existing bformat
 	("bformat2binaural",
-		"OfflinePlayer example-data/mono2bformat.clamnetwork jaume.wav -o -c 4 bformat.wav;"
+		"OfflinePlayer example-data/mono2bformat.clamnetwork b2b/spacialization/inputs/jaume.wav -o -c 4 bformat.wav;"
 		"OfflinePlayer example-data/bformat2binaural.clamnetwork bformat.wav -o -c 2 binaural.wav;"
 		, [
 			"bformat.wav",
@@ -33,7 +34,7 @@ back2BackTests = [
 		]),
 	# TODO: take an existing bformat
 	("bformat2surround",
-		"OfflinePlayer example-data/mono2bformat.clamnetwork jaume.wav -o -c 4 bformat.wav;"
+		"OfflinePlayer example-data/mono2bformat.clamnetwork b2b/spacialization/inputs/jaume.wav -o -c 4 bformat.wav;"
 		"OfflinePlayer example-data/bformat2surround.clamnetwork bformat.wav -o -c 2 surround.wav;"
 		, [
 			"bformat.wav",
