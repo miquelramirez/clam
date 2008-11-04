c325280bc593a80fadf92e3e077ebfa9389c97f9
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2008-11-04 17:27:12 +0000

    * clam_testfarm_client.py : PATH env var is now defined

--- CLAM/scripts/clam_testfarm_client.py
+++ CLAM/scripts/clam_testfarm_client.py
@@ -21,15 +21,10 @@ def ellapsedTime():
 	return time.time() - startTime
 
 HOME = os.environ['HOME']
+PATH = os.environ['PATH']
 os.environ['LD_LIBRARY_PATH']='%s/testfarm_sandboxes/local/lib:/usr/local/lib' % HOME
+os.environ['PATH']='%s/testfarm_sandboxes/local/bin:%s' % (HOME, PATH)
 os.environ['CLAM_PLUGIN_PATH']='%s/testfarm_sandboxes/local/lib/clam' % HOME
-
-def set_qtdir_to_qt4(x) :
-	os.environ['QTDIR']=localDefinitions['qt4dir']
-	if os.environ['QTDIR'] and sys.platform=='darwin':
-		os.environ['PKG_CONFIG_PATH']=os.environ['QTDIR']+'lib/pkgconfig'
-def set_qtdir_to_qt3(x) :
-	os.environ['QTDIR']=localDefinitions['qt3dir']
 
 localDefinitions = {
 	'name': 'linux_ubuntu_hardy',
@@ -53,7 +48,6 @@ clam.set_check_for_new_commits(
 		checking_cmd='cd %(sandbox)s && svn status -u | grep \*'%localDefinitions,
 		minutes_idle=15
 )
-
 clam.add_subtask( 'List of new commits', [
 	'cd %(sandbox)s'%localDefinitions,
 	{CMD:'svn log -r BASE:HEAD', INFO: lambda x:x },
@@ -66,6 +60,8 @@ clam.add_subtask('count lines of code', [
 	{CMD:'echo %(sandbox)s/NetworkEditor'%localDefinitions, STATS: lambda x: {'networkeditor_loc': countLines(x) } },
 ] )
 clam.add_deployment( [
+	{ CMD:'echo path $PATH ld library path $LD_LIBRARY_PATH clam plugin path $CLAM_PLUGIN_PATH', INFO: lambda x:x },
+	{STATUS_OK: lambda x:False},
 	'cd %(sandbox)s/CLAM'%localDefinitions,
 	'rm -rf %(installPath)s/*'%localDefinitions,
 	'cd %(sandbox)s/CLAM/'%localDefinitions,
@@ -93,13 +89,11 @@ clam.add_subtask('Functional Tests', [
 	{STATS : lambda x: {'exectime_functests' : ellapsedTime()} },
 ] )
 clam.add_subtask('CLAM Examples', [
-	{CMD: 'echo set QTDIR to qt4', INFO: set_qtdir_to_qt4},
 	'cd %(sandbox)s/CLAM/examples'%localDefinitions,
 	'scons clam_prefix=%(installPath)s'%localDefinitions,
 ] )
 
 clam.add_subtask('SMSTools installation', [
-	{CMD: 'echo setting QTDIR to qt3 path ', INFO: set_qtdir_to_qt3},
 	'cd %(sandbox)s/SMSTools'%localDefinitions,
 	'scons prefix=%(installPath)s clam_prefix=%(installPath)s'%localDefinitions,
 	'scons install',
@@ -108,28 +102,23 @@ clam.add_subtask('SMSTools installation', [
 
 
 clam.add_subtask('vmqt4 compilation and examples', [
-	{CMD: 'echo setting QTDIR to qt4 path ', INFO: set_qtdir_to_qt4},
 	'cd %(sandbox)s/Annotator/vmqt'%localDefinitions,
 	'scons prefix=%(installPath)s clam_prefix=%(installPath)s release=1 double=1'%localDefinitions,
 	'scons examples',
 ] )
 clam.add_subtask('Annotator installation', [
-	{CMD: 'echo setting QTDIR to qt4 path ', INFO: set_qtdir_to_qt4},
 	'cd %(sandbox)s/Annotator'%localDefinitions,
 	'scons clam_vmqt4_path=vmqt prefix=%(installPath)s clam_prefix=%(installPath)s'%localDefinitions,
 	'scons install',
 ] )
 
 clam.add_subtask('NetworkEditor installation', [
-	{CMD: 'echo setting QTDIR to qt4 path ', INFO: set_qtdir_to_qt4},
 	'cd %(sandbox)s/NetworkEditor'%localDefinitions,
 	'scons prefix=%(installPath)s clam_prefix=%(installPath)s'%localDefinitions,
 	'scons install',
-	'%(sandbox)s/CLAM/scons/sconstools/changeExampleDataPath.py %(installPath)s/share/smstools '%localDefinitions,
 ] )
 
 clam.add_subtask("CLAM Plugins", [
-	{CMD: 'echo setting QTDIR to qt4 path ', INFO: set_qtdir_to_qt4},
 	'cd %(sandbox)s/CLAM/plugins/spacialization'%localDefinitions,
 	'scons clam_prefix=%(installPath)s raytracing=0'%localDefinitions,
 	'scons install',
@@ -150,8 +139,8 @@ clam.add_subtask("CLAM Plugins", [
 	'scons clam_prefix=%(installPath)s'%localDefinitions,
 	'scons install',
 ] )
+
 clam.add_subtask('Back-to-back network tests', [
-	{CMD: 'echo setting QTDIR to qt4 path ', INFO: set_qtdir_to_qt4},
 	'cd %(sandbox)s/CLAM/plugins/spacialization'%localDefinitions,
 	'./back2back.py',
 ] )
@@ -165,7 +154,7 @@ clam.add_subtask('Padova Speech SMS (external repository)', [
 
 Runner( clam, 
 	continuous = False,
-	first_run_always = False,
+	first_run_always = True,
 	remote_server_url = 'http://localhost/testfarm_server'
 #	local_base_dir='/tmp'
 )
