68d7ec133c1327a0468bc9edb64cd1e95e1a6071
nolaiz <nolaiz@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2008-11-04 11:21:50 +0000

    * AudioMixer: added a HasDefaultGains check to avoid asserts

--- CLAM/src/Processing/ArithOps/AudioMixer.cxx
+++ CLAM/src/Processing/ArithOps/AudioMixer.cxx
@@ -56,13 +56,17 @@ void AudioMixer::CreatePortsAndControls()
 		
 		mInputControls.push_back( new FloatInControl("Gain " + number.str(), this) );
 	}
-
-	CLAM::Array<TControlData> gainsArray=mConfig.GetDefaultGains();
+	CLAM::Array<TControlData> gainsArray;
+	bool useConfigGains = mConfig.HasDefaultGains() ? true : false;
+	if (useConfigGains)
+		gainsArray=mConfig.GetDefaultGains();
 	for( int i=0; i<mConfig.GetNumberOfInPorts(); i++ )
 	{
-		/* Set gain = 1 by default */
-//		mInputControls[i]->DoControl(1.);
-		mInputControls[i]->DoControl(gainsArray[i]);
+		if (useConfigGains)
+			mInputControls[i]->DoControl(gainsArray[i]);
+		else
+			/* Set gain = 1 by default */
+			mInputControls[i]->DoControl(1.);
 	}
 	
 	mOutputPort.SetSize( mConfig.GetFrameSize());
