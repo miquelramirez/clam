3a477fa8074bc0521ca51deba6a0dd8c38c36046
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2008-11-07 15:59:57 +0000

    * spacialization/back2back.py improved set up

--- trunk/CLAM/plugins/spacialization/back2back.py
+++ trunk/CLAM/plugins/spacialization/back2back.py
@@ -4,12 +4,20 @@
 Data for this B2B test can be found at the clam-test-data repository.
 Create a link to clam-test-data/b2b to this directory.
 Create links to acustica/bformat2binaural/{Ew,Ex,Ey,Ez} to this directory.
+
+Needed setup:
+ cd ~/acustica/HRTFs/ && ./prepareAll.sh 
+ cd ~/acustica/bformat2binaural/ && ./generateAllEquivalent.sh 
 """
 
-
-import sys
+import sys, os
 sys.path.append('../../../CLAM/scripts/')
 from audiob2b import runBack2BackProgram
+
+
+os.system('rm E?.wav')
+for i in 'xyzw' :
+	os.system('ln -s ~/acustica/bformat2binaural/equivalentIRs/MIT/full-L/E%s.wav .'%i)
 
 data_path="b2b/spacialization"
 
