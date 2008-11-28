f6ebea5adc42dcc3e9c0d89d31dccfdc6d7270a5
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2008-11-28 11:35:24 +0000

    * clam_testfarm_client: checking and updating sandboxes other than clam

--- trunk/CLAM/scripts/clam_testfarm_client.py
+++ trunk/CLAM/scripts/clam_testfarm_client.py
@@ -26,18 +26,18 @@ os.environ['LD_LIBRARY_PATH']='%s/testfarm_sandboxes/local/lib:/usr/local/lib' %
 os.environ['PATH']='%s/testfarm_sandboxes/local/bin:%s' % (HOME, PATH)
 os.environ['CLAM_PLUGIN_PATH']='%s/testfarm_sandboxes/local/lib/clam' % HOME
 
-localDefinitions = {
-	'name': 'linux_ubuntu_hardy',
-	'description': '<img src="http://clam.iua.upf.es/images/linux_icon.png"/> <img src="http://clam.iua.upf.es/images/ubuntu_icon.png"/>',
-	'sandbox': '$HOME/testfarm_sandboxes/clam',
-	'installPath': '$HOME/testfarm_sandboxes/local',
-	'qt3dir':'',
-	'qt4dir':'',
-}
+localDefinitions = dict(
+	name= 'linux_ubuntu_hardy',
+	description= '<img src="http://clam.iua.upf.es/images/linux_icon.png"/> <img src="http://clam.iua.upf.es/images/ubuntu_icon.png"/>',
+	sandbox= '$HOME/testfarm_sandboxes',
+	installPath= '$HOME/testfarm_sandboxes/local',
+	qt3dir='',
+	qt4dir='',
+)
 
 client = Client(localDefinitions['name'])
 client.brief_description = localDefinitions['description']
-	
+
 
 clam = Task(
 		project = Project('CLAM','<a href="http://clam.iua.upf.edu">clam web</a>' ), 
@@ -45,73 +45,77 @@ clam = Task(
 	task_name='svn up|DEBUG' 
 	)
 clam.set_check_for_new_commits( 
-		checking_cmd='cd %(sandbox)s && svn status -u | grep \*'%localDefinitions,
+		checking_cmd='cd %(sandbox)s && svn status -u clam acustica data_acustica clam/testdata | grep \*'%localDefinitions,
 		minutes_idle=15
 )
 clam.add_subtask( 'List of new commits', [
-	'cd %(sandbox)s'%localDefinitions,
-	{CMD:'svn log -r BASE:HEAD', INFO: lambda x:x },
-	{CMD: 'svn up', INFO: lambda x:x },
+	'cd %(sandbox)s/'%localDefinitions,
+	] + [
+		{CMD: 'echo "== Log for repository %(repo)s ==" ; svn log -r BASE:HEAD %(repo)s'%repo , INFO: lambda x:x }
+		for repo in "clam acustica data_acustica clam/testdata".split() 
+	] + [
+		{CMD: 'svn up repo'%repo, INFO: lambda x:x }
+		for repo in "clam acustica data_acustica clam/testdata".split()
 	] )
 
 clam.add_subtask('count lines of code', [
-	{CMD:'echo %(sandbox)s/CLAM'%localDefinitions, STATS: lambda x: {'clam_loc': countLines(x) } },
-	{CMD:'echo %(sandbox)s/SMSTools'%localDefinitions, STATS: lambda x: {'smstools_loc': countLines(x) } },
-	{CMD:'echo %(sandbox)s/NetworkEditor'%localDefinitions, STATS: lambda x: {'networkeditor_loc': countLines(x) } },
+	{CMD:'echo %(sandbox)s/clam/CLAM'%localDefinitions, STATS: lambda x: {'clam_loc': countLines(x) } },
+	{CMD:'echo %(sandbox)s/clam/SMSTools'%localDefinitions, STATS: lambda x: {'smstools_loc': countLines(x) } },
+	{CMD:'echo %(sandbox)s/clam/NetworkEditor'%localDefinitions, STATS: lambda x: {'networkeditor_loc': countLines(x) } },
 ] )
 clam.add_deployment( [
-	'cd %(sandbox)s/CLAM'%localDefinitions,
+	'cd %(sandbox)s/clam/CLAM'%localDefinitions,
 	'rm -rf %(installPath)s/*'%localDefinitions,
-	'cd %(sandbox)s/CLAM/'%localDefinitions,
+	'cd %(sandbox)s/clam/CLAM/'%localDefinitions,
 	'scons configure prefix=%(installPath)s release=0'%localDefinitions,
 	'scons',
 	'scons install',
 ] )
 
 clam.add_subtask('Unit Tests', [
-	'cd %(sandbox)s/CLAM'%localDefinitions,
+	'cd %(sandbox)s/clam/CLAM'%localDefinitions,
 	'cd test',
-	'scons test_data_path=%(sandbox)s/testdata clam_prefix=%(installPath)s'%localDefinitions, # TODO: test_data_path and release
+	'scons test_data_path=%(sandbox)s/clam/testdata clam_prefix=%(installPath)s'%localDefinitions, # TODO: test_data_path and release
 	'cd UnitTests',
 	{INFO : lambda x:startTimer() }, 
 	{CMD: './UnitTests'},
 	{STATS : lambda x:{'exectime_unittests' : ellapsedTime()} },
 ] )
 clam.add_subtask('Functional Tests', [
-	'cd %(sandbox)s/CLAM'%localDefinitions,
+	'cd %(sandbox)s/clam/CLAM'%localDefinitions,
 	'cd test',
-	'scons test_data_path=%(sandbox)s/testdata clam_prefix=%(installPath)s'%localDefinitions, # TODO: test_data_path and release
+	'scons test_data_path=%(sandbox)s/clam/testdata clam_prefix=%(installPath)s'%localDefinitions, # TODO: test_data_path and release
 	'cd FunctionalTests',
 	{INFO : lambda x:startTimer() }, 
 	{CMD:'./FunctionalTests'},
 	{STATS : lambda x: {'exectime_functests' : ellapsedTime()} },
 ] )
 clam.add_subtask('CLAM Examples', [
-	'cd %(sandbox)s/CLAM/examples'%localDefinitions,
+	'cd %(sandbox)s/clam/CLAM/examples'%localDefinitions,
 	'scons clam_prefix=%(installPath)s'%localDefinitions,
 ] )
 
 clam.add_subtask('SMSTools installation', [
-	'cd %(sandbox)s/SMSTools'%localDefinitions,
+	'cd %(sandbox)s/clam/SMSTools'%localDefinitions,
 	'scons prefix=%(installPath)s clam_prefix=%(installPath)s'%localDefinitions,
 	'scons install',
-	'%(sandbox)s/CLAM/scons/sconstools/changeExampleDataPath.py %(installPath)s/share/smstools '%localDefinitions,
+	'%(sandbox)s/clam/CLAM/scons/sconstools/changeExampleDataPath.py %(installPath)s/share/smstools '%localDefinitions,
 ] )
 
 
 clam.add_subtask('vmqt4 compilation and examples', [
-	'cd %(sandbox)s/Annotator/vmqt'%localDefinitions,
+	'cd %(sandbox)s/clam/Annotator/vmqt'%localDefinitions,
 	'scons prefix=%(installPath)s clam_prefix=%(installPath)s release=1 double=1'%localDefinitions,
 	'scons examples',
 ] )
 clam.add_subtask('Annotator installation', [
-	'cd %(sandbox)s/Annotator'%localDefinitions,
+	'cd %(sandbox)s/clam/Annotator'%localDefinitions,
 	'scons clam_vmqt4_path=vmqt prefix=%(installPath)s clam_prefix=%(installPath)s'%localDefinitions,
 	'scons install',
 ] )
 
 clam.add_subtask('NetworkEditor installation', [
-	'cd %(sandbox)s/NetworkEditor'%localDefinitions,
+	'cd %(sandbox)s/clam/NetworkEditor'%localDefinitions,
 	'scons prefix=%(installPath)s clam_prefix=%(installPath)s'%localDefinitions,
 	'scons install',
 ] )
@@ -119,49 +123,49 @@ clam.add_subtask('NetworkEditor installation', [
 
 
 clam.add_subtask('BM-Audio update', [
-	'cd ~/data_acustica',
+	'cd %(sandbox)s/data_acustica'%localDefinitions,
 	{CMD:'svn log -r BASE:HEAD', INFO: lambda x:x },
-	'cd ~/acustica',
+	'cd %(sandbox)s/acustica'%localDefinitions,
 	{CMD:'svn log -r BASE:HEAD', INFO: lambda x:x },
-	'cd ~/acustica/raytracing',
+	'cd %(sandbox)s/acustica/raytracing'%localDefinitions,
 	'scons',
 ] )
 
 
 clam.add_subtask("CLAM Plugins", [
-	'cd %(sandbox)s/CLAM/plugins/spacialization'%localDefinitions,
+	'cd %(sandbox)s/clam/CLAM/plugins/spacialization'%localDefinitions,
 	'scons clam_prefix=%(installPath)s raytracing=0'%localDefinitions,
 	'scons install',
 
-	'cd %(sandbox)s/CLAM/plugins/spacialization'%localDefinitions,
+	'cd %(sandbox)s/clam/CLAM/plugins/spacialization'%localDefinitions,
 	'scons clam_prefix=%(installPath)s raytracing=1'%localDefinitions,
 	'scons install',
 
-	'cd %(sandbox)s/CLAM/plugins/continuousExcitationSynthesizer'%localDefinitions,
+	'cd %(sandbox)s/clam/CLAM/plugins/continuousExcitationSynthesizer'%localDefinitions,
 	'scons clam_prefix=%(installPath)s'%localDefinitions,
 	'scons install',
 
-	'cd %(sandbox)s/CLAM/plugins/osc'%localDefinitions,
+	'cd %(sandbox)s/clam/CLAM/plugins/osc'%localDefinitions,
 	'scons clam_prefix=%(installPath)s'%localDefinitions,
 	'scons install',
 
-	'cd %(sandbox)s/CLAM/plugins/speech'%localDefinitions,
+	'cd %(sandbox)s/clam/CLAM/plugins/speech'%localDefinitions,
 	'scons clam_prefix=%(installPath)s'%localDefinitions,
 	'scons install',
 ] )
 clam.add_subtask('Back-to-back network tests', [
-	'cd %(sandbox)s/CLAM/plugins/spacialization'%localDefinitions,
+	'cd %(sandbox)s/clam/CLAM/plugins/spacialization'%localDefinitions,
 	'./back2back.py',
 ] )
 clam.add_subtask('BM-Audio tests back-to-back', [
-	'cd ~/data_acustica/test_coreos',
+	'cd %(sandbox)s/data_acustica/test_coreos'%localDefinitions,
 	'./back2back.py',
-	'cd ~/acustica/bformat2binaural',
+	'cd %(sandbox)s/acustica/bformat2binaural'%localDefinitions,
 	'./back2back.py',
 ] )
 
 clam.add_subtask('Padova Speech SMS (external repository)', [
-	'cd %(sandbox)s/padova-speech-sms/'%localDefinitions,
+	'cd %(sandbox)s/clam/padova-speech-sms/'%localDefinitions,
 	{CMD:'svn log -r BASE:HEAD', INFO: lambda x:x },
 	{CMD: 'svn up', INFO: lambda x:x },
 	'make',
