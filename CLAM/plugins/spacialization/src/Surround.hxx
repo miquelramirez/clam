8c0a424349ec36f4ec4fbe7e91249e4057e937aa
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2008-12-01 16:47:29 +0000

    * Fixed NSurround so now uses ambisonics components not velocitise  * Renamed ports vx -> X etc (also added migration script)  * Removed beta control from NSurround and Surround  * Surround encoder angles now are -45 45 -135 135 instad of ITU angles

--- CLAM/plugins/spacialization/src/Surround.hxx
+++ CLAM/plugins/spacialization/src/Surround.hxx
@@ -8,15 +8,18 @@
  ITU 5.1 Surround speaker system with channels
  L(30), R(-30), C(0), SL(110), SR(-110).
 
- @param[in] beta [Control] Angle in degrees for a simulated rotation of the receiver in the xy plane to the right.
- @param[in] p [Port] Pressure to be reproduced at the sweet-spot
- @param[in] vx [Port] X component of the velocity to be reproduced at the sweet-spot
- @param[in] vy [Port] Y component of the velocity to be reproduced at the sweet-spot
+ @param[in] W [Port] W ambisonics component.
+ @param[in] X [Port] X ambisonics component.
+ @param[in] Y [Port] Y ambisonics component.
  @param[out] center [Port] Output for channel  placed at 0 degrees.
- @param[out] right [Port] Output for channel  placed at -30 degrees.
- @param[out] surroundRight [Port] Output for channel  placed at -110 degrees.
- @param[out] surroundLeft [Port] Output for channel  placed at 110 degrees.
- @param[out] left [Port] Output for channel  placed at 30 degrees.
+ @param[out] right [Port] Output for channel  placed at -45 degrees.
+ @param[out] surroundRight [Port] Output for channel  placed at -135 degrees.
+ @param[out] surroundLeft [Port] Output for channel  placed at 135 degrees.
+ @param[out] left [Port] Output for channel  placed at 45 degrees.
+
+ The encoding angles are not the ITU angles but -45, 45, 135, -135.
+ @see AmbisonicsConventions
+
  @todo Is that what is called ITU 5.1?
  @todo Control value beta, controls a simulated rotation of the receiver
  @todo Review whether the input should be ambisonics or pressure/velocity
@@ -33,18 +36,18 @@ public:
 	{
 		Configure( config );
 		_nChannels = 5;
-		_beta.SetBounds(-360, 360); //a complete spin on each slider direction
 
 		struct SpeakerPositions {
 			const char * name;
 			int angle;
 		} speaker[] =
 		{
+		// The following angles are "encoding" angles not ITU exhibiton setup angles.
 			{"center", 0},
-			{"right", -30},
-			{"surroundRight", -110},
-			{"surroundLeft", 110},
-			{"left", 30},
+			{"right", -45},
+			{"surroundRight", -135},
+			{"surroundLeft", 135},
+			{"left", 45},
 			{0,0}
 		};
 		
@@ -55,7 +58,7 @@ public:
 			double rad = M_PI*angle/180;
 			_sinAlphas.push_back( std::sin(rad) );
 			_cosAlphas.push_back( std::cos(rad) );
-			std::cout << "sin "<< angle << " "<< std::sin(rad) << std::endl;
+//			std::cout << "sin "<< angle << " "<< std::sin(rad) << std::endl;
 
 			CLAM::AudioOutPort * port = new CLAM::AudioOutPort( speaker[i].name, this);
 			port->SetSize( buffersize );
@@ -64,12 +67,12 @@ public:
 		}
 		_nChannels = _outputs.size();
 		
-		_p.SetSize(buffersize);
-		_p.SetHop(buffersize);
-		_vx.SetSize(buffersize);
-		_vx.SetHop(buffersize);
-		_vy.SetSize(buffersize);
-		_vy.SetHop(buffersize);
+		_w.SetSize(buffersize);
+		_w.SetHop(buffersize);
+		_x.SetSize(buffersize);
+		_x.SetHop(buffersize);
+		_y.SetSize(buffersize);
+		_y.SetHop(buffersize);
 
 	}
 	bool ConcreteConfigure(const CLAM::ProcessingConfig& config)
