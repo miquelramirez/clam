60c30bd383f77a44c956a16685b1babd3850bbdf
nolaiz <nolaiz@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2008-12-09 13:16:28 +0000

    * spacialization/src/ChoreoSequencer: expect version 1.1 of coreo files  * spacialization/src/RoomImpulseResponseSimulator: expect values in meters (positives) instead normalized values between 0-1

--- trunk/CLAM/plugins/spacialization/src/ChoreoSequencer.hxx
+++ trunk/CLAM/plugins/spacialization/src/ChoreoSequencer.hxx
@@ -298,7 +298,7 @@ protected:
 			return false;
 		}
 
-		std::string filetype = "ClamChoreoVersion 1";
+		std::string filetype = "ClamChoreoVersion 1.1";
 		std::string firstLine;
 		std::getline(file,firstLine);
 		if (firstLine != ("#"+filetype))
