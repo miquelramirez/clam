3f6c38ca712e38aeb5ae676cf4701f69d608364a
nolaiz <nolaiz@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2008-12-10 00:12:43 +0000

    * CLAM/scripts/diff_audio_files: using soxsucks[1] --compare to get max difference, with position. [1] to be added in short?...

--- CLAM/scripts/diff_audio_files.py
+++ CLAM/scripts/diff_audio_files.py
@@ -36,22 +36,31 @@ def diff_files(expected, result, diffbase) :
 		print "Expected file not found: ", result
 		return "No expectation for the output. Check the results and accept them with the --accept option."
 	diffwav = diffbase+'.wav'
-	substractResult = silentrun('sox -m -v 1 %s -v -1 %s %s 2>&1 && echo OK '%(expected, result, diffwav))
+	substractResult = silentrun('soxsucks --compare %f %s %s 2>&1 && echo OK '%(threshold_amplitude, expected, result))
 	if 'OK' not in substractResult :
-		print "files substraction with sox failed. They might not be comparable: (different lenght or sample-rate:", expected, result
-		return "Non comparable waves (different lenght, channels or sample-rate)"
-		
-	errorString = silentrun("soxsucks --max-value '%s'" % diffwav)
-	max_amplitude = abs(float(errorString))
-	if not max_amplitude : return None
-
-	max_dBs = 20*math.log10(max_amplitude)
-	if max_dBs > threshold_dBs :
-		silentrun('wav2png --input %s --width 700 --linecolor ff0088 --backgroundcolor dddddd --zerocolor 000000'%(diffwav))
-		print "Files are different with threshold ", threshold_dBs, "(dB's)", threshold_amplitude, "(amplitude)"
-		print "Max diff is :", max_amplitude, "(amplitude)", max_dBs, "(dB's)"
-		return "Differences of %s [%s dB] (threshold: %s [%s dB])" % (
-			max_amplitude, max_dBs, threshold_amplitude, threshold_dBs)
+		substractResult= [line for line in substractResult.split("\n") if line.find("there is a different sample (")!=-1]
+		if substractResult==[]:
+			print "files substraction with sox failed. They might not be comparable: (different lenght or sample-rate: %s %s)" % (expected, result)
+			return "Non comparable waves (different lenght, channels or sample-rate)"
+		substractResult="".join(substractResult)
+# TODO: REFACTOR!!
+		stringToSearch="amplitude diff: "
+		errorString=substractResult[substractResult.find(stringToSearch)+len(stringToSearch):].split(")")[0]
+		stringToSearch="on second "
+		timeErrorString=substractResult[substractResult.find(stringToSearch)+len(stringToSearch):].split("\n")[0]
+		stringToSearch="in the sample "
+		sampleString=substractResult[substractResult.find(stringToSearch)+len(stringToSearch):].split(",")[0]
+		silentrun('sox -m -v 1 %s -v -1 %s %s 2>&1 && echo OK '%(expected, result, diffwav))
+#####
+		max_amplitude = abs(float(errorString))
+		if not max_amplitude : return None
+		max_dBs = 20*math.log10(max_amplitude)
+		if max_dBs > threshold_dBs :
+			silentrun('wav2png --input %s --width 700 --linecolor ff0088 --backgroundcolor dddddd --zerocolor 000000'%(diffwav))
+			print "Files are different with threshold ", threshold_dBs, "(dB's)", threshold_amplitude, "(amplitude)"
+			print "Max diff is :", max_amplitude, "(amplitude)", max_dBs, "(dB's)"
+			return "Differences of %s [%s dB] (threshold: %s [%s dB]) at %s seconds (sample %s)" % (
+				max_amplitude, max_dBs, threshold_amplitude, threshold_dBs, timeErrorString, sampleString)
 	return None
 
 
