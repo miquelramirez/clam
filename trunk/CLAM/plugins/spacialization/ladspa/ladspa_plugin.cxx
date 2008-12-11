5d6a6a94df3625f0e46fe601a5f3d8677e7714d2
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2008-12-11 15:38:43 +0000

    * ladspa plugins: enabled working plugins only

--- trunk/CLAM/plugins/spacialization/ladspa/ladspa_plugin.cxx
+++ trunk/CLAM/plugins/spacialization/ladspa/ladspa_plugin.cxx
@@ -1,14 +1,25 @@
+
 #include <CLAM/LadspaNetworkExporter.hxx>
 #include <CLAM/LadspaLibrary.hxx>
 static CLAM::LadspaLibrary library;
 
 CLAM_EXTERNAL_FILE_DATA(mono2bformat_embededNetwork,"../example-data/mono2bformat.clamnetwork")
 CLAM_EXTERNAL_FILE_DATA(bformatRotationZoom_embededNetwork,"../example-data/mono2bformat.clamnetwork")
+CLAM_EXTERNAL_FILE_DATA(bformat2surround_embededNetwork,"../example-data/bformat2surround.clamnetwork")
 CLAM_EXTERNAL_FILE_DATA(bformat2binaural_embededNetwork,"../example-data/bformat2binaural.clamnetwork")
 CLAM_EXTERNAL_FILE_DATA(mono2binaural_embededNetwork,"../example-data/mono2binaural.clamnetwork")
+CLAM_EXTERNAL_FILE_DATA(lalanetwork,"../lala.clamnetwork")
+
+#include <iostream>
 
 extern "C" const LADSPA_Descriptor * ladspa_descriptor(unsigned long index)
 {
+
+#if 0
+CLAM::Network network;
+CLAM::XMLStorage::Restore( network, "../example-data/bformat2binaural.clamnetwork");
+return 0;
+#endif
 
 	static CLAM::LadspaNetworkExporter n100(library, mono2bformat_embededNetwork, 100,
 			"mono2bformat", "Mono to BFormat",
@@ -18,13 +29,19 @@ extern "C" const LADSPA_Descriptor * ladspa_descriptor(unsigned long index)
 			"bformat_rotation_zoom", "BFormat Rotation and Zoom",
 			"BarcelonaMedia-Audio", "GNU GPL");
 /*
-	static CLAM::LadspaNetworkExporter n102(library, bformat2binaural_embededNetwork, 102,
+	static CLAM::LadspaNetworkExporter n102(library, bformat2surround_embededNetwork, 102,
+			"bformat2surround", "BFormat to Surround 5.0",
+			"BarcelonaMedia-Audio", "GNU GPL");
+	static CLAM::LadspaNetworkExporter n103(library, bformat2binaural_embededNetwork, 103,
 			"bformat2binaural", "BFormat to binaural (HRTF)",
 			"BarcelonaMedia-Audio", "GNU GPL");
-
-	static CLAM::LadspaNetworkExporter n103(library, mono2binaural_embededNetwork, 103,
+	static CLAM::LadspaNetworkExporter n104(library, mono2binaural_embededNetwork, 104,
 			"mono2binaural", "Mono to binaural (HRTF)",
 			"BarcelonaMedia-Audio", "GNU GPL");
+	static CLAM::LadspaNetworkExporter n105(library, lalanetwork, 105,
+			"dummy", "dummy network with configurations",
+			"BarcelonaMedia-Audio", "GNU GPL");
 */
+
 	return library.pluginAt(index);
 }
