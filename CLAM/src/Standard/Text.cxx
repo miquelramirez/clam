4fa2efd0132a3ab671f751115aab651658889b7e
David García Garzón <vokimon@users.noreply.github.com>
2008-12-18 00:13:28 +0000

    * Text: deserialization turned the stream into failure status.    Not having to converge with MSVC the elegant and correct implementation has been recovered.

--- CLAM/src/Standard/Text.cxx
+++ CLAM/src/Standard/Text.cxx
@@ -29,25 +29,9 @@ namespace CLAM
 
 	std::istream & operator >> (std::istream & stream, Text & text)
 	{
-#if 0
-		stream.unsetf(std::ios::skipws);
-		text assign(
-			(std::istream_iterator<char>(stream)),
-			std::istream_iterator<char>());
-#elif 0
 		text.assign(
 			(std::istreambuf_iterator<char>(stream)),
 			std::istreambuf_iterator<char>());
-#elif 1
-		text = "";
-		char buffer[1024];
-		stream.read( buffer, 1023 );
-		do 
-		{
-			text.append( buffer, stream.gcount() );
-		}
-		while( stream.read( buffer, 1023 ) );
-#endif
 		return stream;
 
 	}
