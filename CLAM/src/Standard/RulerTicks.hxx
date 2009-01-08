bdfbc54d6ef35497c37206b68c9cecf5ba557fe3
David García Garzón <vokimon@users.noreply.github.com>
2009-01-08 10:55:43 +0000

    * RulerTicks: Lazy and cached evaluation (minimizes the computation, and does not compute until first needed)  * RulerTicks: Getters to use the computed values (centralizes outside replicated code)  * RulerTicks: Doxygenated

--- CLAM/src/Standard/RulerTicks.hxx
+++ CLAM/src/Standard/RulerTicks.hxx
@@ -5,64 +5,118 @@
 namespace CLAM
 {
 
+/*
+	Implements the logic to place graphically several markers
+	in a ruler so that the markers are placed with a given
+	minimal separation and corresponds to round numbers
+	on the mapped domain interval.
+	So, given a domain value range, a pixel size and a minimal pixel gap,
+	it gives you the domain offset for the first tick and the intertick gap
+	also in domain units.
+	Chosen round numbers are series so that the last non zero
+	decimal steps in 1, 2 or 5 jumps and include 0. For instance:
+	- 3.1, 3.2, 3.3...
+	- 22, 24, 26...
+	- 0.35, 0.40, 0.45...
+*/
 class RulerTicks
 {
+		mutable bool _needsUpdate;
+
 		double _min;
 		double _max;
-		double _markGap;
 		double _minGap;
 		double _width;
+
+		mutable double _markOffset;
+		mutable double _markGap;
 	public:
+		RulerTicks()
+			: _needsUpdate(false)
+			, _min(0)
+			, _max(1)
+			, _minGap(5)
+			, _width(100)
+		{
+		}
 		/// Sets the domain value range
 		void setRange(double min, double max)
 		{
+			CLAM_ASSERT(min<max, "RulerTicks: Empty or inverse order range");
 			_min = min;
 			_max = max;
+			_needsUpdate = true;
 		}
 		/// Sets the width in pixels to be mapped the range into
 		void setWidth(double width)
 		{
+			CLAM_ASSERT(width>0, "RulerTicks: Pixel width should be greater than zero");
 			_width = width;
+			_needsUpdate = true;
 		}
 		/// Sets the minimum phisical space in pixels between between marks
 		void setMinGap(double minGap)
 		{
 			_minGap = minGap;
+			_needsUpdate = true;
 		}
 		double markOffset() const
 		{
-			double gap = markGap();
-			double ceil = std::ceil(_min/gap);
-			if (std::fabs(ceil)<1e-5) return 0.;
-			return ceil*gap;
+			if (_needsUpdate) computeMarkOffset();
+			return _markOffset;
 		}
 		double markGap() const
 		{
+			if (_needsUpdate) computeMarkOffset();
+			return _markGap;
+		}
+		void update() const
+		{
 			double mappedMinGap = std::fabs(_minGap*(_max-_min)/_width);
-			double markGap=1;
+			double _markGap=1;
 			if (mappedMinGap>=1)
 			{
 				while (true)
 				{
-					if (markGap>=mappedMinGap) return markGap;
-					if (markGap*2>=mappedMinGap) return markGap*2;
-					if (markGap*5>=mappedMinGap) return markGap*5;
-					markGap*=10;
+					if (_markGap>=mappedMinGap) break;
+					if (_markGap*2>=mappedMinGap) {_markGap*=2; break;}
+					if (_markGap*5>=mappedMinGap) {_markGap*=5; break;}
+					_markGap*=10;
 				}
 			}
 			else
 			{
 				while (true)
 				{
-					if (markGap<1e-8) return 1e-8;
-					if (markGap<mappedMinGap) return markGap;
-					if (markGap/2<mappedMinGap) return markGap/2;
-					if (markGap/5<mappedMinGap) return markGap/5;
-					markGap/=10;
+					if (_markGap<1e-8) {_markGap=1e-8; break;}
+					if (_markGap<mappedMinGap) {break;}
+					if (_markGap/2<mappedMinGap) {_markGap/=2; break;}
+					if (_markGap/5<mappedMinGap) {_markGap/=5; break;}
+					_markGap/=10;
 				}
 			}
-			return markGap;
+			double ceil = std::ceil(_min/_markGap);
+			_markOffset = (std::fabs(ceil)<1e-5)? 0. : ceil*_markGap;
+
+			_needsUpdate = false;
 		}
+		/// Returns the domain value corresponding to tick i
+		double tickValue(unsigned i) const
+		{
+			if (_needsUpdate) computeMarkOffset();
+			return _markOffset+_markGap*i;
+		}
+		/// Returns the pixel position of tick i
+		double pixelTickPos(unsigned i) const
+		{
+			return toPixel(tickValue(i));
+		}
+		// Returns the pixel position of a given domain value
+		double toPixel(double value) const
+		{
+			return (value-min)*size/(_max-_min);
+		}
+
 };
 
 } // namespace CLAM
