5c59ae282d855960da99b59978dfe1f903402fd5
junjun <junjun@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2009-02-01 01:09:50 +0000

    × SemWebExtractor: an extractor for getting information from (semantic) web, including the algorithms of guessing any songs' musicBrainz ID

---
+++ trunk/Annotator/src/ChordExtractor/mopy/foaf/__init__.py
@@ -0,0 +1,14 @@
+import mopy.model
+
+from mopy.model import Agent
+from mopy.model import Document
+from mopy.model import Group
+from mopy.model import Image
+from mopy.model import OnlineAccount
+from mopy.model import OnlineChatAccount
+from mopy.model import OnlineEcommerceAccount
+from mopy.model import OnlineGamingAccount
+from mopy.model import Organization
+from mopy.model import Person
+from mopy.model import PersonalProfileDocument
+from mopy.model import Project
