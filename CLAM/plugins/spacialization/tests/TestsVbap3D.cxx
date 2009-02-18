6008046aa61b219dfe99d2e0377eb3c1bc61a1b8
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2009-02-18 09:51:37 +0000

    * tests orientation

---
+++ CLAM/plugins/spacialization/tests/TestsVbap3D.cxx
@@ -0,0 +1,23 @@
+
+#include "MiniCppUnit.hxx"
+#include "Vbap3D.hxx"
+
+class TestsVbap3D : public TestFixture<TestsVbap3D>
+{
+public:
+	TEST_FIXTURE( TestsVbap3D )
+	{
+		TEST_CASE( test_front );
+	}
+	void test_front()
+	{
+		Vbap3D v;
+		ASSERT_EQUALS( 1, v.findTriangle(0., 0.));
+		ASSERT_EQUALS( 0, v.findTriangle(0., -70.));
+		ASSERT_EQUALS( 3, v.findTriangle(45., -44.));
+	}
+};
+
+REGISTER_FIXTURE(TestsVbap3D)
+
+
