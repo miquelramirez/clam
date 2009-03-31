946b741434ac7ac6e2d4fc328d1f5f66898e1457
David García Garzón <vokimon@users.noreply.github.com>
2009-03-31 20:00:45 +0000

    * Hoa2RegularSpeakerArray: Configurable order

--- CLAM/plugins/spacialization/src/Hoa2RegularSpeakerArray.hxx
+++ CLAM/plugins/spacialization/src/Hoa2RegularSpeakerArray.hxx
@@ -10,6 +10,15 @@
 #include "Orientation.hxx"
 #include <fstream>
 
+/**
+ An spatial layout of labeled speakers.
+ A layout contains the orientation of each speaker referred to the listener or the center of the setup.
+ A layout can be read from a .layout file which contains a row for each speaker and for each row:
+ # elevation in degrees
+ # azimuth in degrees
+ # the speaker label (may containing spaces as it takes the rest of the line)
+ Empty lines are ignored. 
+*/
 class SpeakerLayout
 {
 	std::vector<std::string> _names;
@@ -57,21 +66,23 @@ public:
 };
 
 
-
 /**
  Decodes a Nth order signal for a regular array of speakers.
- @param NChannels [Config] The number of speakers that are to be generated. That configuration parameter changes number of output ports the processing will have and the separation among the speakers.
- @param[in] W [Port] W to be reproduced at the sweet-spot
- @param[in] X [Port] X ambisonics component
- @param[in] Y [Port] Y ambisonics component
- @param[out] "AAA" [Ports] Audio signals to be emitted by the speaker at AAA. Here AAA is the angle in degrees for each one of the speakers.
- @bug It should take W,X,Y,Z instead pressure and velocity according to conventions
- @todo Review the rotation direction with the conventions.
+ Regular arrays can be decoded with a single constant for all the components 
+ of the same degree, given an order of ambisonics.
+ @param Order [Config] The order of ambisonics that it will be fed with.
+ @param SpeakerLayout [Config] A file containing the target speaker layout.
+ @param[out] XX [Port] Ambisonics component were XX is one of W,X,Y,Z,R,S,T,U,V,K,L,M,N,O,P,Q.
+ @param[out] AAA [Ports] Audio signals to be emitted by the speaker AAA. Here AAA is the label in the layout file.
+ @see SpeakerLayout for a description of the file format.
+ @todo Make the order configurable
+ @todo Make the decoding configurable
  @ingroup SpatialAudio
+ @see AmbisonicsConventions
 */
 class Hoa2RegularSpeakerArray : public CLAM::Processing
 { 
-protected:
+public:
 	class Config : public CLAM::ProcessingConfig
 	{
 		DYNAMIC_TYPE_USING_INTERFACE( Config, 2, ProcessingConfig );
@@ -85,27 +96,19 @@ protected:
 			SetOrder(1);
 		};
 	};
+private:
 	SpeakerLayout _layout;
 	typedef std::vector<CLAM::AudioOutPort*> OutPorts;
+	typedef std::vector<CLAM::AudioInPort*> InPorts;
 	OutPorts _outputs;
-	unsigned _nChannels;
+	InPorts _inputs;
 	Config _config;
-	CLAM::AudioInPort _w;
-	CLAM::AudioInPort _x;
-	CLAM::AudioInPort _y;
-	CLAM::AudioInPort _z;
 
 public:
 	const char* GetClassName() const { return "Hoa2RegularSpeakerArray"; }
 	Hoa2RegularSpeakerArray(const Config& config = Config()) 
-		: _nChannels(0)
-		, _w("W", this)
-		, _x("X", this)
-		, _y("Y", this)
-		, _z("Z", this)
 	{
 		Configure( config );
-
 	}
 	const CLAM::ProcessingConfig & GetConfig() const
 	{
@@ -115,85 +118,113 @@ public:
 	bool ConcreteConfigure(const CLAM::ProcessingConfig& config)
 	{
 		CopyAsConcreteConfig(_config, config);
+		unsigned buffersize = BackendBufferSize();
+
+		unsigned order = _config.GetOrder();
+		bool errorHappened = false;
+		if (order>3)
+		{
+			// If never configured just initialize a sane default to keep connections
+			if (_inputs.size() == 0) ResizePortsToOrder(1, buffersize);
+			errorHappened |= AddConfigErrorMessage("Ambisonics orders beyond 3rd are not supported");
+			// Don't exit yet, we want to keep outports connections when loading from a network
+		}
+		else ResizePortsToOrder(order, buffersize);
+
 		std::string errorMessage;
 		if (not _layout.load(_config.GetSpeakerLayout(), errorMessage))
 			return AddConfigErrorMessage(errorMessage);
+		else ResizePortsToLayout(buffersize);
 
-		unsigned buffersize = BackendBufferSize();
-
-		// Set up the outputs according to the layout
-		// TODO: Keep the connections
-		RemovePorts();
-		for (unsigned speaker=0; speaker<_layout.size(); speaker++)
-		{
-			CLAM::AudioOutPort * port = new CLAM::AudioOutPort( _layout.name(speaker), this);
-			port->SetSize( buffersize );
-			port->SetHop( buffersize );
-			_outputs.push_back( port );
-		}
-		_nChannels=_outputs.size();
-
-		// TODO: configurable ambisonics order
-		_w.SetSize(buffersize);
-		_w.SetHop(buffersize);
-		_x.SetSize(buffersize);
-		_x.SetHop(buffersize);
-		_y.SetSize(buffersize);
-		_y.SetHop(buffersize);
-		_z.SetSize(buffersize);
-		_z.SetHop(buffersize);
-		return true;
+		return not errorHappened;
 	}
  
 	bool Do()
 	{
-		const CLAM::DataArray& w =_w.GetAudio().GetBuffer();
-		const CLAM::DataArray& x =_x.GetAudio().GetBuffer();
-		const CLAM::DataArray& y =_y.GetAudio().GetBuffer();
-		const CLAM::DataArray& z =_z.GetAudio().GetBuffer();
-		//const float sqrt2 = sqrt(2);
-		//const float sqrtN = sqrt(_nChannels);
-		CLAM::TData* speakers[_nChannels];
-
-		for (unsigned speaker=0; speaker<_nChannels; speaker++)
+		// Ambisonics definition assures us one component for order 0 so we can do that:
+		const unsigned nSamples = _inputs[0]->GetAudio().GetBuffer().Size();
+		const unsigned nComponents = _inputs.size();
+		const unsigned nSpeakers = _outputs.size();
+		const CLAM::TData* components[nComponents];
+		for (unsigned component=0; component<nComponents; component++)
+			components[component] = &_inputs[component]->GetAudio().GetBuffer()[0];
+		CLAM::TData* speakers[nSpeakers];
+		for (unsigned speaker=0; speaker<nSpeakers; speaker++)
 			speakers[speaker] = &_outputs[speaker]->GetAudio().GetBuffer()[0];
 
-		const unsigned nComponents = 4;
 		double componentWeight[nComponents];
 		CLAM::SphericalHarmonicsDefinition * sh = CLAM::Orientation::sphericalHarmonics();
-
 		for (unsigned speaker=0; speaker<_outputs.size(); speaker++)
 		{
 			for (unsigned hoaComponent=0; hoaComponent<nComponents; hoaComponent++)
 				componentWeight[hoaComponent] = _layout.orientation(speaker).sphericalHarmonic(sh[hoaComponent]);
-			for (int sample=0; sample<w.Size(); sample++)
+			CLAM::TData * speakerBuffer = speakers[speaker];
+			for (unsigned sample=0; sample<nSamples; sample++)
 			{
-				speakers[speaker][sample] =
-					w[sample] * componentWeight[0] +
-					x[sample] * componentWeight[1] +
-					y[sample] * componentWeight[2] +
-					z[sample] * componentWeight[3] ;
+				double sampleValue = 0;
+				for (unsigned component=0; component<nComponents; component++)
+					sampleValue += components[component][sample] * componentWeight[component];
+				speakerBuffer[sample] = sampleValue;
 			}
 		}
-		_w.Consume();
-		_x.Consume();
-		_y.Consume();
-		_z.Consume();
-		// Tell the ports this is done
-		for (unsigned channel=0; channel<_nChannels; channel++)
-			_outputs[channel]->Produce();
-		
-		return true;
-	}
-	void RemovePorts()
-	{
 		for (unsigned speaker=0; speaker<_outputs.size(); speaker++)
-			delete _outputs[speaker];
-		_outputs.clear();
+			_outputs[speaker]->Produce();
+		for (unsigned component=0; component<nSpeakers; component++)
+			_inputs[component]->Consume();
+		return true;
 	}
 	~Hoa2RegularSpeakerArray()
 	{
-		RemovePorts();
+		for (unsigned speaker=0; speaker<_outputs.size(); speaker++)
+			delete _outputs[speaker];
+		for (unsigned component=0; component<_inputs.size(); component++)
+			delete _inputs[component];
+	}
+private:
+	void ResizePortsToOrder(unsigned order, unsigned buffersize)
+	{
+		// Set up the inputs according to ambisonics order
+		CLAM::SphericalHarmonicsDefinition *sh = CLAM::Orientation::sphericalHarmonics();
+		unsigned i=0;
+		for (;sh[i].name; i++)
+		{
+			if (sh[i].order > order) break;
+			if (i<_inputs.size()) continue;
+			CLAM::AudioInPort * port = new CLAM::AudioInPort( sh[i].name, this);
+			port->SetSize( buffersize );
+			port->SetHop( buffersize );
+			_inputs.push_back( port );
+		}
+		unsigned actualSize=i;
+		for (;i<_inputs.size(); i++)
+			delete _inputs[i];
+		_inputs.resize(actualSize);
+	}
+	void ResizePortsToLayout(unsigned buffersize)
+	{
+		// Set up the outputs according to the layout
+		unsigned speakerToUpdate = firstDirtySpeaker();
+		// delete existing speakers from the first one with different name
+		for (unsigned oldSpeaker=speakerToUpdate ; oldSpeaker<_outputs.size(); oldSpeaker++)
+			delete _outputs[oldSpeaker];
+		_outputs.resize(speakerToUpdate);
+		// adding new speakers
+		for ( ; speakerToUpdate<_layout.size(); speakerToUpdate++)
+		{
+			CLAM::AudioOutPort * port = new CLAM::AudioOutPort( _layout.name(speakerToUpdate), this);
+			port->SetSize( buffersize );
+			port->SetHop( buffersize );
+			_outputs.push_back( port );
+		}
+	}
+	unsigned firstDirtySpeaker() const
+	{
+		for (unsigned speaker ; speaker<_layout.size(); speaker++)
+		{
+			if (speaker>=_outputs.size()) return speaker; 
+			if (_outputs[speaker]->GetName() != _layout.name(speaker)) return speaker;
+		}
+		return _layout.size();
 	}
 };
 #endif
