54e30dcbe89e2ba113ee0c3c5da056520e762d1e
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2009-04-07 19:52:07 +0000

    * EmbeddedFile: added COFF binaries version for mingw

--- trunk/CLAM/src/Defines/EmbeddedFile.hxx
+++ trunk/CLAM/src/Defines/EmbeddedFile.hxx
@@ -4,7 +4,24 @@
 /// Include a given file as binary data
 /// @arg varname is the variable that will hold the var (const char[])
 /// @arg file is an absolute path or a relative path from the build point (not the source file!!)
-/// @todo Try to make CLAM_EXTERNAL_FILE_DATA work with path relatives to the source that uses the macro.
+/// @todo Try to make CLAM_EMBEDDED_FILE work with path relatives to the source that uses the macro.
+
+#if defined __MINGW32__
+// COFF binary target version
+#define CLAM_EMBEDDED_FILE(varname, file) \
+asm ( \
+"  .section .rodata\n" \
+"  .def _" #varname "\n" \
+" _" #varname ":\n" \
+"   .incbin \"" file "\"\n" \
+"   .byte 0\n" \
+"   .scl 1\n" \
+"   .size . - _" #varname "\n" \
+" .endef \n" \
+); \
+extern const char varname[];
+#elif defined __ELF__
+// ELF binary target version
 #define CLAM_EMBEDDED_FILE(varname, file) \
 asm ( \
 "  .section \".rodata\"\n" \
@@ -14,7 +31,11 @@ asm ( \
 "   .byte 0\n" \
 "   .size " #varname ", .-" #varname "\n" \
 ); \
-extern const char varname[]; \
+extern const char varname[];
+#else
+#error "No support for embedded files in your platform"
+#endif
+
 
 /// Alias to CLAM_EMBEDDED_FILE, for compatibility
 #define CLAM_EXTERNAL_FILE_DATA(varname, file) CLAM_EMBEDDED_FILE(varname, file)
