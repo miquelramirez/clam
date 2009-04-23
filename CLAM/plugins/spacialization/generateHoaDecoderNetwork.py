58038fd30617610f24ddad4b92451d3475acc443
David García Garzón <vokimon@users.noreply.github.com>
2009-04-23 19:23:50 +0000

    * generateHoaDecoderNetwork: a script to generate HoaDecoders of any layout and order from 0 to 3

---
+++ CLAM/plugins/spacialization/generateHoaDecoderNetwork.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+
+import sys
+
+if len(sys.argv)!=3 :
+	print >> sys.stderr, "Usage: %s <layout_file> <order>"%sys.argv[0]
+	sys.exit()
+
+
+layoutFileName = sys.argv[1]
+order = sys.argv[2]
+if order not in '0123':
+	print >> sys.stderr, "Order",order,"is not supported. It must be either 0, 1, 2 or 3."
+	sys.exit(-1)
+order=int(order)
+channels="".join(['W','XYZ','RSTUV','KLMNOPQ'][0:order+1])
+speakers = [ label for label in [" ".join(line.split()[2:]) for line in open(layoutFileName).readlines() if line[0]!='#' ] if label ]
+nSpeakers = len(speakers)
+
+print >> sys.stderr, "Speakers:", nSpeakers
+
+print """<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
+<network clamVersion="1.3.1" id="Unnamed">
+"""
+
+for speaker, label in enumerate(speakers) :
+	print """
+  <processing id="%i %s" position="%i,%i" size="128,111" type="AudioSink"/>""" % (
+	speaker+1, label, 600+64*(speaker%6), 80+(speaker%6)*6+(speaker//6)*95 )
+
+for i, channel in enumerate(channels) :
+	print """
+  <processing id="%i %s in" position="%i,%i" size="128,108" type="AudioSource"/>\
+""" % (i+1, channel, 10, 10+i*110)
+
+print """\
+  <processing id="Hoa2RegularSpeakerArray" position="257,290" size="128,270" type="Hoa2RegularSpeakerArray">
+	<Order>%(order)i</Order>
+	<SpeakerLayout>%(layoutFile)s</SpeakerLayout>
+  </processing>
+""" % dict(order=order, layoutFile=layoutFileName)
+
+for i, channel in enumerate(channels) :
+	print """\
+  <port_connection>
+    <out>%i %s in.AudioOut</out>
+    <in>Hoa2RegularSpeakerArray.%s</in>
+  </port_connection>
+""" % (i+1, channel, channel)
+
+for speaker, label in enumerate(speakers) :
+	print """\
+  <port_connection>
+    <out>Hoa2RegularSpeakerArray.%(speakerName)s</out>
+    <in>%(speakerNumber)s %(speakerName)s.AudioIn</in>
+  </port_connection>
+""" % dict(
+	speakerNumber=speaker+1,
+	speakerName=label,
+	)
+
+print """
+</network>
+"""
+
