5c5d65b1aca0a51cc952a0c76d0bbfd10256ac0e
David García Garzón <vokimon@users.noreply.github.com>
2009-04-25 12:59:04 +0000

    * generateLayoutLinks: linking 22 speakers configuration to twyford guessed configurations

--- CLAM/plugins/spacialization/generateLayoutLinks.sh
+++ CLAM/plugins/spacialization/generateLayoutLinks.sh
@@ -10,6 +10,10 @@ do
 done
 sudo rm /usr/local/share/clam/layouts/15.layout
 sudo rm /usr/local/share/clam/layouts/15.triangulation
+sudo rm /usr/local/share/clam/layouts/22.layout
+sudo rm /usr/local/share/clam/layouts/22.triangulation
 sudo ln -s  `pwd`/layouts/sixteen.layout /usr/local/share/clam/layouts/15.layout
 sudo ln -s  `pwd`/layouts/sixteen.triangulation /usr/local/share/clam/layouts/15.triangulation
+sudo ln -s  `pwd`/layouts/22_twyford_design.layout /usr/local/share/clam/layouts/22.layout
+sudo ln -s  `pwd`/layouts/22_twyford_design_stereographic.triangulation /usr/local/share/clam/layouts/22.triangulation
 
