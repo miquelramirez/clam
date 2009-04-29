78a5a5528b2cbec507da683938b20226d443570e
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2009-04-29 10:59:43 +0000

    * SpeakerLayout: comment lines are ignored and more details on the error messages is given

--- trunk/CLAM/plugins/spacialization/src/SpeakerLayout.cxx
+++ trunk/CLAM/plugins/spacialization/src/SpeakerLayout.cxx
@@ -17,16 +17,24 @@ bool SpeakerLayout::load(
 	std::ifstream file(path.c_str());
 	if (!file) return error(errorMsg, "Could not open the speaker layout file "+path);
 	clear();
-	while (true)
+	for (unsigned i=0; true; i++)
 	{
+		std::string line;
+		getline(file,line);
+		if (file.eof()) break;
+		if (line[0]=='#') continue;
+		std::ostringstream os;
+		os << i;
+		std::istringstream is(line);
 		double elevation;
 		double azimuth;
-		file >> elevation;
-		file >> azimuth;
-		if (!file) break;
-		file >> std::ws;
+		if (not (is >> elevation))
+			return error(errorMsg, "Expected elevation at line "+os.str());
+		if (not (is >> azimuth))
+			return error(errorMsg, "Expected azimuth at line "+os.str());
+		is >> std::ws;
 		std::string name;
-		std::getline(file, name);
+		std::getline(is, name);
 		// TODO: Issue an error if illegal port name
 		add(azimuth, elevation, name);
 	}
