345bd3d3e5b24cd7fd8ac502689d9a3e723baccd
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2009-04-30 11:07:16 +0000

    * change twyford configurations script

---
+++ CLAM/plugins/spacialization/change_twyford_configuration.sh
@@ -0,0 +1,25 @@
+# MEASURED CORRECTED
+
+sudo rm /usr/local/share/clam/layouts/22.triangulation
+sudo ln -s  /home/acustica/clam/CLAM/plugins/spacialization/layouts/22_twyford_mesured_config1_corrected.triangulation /usr/local/share/clam/layouts/22.triangulation
+
+sudo rm /usr/local/share/clam/layouts/22.layout
+sudo ln -s  /home/acustica/clam/CLAM/plugins/spacialization/layouts/22_twyford_mesured_config1_rotation_corrected.layout /usr/local/share/clam/layouts/22.layout
+
+# MEASURED
+
+sudo rm /usr/local/share/clam/layouts/22.triangulation
+sudo ln -s  /home/acustica/clam/CLAM/plugins/spacialization/layouts/22_twyford_mesured_config1.triangulation /usr/local/share/clam/layouts/22.triangulation
+
+sudo rm /usr/local/share/clam/layouts/22.layout
+sudo ln -s  /home/acustica/clam/CLAM/plugins/spacialization/layouts/22_twyford_mesured_config1.layout /usr/local/share/clam/layouts/22.layout
+
+
+# GUESSED
+
+sudo rm /usr/local/share/clam/layouts/22.triangulation
+sudo ln -s  /home/acustica/clam/CLAM/plugins/spacialization/layouts/22_twyford_guessed_stereographic.triangulation /usr/local/share/clam/layouts/22.triangulation
+
+sudo rm /usr/local/share/clam/layouts/22.layout
+sudo ln -s  /home/acustica/clam/CLAM/plugins/spacialization/layouts/22_twyford_guessed.layout /usr/local/share/clam/layouts/22.layout
+
