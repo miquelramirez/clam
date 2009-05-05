dce7b8e80a3a9cad7b26680c14d435b79922aa13
David García Garzón <vokimon@users.noreply.github.com>
2009-05-05 17:51:08 +0000

    * generateUserTests: Fix: error specifying the impedance and diffusion  * generateUserTests: reworked to have direct sound and reverb in separate files for all the algorithms

--- CLAM/plugins/spacialization/generateUserTests.py
+++ CLAM/plugins/spacialization/generateUserTests.py
@@ -16,10 +16,10 @@ wavs = [
 ]
 
 ambients = [
-	# name, zr
-	("anechoich", 0),
-	("littlereverb", 100),
-	("fullreverb", 1000000),
+	# name, zr, d, reverbGain
+	("anechoich", 1, 0.0, .5),
+	("littlereverb", 100., 0.5, 0.5),
+	("fullreverb", 1000000, 0.5, 0.5),
 ]
 
 
@@ -66,7 +66,7 @@ for i, (e,a) in enumerate(locations) :
 	choreo.close()
 
 print "Generate geometries"
-for space_name, z in ambients :
+for space_name, z, d, reverbGain in ambients :
 	print "Generate geometry for", space_name
 	geometryFileName = 'usertest/geometry_%s.data'%space_name
 	geometry = open(geometryFileName, 'w')
@@ -81,68 +81,110 @@ for space_name, z in ambients :
 -2.7 2.7 2.7
 2.7 2.7 2.7
 <FACES>
-3 1 4 %(z)s 0.0 0.5 material0
-2 4 1 %(z)s 0.0 0.5 material0
-6 5 8 %(z)s 0.0 0.5 material0
-7 8 5 %(z)s 0.0 0.5 material0
-2 1 6 %(z)s 0.0 0.5 material0
-5 6 1 %(z)s 0.0 0.5 material0
-4 2 8 %(z)s 0.0 0.5 material0
-6 8 2 %(z)s 0.0 0.5 material0
-3 4 7 %(z)s 0.0 0.5 material0
-8 7 4 %(z)s 0.0 0.5 material0
-1 3 5 %(z)s 0.0 0.5 material0
-7 5 3 %(z)s 0.0 0.5 material0
-"""%dict(z=z)
+3 1 4 %(z)s 0.0 %(d)s material0
+2 4 1 %(z)s 0.0 %(d)s material0
+6 5 8 %(z)s 0.0 %(d)s material0
+7 8 5 %(z)s 0.0 %(d)s material0
+2 1 6 %(z)s 0.0 %(d)s material0
+5 6 1 %(z)s 0.0 %(d)s material0
+4 2 8 %(z)s 0.0 %(d)s material0
+6 8 2 %(z)s 0.0 %(d)s material0
+3 4 7 %(z)s 0.0 %(d)s material0
+8 7 4 %(z)s 0.0 %(d)s material0
+1 3 5 %(z)s 0.0 %(d)s material0
+7 5 3 %(z)s 0.0 %(d)s material0
+"""%dict(z=z, d=d)
 	geometry.close()
+	directSoundGain = 1 if  reverbGain<1 else 1./reverbGain
+	reverbGain = reverbGain if reverbGain<1. else 1
+	run("./generateNChannelMixer.py 15 %(directSoundGain)s %(reverbGain)s > usertest/mixer_%(space_name)s.clamnetwork"%globals())
 
 
 for i in 1, 2, 3 :
-	run("./generateHoaRoomSimulator.py %(i)i usertest/coreo usertest/geometry > usertest/hoa%(i)iroomsimulator.clamnetwork"%globals())
-	run("./generateHoaDecoderNetwork.py layouts/sixteen.layout %(i)i > usertest/hoa%(i)ito15decoder.clamnetwork"%globals())
-run("cp usertest/hoa1roomsimulator.clamnetwork usertest/hoa1revroomsimulator.clamnetwork")
-run("./generateVbapDecoderNetwork.py layouts/sixteen.layout layouts/sixteen.triangulation > usertest/vbap15decoder.clamnetwork ")
-run("clamrefactor.py usertest/hoa1roomsimulator.clamnetwork -c 'setConfig RoomImpulseResponseSimulator SeparateDirectSoundAndReverb 1' > usertest/hoa1revroomsimulator.clamnetwork")
+	run("./generateHoaRoomSimulator.py %(i)i usertest/coreo usertest/geometry > usertest/hoa%(i)i_room.clamnetwork"%globals())
+	run("./generateHoaDecoderNetwork.py layouts/sixteen.layout %(i)i > usertest/hoa%(i)i_15decoder.clamnetwork"%globals())
+	run("clamrefactor.py usertest/hoa%(i)i_room.clamnetwork -c 'setConfig RoomImpulseResponseSimulator NRebounds 0' > usertest/hoa%(i)i_room_ds.clamnetwork"%globals())
+	run("clamrefactor.py usertest/hoa%(i)i_room.clamnetwork -c 'setConfig RoomImpulseResponseSimulator SeparateDirectSoundAndReverb 1' > usertest/hoa%(i)i_room_ds.clamnetwork"%globals())
 
-directSoundGain = 1 if  reverbGain<1 else 1./reverbGain
-reverbGain = reverbGain if reverbGain<1. else 1
+run("./generateVbapDecoderNetwork.py layouts/sixteen.layout layouts/sixteen.triangulation > usertest/vbap_ds.clamnetwork ")
 
-run("./generateNChannelMixer.py 15 %(directSoundGain)s %(reverbGain)s > usertest/vbaphoa1mixer.clamnetwork"%dict(globals()))
 
-for space_name, z in ambients :
+# Generating direct sounds
+for space_name, z, d, reverbGain in ambients :
 	run("cp usertest/geometry_%s.data usertest/geometry"%space_name)
 	for posi, pos in enumerate(locations) :
 		run("cp usertest/position_%02i.coreo usertest/coreo"%posi)
-		for algorithm_name, channels, encoder, decoder in algorithms :
+		for order in 1, 2, 3 :
+			channels = (order+1)**2
+			speakers = 15
+			encoder = "usertest/hoa%(order)i_room_ds.clamnetwork"%globals()
+			decoder = "usertest/hoa%(order)i_15decoder.clamnetwork"%globals()
 			for wav in wavs :
-				print "Generating", algorithm_name, "for", wav, "with absortion", z
-				run("OfflinePlayer usertest/%(encoder)s %(wav)s -o -c %(channels)s usertest/encoded_%(algorithm_name)s_%(space_name)s_%(posi)02i_%(wav)s"%dict(globals()))
-				if algorithm_name != "vbap" :
-					run("OfflinePlayer usertest/%(decoder)s usertest/encoded_%(algorithm_name)s_%(space_name)s_%(posi)02i_%(wav)s -o -c 15 usertest/15_%(algorithm_name)s_%(space_name)s_%(posi)02i_%(wav)s"%dict(globals()))
-				else :
-					run("OfflinePlayer usertest/%(decoder)s usertest/encoded_%(algorithm_name)s_%(space_name)s_%(posi)02i_%(wav)s usertest/15_hoa1rev_%(space_name)s_%(posi)02i_%(wav)s -o -c 15 usertest/15_%(algorithm_name)s_%(space_name)s_%(posi)02i_%(wav)s"%dict(globals()))
-					
+				encoded = "usertest/encoded_hoa%(order)i_ds_%(space_name)s_%(posi)02i_%(wav)s"%globals()
+				decoded = "usertest/decoded_hoa%(order)i_ds_%(space_name)s_%(posi)02i_%(wav)s"%globals()
+				run("OfflinePlayer %(encoder)s %(wav)s -o -c %(channels)s %(encoded)s"%globals())
+				run("OfflinePlayer %(decoder)s %(encoded)s -o -c %(speakers)i %(decoded)s"%globals())
+		for wav in wavs :
+			decoder = "usertest/vbap_ds.clamnetwork"%globals()
+			decoded = "usertest/decoded_directSound_%(space_name)s_%(posi)02i_%(wav)s"%globals()
+			run("OfflinePlayer %(decoder)s %(wav)s -o -c %(speakers)s %(decoded)s"%globals())
 
-for space_name, z in ambients :
+# Generating reverbs
+for space_name, z, d, reverbGain in ambients :
+	run("cp usertest/geometry_%s.data usertest/geometry"%space_name)
 	for posi, pos in enumerate(locations) :
-		for algorithm_name, channels, encoder, decoder in algorithms :
-			if algorithm_name == "hoa1rev" : continue
+		run("cp usertest/position_%02i.coreo usertest/coreo"%posi)
+		for order in 1, 2, 3 :
+			channels = (order+1)**2
+			speakers = 15
+			encoder = "usertest/hoa%(order)i_room_rev.clamnetwork"%globals()
+			decoder = "usertest/hoa%(order)i_15decoder.clamnetwork"%globals()
 			for wav in wavs :
-				run("soxsucks -N usertest/15_%(algorithm_name)s_%(space_name)s_%(posi)02i_%(wav)s usertest/tocat_%(algorithm_name)s_%(space_name)s_%(posi)02i_%(wav)s"%dict(globals()))
+				encoded = "usertest/encoded_hoa%(order)i_rev_%(space_name)s_%(posi)02i_%(wav)s"%globals()
+				decoded = "usertest/decoded_hoa%(order)i_rev_%(space_name)s_%(posi)02i_%(wav)s"%globals()
+				run("OfflinePlayer %(encoder)s %(wav)s -o -c %(channels)s %(encoded)s"%globals())
+				run("OfflinePlayer %(decoder)s %(encoded)s -o -c %(speakers)i %(decoded)s"%globals())
 
-import glob, random
-run("sox -c 15 -r 48000 -n -c 15 usertest/silence.wav trim 0 %(interTestSilence)s"%dict(globals()))
+# Mixing reverb and direct sound
+for space_name, z, d, reverbGain in ambients :
+	for posi, pos in enumerate(locations) :
+		for order in 1, 2, 3 :
+			channels = (order+1)**2
+			speakers = 15
+			mixer = "usertest/mix%(order)i_%(space_name)s.clamnetwork"%globals()
+			decoder = "usertest/hoa%(order)i_15decoder.clamnetwork"%globals()
+			for wav in wavs :
+				reverb = "usertest/decoded_hoa%(order)i_rev_%(space_name)s_%(posi)02i_%(wav)s"%globals()
+				direct_sound = "usertest/decoded_hoa%(order)i_ds_%(space_name)s_%(posi)02i_%(wav)s"%globals()
+				mixed = "usertest/mixed_hoa%(order)i_%(space_name)s_%(posi)02i_%(wav)s"%globals()
+				run("OfflinePlayer %(mixer)s %(direct_sound)s %(reverb)s -o -c %(channels)s %(mixed)s"%globals())
+		for wav in wavs :
+			order = 1
+			reverb       = "usertest/decoded_hoa%(order)i_rev_%(space_name)s_%(posi)02i_%(wav)s"%globals()
+			direct_sound = "usertest/decoded_vbap_ds_%(space_name)s_%(posi)02i_%(wav)s"%globals()
+			mixed        = "usertest/mixed_vbap_%(space_name)s_%(posi)02i_%(wav)s"%globals()
+			run("OfflinePlayer %(mixer)s %(direct_sound)s %(reverb)s -o -c %(channels)s %(mixed)s"%globals())
+
+
+for space_name, z, d, reverbGain in ambients :
+	for posi, pos in enumerate(locations) :
+		for algorithm_name in 'hoa1', 'hoa2', 'hoa3', 'vbap' :
+			for wav in wavs :
+				run("soxsucks -N usertest/mixed_%(algorithm_name)s_%(space_name)s_%(posi)02i_%(wav)s usertest/tocat_%(algorithm_name)s_%(space_name)s_%(posi)02i_%(wav)s"%globals())
+
+import random
+import glob
+run("sox -c 15 -r 48000 -n -c 15 usertest/silence.wav trim 0 %(interTestSilence)s"%globals())
 
 testFiles = glob.glob('usertest/tocat_*wav')
 random.shuffle(testFiles)
-print testFiles
 f=file("usertest/usertest.txt",'w')
 print >> f, "Order:"
 print >> f, "\n".join(testFiles)
 print >> f, "\nPoints (index: elevation, azimuth):"
 print >> f, "\n".join(["\t%02i: %s %s"%(i,e,a) for i,(e,a) in enumerate(locations)])
 print >> f, "\nAmbients (name: Re(Z)):"
-print >> f, "\n".join(["\t%s: %f"%(name,z) for name,z in ambients])
+print >> f, "\n".join(["\t%s: %f"%(name,z) for name,z,d, reverbGain in ambients])
 print >> f
 f.close()
 
