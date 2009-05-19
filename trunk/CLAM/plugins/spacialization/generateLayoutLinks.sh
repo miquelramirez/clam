2cdd2c132baf26c0f7583e337bb9140bd6fa6011
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2009-05-19 15:33:48 +0000

    fixed last commit regarding generateLayoutLinks.sh

--- trunk/CLAM/plugins/spacialization/generateLayoutLinks.sh
+++ trunk/CLAM/plugins/spacialization/generateLayoutLinks.sh
@@ -1,5 +1,5 @@
 sudo mkdir -p /usr/local/share/clam/layouts/
-for ((a=15; a<23; a++))
+for ((a=14; a<23; a++))
 do
 	dummylayout=`pwd`/layouts/$a''_dummy.layout
 	dummytriangulation=`pwd`/layouts/$a''_dummy.triangulation
@@ -8,6 +8,8 @@ do
 	sudo ln -s  $dummylayout /usr/local/share/clam/layouts/$a.layout
 	sudo ln -s  $dummytriangulation /usr/local/share/clam/layouts/$a.triangulation
 done
+sudo rm /usr/local/share/clam/layouts/14.layout
+sudo rm /usr/local/share/clam/layouts/14.triangulation
 sudo rm /usr/local/share/clam/layouts/15.layout
 sudo rm /usr/local/share/clam/layouts/15.triangulation
 sudo rm /usr/local/share/clam/layouts/22.layout
