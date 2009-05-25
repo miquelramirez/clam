0ccf6663e2b35003656cfe59308a7066e2e105bb
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2009-05-25 02:55:45 +0000

    * SndfileWriter: extracted inner class that abstracts the RingBuffer and the wait conditions  * SndfileWriter: Fix: when resizing ports, ports were pushed back and already were 'resize'd  * SndfileWriter: Fix: _sampleSize multiplied by _nChannels, and on some points it was multiplied twice

--- trunk/CLAM/plugins/sndfile/SndfileWriter.hxx
+++ trunk/CLAM/plugins/sndfile/SndfileWriter.hxx
@@ -27,6 +27,7 @@
 #include <CLAM/Audio.hxx>
 #include <sndfile.hh>
 #include <CLAM/Enum.hxx>
+#include <CLAM/FileSystem.hxx>
 #include <pthread.h>
 #include <jack/ringbuffer.h>
 
@@ -71,61 +72,121 @@ namespace CLAM
 		static  EAudioFileWriteFormat FormatFromFilename( std::string filename );
 	};
 
-	class SndfileWriterConfig : public ProcessingConfig
-	{
-		DYNAMIC_TYPE_USING_INTERFACE( SndfileWriterConfig, 4, ProcessingConfig );
-		DYN_ATTRIBUTE( 0, public, AudioOutFilename, TargetFile);
-		DYN_ATTRIBUTE( 1, public, EAudioFileWriteFormat, Format);
-		DYN_ATTRIBUTE( 2, public, int, SampleRate);
-		DYN_ATTRIBUTE( 3, public, unsigned, NumberChannels);
-
-		protected:
-		void DefaultInit()
-		{
-			AddAll();
-			UpdateData();
-			SetSampleRate(44100);
-			SetNumberChannels(0);
-		};
-	};
-
 	class SndfileWriter : public  Processing
 	{
-		class ScopedLock
+		class Config : public ProcessingConfig
 		{
-			pthread_mutex_t & _mutex;
-			public:
-				ScopedLock(pthread_mutex_t & mutex)
-					: _mutex(mutex)
-				{
-					pthread_mutex_lock(&_mutex);
-				}
-				~ScopedLock()
-				{
-					pthread_mutex_unlock(&_mutex);
-				}
+			DYNAMIC_TYPE_USING_INTERFACE( Config, 5, ProcessingConfig );
+			DYN_ATTRIBUTE( 0, public, AudioOutFilename, TargetFile);
+			DYN_ATTRIBUTE( 1, public, EAudioFileWriteFormat, Format);
+			DYN_ATTRIBUTE( 2, public, int, SampleRate);
+			DYN_ATTRIBUTE( 3, public, unsigned, NumberChannels);
+			DYN_ATTRIBUTE( 4, public, unsigned, BufferSize);
+
+		protected:
+			void DefaultInit()
+			{
+				AddAll();
+				UpdateData();
+				SetSampleRate(44100);
+				SetNumberChannels(0);
+				SetBufferSize(16384);
+			};
 		};
 
-		typedef SndfileWriterConfig Config;
+		class RingBuffer
+		{
+		public:
+			typedef CLAM::TData TokenType;
+			RingBuffer(unsigned size)
+				: _overruns(0)
+			{
+				pthread_cond_t pthreadCondInitializer = PTHREAD_COND_INITIALIZER;
+				_dataReadyCondition = pthreadCondInitializer;
+
+				pthread_mutex_t pthreadMutexInitializer = PTHREAD_MUTEX_INITIALIZER;
+				_diskThreadLock = pthreadMutexInitializer;
+
+				_ringBuffer = jack_ringbuffer_create (size*_sampleSize);
+				memset(_ringBuffer->buf, 0, _ringBuffer->size);
+			}
+			~RingBuffer()
+			{
+				jack_ringbuffer_free (_ringBuffer);
+			}
+			void write(TokenType * buffer, unsigned nFrames)
+			{
+				unsigned sizeInBytes = nFrames * _sampleSize;
+				unsigned actuallyRead = jack_ringbuffer_write(_ringBuffer, (char*)buffer, sizeInBytes);
+				if (actuallyRead < nFrames) _overruns++;
+			}
+			bool read(TokenType * buffer, unsigned nFrames)
+			{
+				unsigned sizeInBytes = nFrames * _sampleSize;
+				if (jack_ringbuffer_read_space(_ringBuffer) < sizeInBytes) return false;
+				jack_ringbuffer_read(_ringBuffer,(char*) buffer, sizeInBytes);
+				return true;
+			}
+			bool hadOverRuns() { return _overruns>0; }
+			void signalAvailableData()
+			{
+				/* Tell the disk thread there is work to do.  If it is already
+				 * running, the lock will not be available.  We can't wait
+				 * here in the process() thread, but we don't need to signal
+				 * in that case, because the disk thread will read all the
+				 * data queued before waiting again. */
+				if (pthread_mutex_trylock(&_diskThreadLock) == 0)
+				{
+					pthread_cond_signal (&_dataReadyCondition);
+					pthread_mutex_unlock (&_diskThreadLock);
+				}
+			}
+			/// Wait until the real-time process provides data
+			/// To be called by the non-real-time worker thread
+			void waitForDataAvailable()
+			{
+				pthread_cond_wait (&_dataReadyCondition, &_diskThreadLock);
+			}
+
+			class ScopedLock
+			{
+				pthread_mutex_t & _mutex;
+				public:
+					ScopedLock(RingBuffer & ringBuffer)
+						: _mutex(ringBuffer._diskThreadLock)
+					{
+						pthread_mutex_lock(&_mutex);
+					}
+					~ScopedLock()
+					{
+						pthread_mutex_unlock(&_mutex);
+					}
+			};
+
+		private:
+			const static unsigned _ringBufferSize = 16384;
+			const static unsigned _sampleSize = sizeof(TokenType);
+			long _overruns;
+			jack_ringbuffer_t* _ringBuffer;
+			pthread_mutex_t _diskThreadLock;
+			pthread_cond_t _dataReadyCondition;
+		};
+
 		typedef std::vector<CLAM::AudioInPort*> InPorts;
 		InPorts _inports;
 		SndfileHandle* _outfile;
-		SndfileWriterConfig _config;
+		Config _config;
 		unsigned _numChannels;
-		unsigned _sampleSize;
 		int _sampleRate;
 		int _format;
 
 		pthread_t _threadId;
-		long _overruns;
 		const static unsigned _ringBufferSize = 16384;
 		volatile int _canCapture;
 		volatile int _canProcess;
 		volatile int _isStopped;
 		/* Synchronization between process thread and disk thread. */
-		pthread_mutex_t _diskThreadLock;
-		pthread_cond_t  _dataReadyCondition;
-		jack_ringbuffer_t* _ringBuffer;
+		RingBuffer * _ringBuffer;
 
 	public:
 		const char* GetClassName() const { return "SndfileWriter"; }
@@ -133,15 +194,8 @@ namespace CLAM
 		SndfileWriter(const ProcessingConfig& config =  Config())
 			: _outfile(0)
 			, _numChannels(0)
-			, _overruns(0)
 			, _isStopped(true)
 		{
-			pthread_cond_t pthreadCondInitializer = PTHREAD_COND_INITIALIZER;
-			_dataReadyCondition = pthreadCondInitializer;
-
-			pthread_mutex_t pthreadMutexInitializer = PTHREAD_MUTEX_INITIALIZER;
-			_diskThreadLock = pthreadMutexInitializer;
-
 			Configure( config );
 		}
 
@@ -160,55 +214,41 @@ namespace CLAM
 
 			TData buffer [_numChannels];
 			// produce the portion write (normally the whole buffer)
-			for(unsigned frameIndex=0;frameIndex<portSize;frameIndex++)
+			for (unsigned frameIndex=0;frameIndex<portSize;frameIndex++)
 			{
-				for(unsigned channel= 0;channel<_numChannels;channel++)
+				for (unsigned channel= 0;channel<_numChannels;channel++)
 				{
 					buffer[channel] = channels[channel][frameIndex];
 				}
-
-				if(jack_ringbuffer_write(_ringBuffer, (char*)buffer, _sampleSize) < _sampleSize)
-					_overruns++;
+				_ringBuffer->write(buffer, _numChannels);
 			}
-			/* Tell the disk thread there is work to do.  If it is already
-			 * running, the lock will not be available.  We can't wait
-			 * here in the process() thread, but we don't need to signal
-			 * in that case, because the disk thread will read all the
-			 * data queued before waiting again. */
 
-			if (pthread_mutex_trylock(&_diskThreadLock) == 0)
-			{
-				pthread_cond_signal (&_dataReadyCondition);
-				pthread_mutex_unlock (&_diskThreadLock);
-			}
+			_ringBuffer->signalAvailableData();
+
 			for (unsigned channel=0; channel<_numChannels; channel++)
 				_inports[channel]->Consume();
 			return true;
 		}
-
 		void DiskThread()
 		{
-			static unsigned total_captured = 0;
 			TData framebuf[_numChannels];
 			pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, NULL);
-			ScopedLock lock(_diskThreadLock);
+			RingBuffer::ScopedLock lock(*_ringBuffer);
 
 			while (true)
 			{
-				while ((jack_ringbuffer_read_space (_ringBuffer) >=_sampleSize)) {
-					jack_ringbuffer_read (_ringBuffer,(char*) framebuf,_sampleSize);
+				while (_ringBuffer->read(framebuf, _numChannels))
+				{
 					unsigned writeSize = _outfile->write(framebuf,_numChannels);
 					if (writeSize != _numChannels)
 					{
-						CLAM_ASSERT(_outfile,"Error writing the file.");
-						return;
+						// TODO: Not an assert!
+						CLAM_WARNING(false,"Error writing the file.");
 					}
-					total_captured++;
 				}
-
 				if(_isStopped) return;
 				// wait until process() signals more data
-				pthread_cond_wait (&_dataReadyCondition, &_diskThreadLock);
+				_ringBuffer->waitForDataAvailable();
 			}
 		}
 
@@ -223,9 +263,7 @@ namespace CLAM
 		{
 			_isStopped = false;
 			_canProcess = 0;
-			_ringBuffer = jack_ringbuffer_create (_sampleSize*_ringBufferSize*_numChannels);
-			memset(_ringBuffer->buf, 0, _ringBuffer->size);
-
+			_ringBuffer = new RingBuffer(_ringBufferSize*_numChannels);
 			_outfile = new SndfileHandle(_config.GetTargetFile().c_str(), SFM_WRITE,_format,_numChannels,_sampleRate);
 
 			// check if the file is open
@@ -250,17 +288,11 @@ namespace CLAM
 		bool ConcreteStop()
 		{
 			_isStopped = true;
-			if (pthread_mutex_trylock(&_diskThreadLock) == 0)
-			{
-				pthread_cond_signal (&_dataReadyCondition);
-				pthread_mutex_unlock (&_diskThreadLock);
-			}
+			_ringBuffer->signalAvailableData();
 			pthread_join (_threadId, NULL);
-			
 			if (_outfile) delete _outfile;
-			// TODO: This will halt the app!!!! Can not be an assert!
-			CLAM_ASSERT(_overruns,"Overruns is greater than 0. Try a bigger buffer");			
-			jack_ringbuffer_free (_ringBuffer);
+			CLAM_WARNING(not _ringBuffer->hadOverRuns(),"SndfileWriter: Overruns detected, try a higher buffer.");			
+			delete _ringBuffer;
 			return true;
 		}
 
@@ -312,6 +344,13 @@ namespace CLAM
 
 		bool ConcreteConfigure(const ProcessingConfig & config)
 		{
+			if ( _config.HasTargetFile() )
+			{
+				const AudioOutFilename & oldFilename = _config.GetTargetFile();
+				if ( not oldFilename.empty() )
+					FileSystem::GetInstance().UnlockFile( oldFilename );
+			}
+
 			CopyAsConcreteConfig(_config, config);
 			if(_inports.empty()) ResizePorts(1);
 
@@ -321,7 +360,6 @@ namespace CLAM
 
 			_format=ChooseFormat(location);
 			_numChannels = _config.GetNumberChannels();
-			_sampleSize = _numChannels*sizeof(TData);
 			_sampleRate = _config.GetSampleRate();
 
 			if (_numChannels == 0)
@@ -343,7 +381,7 @@ namespace CLAM
 				std::ostringstream nameStream;
 				nameStream << nameInPort << i+1;
 				AudioInPort * port = new AudioInPort( nameStream.str(), this);
-				_inports.push_back( port );
+				_inports[i] = port;
 			}
 			unsigned portSize = BackendBufferSize();
 			for (unsigned i=0; i<nPorts; i++)
