9a0d04d72d40ea7dcd9dcd19ea68cc538d48dcd4
nolaiz <nolaiz@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2009-07-30 10:19:31 +0000

    * plugins/spacialization/ladspa/clam_plugin: uncommented bformat2binaural and mono2binaural networks ladspa plugins (seems to be working with the new AudioStream2Buffer...)

--- CLAM/plugins/spacialization/ladspa/clam_plugin.cxx
+++ CLAM/plugins/spacialization/ladspa/clam_plugin.cxx
@@ -5,8 +5,8 @@
 CLAM_EXTERNAL_FILE_DATA(mono2bformat_embededNetwork,"../example-data/mono2bformat_nofaders.clamnetwork")
 CLAM_EXTERNAL_FILE_DATA(bformatRotationZoom_embededNetwork,"../example-data/bformatRotationZoom.clamnetwork")
 CLAM_EXTERNAL_FILE_DATA(bformat2surround_embededNetwork,"../example-data/bformat2surround.clamnetwork")
-//CLAM_EXTERNAL_FILE_DATA(bformat2binaural_embededNetwork,"../example-data/bformat2binaural.clamnetwork")
-//CLAM_EXTERNAL_FILE_DATA(mono2binaural_embededNetwork,"../example-data/mono2binaural_nofaders.clamnetwork")
+CLAM_EXTERNAL_FILE_DATA(bformat2binaural_embededNetwork,"../example-data/bformat2binaural.clamnetwork")
+CLAM_EXTERNAL_FILE_DATA(mono2binaural_embededNetwork,"../example-data/mono2binaural_nofaders.clamnetwork")
 CLAM_EXTERNAL_FILE_DATA(bformat2cube_embededNetwork,"../example-data/bformat2cube.clamnetwork")
 
 //fakes:
@@ -59,16 +59,16 @@ extern "C" const LADSPA_Descriptor * ladspa_descriptor(unsigned long index)
 	static CLAM::LadspaNetworkExporter n102(library, bformat2surround_embededNetwork, 102,
 			"bformat2surround", "BFormat to Surround 5.0",
 			"BarcelonaMedia-Audio", "GNU GPL");
-
-/*
-	static CLAM::LadspaNetworkExporter n107(library, dummyControls_embededNetwork, 107,
-			"dummy_controls", "Dummy controls for azimuth - elevation",
-			"BarcelonaMedia-Audio", "GNU GPL");
 	static CLAM::LadspaNetworkExporter n103(library, bformat2binaural_embededNetwork, 103,
 			"bformat2binaural", "BFormat to binaural (HRTF)",
 			"BarcelonaMedia-Audio", "GNU GPL");
 	static CLAM::LadspaNetworkExporter n104(library, mono2binaural_embededNetwork, 104,
 			"mono2binaural", "Mono to binaural (HRTF)",
+			"BarcelonaMedia-Audio", "GNU GPL");
+
+/*
+	static CLAM::LadspaNetworkExporter n107(library, dummyControls_embededNetwork, 107,
+			"dummy_controls", "Dummy controls for azimuth - elevation",
 			"BarcelonaMedia-Audio", "GNU GPL");
 	static CLAM::LadspaNetworkExporter n106(library, fake_bformat2surround_embededNetwork, 108,
 			"bformat2surround", "BFormat to Surround 5.0",
@@ -83,7 +83,6 @@ extern "C" const LADSPA_Descriptor * ladspa_descriptor(unsigned long index)
 			"bformat2cube", "BFormat to Cube for 8 speakers",
 			"BarcelonaMedia-Audio", "GNU GPL");
 */
-
 
 	static CLAM::LadspaNetworkExporter n1115(library, vbap15_shorter_distance, 1115,
 			"id_vbap_15_shorter_distance", "vbap15_shorter_distance",
