dc6525010a0e2c6e6e6985210957ed062fbd1ab5
dgriffioen <dgriffioen@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2009-08-31 11:05:55 +0000

    * ditec: removed setting buffersize, as it did not behave as expected

--- CLAM/plugins/spacialization/src/BufferDelayPrediction.hxx
+++ CLAM/plugins/spacialization/src/BufferDelayPrediction.hxx
@@ -39,11 +39,11 @@ namespace CLAM
 class BufferDelayPredictionConfig : public ProcessingConfig
 {
 public:
-	DYNAMIC_TYPE_USING_INTERFACE (BufferDelayPredictionConfig , 4, ProcessingConfig);
+	DYNAMIC_TYPE_USING_INTERFACE (BufferDelayPredictionConfig , 3, ProcessingConfig);
 	DYN_ATTRIBUTE (0, public, int, NumberOfInPorts);
 	DYN_ATTRIBUTE (1, public, int, Quality);
-	DYN_ATTRIBUTE (2, public, int, BufferSize);
-	DYN_ATTRIBUTE (3, public, int, SampleRate);
+	DYN_ATTRIBUTE (2, public, int, SampleRate);
+	//DYN_ATTRIBUTE (2, public, int, BufferSize);
 
 protected:
 	void DefaultInit(void)
@@ -52,8 +52,8 @@ protected:
 		UpdateData();
 		SetNumberOfInPorts(1);
 		SetQuality(40);
-		SetBufferSize(1024);
 		SetSampleRate(48000.0);
+		//SetBufferSize(1024);
 	}
 
 };
