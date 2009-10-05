78c7a659993fb7b76629aef36b4dc90aa0b8304c
nolaiz <nolaiz@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2009-10-05 14:46:32 +0000

    * plugins/osc/LibloSink: updated. First task of a refactor to be able to use TypedControls. And now uses path and port defined in the configuration.

--- CLAM/plugins/osc/LibloSink.hxx
+++ CLAM/plugins/osc/LibloSink.hxx
@@ -2,8 +2,11 @@
 #define LibloSink_hxx
 
 #include <CLAM/Processing.hxx>
+#include <CLAM/TypedInControl.hxx>
+#include <CLAM/TypeInfo.hxx>
 #include <CLAM/InControl.hxx>
 #include <string>
+#include <algorithm>
 #include <cstdio>
 #include <cmath>
 #include "lo/lo.h"
@@ -15,15 +18,22 @@ namespace CLAM
 
 class LibloSinkConfig : public CLAM::ProcessingConfig
 { 
-    DYNAMIC_TYPE_USING_INTERFACE( LibloSinkConfig, 1, ProcessingConfig );
-    DYN_ATTRIBUTE( 0, public, std::string, OscPath);
+	DYNAMIC_TYPE_USING_INTERFACE( LibloSinkConfig, 4, ProcessingConfig );
+	DYN_ATTRIBUTE( 0, public, std::string, OscPath);
+	DYN_ATTRIBUTE( 1, public, unsigned int, ServerPort);
+	DYN_ATTRIBUTE( 2, public, std::string, OSCTypeSpec);
+	DYN_ATTRIBUTE( 3, public, std::string, IPAddress);
+
     //TODO number of arguments/ports
 protected:
     void DefaultInit()
     {
-          AddAll();
-          UpdateData();
-          SetOscPath("/clam/target");
+	AddAll();
+	UpdateData();
+	SetOscPath("/clam/target");
+	SetServerPort(0);
+	SetOSCTypeSpec("fff");
+	SetIPAddress("127.0.0.1");
     };
 };
 class X
@@ -38,42 +48,42 @@ class LibloSink : public CLAM::Processing
 	typedef LibloSinkConfig Config;
 	lo_address _oscPort;
 	Config _config;
-	InControl _in1;
-	InControl _in2;
-	InControl _in3;
+	std::vector <InControlBase *> _inControls;
 public:
 
 	LibloSink(const Config& config = Config()) 
-		: _in1("osc argument 1", this)
-		, _in2("osc argument 2", this)
-		, _in3("osc argument 3", this)
 	{
 		Configure( config );
-		_oscPort = lo_address_new(NULL, "7780");
 	}
+
+	~LibloSink()
+	{
+		RemoveOldControls();
+	}
+
+
 	const CLAM::ProcessingConfig & GetConfig() const
 	{
 		return _config;
 	}
-	void SendOsc(float f1, float f2, float f3)
-	{
-		//TODO this code is provisional. OSC path should be configurable
-	
-	//	std::cout << f1 << " " << f2 << " " << f3 << std::endl;
-		if (lo_send(_oscPort, "/navigator", "fff", f1, f2, f3) == -1) {
-			printf("OSC error %d: %s\n", lo_address_errno(_oscPort), lo_address_errstr(_oscPort));
-	      }
-	}
 	bool Do()
 	{
-
-		SendOsc(	
-			_in1.GetLastValue(), 
-			_in2.GetLastValue(), 
-			_in3.GetLastValue()
-			);
 		return true;
 	}
+
+
+	void InControlCallback(const TControlData & value)
+	{
+
+		float a=dynamic_cast<FloatInControl *>(_inControls[0])->GetLastValue();
+		float b=dynamic_cast<FloatInControl *>(_inControls[1])->GetLastValue();
+		float c=dynamic_cast<FloatInControl *>(_inControls[2])->GetLastValue();
+		if (lo_send(_oscPort, _config.GetOscPath().c_str(), "fff", a,b,c) == -1) 
+		{
+			printf("OSC error %d: %s\n", lo_address_errno(_oscPort), lo_address_errstr(_oscPort));
+		}
+	}
+
 	const char* GetClassName() const
 	{
 		return "LibloSink";
@@ -81,9 +91,90 @@ public:
 protected:
 	bool ConcreteConfigure(const CLAM::ProcessingConfig & config)
 	{
+		RemoveOldControls();
 		CopyAsConcreteConfig(_config, config);
 
+		char portCString[10];
+		sprintf(portCString,"%u",_config.GetServerPort());
+	
+		_oscPort = lo_address_new(_config.GetIPAddress().c_str(), portCString);
+		unsigned nInputs = GetInputsNumber();
+		if (nInputs==0)
+		{
+			AddConfigErrorMessage("No proper OSCTypeSpec setup. Use: 'f' for float, 'd' for double, 'i' for integer, 'h' for integer 64.");
+			return false;
+		}
+
+		// multi-port names share user-configured identifier
+		std::string oscPath=_config.GetOscPath();
+		std::replace(oscPath.begin(),oscPath.end(),'/','_');
+
+		for (unsigned i=0;i<nInputs;i++)
+		{
+			std::ostringstream controlName;
+			controlName<<i;
+			std::string type;
+			type=_config.GetOSCTypeSpec()[i];
+			_inControls.push_back(createControl(type,controlName.str()));
+		}
+
 		return true; // Configuration ok
+	}
+
+        void RemoveOldControls()
+        {
+		std::vector<InControlBase *>::iterator it;
+		for (it=_inControls.begin();it!=_inControls.end();it++)
+		{
+			delete *it;
+		}
+		_inControls.clear();
+		GetInControls().Clear();
+	}
+
+	InControlBase * createControl(const std::string & type, const std::string & name)
+	{
+/*		if (type=="s")
+			return new TypedInControl<std::string> (name, this, &LibloSink::InControlCallback);*/
+		if (type=="f")
+			return new FloatInControl (name, this, &LibloSink::InControlCallback);
+/*		if (type=="d")
+			return new TypedInControl<double> (name, this, &LibloSink::InControlCallback);
+		if (type=="i" or type=="h")
+			return new TypedInControl<int> (name,this, &LibloSink::InControlCallback);*/
+		// TODO: Decide whether ASSERTing (contract) or throw (control) 
+		return 0;
+	}
+
+/*	static void sendControl(OutControlBase * control, lo_arg * valueToSend)
+	{
+		const std::string typeName=control->GetTypeId().name();
+		if (typeName=="Ss")
+			dynamic_cast<TypedOutControl<std::string> *> (control)->SendControl(&(valueToSend->s));
+		if(typeName=="f")
+			dynamic_cast<FloatOutControl *>(control)->SendControl(valueToSend->f);
+		if(typeName=="d")
+			dynamic_cast<TypedOutControl<double> *> (control)->SendControl(valueToSend->f32);
+		if(typeName=="i")
+			dynamic_cast<TypedOutControl<int> *> (control)->SendControl(valueToSend->i);
+		return;
+	}
+*/
+
+	const unsigned int GetInputsNumber() const
+	{
+		unsigned nInputs;
+		std::string typespec;
+		typespec=_config.GetOSCTypeSpec();
+		for (nInputs=0; nInputs<typespec.size();nInputs++)
+		{
+			const char oscType = typespec[nInputs];
+			if (oscType!='s' and oscType!='i' 
+				and oscType != 'f' and oscType !='d'
+				and oscType != 'h')
+				return 0; // return 0 if there is any non-compatible type
+		}
+		return nInputs;
 	}
 
 };
