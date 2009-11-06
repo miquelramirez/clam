5d8b6b77917efb17a0ba14412451e5c59e50baf9
dgriffioen <dgriffioen@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2009-11-06 11:48:04 +0000

    * ditec: sample delay with crossfade

--- CLAM/plugins/spacialization/src/SampleAccurateBufferDelay.hxx
+++ CLAM/plugins/spacialization/src/SampleAccurateBufferDelay.hxx
@@ -22,107 +22,28 @@
 #define SAMPLEACCURATEBUFFERDELAY_INCLUDED
 
 #include <CLAM/InPort.hxx>
-#include <CLAM/Processing.hxx>
-#include <CLAM/AudioWindowingConfig.hxx>
-#include <CLAM/InControl.hxx>
-
-#include <vector>
+#include "SampleAccurateDelay.hxx"
 
 namespace CLAM
 {
 
-class SampleAccurateBufferDelayConfig : public ProcessingConfig
-{
-public:
-	DYNAMIC_TYPE_USING_INTERFACE (SampleAccurateBufferDelayConfig , 2, ProcessingConfig);
-	DYN_ATTRIBUTE (0, public, int, MaxDelayInSamples);
-	DYN_ATTRIBUTE (1, public, int, SampleRate);
-
-protected:
-	void DefaultInit(void)
-	{
-		AddAll();
-		UpdateData();
-		SetMaxDelayInSamples(65536);
-		SetSampleRate(48000);
-	}
-};
-
-
 /**
  *
  */
-class SampleAccurateBufferDelay : public Processing
+class SampleAccurateBufferDelay : public SampleAccurateDelay
 {
 	InPort<Audio> _in1;
 	OutPort<Audio> _out1;
-	InControl _delayControl;
-	
-	SampleAccurateBufferDelayConfig _config;
-	
-	typedef std::vector<TData> DelayBuffer;
-	DelayBuffer _delayBuffer;
-	
-	unsigned _sampleRate, _delayBufferSize, _delayInSamples, 
-			_readIndex, _writeIndex, _crossFadeIndex;
-	
-	enum { CROSSFADESIZE = 100 };
-	DelayBuffer _crossFadeBuffer;
 
-	void setDelay(float delayInSec) 
-	{
-		unsigned delayInSamples = delayInSec * _sampleRate;
-		
-		if (_delayInSamples > _delayBufferSize) 
-			return;
-		
-		unsigned readIndex = (_writeIndex - delayInSamples) % _delayBufferSize ;
-		
-		if (readIndex == _readIndex)
-			return;
-		
-		_readIndex = readIndex;
-		_crossFadeIndex = CROSSFADESIZE;
-		
-		for (unsigned i = 0; i < CROSSFADESIZE; i++) 
-			_crossFadeBuffer[i]=_delayBuffer[readIndex++%_delayBufferSize] * (1./(CROSSFADESIZE-i));
-						
-		return;
-	}
-	
-	TData delayLine(TData x)
-	{	
-		_delayBuffer[_writeIndex++%_delayBufferSize] = x;
-		TData y = _delayBuffer[_readIndex++%_delayBufferSize];
-		
-		if (_crossFadeIndex > 0)  
-			y *= (1./_crossFadeIndex) + _crossFadeBuffer[--_crossFadeIndex];
-				
-		return y;
-	}
-		
 public:
-	const char* GetClassName() const { return "SampleAccurateBufferDelay"; }
-	SampleAccurateBufferDelay() 
+	SampleAccurateBufferDelay(const Config& config = Config()) 
 		: _in1("InputBuffer", this)
 		, _out1("OutputBuffer", this)
-		, _delayControl("VaryingDelay", this)
-		, _delayInSamples(0.)
-		, _crossFadeIndex(0)
-		
 	{
-		Configure( _config );
-		_sampleRate = _config.GetSampleRate();
-		
-		_crossFadeBuffer.resize(CROSSFADESIZE);
-		std::fill(_crossFadeBuffer.begin(), _crossFadeBuffer.end(), 0.);
-		
-		_delayBuffer.resize(_config.GetMaxDelayInSamples());
-		_delayBufferSize = _delayBuffer.size(); 
-		_readIndex = _writeIndex = (_delayBufferSize-1); 
-		
-		std::fill(_delayBuffer.begin(), _delayBuffer.end(), 0.);
+		Configure( config );
 	}
+	
+	const char* GetClassName() const { return "SampleAccurateBufferDelay"; }
 	
 	bool Do()
 	{
@@ -140,7 +61,6 @@ public:
 		for (unsigned i = 0; i < size; ++i) 
 			outpointer[i] = delayLine(inpointer[i]);
 		
-		// Tell the ports this is done
 		_in1.Consume();
 		_out1.Produce();
 		return true;
