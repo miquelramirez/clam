ae5c8b7613c872b742316bc2d99c86fb1608dea5
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2010-01-04 16:48:54 +0000

    * Moved Vector arithmetic from Vbap3D.hxx to Orientation.hxx (more generic header)  * Renamed Vector->Vector3D to avoid collision with raytracing scene.hxx

--- trunk/CLAM/plugins/spacialization/src/Orientation.hxx
+++ trunk/CLAM/plugins/spacialization/src/Orientation.hxx
@@ -198,6 +198,63 @@ private:
 	}
 };
 
+// Basic vector arithmetic
+struct Vector3D
+{
+	float x;
+	float y;
+	float z;
+};
+
+static Vector3D vectorialProduct(const Vector3D& v1, const Vector3D& v2)
+{
+	Vector3D result = {
+		v1.y * v2.z - v1.z * v2.y ,
+		v1.z * v2.x - v1.x * v2.z ,
+		v1.x * v2.y - v1.y * v2.x ,
+		};
+	return result;
+}
+static Vector3D product(const float& factor, const Vector3D& v)
+{
+	Vector3D result = { factor * v.x, factor * v.y, factor * v.z };
+	return result;
+}
+
+static float escalarProduct(const Vector3D& v1, const Vector3D& v2)
+{
+	return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+}
+static Vector3D substract(const Vector3D& v1, const Vector3D& v2)
+{
+	Vector3D result = {
+		v1.x - v2.x ,
+		v1.y - v2.y ,
+		v1.z - v2.z ,
+		};
+	return result;
+}
+static float mod(const Vector3D& v)
+{
+	return sqrt(v.x*v.x + v.y*v.y + v.z*v.z);
+}
+static float angle(const Vector3D& v1, const Vector3D& v2)
+{
+	float divisor = mod(v1)*mod(v2);
+	const float deltaNumeric = 0.00001;
+	CLAM_ASSERT( divisor > deltaNumeric, "Cannot compute an angle of a zero vector"); 
+	float arg =  escalarProduct(v1,v2) / (mod(v1)*mod(v2));
+	if (arg <-1 or arg >1) return arg < 0 ? M_PI : 0;
+	return acos( arg );
+}
+static void print(const Vector3D& v, std::string name="")
+{
+	std::cout << name << " (" << v.x << ", " << v.y << ", " << v.z << ")" << std::endl;
+}
+static float rad( float deg )
+{
+	return deg / 180 * M_PI;
+}
 
 } // namespace
 #endif // Orientation_hxx
