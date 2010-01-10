63d6b597ec3c285b1a05244dd1197d45b770e006
David García Garzón <vokimon@users.noreply.github.com>
2010-01-10 06:19:00 +0000

    * sndfile testing: enhanced script

--- CLAM/plugins/sndfile/testing/test.py
+++ CLAM/plugins/sndfile/testing/test.py
@@ -1,30 +1,26 @@
 #!/usr/bin/python
-import glob,os,re
-#listOfWavs=glob.glob("sine*.wav")
-listOfWavs=[]
+import glob,os,re,sys
 
-for number in range(1,65):
-	listOfWavs.append("sine%i.wav"%number)
-#	os.system("sox -M %s %s" % (("sine.wav "*number),listOfWavs[-1]))
-	
+def run(command) :
+	print "\033[32m%s\033[0m"%command
+	result = os.system(command)
+	if result == 0 : return
+	print "\033[31mExecution failed\033[0m"
+	sys.exit(-1)
+
 errorLevel=os.system("(cd .. ; scons && scons install)")
 if errorLevel != 0:
-	import sys
 	sys.exit()
-for file in listOfWavs:
-	os.system("cp test_main.clamnetwork tmp.clamnetwork")
-	numberOfChannels=re.search("[0-9]+",file).group(0)
-	line='clamrefactor.py -x -c \'setConfig  SndfilePlayer SourceFile "%s"\' -c \'setConfig SndfilePlayer SavedNumberOfChannels %s\' tmp.clamnetwork' % (file,numberOfChannels) 
-	print line
-	errorLevel=os.system(line)
-	if errorLevel!=0:
-		import sys
-		sys.exit()
-	line='LADSPA_PATH=/dev/null OfflinePlayer tmp.clamnetwork sine1.wav -o salida%s.wav'%numberOfChannels
-	print line
-	errorLevel=os.system(line)
-	if errorLevel!=0:
-		import sys
-		sys.exit()
+
+for n in range(1,18) :
+	file = "sine%02i.wav"%n
+	if not os.access(file, os.R_OK) :
+		if n==1 :
+			run("cp sine.wav sine01.wav")
+		else :
+			run("sox -M %s %s" % ("sine.wav "*n,file))
+	run("cp test_main.clamnetwork tmp.clamnetwork")
+	run('clamrefactor.py -x -c \'setConfig  SndfilePlayer SourceFile "%s"\' -c \'setConfig SndfilePlayer SavedNumberOfChannels %s\' tmp.clamnetwork' % (file,n) )
+	run('LADSPA_PATH=/dev/null OfflinePlayer tmp.clamnetwork sine.wav -o salida%s.wav'%n)
 
 	
