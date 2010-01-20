ffc6c66faf6526a5e0d1b8a84f0becb19fea76de
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2010-01-20 17:24:15 +0000

    * pkg-config file generator moved to an scons tool

--- trunk/CLAM/scons/libs/clam_build_helpers.py
+++ trunk/CLAM/scons/libs/clam_build_helpers.py
@@ -354,39 +354,6 @@ def SetupSpawn( env ):
 
 
 def create_custom_builders( env ) :
-	def PkgConfigFile(env, package, version, prefix, description=None, url=None, requires=[], conflicts=[], cflags=[], libs=[]) :
-		pkgConfigTemplate = env.Value("""\
-name = @name@
-prefix = @prefix@
-libdir = ${prefix}/lib
-includedir = ${prefix}/include
-
-Name: ${name}
-Description: @description@
-Url: @url@
-Version: @version@
-Requires: @requires@
-Conflicts: @conflicts@
-Libs: -L${libdir} -l${name} @libs@
-Cflags: -I${includedir} @cflags@
-""")
-		return env.Textfile(target = package,
-			source = [pkgConfigTemplate],
-			TEXTFILESUFFIX='.pc',
-			SUBST_DICT=[
-				('@prefix@', prefix ),
-				('@name@', package ),
-				('@version@', version ),
-				('@description@', "C++ Framework for analysis, synthesis and transformation of music audio signals" ),
-				('@url@', 'http://clam-project.org' ),
-				('@requires@', ", ".join(requires) ),
-				('@conflicts@', ", ".join(conflicts) ),
-				('@cflags@', " ".join(cflags) ),
-				('@libs@', " ".join(libs) ),
-				]
-			)
-	env.AddMethod(PkgConfigFile)
-
 	def generate_so_name( target, source, env ) :
 		source_dir = os.path.dirname( str(source[0]) )
 		cwd = os.getcwd()
