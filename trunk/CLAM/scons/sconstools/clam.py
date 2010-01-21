66e3be95a88279687daad41966b8d99ae3cffb63
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2010-01-21 08:45:48 +0000

    * clam.py: common module coded moved to the ClamModule function  * clam.py: common functions to search sources moved here  * clam.py: added soname support for the plugins

--- trunk/CLAM/scons/sconstools/clam.py
+++ trunk/CLAM/scons/sconstools/clam.py
@@ -1,4 +1,80 @@
-import sys
+import sys, os, glob
+
+def scanFiles(env, pattern, paths, blacklist=[]) :
+	files = []
+	for path in paths :
+		files+=glob.glob(os.path.join(path,pattern))
+	return [file for file in files if file not in blacklist]
+
+def recursiveDirs(env, root) :
+	return filter( 
+		(lambda a : a.rfind( ".svn")==-1 ), 
+		[ a[0] for a in os.walk(root)]  )
+
+def ClamModule(env, moduleName, version,
+		description="",
+		url='http://clam-project.org',
+		clamDependencies=[],
+		otherDependencies=[],
+		sources=[],
+		headers=[],
+		) :
+	try: env.PkgConfigFile
+	except : env.Tool('pc', toolpath=[os.path.dirname(__file__)])
+	
+	libraryName = 'clam_'+moduleName
+	if not env['verbose'] : env.ClamQuietCompilation()
+
+	# The empty plugin linking to the module library
+	envPlugin = env.Clone()
+	envPlugin.AppendUnique(
+		LIBS=[libraryName],
+		LIBPATH=['.'],
+		)
+	plugin = envPlugin.LoadableModule(target=libraryName+'_plugin', source = [])
+
+	# pkg-config file
+	pcfile = env.PkgConfigFile(
+		package = libraryName,
+		version = version,
+		prefix = env['prefix'],
+		description = description,
+		url = url,
+		requires = clamDependencies+otherDependencies,
+		cflags = [],
+		)
+
+	versionNumbers=tuple(version.split("."))
+
+	if sys.platform == 'linux2' :
+		# * Lib name: the actual fully versioned name of the library.
+		# * Soname: is the name of a link that dependant executables will look
+		# for at runtime. It does not contain the bugfix version. This enables
+		# abi compatible versions.
+		# The soname is embeded into the library by using "-Wl,-soname,..."
+		# and the linker embeds it also in the dependant binaries.
+		# * Linker name: it a soft link without version numbers, pointing to
+		# the current development library version to be provided to the linker
+		# in order the build system to be version agnostic.
+		linkname = 'libclam_'+moduleName+'.so'
+		soname   = 'libclam_'+moduleName+'.so.%s.%s' % versionNumbers[:2]
+		libname  = 'libclam_'+moduleName+'.so.%s.%s.%s' % versionNumbers
+		env.Append(SHLINKFLAGS=['-Wl,-soname,%s'%soname ] )
+		lib = env.SharedLibrary( libraryName, sources,
+				SHLIBSUFFIX='.so.%s.%s.%s'%versionNumbers )
+		localSoName   = env.LinkerNameLink( soname, lib )       # lib***.so.XY -> lib***.so.XY.Z
+		localLinkName = env.LinkerNameLink( target=linkname, source=lib ) # lib***.so    -> lib***.so.XY.Z
+		libraries = [lib, localSoName, localLinkName]
+	install = [
+		env.Install(os.path.join(env['prefix'],'lib'), libraries),
+		env.Install(os.path.join(env['prefix'],'lib','clam'), plugin),
+		env.Install(os.path.join(env['prefix'],'lib','pkgconfig'), pcfile),
+		env.Install(os.path.join(env['prefix'],'include','CLAM',moduleName), headers),
+		]
+
+	return install, (libraries, plugin, pcfile)
+
+	
 
 def ClamQuietCompilation(env) :
 	env['CXXCOMSTR'] = '== Compiling C++ $SOURCE'
@@ -38,8 +114,40 @@ def enable_modules( self, libs, path) :
 
 
 def generate(env) :
+	def generate_so_name( target, source, env ) :
+		source_dir = os.path.dirname( str(source[0]) )
+		print "Generating soname", source[0], target[0], source_dir
+		cwd = os.getcwd()
+		os.chdir( source_dir )
+		if sys.platform == 'linux2' :
+			os.system( "/sbin/ldconfig -n ." )
+		os.chdir(cwd)
+
+	bld = env.Builder( action=env.Action(generate_so_name,
+		"== Generating soname $TARGET") )
+	env.Append( BUILDERS={'SonameLink' : bld} )
+
+	def generate_linker_name( target, source, env ) :
+		print "Generating linker"
+		source_file = os.path.basename( str(source[0]) )
+		os.system( "ln -sf %s %s"%(source_file,str(target[0]) ))
+
+	bld = env.Builder( action=env.Action(generate_linker_name,
+		"== Generating linker name $TARGET to $SOURCE") )
+	env.Append( BUILDERS={'LinkerNameLink' : bld} )
+
+	import shutil
+	bld = env.Builder( action =  env.Action( 
+		lambda target, source, env:
+			shutil.copy(str(source[0]), str(target[0])),
+			"== Build copying $SOURCE"))
+
+	env.Append( BUILDERS={'CopyFileAndUpdateIncludes' : bld} )	
 	env.AddMethod(enable_modules, "EnableClamModules")
 	env.AddMethod(ClamQuietCompilation)
+	env.AddMethod(ClamModule)
+	env.AddMethod(scanFiles)
+	env.AddMethod(recursiveDirs)
 
 def exists(env):
 	return True
