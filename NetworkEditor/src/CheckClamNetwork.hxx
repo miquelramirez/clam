c49af8a29a15e4b3ecf2bd2e059c5f37bb212a2f
ascorza <ascorza@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2010-03-12 17:13:05 +0000

    * added check_clam_networks_recursively.py (script) to review the networks and fixed tab problem

--- NetworkEditor/src/CheckClamNetwork.hxx
+++ NetworkEditor/src/CheckClamNetwork.hxx
@@ -16,44 +16,41 @@
 
 class CheckClamNetwork
 {
-    CheckClamNetwork()  {}
-    ~CheckClamNetwork() {}
+	CheckClamNetwork()  {}
+	~CheckClamNetwork() {}
 
-
-    static bool FileExists( const std::string filename )
-    {
-        //Check for existence of XML Network file
-        std::ifstream file( filename.c_str() );
-        if( !file ) return false;
-        return true;
-    }
+	static bool FileExists( const std::string filename )
+	{
+		//Check for existence of XML Network file
+		std::ifstream file( filename.c_str() );
+		if( !file ) return false;
+		return true;
+	}
 
 public:
 
-    static bool isCorrectNetwork(const std::string _networkFile)
-    {
-        try
-        {
-            CLAM::Network _network;
-            CLAM::XMLStorage::Restore(_network, _networkFile);
-            return true;
-        }
-        catch (CLAM::XmlStorageErr & e)
-        {
-            if(FileExists(_networkFile))
-            {
-                std::cout << "An occurred while loading the network file: ";
-                std::cout << e.what() << std::endl;
-            }
-            else
-            {
-                std::cout << "Network file " << _networkFile << std::endl;
-            }
-
-        }
-        return false;
-    }
-
+	static bool isCorrectNetwork(const std::string _networkFile)
+	{
+		try
+		{
+			CLAM::Network _network;
+			CLAM::XMLStorage::Restore(_network, _networkFile);
+			return true;
+		}
+		catch (CLAM::XmlStorageErr & e)
+		{
+			if(FileExists(_networkFile))
+			{
+				std::cout << "An occurred while loading the network file: ";
+				std::cout << e.what() << std::endl;
+			}
+			else
+			{
+				std::cout << "Network file " << _networkFile << std::endl;
+			}
+		}
+	return false;
+	}
 };
 
 #endif //CheckClamNetwork_hxx
