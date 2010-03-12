c6eb6189e0f6ffc61795cb483d07f7821dfd1b34
David García Garzón <vokimon@users.noreply.github.com>
2010-03-12 17:36:00 +0000

    * VstNetworkExporter: migrated to NetworkPlayer

--- CLAM/examples/PluginExamples/VstPluginExample/VstNetworkExporter.cxx
+++ CLAM/examples/PluginExamples/VstPluginExample/VstNetworkExporter.cxx
@@ -7,6 +7,23 @@
 namespace CLAM
 {
 
+VstNetworkExporter::VstNetworkExporter (
+		const std::string & networkXml,
+		VstInt32 uniqueId,
+		const std::string & effectName,
+		const std::string & productString,
+		const std::string & vendor,
+		VstInt32 version
+	)
+	: _uniqueId(uniqueId)
+	, _embeddedNetwork(networkXml)
+	, _effectName(effectName)
+	, _productString(productString)
+	, _vendor(vendor)
+	, _version(version)
+{
+}
+
 VstNetworkExporter::Plugin * VstNetworkExporter::createEffect(audioMasterCallback audioMaster)
 {
 	return new VstNetworkExporter::Plugin(
@@ -21,37 +38,37 @@ VstNetworkExporter::Plugin * VstNetworkExporter::createEffect(audioMasterCallbac
 
 // VST interface: Metadata
 
-bool VstNetworkExporter::getEffectName (char* name)
+bool VstNetworkPlayer::getEffectName (char* name)
 {
 	strncpy (name, _effectName.c_str(), kVstMaxEffectNameLen);
 	return true;
 }
 
-bool VstNetworkExporter::getProductString (char* text)
+bool VstNetworkPlayer::getProductString (char* text)
 {
 	strncpy (text, _productString.c_str(), kVstMaxProductStrLen);
 	return true;
 }
 
-bool VstNetworkExporter::getVendorString (char* text)
+bool VstNetworkPlayer::getVendorString (char* text)
 {
 	strncpy (text, _vendor.c_str(), kVstMaxVendorStrLen);
 	return true;
 }
 
-VstInt32 VstNetworkExporter::getVendorVersion ()
+VstInt32 VstNetworkPlayer::getVendorVersion ()
 {
 	return _version;
 }
 
 // VST interface: Program management
 
-void VstNetworkExporter::setProgramName (char *name)
+void VstNetworkPlayer::setProgramName (char *name)
 {
 	_programName = name;
 }
 
-void VstNetworkExporter::getProgramName (char *name)
+void VstNetworkPlayer::getProgramName (char *name)
 {
 	strncpy (name, _programName.c_str(), kVstMaxProgNameLen);
 }
@@ -59,37 +76,37 @@ void VstNetworkExporter::getProgramName (char *name)
 
 // VST interface: Parameter management
 
-void VstNetworkExporter::setParameter (VstInt32 index, float value)
+void VstNetworkPlayer::setParameter (VstInt32 index, float value)
 {
 	ExternControlInfo & controlInfo = mInControlList[index];
 	float realval = controlInfo.min + value * ( controlInfo.max - controlInfo.min ) ;
-
+	
 	controlInfo.lastvalue = realval;
-	controlInfo.processing->Do(realval);
-	std::cout << "== setParameter " << value << " -> " << realval << std::endl;
+	ReadControlSource(index, &realval);
+	std::cout << "== setParameter " << GetControlSourceName(index) << ": " << value << " -> " << realval << std::endl;
 }
 
-float VstNetworkExporter::getParameter (VstInt32 index)
+float VstNetworkPlayer::getParameter (VstInt32 index)
 {
 	ExternControlInfo & control = mInControlList[index];
 	float interfaceValue = (control.lastvalue-control.min)/(control.max-control.min);
-	std::cout << "== getParameter " << control.lastvalue << " -> " << interfaceValue << std::endl;
+	std::cout << "== getParameter " << GetControlSourceName(index) << ": " << control.lastvalue << " -> " << interfaceValue << std::endl;
 	return interfaceValue;
 }
 
-void VstNetworkExporter::getParameterName (VstInt32 index, char *label)
+void VstNetworkPlayer::getParameterName (VstInt32 index, char *label)
 {
-	strncpy ( label, mInControlList[index].name.c_str(), kVstMaxParamStrLen ); 
+	strncpy ( label, GetControlSourceName().c_str(), kVstMaxParamStrLen ); 
 	std::cout << "== getParameterName " << label << std::endl;
 }
 
-void VstNetworkExporter::getParameterDisplay (VstInt32 index, char *text)
+void VstNetworkPlayer::getParameterDisplay (VstInt32 index, char *text)
 {
 	float2string ( mInControlList[index].lastvalue, text, kVstMaxParamStrLen);
 	std::cout << "== getParameterDisplay " << text << std::endl;
 }
 
-void VstNetworkExporter::getParameterLabel(VstInt32 index, char *label)
+void VstNetworkPlayer::getParameterLabel(VstInt32 index, char *label)
 {
 	ControlSourceConfig& conf=const_cast<ControlSourceConfig&>(
 		dynamic_cast<const ControlSourceConfig&>(
@@ -97,27 +114,7 @@ void VstNetworkExporter::getParameterLabel(VstInt32 index, char *label)
 	strncpy (label, conf.GetUnitName().c_str(), kVstMaxParamStrLen);
 }
 
-//---------------------------------------------------------------------
-VstNetworkExporter::VstNetworkExporter (
-		const std::string & networkXml,
-		VstInt32 uniqueId,
-		const std::string & effectName,
-		const std::string & productString,
-		const std::string & vendor,
-		VstInt32 version
-	)
-	: AudioEffectX (0, 1 /*nPrograms*/, GetNumberOfParameters(networkXml) )
-	, _uniqueId(uniqueId)
-	, _embeddedNetwork(networkXml)
-	, _effectName(effectName)
-	, _productString(productString)
-	, _vendor(vendor)
-	, _version(version)
-{
-	
-}
-
-VstNetworkExporter::VstNetworkExporter (
+VstNetworkPlayer::VstNetworkPlayer (
 		audioMasterCallback audioMaster,
 		const std::string & networkXml,
 		VstInt32 uniqueId,
@@ -134,8 +131,7 @@ VstNetworkExporter::VstNetworkExporter (
 	, _vendor(vendor)
 	, _version(version)
 {
-	mClamBufferSize=512;
-	mExternBufferSize=mClamBufferSize;
+	mExternBufferSize=512;
 	std::istringstream xmlfile(networkXml);
 	try
 	{
@@ -164,20 +160,20 @@ VstNetworkExporter::VstNetworkExporter (
 	}
 	LocateConnections();
 
-	setNumInputs (mReceiverList.size());
-	setNumOutputs (mSenderList.size());
+	setNumInputs (GetNSources());
+	setNumOutputs (GetNSinks());
 	setUniqueID (_uniqueId);
 	_programName = "Default";
 	_network.Start();
 }
 
-VstNetworkExporter::~VstNetworkExporter ()
+VstNetworkPlayer::~VstNetworkPlayer ()
 {
 	_network.Stop();
 }
 
-//---------------------------------------------------------------------
-int VstNetworkExporter::GetNumberOfParameters( const std::string & networkXmlContent )
+
+int VstNetworkPlayer::GetNumberOfParameters( const std::string & networkXmlContent )
 {
 	Network net;
 	std::istringstream file(networkXmlContent);
@@ -187,7 +183,7 @@ int VstNetworkExporter::GetNumberOfParameters( const std::string & networkXmlCon
 	}
 	catch ( XmlStorageErr err)
 	{
-		std::cerr << "VstNetworkExporter WARNING: error loading embedded network. "
+		std::cerr << "VstNetworkPlayer WARNING: error loading embedded network. "
 			"Plugin not loaded" <<std::endl;
 		return -2;
 	}
@@ -205,7 +201,7 @@ int VstNetworkExporter::GetNumberOfParameters( const std::string & networkXmlCon
 	return count;
 }
 
-void VstNetworkExporter::processReplacing (float **inputs, float **outputs, VstInt32 sampleFrames)
+void VstNetworkPlayer::processReplacing (float **inputs, float **outputs, VstInt32 sampleFrames)
 {
 	std::cout << "." << std::flush;
 	if (unsigned(sampleFrames)!=mExternBufferSize)
@@ -215,68 +211,37 @@ void VstNetworkExporter::processReplacing (float **inputs, float **outputs, VstI
 		UpdatePortFrameAndHopSize();
 	}
 
-	for (unsigned i=0; i<mReceiverList.size(); i++)
-		mReceiverList[i].processing->SetExternalBuffer( inputs[i], mExternBufferSize, 0 );
-
-	for (unsigned i=0; i<mSenderList.size(); i++)
-		mSenderList[i].processing->SetExternalBuffer( outputs[i], mExternBufferSize, 0 );
+	for (unsigned i=0; i<GetNSources(); i++)
+		SetSourceBuffer(i, inputs[i], mExternBufferSize);
+	for (unsigned i=0; i<GetNSinks(); i++)
+		SetSinkBuffer(i, outputs[i], mExternBufferSize);
 
 	_network.Do();
 }
 
-////////////////////////////////////////////////////////////////////////////////////
-void VstNetworkExporter::LocateConnections()
+void VstNetworkPlayer::LocateConnections()
 {
-	CLAM_ASSERT( mReceiverList.empty(), "VstNetworkExporter::LocateConnections() : there are already registered input ports");
-	CLAM_ASSERT( mSenderList.empty(), "VstNetworkExporter::LocateConnections() : there are already registered output ports");
-	CLAM_ASSERT( mInControlList.empty(), "VstNetworkExporter::LocateConnections() : there are already registered controls");
-
-	for (Network::ProcessingsMap::const_iterator it=_network.BeginProcessings(); it!=_network.EndProcessings(); it++)
+	CacheSourcesAndSinks();
+	for (unsigned i=0; i<GetNControlSources(); i++)
 	{
-		CLAM::Processing * processing = it->second;
-		std::string className = processing->GetClassName();
-		if (className == "AudioSource")
-		{
-			std::cout << "AudioSource: " << it->first << std::endl;
-			ConnectorInfo<AudioSource> info;
-			info.name=it->first;
-			info.processing=(AudioSource*)processing;
-			info.processing->SetFrameAndHopSize( mExternBufferSize,0 );
-			mReceiverList.push_back(info);
-		}
-		if (className == "AudioSink")
-		{
-			std::cout << "AudioSink: " << it->first << std::endl;
-			ConnectorInfo<AudioSink> info;
-			info.name=it->first;
-			info.processing=(AudioSink*)processing;
-			info.processing->SetFrameAndHopSize( mExternBufferSize,0 );
-			mSenderList.push_back(info);
-		}
-
-		if (className == "ControlSource")
-		{
-			std::cout << "ControlSource: " << it->first << std::endl;
-			const ControlSourceConfig & conf= dynamic_cast<const ControlSourceConfig&>(it->second->GetConfig() );
-
+		const ControlSourceConfig & conf = 
+			dynamic_cast<const ControlSourceConfig&>(
+				_controlSources[i]->GetConfig() );
 			ExternControlInfo info;
-			info.name=it->first;
-			info.processing=(ControlSource*)processing;
 			info.min=conf.GetMinValue();
 			info.max=conf.GetMaxValue();
-			info.lastvalue=info.max;
+			info.lastvalue=conf.GetDefaultValue();
 			mInControlList.push_back(info);
-		}
 	}
+	UpdatePortFrameAndHopSize();
 }
 
-void VstNetworkExporter::UpdatePortFrameAndHopSize()
+void VstNetworkPlayer::UpdatePortFrameAndHopSize()
 {
-	for (unsigned i=0; i<mReceiverList.size(); i++)
-		mReceiverList[i].processing->SetFrameAndHopSize(mExternBufferSize,0);
-		
-	for (unsigned i=0; i<mSenderList.size(); i++)
-		mSenderList[i].processing->SetFrameAndHopSize(mExternBufferSize,0);
+	for (unsigned i=0; i<GetNSources(); i++)
+		SetSourceFrameSize(i, mExternBufferSize);
+	for (unsigned i=0; i<GetNSinks(); i++)
+		SetSinkFrameSize(i, mExternBufferSize);
 }
 
 } // CLAM
