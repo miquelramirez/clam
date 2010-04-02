f7329d32e1e5e5ade12c5e365186463ceaf4dcfe
David García Garzón <vokimon@users.noreply.github.com>
2010-04-02 01:16:59 +0000

    * LibloSink/MultiLibloSource: safer reconfigure without disconnecting wires  * MultiLibloSource: safer reconfigure always unregistering if needed before any other check

--- CLAM/plugins/osc/MultiLibloSource.cxx
+++ CLAM/plugins/osc/MultiLibloSource.cxx
@@ -29,19 +29,14 @@ static const char * metadata[] = {
 
 static CLAM::FactoryRegistrator<CLAM::ProcessingFactory, CLAM::MultiLibloSource> registrator(metadata);
 
-int CLAM::MultiLibloSource::controls_handler(const char *path, const char *types, lo_arg **argv, int argc, void *data, void *user_data)
+int CLAM::MultiLibloSource::controls_handler(
+	const char *path, const char *types, 
+	lo_arg **argv, int argc,
+	void *data, void *user_data)
 {
 	CLAM::MultiLibloSource & self = *((CLAM::MultiLibloSource*)user_data);
-	/* example showing pulling the argument values out of the argv array */
-//	printf("%s <- f:",path);
 	for (int i=0;i<argc;i++)
-	{
-		//TODO: replace by ASSERT?
-		if (self._outControls.size()>(unsigned)i)
-			CLAM::MultiLibloSource::sendControl(self._outControls[i],argv[i]);
-	}
-//	printf("\n\n");
-//	fflush(stdout);
+		self.sendControl(i, argv[i]);
 	return 0;
 }
 
