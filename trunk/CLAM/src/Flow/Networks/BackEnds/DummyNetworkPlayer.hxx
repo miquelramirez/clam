f412fdb502a95ac9b5d7a23db390766ec185e328
ascorza <ascorza@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2010-04-29 16:45:47 +0000

    * added DummyNetworkPlayer

---
+++ trunk/CLAM/src/Flow/Networks/BackEnds/DummyNetworkPlayer.hxx
@@ -0,0 +1,30 @@
+#ifndef DummyNetworkPlayer_hxx
+#define DummyNetworkPlayer_hxx
+
+#include "NetworkPlayer.hxx"
+#include "AudioSource.hxx"
+#include "AudioSink.hxx"
+
+namespace CLAM
+{
+
+	class DummyNetworkPlayer : public NetworkPlayer
+	{
+
+	public:
+		DummyNetworkPlayer()	{}
+		~DummyNetworkPlayer()	{}
+
+		virtual bool IsRealTime() const 	{ return false; }
+		virtual void Start();
+		virtual void Stop();
+
+
+		virtual bool IsWorking() 				{return true;}
+		virtual std::string NonWorkingReason()	{return "ejemplo";}
+
+	};
+
+}
+#endif
+
