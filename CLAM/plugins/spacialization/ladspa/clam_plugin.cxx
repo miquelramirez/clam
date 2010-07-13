b9adeaec5f4f6f82b7dbce696cebf6c9c53219d4
nolaiz <nolaiz@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2010-07-13 09:09:42 +0000

    * vbap16_simplified: added  * clam_plugin: added vbap16_simplified generation

--- CLAM/plugins/spacialization/ladspa/clam_plugin.cxx
+++ CLAM/plugins/spacialization/ladspa/clam_plugin.cxx
@@ -49,6 +49,7 @@ CLAM_EMBEDDED_FILE(vbap15_shorter_distance,"vbap15_shorter_distance.clamnetwork"
 CLAM_EMBEDDED_FILE(vbap22,"vbap22.clamnetwork")
 CLAM_EMBEDDED_FILE(vbap22_simplified,"vbap22_simplified.clamnetwork")
 CLAM_EMBEDDED_FILE(vbap22_pressure,"vbap22_pressure.clamnetwork")
+CLAM_EMBEDDED_FILE(vbap16_simplified,"vbap16_simplified.clamnetwork")
 
 CLAM_EMBEDDED_FILE(decoded_bformat15,"decoded_bformat15.clamnetwork")
 
@@ -152,6 +153,10 @@ extern "C" const LADSPA_Descriptor * ladspa_descriptor(unsigned long index)
 
 	static CLAM::LadspaNetworkExporter n8622(library, vbap22_pressure, 8622,
 			"id_vbap_22_pressure", "vbap22_pressure",
+			"BarcelonaMedia-Audio", "GNU GPL");
+
+	static CLAM::LadspaNetworkExporter n7616(library, vbap16_simplified, 7616,
+			"id_vbap_16_simplified", "vbap16_simplified",
 			"BarcelonaMedia-Audio", "GNU GPL");
 
 	static CLAM::LadspaNetworkExporter n2014(library, bformat14, 2014,
