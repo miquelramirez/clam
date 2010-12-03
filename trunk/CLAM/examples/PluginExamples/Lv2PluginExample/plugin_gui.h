a89f44d345462e438ab5607b6144f06c5c5fc96a
ascorza <ascorza@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2010-12-03 16:44:23 +0000

    Controls in plugingui are connect to doubleSpinBox in .ui

--- trunk/CLAM/examples/PluginExamples/Lv2PluginExample/plugin_gui.h
+++ trunk/CLAM/examples/PluginExamples/Lv2PluginExample/plugin_gui.h
@@ -6,6 +6,10 @@
 #include <gtk/gtk.h>
 #include <QtGui/QX11EmbedWidget>
 #include <QtUiTools/QUiLoader>
+#include <QtGui/QDoubleSpinBox>
+#include <iostream>
+
+class Lv2GuiContainer;
 
 class PluginGui : public QX11EmbedWidget
 {
@@ -16,12 +20,71 @@ public:
 	QWidget   *	qtWidget;
 	GtkWidget *	widgetSocket;
 
+	std::vector<Lv2GuiContainer*> containers;
+
 	LV2UI_Write_Function 	write_function;
 	LV2UI_Controller 	controller;
 	const char * 		bundlePath;
 	
 	PluginGui()
 	{}
+	
+	void setValueControlAtPlugin(unsigned, double);
+	void setValueControlAtWidget(unsigned, double);
+};
+
+/**************************************************/
+
+class Lv2GuiContainer : public QObject
+{
+	Q_OBJECT
+
+public:
+	Lv2GuiContainer(QObject * parent = 0)
+			: QObject(parent)
+	{}
+
+public slots:
+	virtual void setValueAtPlugin(double)=0;
+	virtual void setValueAtWidget(unsigned,double)=0;
+};
+
+/**************************************************/
+
+class Lv2DoubleSpinBox : public Lv2GuiContainer
+{
+	Q_OBJECT
+	
+	unsigned _id;
+	PluginGui * _pgui;
+	QDoubleSpinBox * _doubleSpinBox;
+
+public:
+	Lv2DoubleSpinBox( unsigned id
+			, QDoubleSpinBox * _doubleSpinBox
+			, PluginGui * pgui)
+			: Lv2GuiContainer(pgui)
+			, _id(id)
+			, _pgui(pgui)
+			, _doubleSpinBox(_doubleSpinBox) 
+	{
+		if(pgui==0)
+			std::cerr << "The PluginGui cann't be 0" << std::endl;		
+
+		connect(_doubleSpinBox, SIGNAL(valueChanged(double)),
+			this, SLOT(setValueAtPlugin(double)));
+	}
+
+public slots:
+	virtual void setValueAtPlugin(double value)
+	{
+		_pgui->setValueControlAtPlugin(_id, value);
+	}
+	virtual void setValueAtWidget(unsigned id, double value)
+	{
+		if(id==_id)
+			_doubleSpinBox->setValue(value);
+	}
 };
 
 #endif
