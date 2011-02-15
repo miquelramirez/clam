3b6047ef605ec9e01d2b3f1cc5548c79ab293610
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2011-02-15 21:57:47 +0000

    * PeerConnectors: removing from/import
    Original commit author: dgarcia.
    Original commit date: mar feb 15 17:03:52 CET 2011
    
    From: dgarcia <dgarcia>

--- trunk/ipyclam/PeerConnectors.py
+++ trunk/ipyclam/PeerConnectors.py
@@ -1,6 +1,6 @@
 import unittest
 import definition
-from Connector import Connector
+import Connector
 
 class PeerConnectors(object):
 	def __init__(self, hostName, connectorName, kind, direction, proxy, type) :
@@ -14,18 +14,19 @@ class PeerConnectors(object):
 			direction = Connector.Out
 		else:
 			direction = Connector.In
+
 		for connector in self._proxy.connectorPeers(self._hostName, self._connectorName, self._kind, self._direction):
 			index, type = self._proxy.connectorInfo(self._hostName, connector[1], self._kind, self._direction)
-			yield Connector(self._proxy, connector[0], name=connector[1], kind=self._kind, direction=direction, index=index, type=type)
+			yield Connector.Connector(self._proxy, connector[0], name=connector[1], kind=self._kind, direction=direction, index=index, type=type)
 
 class PeerConnectorsTests(unittest.TestCase):				
 	def test_iterable(self):
 		peers = PeerConnectors( "Processing2", "Inport2", Connector.Port, Connector.Out, definition.proxy(), 'type1' )
 		listConnectors = [ (connector.host.name, connector.name, connector.type, connector.index) for connector in peers ]
 		self.assertEqual([
-						('Processing1', 'Outport1', 'type1', 0),
-						('Processing1', 'Outport2', 'type1', 1)
-						], listConnectors)
+			('Processing1', 'Outport1', 'type1', 0),
+			('Processing1', 'Outport2', 'type1', 1)
+			], listConnectors)
 
 if __name__ == '__main__':
 	unittest.main()
