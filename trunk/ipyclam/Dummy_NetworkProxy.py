a14396f9fe23eaba39e748fe38d5bc25bec09e11
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2011-02-15 21:56:29 +0000

    * pyclam -> ipyclam
    Original commit author: dgarcia.
    Original commit date: mar ene 25 12:46:23 CET 2011
    
    From: dgarcia <dgarcia>

---
+++ trunk/ipyclam/Dummy_NetworkProxy.py
@@ -0,0 +1,123 @@
+import unittest
+
+class Dummy_NetworkProxy :
+	def __init__(self, definition) :
+		self._processings = dict()
+		for process in definition:
+			self._processings[process['name']] = process
+	def processingType(self, name) :
+		return self._processings[name]["type"]
+	def processingConfig(self, name) :
+		return self._processings[name]["config"]
+	def processingInports(self, name) :
+		return self._processings[name]["inports"]
+	def processingOutports(self, name) :
+		return self._processings[name]["outports"]
+	def processingIncontrols(self, name) :
+		return self._processings[name]["incontrols"]
+	def processingOutcontrols(self, name) :
+		return self._processings[name]["outcontrols"]
+
+class Dummy_NetworkProxyTest(unittest.TestCase) :
+	def definition(self) :
+		return [
+			dict(
+				type = "AudioSource",
+				name = "Processing1",
+				config = dict(
+					ConfigParam1 = "default1",
+					ConfigParam2 = "default2",
+				),
+				inports = [
+					["Inport1", "type1"],
+					["Inport2", "type1"]
+				],
+				outports = [
+					["Outport1", "type1"],
+					["Outport2", "type1"]
+				],
+				incontrols = [
+					["Incontrol1", "type2"],
+					["Incontrol2", "type2"]
+				],
+				outcontrols = [
+					["Outcontrol1", "type2"],
+					["Outcontrol2", "type2"]
+				],
+			),
+			dict(
+				type = "AudioSink",
+				name = "Processing2",
+				config = dict(
+					ConfigParam1 = "default1",
+					ConfigParam2 = "default2",
+				),
+				inports = [
+					["Inport1", "type1"],
+					["Inport2", "type1"]
+				],
+				outports = [
+					["Outport1", "type1"],
+					["Outport2", "type1"]
+				],
+				incontrols = [
+					["Incontrol1", "type2"],
+					["Incontrol2", "type2"]
+				],
+				outcontrols = [
+					["Outcontrol1", "type2"],
+					["Outcontrol2", "type2"]
+				],
+			),
+		]
+
+	def test_type(self) :
+		proxy = Dummy_NetworkProxy(self.definition())
+		self.assertEquals("AudioSource", proxy.processingType("Processing1"))
+
+	def test_config(self) :
+		proxy = Dummy_NetworkProxy(self.definition())
+		self.assertEquals(dict(
+				ConfigParam1 = "default1",
+				ConfigParam2 = "default2",
+			), proxy.processingConfig("Processing1"))
+
+	def test_inports(self) :
+		proxy = Dummy_NetworkProxy(self.definition())
+		self.assertEquals([
+				["Inport1", "type1"],
+				["Inport2", "type1"]
+			], proxy.processingInports("Processing1"))
+
+	def test_outports(self) :
+		proxy = Dummy_NetworkProxy(self.definition())
+		self.assertEquals([
+				["Outport1", "type1"],
+				["Outport2", "type1"]
+			], proxy.processingOutports("Processing1"))
+
+	def test_incontrols(self) :
+		proxy = Dummy_NetworkProxy(self.definition())
+		self.assertEquals([
+				["Incontrol1", "type2"],
+				["Incontrol2", "type2"]
+			], proxy.processingIncontrols("Processing1"))
+
+	def test_outcontrols(self) :
+		proxy = Dummy_NetworkProxy(self.definition())
+		self.assertEquals([
+				["Outcontrol1", "type2"],
+				["Outcontrol2", "type2"]
+			], proxy.processingOutcontrols("Processing1"))
+
+	def test_withBadProcessingName(self) :
+		proxy = Dummy_NetworkProxy(self.definition())
+		self.assertRaises(KeyError, proxy.processingType, "Processing3")
+
+	def test_typeSecondProcessing(self) :
+		proxy = Dummy_NetworkProxy(self.definition())
+		self.assertEquals("AudioSink", proxy.processingType("Processing2"))
+
+if __name__ == '__main__':
+	unittest.main()
+
