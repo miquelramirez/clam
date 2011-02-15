258fb7989fbf0713212d9edc0ceff7cb32f1c1f4
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2011-02-15 18:31:34 +0000

    * Processing: removed use of Dummy_NetworkProxy::processingPort/ControlIn/Out

--- trunk/ipyclam/Processing.py
+++ trunk/ipyclam/Processing.py
@@ -9,10 +9,10 @@ class Processing(object):
 		self.__dict__["name"] = name
 		self.__dict__["type"] = proxy.processingType(self.name)
 		self.__dict__["_config"] = Configuration(proxy.processingConfig(self.name))
-		self.__dict__["_inports"] = Connectors(Connector.Port, Connector.In, proxy.processingInports(self.name))
-		self.__dict__["_outports"] = Connectors(Connector.Port, Connector.Out, proxy.processingOutports(self.name))
-		self.__dict__["_incontrols"] = Connectors(Connector.Control, Connector.In, proxy.processingIncontrols(self.name))
-		self.__dict__["_outcontrols"] = Connectors(Connector.Control, Connector.Out, proxy.processingOutcontrols(self.name))
+		self.__dict__["_inports"] = Connectors(Connector.Port, Connector.In, proxy.processingConnectors(self.name, Connector.Port, Connector.In))
+		self.__dict__["_outports"] = Connectors(Connector.Port, Connector.Out, proxy.processingConnectors(self.name, Connector.Port, Connector.Out))
+		self.__dict__["_incontrols"] = Connectors(Connector.Control, Connector.In, proxy.processingConnectors(self.name, Connector.Control, Connector.In))
+		self.__dict__["_outcontrols"] = Connectors(Connector.Control, Connector.Out, proxy.processingConnectors(self.name,Connector.Control, Connector.Out))
 	def __getitem__(self, name):
 		if name in dir(self._config):
 			return self._config[name]
