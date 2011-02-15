b7fd9de75c172ae4a2ce28b4b49fe25b8a181bee
David García Garzón <vokimon@users.noreply.github.com>
2011-02-15 21:57:50 +0000

    * PeerConnectors: using tuple as iteration target to give semantics  * PeerConnectors: moved test imports below
    Original commit author: dgarcia.
    Original commit date: mar feb 15 17:09:10 CET 2011
    
    From: dgarcia <dgarcia>

--- ipyclam/PeerConnectors.py
+++ ipyclam/PeerConnectors.py
@@ -1,5 +1,3 @@
-import unittest
-import definition
 import Connector
 
 class PeerConnectors(object):
@@ -9,16 +7,20 @@ class PeerConnectors(object):
 		self._hostName = hostName
 		self._kind = kind
 		self._direction = direction
+
 	def __iter__(self):
 		if self._direction == Connector.In:
 			direction = Connector.Out
 		else:
 			direction = Connector.In
 
-		for connector in self._proxy.connectorPeers(self._hostName, self._connectorName, self._kind, self._direction):
-			index, type = self._proxy.connectorInfo(self._hostName, connector[1], self._kind, self._direction)
-			yield Connector.Connector(self._proxy, connector[0], name=connector[1], kind=self._kind, direction=direction, index=index, type=type)
+		for peerHost, peerConnector in self._proxy.connectorPeers(self._hostName, self._connectorName, self._kind, self._direction):
+			index, type = self._proxy.connectorInfo(self._hostName, peerConnector, self._kind, self._direction)
+			yield Connector.Connector(self._proxy, peerHost, peerConnector, self._kind, direction, index, type)
 
+
+import unittest
+import definition
 class PeerConnectorsTests(unittest.TestCase):				
 	def test_iterable(self):
 		peers = PeerConnectors( "Processing2", "Inport2", Connector.Port, Connector.Out, definition.proxy(), 'type1' )
