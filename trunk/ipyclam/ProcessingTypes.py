98ebfd50273371c5b4298b445a8d47bfb3defe91
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2011-02-28 21:18:51 +0000

    * ProcessingTypes: adding spaces between methods

--- trunk/ipyclam/ProcessingTypes.py
+++ trunk/ipyclam/ProcessingTypes.py
@@ -1,21 +1,27 @@
 class ProcessingTypes(object):
+
 	def __init__(self, proxy):
 		self._proxy = proxy
+
 	def __getattr__(self, name) :
 		types = self._proxy.availableTypes()
 		if name not in types:
 			raise AttributeError(name)
 		return types[types.index(name)]
+
 	def __dir__(self) :
 		return self._proxy.availableTypes()
+
 
 import unittest
 import TestFixtures
 
 class ProcessingTypesTests(unittest.TestCase):
+
 	def test_ProcessingGettingAsAttribute(self) :
 		type = ProcessingTypes(TestFixtures.proxy())
 		self.assertEqual("MinimalProcessing", type.MinimalProcessing)
+
 	def test_BadProcessingGettingAsAttributeAndFail(self) :
 		type = ProcessingTypes(TestFixtures.proxy())
 		try:
@@ -23,6 +29,7 @@ class ProcessingTypesTests(unittest.TestCase):
 			self.fail("Exception expected")
 		except AttributeError, e:
 			self.assertEqual(("BadProcessingType",), e.args)
+
 	def test_dir(self) :
 		type = ProcessingTypes(TestFixtures.proxy())
 		self.assertEqual(["ControlSink", "ControlSource", "MinimalProcessing", "OtherControlSink", "PortSink", "PortSource"], dir(type))
