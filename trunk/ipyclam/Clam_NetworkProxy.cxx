1a36f46ca8c400a81c9d0ca809b5eedc9dac925c
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2011-02-28 22:49:49 +0000

    * Clam_NetworkProxy: the skeleton for the clam based proxy  * ipyclam_console: turned into a class  * ipyclam_console: --dummy is an option by default use clam

---
+++ trunk/ipyclam/Clam_NetworkProxy.cxx
@@ -0,0 +1,62 @@
+#include <boost/python.hpp>
+#include <CLAM/Network.hxx>
+#include <CLAM/ProcessingFactory.hxx>
+#include <CLAM/XMLStorage.hxx>
+
+
+namespace py = boost::python;
+
+std::string Dump(CLAM::Network & net)
+{
+	std::ostringstream str;
+	CLAM::XMLStorage::Dump(net, "Network", str);
+	return str.str();
+}
+
+/// Helper to turn an stl list into a python list
+py::list pythonizeList(std::list<std::string> & list)
+{
+	py::list result; 
+	for (std::list<std::string>::iterator it=list.begin(); it!=list.end(); it++)
+		result.append(*it);
+	return result;
+}
+
+py::list processingTypes(CLAM::Network & network)
+{
+	CLAM::ProcessingFactory::Keys types;
+	CLAM::ProcessingFactory::GetInstance().GetRegisteredNames(types);
+	return pythonizeList(types);
+}
+
+py::list processingNames(CLAM::Network & network)
+{
+	std::list<std::string> names;
+	// TODO:
+	return pythonizeList(names);
+}
+
+BOOST_PYTHON_MODULE(Clam_NetworkProxy)
+{
+	// Keep 'using namespace' in the inner scope
+	using namespace boost::python;
+	using namespace CLAM;
+
+	typedef const std::string & cstringref;
+
+	class_<Network>("Clam_NetworkProxy")
+		.def("xml",
+			Dump,
+			"Returns the xml representation" )
+		.def("availableTypes",
+			processingTypes,
+			"Returns the names of the processing types than can be added to the network"
+			)
+		.def("processingNames",
+			processingNames,
+			"Returns the names of all the processing modules"
+			)
+		;
+}
+
+
