53711a91802fca95ad0a620f0ebee5a77e768867
David García Garzón <vokimon@users.noreply.github.com>
2011-02-28 22:49:49 +0000

    * Clam_NetworkProxy: the skeleton for the clam based proxy  * ipyclam_console: turned into a class  * ipyclam_console: --dummy is an option by default use clam

--- ipyclam/ipyclam_console.cxx
+++ ipyclam/ipyclam_console.cxx
@@ -4,42 +4,76 @@
 #include <iostream>
 #include <string>
 
-int main( int argc, char ** argv )
+
+namespace py = boost::python;
+
+class IpyclamShell
 {
-	try
+	py::object _main;
+	py::object _main_ns;
+public:
+	IpyclamShell()
+		: _main(py::import("__main__"))
+		, _main_ns(_main.attr("__dict__"))
 	{
-		Py_Initialize();
-		using namespace boost::python;
+		exec("import sys; sys.path.append('.')" , _main_ns, _main_ns);
+	}
 
-		object main = import("__main__");
-		object main_ns = main.attr("__dict__");
-		exec("import sys; sys.path.append('.')" , main_ns, main_ns);
-		object networkClass = import("Network").attr("Network");
+	void setupClamNetwork(CLAM::Network & network)
+	{
+		py::object clamProxyModule = py::import("Clam_NetworkProxy");
+		py::object proxy = py::object(py::ptr(&network));
+		setupNetwork(proxy);
+	}
 
-		CLAM::Network network;
-//		object proxy = ptr(&network);
-		object proxy = import("Dummy_NetworkProxy").attr("Dummy_NetworkProxy")();
-
-		main_ns["net"] = networkClass(proxy);
-
-
+	void setupDummyNetwork()
+	{
+		py::object proxy = py::import("Dummy_NetworkProxy").attr("Dummy_NetworkProxy")();
+		setupNetwork(proxy);
+	}
+private:
+	void setupNetwork(py::object & proxy)
+	{
+		py::object networkClass = py::import("Network").attr("Network");
+		_main_ns["net"] = networkClass(proxy);
+	}
+public:
+	void run()
+	{
 		std::string shellBanner = "Interactive python console for CLAM "+std::string(CLAM::GetFullVersion());
 		std::string shellHeader = "Start by typing 'net.' and pressing the tab key";
 		std::string shellFarewell = "Bye";
 
 		// This needs to be defined for ipython
-		exec("sys.argv=[]\n", main_ns, main_ns);
+		py::exec("sys.argv=[]\n", _main_ns, _main_ns);
 
-		object shellmodule = import("IPython.Shell");
-		list shellOptions;
+		py::object shellmodule = py::import("IPython.Shell");
+		py::list shellOptions;
 		shellOptions.append("-prompt_in1");
 		shellOptions.append("ipyclam > ");
-		object shell = shellmodule.attr("IPShellEmbed")
+		py::object shell = shellmodule.attr("IPShellEmbed")
 			(shellOptions, shellBanner, shellFarewell);
-		shell(shellHeader, main_ns, main_ns);
-
+		shell(shellHeader, _main_ns, _main_ns);
 	}
-	catch (boost::python::error_already_set & e)
+};
+
+
+int main( int argc, char ** argv )
+{
+	try
+	{
+		Py_Initialize();
+
+		CLAM::Network network;
+		IpyclamShell shell;
+		if (argc>1 and std::string(argv[1])=="--dummy")
+			shell.setupDummyNetwork();
+		else
+			shell.setupClamNetwork(network);
+
+		shell.run();
+	}
+	catch (py::error_already_set & e)
 	{
 		std::cerr << "Run time Python error!" << std::endl;
 		PyErr_Print();
