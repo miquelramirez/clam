4c71dc6f173dbdab0df376228772887e5031f23d
xserra <xserra@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2011-04-26 17:58:57 +0000

    * ipyclam/Clam_NetworkProxy: deleted untested code * ipyclam/Clam_NetworkProxyTests: test for addind a processing

--- trunk/ipyclam/Clam_NetworkProxy.cxx
+++ trunk/ipyclam/Clam_NetworkProxy.cxx
@@ -29,87 +29,9 @@ py::list processingTypes(CLAM::Network & network)
 	return pythonizeList(types);
 }
 
-py::list processingNames(CLAM::Network & network)
-{
-	std::list<std::string> names;
-	CLAM::Network::ProcessingsMap::iterator it;
-	for (it=network.BeginProcessings(); it!=network.EndProcessings(); it++)
-	{
-		names.push_back(it->first);
-	}
-	return pythonizeList(names);
-}
-
 void addProcessing(CLAM::Network & network, char * type, char * processingName)
 {
 	network.AddProcessing(processingName, type);
-}
-
-bool hasProcessing(CLAM::Network & network, char * processingName)
-{
-	return network.HasProcessing(processingName);
-}
-
-std::string processingType(CLAM::Network & network, char * processingName)
-{
-	return network.GetProcessing(processingName).GetClassName();
-}
-//TODO: Implement correctly
-py::dict processingConfig(CLAM::Network & network, char * processingName)
-{
-	py::dict processingConfig;
-	return processingConfig;
-}
-//TODO: Implement correctly
-py::list processingConnectors(CLAM::Network & network, char * processingName, char * kind, char * direction)
-{
-	py::list connectors;
-	if (strcmp(kind, "Port") == 0)
-	{
-		if (strcmp(direction, "In") == 0)
-			connectors.append("InPort1");
-		else
-			connectors.append("OutPort1");
-	}
-	else
-	{
-		if (strcmp(direction, "In") == 0)
-			connectors.append("InControl1");
-		else
-			connectors.append("OutControl1");
-	}
-	//network.GetProcessing(processingName).GetInControls();
-	return connectors;
-}
-
-bool processingHasConnector(CLAM::Network & network, char * processingName, char * kind, char * direction, char * connectorName)
-{
-	if (strcmp(kind, "Port") == 0)
-	{
-		if (strcmp(direction, "In") == 0)
-			return network.GetProcessing(processingName).HasInPort(connectorName);
-		else
-			return network.GetProcessing(processingName).HasOutPort(connectorName);
-	}
-	else
-	{
-		if (strcmp(direction, "In") == 0)
-			return network.GetProcessing(processingName).HasInControl(connectorName);
-		else
-			return network.GetProcessing(processingName).HasOutControl(connectorName);
-	}
-}
-//TODO: Implement correctly
-py::list portConnections(CLAM::Network & network)
-{
-	py::list portConnections;
-	return portConnections;
-}
-//TODO: Implement correctly
-py::list controlConnections(CLAM::Network & network)
-{
-	py::list controlConnections;
-	return controlConnections;
 }
 
 BOOST_PYTHON_MODULE(Clam_NetworkProxy)
@@ -130,41 +52,9 @@ BOOST_PYTHON_MODULE(Clam_NetworkProxy)
 			processingTypes,
 			"Returns the names of the processing types than can be added to the network"
 			)
-		.def("processingNames",
-			processingNames,
-			"Returns the names of all the processing modules"
-			)
 		.def("addProcessing",
 			addProcessing,
 			"Adds a Processing with the name and type specified to the network"
-			)
-		.def("hasProcessing",
-			hasProcessing,
-			"Tells whether or not there is a processing with the given name on the network"
-			)
-		.def("processingType",
-			processingType,
-			"Returns the type of the processing"
-			)
-		.def("processingConfig",
-			processingConfig, //TODO: Implement correctly
-			"Returns the config dictionary of the processing"
-			)
-		.def("processingConnectors", //TODO: Implement correctly
-			processingConnectors,
-			""
-			)
-		.def("processingHasConnector",
-			processingHasConnector,
-			"Returns true if the processing has the specified connector"
-			)
-		.def("portConnections", //TODO: Implement correctly
-			portConnections,
-			""
-			)
-		.def("controlConnections", //TODO: Implement correctly
-			controlConnections,
-			""
 			)
 		;
 }
--- trunk/ipyclam/Clam_NetworkProxyTests.py
+++ trunk/ipyclam/Clam_NetworkProxyTests.py
@@ -7,6 +7,19 @@ class Clam_NetworkProxyTests(unittest.TestCase):
 	def test_availableTypes(self):
 		proxy = Clam_NetworkProxy.Clam_NetworkProxy()
 		self.assertTrue('DummyProcessing' in proxy.availableTypes())
+	def test_addProcessing(self):
+		import Network
+		proxy = Clam_NetworkProxy.Clam_NetworkProxy()
+		net = Network.Network(proxy)
+		net.Processing1 = "DummyProcessing"
+		self.assertEqual(
+			"<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\" ?>\n"
+			"<Network clamVersion=\"1.4.1\" id=\"Unnamed Network\">\n"
+			"\n"
+			"  <processing id=\"Processing1\" type=\"DummyProcessing\"/>\n"
+			"\n"
+			"</Network>\n"
+			, proxy.xml())
 
 if __name__ == '__main__':
 	unittest.main()
