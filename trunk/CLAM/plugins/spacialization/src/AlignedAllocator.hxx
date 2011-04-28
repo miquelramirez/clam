0ae8d5aed98547a8f8c69fd2b9e5e18a5266494d
eaylon <eaylon@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2011-04-28 15:19:51 +0000

    * spacialization: removed AlignedAllocator from spacialization, so it uses directly the one in spectral lib. This is due to not being able to typedef a template

--- trunk/CLAM/plugins/spacialization/src/AlignedAllocator.hxx
+++
@@ -1,8 +0,0 @@
-#ifndef AlignedAllocator_hxx
-#define AlignedAllocator_hxx
-
-#include <spectral/AlignedAllocator.hxx>
-
-typedef ::AlignedAllocator AlignedAllocator;
-
-#endif//AlignedAllocator_hxx
