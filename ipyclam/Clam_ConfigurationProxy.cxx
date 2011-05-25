e2c0eead014d934019696f7b95a774058dd473fe
xserra <xserra@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2011-05-25 19:33:35 +0000

    * ipyclam: get attributes from configproxy but only if strings

--- ipyclam/Clam_ConfigurationProxy.cxx
+++ ipyclam/Clam_ConfigurationProxy.cxx
@@ -7,6 +7,16 @@
 
 namespace py = boost::python;
 
+int getAttributeIndex(ConfigurationProxy & config, const std::string & attribute)
+{
+	for(unsigned int i = 0; i < config._processingConfig->GetNDynamicAttributes(); ++i)
+	{
+		if ( attribute == config._processingConfig->GetDynamicAttributeName(i) )
+			return (int) i;
+	}
+	return -1;
+}
+
 ConfigurationProxy * createConfigurationProxy(const std::string & type)
 {
 	CLAM::Processing * proc = CLAM::ProcessingFactory::GetInstance().CreateSafe( type );
@@ -24,6 +34,17 @@ std::string Dump(ConfigurationProxy & config)
 	return str.str();
 }
 
+std::string getAttribute(ConfigurationProxy & config, const std::string & attribute)
+{
+	int index = getAttributeIndex(config, attribute);
+	if (index == -1)
+	{
+		PyErr_SetString(PyExc_AttributeError, attribute.c_str() );
+		py::throw_error_already_set();
+	}
+	return *(std::string *) config._processingConfig->GetAttributeAsVoidPtr(index);
+}
+
 BOOST_PYTHON_MODULE(Clam_ConfigurationProxy)
 {
 	using namespace boost::python;
@@ -37,6 +58,10 @@ BOOST_PYTHON_MODULE(Clam_ConfigurationProxy)
 		.def("xml",
 			Dump,
 			"Returns the xml representation"
+			)
+		.def("__getitem__",
+			getAttribute,
+			""
 			)
 		;
 }
