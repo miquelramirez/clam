b65e51dea4727cc04d39883718369a8eef5c2809
xserra <xserra@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2011-06-16 18:01:35 +0000

    * ipyclam: adapted PeerConnectors tests for CLAM and dummy data

--- trunk/ipyclam/PeerConnectors.py
+++ trunk/ipyclam/PeerConnectors.py
@@ -37,18 +37,32 @@ class PeerConnectors(object):
 
 import unittest
 import TestFixtures
-class PeerConnectorsTests(unittest.TestCase):				
-	def _test_iterable(self):
+class PeerConnectorsTests(unittest.TestCase):
+	def test_iterable(self):
 		peers = PeerConnectors(TestFixtures.proxy(), "Processing2", Connector.Port, Connector.In, "Inport2")
 		listConnectors = [ (connector.host.name, connector.name, connector.type, connector.index) for connector in peers ]
 		self.assertEqual([
-			('Processing1', 'OutPort1', 'f', 0),
-			('Processing1', 'OutPort2', 'f', 1)
+			('Processing1', 'OutPort1', 'f', 0)
 			], listConnectors)
 
-	def _test_len(self):
+	def test_len(self):
 		peers = PeerConnectors(TestFixtures.proxy(), "Processing2", Connector.Port, Connector.In, "Inport2")
-		self.assertEqual(2, peers.__len__() )
+		self.assertEqual(1, peers.__len__() )
+
+class Clam_PeerConnectorsTests(PeerConnectorsTests):
+	def proxy(self):
+		import Clam_NetworkProxy
+		import Network
+		proxy = Clam_NetworkProxy.Clam_NetworkProxy()
+		network = Network.Network(proxy)
+		network.Processing1 = "DummyTypeProcessing1"
+		network.Processing2 = "DummyTypeProcessing2"
+		network.Processing1.OutPort1 > network.Processing2.Inport2
+		network.Processing2.Outport2 > network.Processing1.InPort2
+		network.Processing1.OutControl1 > network.Processing2.Incontrol2
+		network.Processing1.OutControl2 > network.Processing2.Incontrol2
+		network.Processing2.Outcontrol1 > network.Processing1.InControl2
+		return proxy
 
 if __name__ == '__main__':
 	unittest.main()
