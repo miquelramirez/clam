3a63a0319f4bc6d40a5805512f864f7e10743441
David García Garzón <vokimon@users.noreply.github.com>
2011-06-26 16:50:45 +0000

    * Voice2MIDI: no need to initializa CLAM modules

--- Voice2MIDI/src/main.cxx
+++ Voice2MIDI/src/main.cxx
@@ -7,23 +7,11 @@
 #include <qapplication.h>
 #include "V2MGUI.hxx"
 
-#ifdef _MSVC_VER
-	#include <CLAM/InitAudioIO.hxx>
-	#include <CLAM/InitProcessing.hxx>
-#endif
-
 int main(int argc,char** argv)
 {
-
-#ifdef _MSVC_VER
-	CLAM::ProcessingModule::init();
-	CLAM::AudioIOModule::init();
-#endif
-
 	QApplication app(argc,argv);
 	V2MGUI* mw = new V2MGUI();
 	mw->show();
-	app.connect(&app,SIGNAL(lastWindowClosed()),&app,SLOT(quit()));
 	return app.exec();
 }
 
