c3a8ccb18550bf4f82e4cdd44ff600e27088b55c
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2011-08-03 23:24:58 +0000

    * new QtBinder and MonitorQtBinder to substitue Binder and MonitorBinder (old binder base clas still there while transition)  * MonitorQtBinders allows binding by adding custom qt property 'clamOutPort'  * Extracted QtBinder classes into their own headers  * Splitted the bindWidget into smaller methods so it can be reused outside prototyper  * Lot's of doxygenation and helper functions

---
+++ trunk/NetworkEditor/src/prototyper/QtBinder.cxx
@@ -0,0 +1,123 @@
+#include "QtBinder.hxx"
+#include <CLAM/Network.hxx>
+
+
+CLAM::QtBinder::QtBinder()
+{
+	// auto registering
+	binders().push_back(this);
+}
+
+CLAM::QtBinder::~QtBinder()
+{
+	// TODO: maybe unregister
+}
+
+CLAM::QtBinder::Binders & CLAM::QtBinder::binders()
+{
+	static Binders binders;
+	return binders;
+}
+
+void CLAM::QtBinder::bindRecursively( QObject * userInterface, CLAM::Network & network, QStringList & errors)
+{
+	if (handles(userInterface))
+		bind(userInterface, network, errors);
+	foreach (QObject * child, userInterface->children())
+	{
+		bindRecursively(child, network, errors);
+	}
+}
+
+void CLAM::QtBinder::bindAllBinders(QObject * userInterface, CLAM::Network & network, QStringList & errors)
+{
+	for (Binders::iterator it=binders().begin(); it!=binders().end(); it++)
+		(*it)->bindRecursively(userInterface, network, errors);
+}
+
+CLAM::QtBinder * CLAM::QtBinder::findHandler(QObject * uiElement)
+{
+	for (Binders::iterator it=binders().begin(); it!=binders().end(); it++)
+		if ((*it)->handles(uiElement)) return *it;
+	return 0;
+}
+
+bool CLAM::QtBinder::error(QStringList & errors, const QString & message)
+{
+	errors.append(message);
+	return false;
+}
+
+bool CLAM::QtBinder::missingProcessing(const std::string & processingName, Network & network, QStringList & errors)
+{
+	if (network.HasProcessing(processingName))
+		return false;
+	error(errors, 
+		QString("The interface asked to connect to the "
+			"processing '%1' which is not in the network.")
+			.arg(processingName.c_str()));
+	return true;
+}
+
+bool CLAM::QtBinder::missingOutPort(const std::string & portName, Network & network, QStringList & errors)
+{
+	std::string processingName = network.GetProcessingIdentifier(portName);
+	if (missingProcessing(processingName,network,errors)) return true;
+	std::string shortPortName = network.GetConnectorIdentifier(portName);
+	if (network.GetProcessing(processingName).HasOutPort(shortPortName))
+		return false; // no problem :-)
+	error(errors, 
+		QString("The interface asked to connect to a port '%1' "
+			"not available in the processing '%2'.")
+			// TODO: Try with...
+			.arg(shortPortName.c_str())
+			.arg(processingName.c_str())
+			);
+	return true;
+}
+
+bool CLAM::QtBinder::missingInControl(const std::string & controlName, Network & network, QStringList & errors)
+{
+	std::string processingName = network.GetProcessingIdentifier(controlName);
+	if (missingProcessing(processingName,network,errors)) return true;
+	std::string shortControlName = network.GetConnectorIdentifier(controlName);
+	if (network.GetProcessing(processingName).HasInControl(shortControlName))
+		return false; // no problem :-)
+	error(errors, 
+		QString("The interface asked to connect to a control '%1' "
+			"not available in the processing '%2'.")
+			// TODO: Try with...
+			.arg(shortControlName.c_str())
+			.arg(processingName.c_str()
+			));
+	return true;
+}
+
+#include "ControlSurfaceWidget.hxx"
+
+namespace CLAM { class ControlSurfaceBinder; }
+
+class CLAM::ControlSurfaceBinder : public QtBinder
+{
+public:
+	bool handles(QObject * qobject)
+	{
+		return isSubClassOf<ControlSurfaceWidget>(qobject);
+	}
+	
+	bool bind(QObject * uiElement, CLAM::Network & network, QStringList & errors)
+	{
+		QString xcontrol = uiElement->property("clam_xcontrol").toString();
+		QString ycontrol = uiElement->property("clam_ycontrol").toString();
+		if (xcontrol.isEmpty()) return error(errors,
+			QString("clam_xcontrol property required to bind a ControlSurfaceWidget"));
+		if (ycontrol.isEmpty()) return error(errors,
+			QString("clam_ycontrol property required to bind a ControlSurfaceWidget"));
+
+		if (missingInControl(xcontrol.toStdString(), network, errors)) return false;
+		if (missingInControl(ycontrol.toStdString(), network, errors)) return false;
+	}
+};
+
+
+
---
+++ trunk/NetworkEditor/src/prototyper/QtBinder.hxx
@@ -0,0 +1,137 @@
+#ifndef CLAM_QtBinder_hxx
+#define CLAM_QtBinder_hxx
+
+#include <QtGui/QWidget>
+#include <QtCore/QVariant>
+
+namespace CLAM { class Network; }
+namespace CLAM { class QtBinder; }
+
+/**
+	A QtBinder relates widgets of a Qt UI to a CLAM network.
+	Subclasses of this class binds
+	Usually a binder does the following:
+	- Evaluates if it can handle a given widget
+	- Does the actual binding
+
+	Doing the binding normally requires:
+	- Taking extra information for the binding from
+	  the object name, its class or its qt properties.
+	- Creating the structures to make the binding such as
+	  QObjects that bridge signals and slots or 
+	  CLAM processings to safely obtain or send processing
+	  and control data from the processing thread.
+	- Relates them all.
+
+	Instances of QtBinder subclasses register on a list
+	that is processed each time a binding is done.
+	You can use convenience static methods to apply them all
+	to a given ui recursively.
+
+	Notice that the binder may bind many widgets, or reused
+	for several networks/ui's so subclasses should not keep 
+	the state of a single binding.
+	Instead consider transfer ownership to either, the interface
+	using the QObject ownership hierarchy or to the network,
+	in the case of creating processings.
+	If both worlds reference themselves, ensure that when one
+	is destroyed the other don't keep dangling references.
+
+	This class also provides helper methods to do common tasks
+	in the sublcasses.
+
+	@ingroup UiBinders
+*/
+class CLAM::QtBinder
+{
+public:
+	typedef std::list<QtBinder*> Binders;
+	static Binders & binders();
+	QtBinder();
+	virtual ~QtBinder();
+
+	/**
+		Binds any handled user interface element to the network.
+		@see bind, handle
+	*/
+	void bindRecursively(QObject * userInterface, CLAM::Network & network, QStringList & errors);
+
+	/**
+		Applies bindRecursively to all the registered binders.
+	*/
+	static void bindAllBinders(QObject * userInterface, CLAM::Network & network, QStringList & errors);
+
+	/**
+		Returns a binder that handles the concrete uiElement.
+	*/
+	QtBinder * findHandler(QObject * uiElement);
+
+	/**
+		Returns true if the uiElement is handled by the binder.
+		For example when it is of a given type or the name or any property match a given criterion.
+	*/
+	virtual bool handles(QObject * uiElement) = 0;
+
+	/**
+		Do the actual binding between the ui element and the network
+		Returns false if the bind could not be made.
+		If so, any error message is appended to errors.
+	*/
+	virtual bool bind(QObject * uiElement, Network & network, QStringList & errors) = 0;
+
+// Helpers for subclass implementation of handles
+
+	template <typename Type>
+	static bool isSubClassOf(QObject * object)
+	{
+		return qobject_cast<Type*>(object) != 0;
+	}
+	static bool hasProperty(QObject * object, const char * propertyName)
+	{
+		return object->property(propertyName).isValid();
+	}
+	template <typename ValueType>
+	static bool propertyHasValue(QObject * object, const char * propertyName, const ValueType & value)
+	{
+		const QVariant & propertyValue = object->property(propertyName);
+		if (not propertyValue.isValid()) return false;
+		if (not propertyValue.canConvert<ValueType>()) return false;
+		// TODO documentation says that convert is safer than canConvert
+		// but how do i get the QVariant::Type of ValueType
+//		if (not propertyValue.convert(QVariant::<ValueType>())) return false;
+
+		return propertyValue.value<ValueType>() == value;
+	}
+
+// Helpers for subclass implementation of bind
+	/**
+		Enables the following idiom:
+		@code
+		if (error_condition)
+			return error(errors, "Message");
+		@endcode
+	*/
+	static bool error(QStringList & errors, const QString & message);
+
+	/**
+		Adds an error to errors and returns true if 
+		the name processing can not be found on the network.
+	*/
+	static bool missingProcessing(const std::string & processingName, Network & network, QStringList & errors);
+
+	/**
+		Adds an error to errors and returns true if 
+		the out port or its containing processing can not be found on the network
+		The port is refered as a 'processing.outport' like strings.
+	*/
+	static bool missingOutPort(const std::string & portName, Network & network, QStringList & errors);
+	/**
+		Adds an error to errors and returns true if 
+		the in control or its containing processing can not be found on the network.
+		The control is refered as a 'processing.incontrol' like string.
+	*/
+	static bool missingInControl(const std::string & controlName, Network & network, QStringList & errors);
+};
+
+#endif//CLAM_QtBinder_hxx
+
