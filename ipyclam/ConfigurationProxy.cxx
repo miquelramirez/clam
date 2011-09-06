165122c1029e51e1d8bce676757b075fdfc1620f
David García Garzón <vokimon@users.noreply.github.com>
2011-09-06 19:09:58 +0000

    * ConfigurationProxy: support for AudioIn/OutFileName  * ConfigurationProxy: plugins can use a bridge type to python (ie. std::string for AudioInFileName)

--- ipyclam/ConfigurationProxy.cxx
+++ ipyclam/ConfigurationProxy.cxx
@@ -14,7 +14,7 @@ std::string pythonType(py::object value)
 	return py::extract<std::string>(value.attr("__class__").attr("__name__"));
 }
 
-template <typename Type>
+template <typename Type, typename BridgeType=Type>
 class BasicConfigurationPlugin : public ConfigurationProxyPlugin
 {
 	std::string _typeName;
@@ -31,14 +31,14 @@ public:
 	{
 		CLAM_ASSERT(accepts(config,index),
 			"Configuration plugins should be accepted by the parameter before using them");
-		Type value = config.attributeValue<Type>(index);
+		BridgeType value = config.attributeValue<Type>(index);
 		return boost::python::object(value);
 	}
 	virtual void setAttribute(ConfigurationProxy & config, unsigned index, boost::python::object value)
 	{
 		CLAM_ASSERT(accepts(config,index),
 			"Configuration plugins should be accepted by the parameter before using them");
-		py::extract<Type> extractor(value);
+		py::extract<BridgeType> extractor(value);
 		if (not extractor.check())
 			throwPythonError(PyExc_TypeError, std::string() +
 				"While setting parameter '" + config.attributeName(index) +
@@ -46,14 +46,17 @@ public:
 				"' into a C++ value of type '" + _typeName +
 				"'.");
 
-		config.setAttributeValue<Type>(index, extractor() );
+		config.setAttributeValue<Type>(index, Type(extractor()) );
 	}
 	virtual bool nonDefault(ConfigurationProxy & config, unsigned index)
 	{
 		return config.nonDefault<Type>(index);;
 	}
 };
-
+#include <CLAM/AudioInFilename.hxx>
+#include <CLAM/AudioOutFilename.hxx>
+static BasicConfigurationPlugin<CLAM::AudioOutFilename, std::string> audioFileOutRegistrator("AudioOutFilename");
+static BasicConfigurationPlugin<CLAM::AudioInFilename, std::string> audioFileInRegistrator("AudioInFilename");
 static BasicConfigurationPlugin<std::string> stringRegistrator("string");
 static BasicConfigurationPlugin<int> intRegistrator("int");
 static BasicConfigurationPlugin<bool> boolRegistrator("bool");
