6a45132352cb8f57095d30672fb13338f1107095
David García Garzón <vokimon@users.noreply.github.com>
2013-02-08 15:55:28 +0000

    * FileCleaner for helping test cleanup

---
+++ CLAM/test/UnitTests/CommonHelpers/FileCleaner.hxx
@@ -0,0 +1,49 @@
+/*
+* Copyright (c) 2013 David Garcia Garzón
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*
+*/
+
+#ifndef ClamTest_FileCleaner_hxx
+#define ClamTest_FileCleaner_hxx
+
+
+#include <string>
+#include <list>
+#include <cstdio>
+
+namespace CLAMTest { class FileCleaner; }
+
+class CLAMTest::FileCleaner
+{
+	typedef std::list<std::string> FileNames;
+	FileNames _toRemove;
+public:
+	void add(const std::string & filename)
+	{
+		_toRemove.push_back(filename);
+	}
+	~FileCleaner()
+	{
+		for (FileNames::iterator it = _toRemove.begin();
+				it != _toRemove.end(); it++)
+			std::remove(it->c_str());
+	}
+};
+
+
+#endif//ClamTest_FileCleaner_hxx
+
---
+++ CLAM/test/UnitTests/CommonHelpers/FileCleanerTest.cxx
@@ -0,0 +1,138 @@
+/*
+* Copyright (c) 2001-2002 MUSIC TECHNOLOGY GROUP (MTG)
+*                         UNIVERSITAT POMPEU FABRA
+*
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*
+*/
+
+#include "FileCleaner.hxx"
+#include <cppunit/extensions/HelperMacros.h>
+#include <fstream>
+
+namespace CLAMTest{
+
+class FileCleanerTest;
+CPPUNIT_TEST_SUITE_REGISTRATION( FileCleanerTest );
+
+class FileCleanerTest : public CppUnit::TestFixture
+{
+
+	CPPUNIT_TEST_SUITE( FileCleanerTest );
+
+	CPPUNIT_TEST( test_exists_whenExists );
+	CPPUNIT_TEST( test_destructor_whenEmpty );
+	CPPUNIT_TEST( test_destructor_whenAddedOne );
+	CPPUNIT_TEST( test_destructor_whenAddedMany );
+	CPPUNIT_TEST( test_destructor_whenNotCreated );
+
+	CPPUNIT_TEST_SUITE_END();
+
+private:
+	bool exists(const std::string & filename)
+	{
+		return std::ifstream(filename.c_str());
+	}
+	std::string tempfilename()
+	{
+		return std::string(std::tmpnam(0));
+	}
+	void create(const std::string & filename)
+	{
+		std::ofstream(filename.c_str());
+	}
+	void remove(const std::string & filename)
+	{
+		std::remove(filename.c_str());
+	}
+
+public:
+	void setUp()
+	{
+	}
+
+public:
+	FileCleanerTest()
+	{
+	}
+
+private:
+	void test_exists_whenExists()
+	{
+		std::string filename = tempfilename();
+		CPPUNIT_ASSERT(not exists(filename));
+
+		create(filename);
+		CPPUNIT_ASSERT(exists(filename));
+
+		remove(filename);
+		CPPUNIT_ASSERT(not exists(filename));
+	}
+
+	void test_destructor_whenEmpty()
+	{
+		std::string filename = tempfilename();
+		{
+			FileCleaner cleaner;
+			create(filename);
+		}
+		CPPUNIT_ASSERT(exists(filename));
+		remove(filename);
+	}
+
+	void test_destructor_whenAddedOne()
+	{
+		std::string filename = tempfilename();
+		{
+			FileCleaner cleaner;
+			create(filename);
+			cleaner.add(filename);
+		}
+		CPPUNIT_ASSERT(not exists(filename));
+	}
+
+	void test_destructor_whenAddedMany()
+	{
+		std::string filename1 = tempfilename();
+		std::string filename2 = tempfilename();
+		{
+			FileCleaner cleaner;
+			create(filename1);
+			cleaner.add(filename1);
+			create(filename2);
+			cleaner.add(filename2);
+		}
+		CPPUNIT_ASSERT(not exists(filename1));
+		CPPUNIT_ASSERT(not exists(filename2));
+	}
+
+	void test_destructor_whenNotCreated()
+	{
+		std::string filename1 = tempfilename();
+		std::string filename2 = tempfilename();
+		{
+			FileCleaner cleaner;
+			cleaner.add(filename1);
+			cleaner.add(filename2);
+		}
+		CPPUNIT_ASSERT(not exists(filename1));
+		CPPUNIT_ASSERT(not exists(filename2));
+	}
+
+};
+
+}
+
