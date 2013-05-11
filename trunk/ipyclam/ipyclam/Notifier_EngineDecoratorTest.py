1dde1d30e6862962bc95c60d20cf569125e8bc1b
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2013-05-11 13:58:55 +0000

    Notifier_EngineDecorator

---
+++ trunk/ipyclam/ipyclam/Notifier_EngineDecoratorTest.py
@@ -0,0 +1,39 @@
+from Notifier_EngineDecorator import Notifier, Notifier_EngineDecorator, notified
+import unittest
+
+
+class NotifierTest(unittest.TestCase) :
+
+	class Adaptee(object) :
+		def __init__(self) : pass
+		def setter(self, value) : return "Setting " + value
+		def getter(self) : return "Getting"
+
+	class Notifier(Notifier) :
+		def __init__(self, adaptee) :
+			super(NotifierTest.Notifier, self).__init__(adaptee)
+			self._notified = False
+
+		@notified
+		def setter(self, value) :
+			return ""
+
+		def notify(self) :
+			self._notified = True
+
+	def testGetter(self) :
+		o  = self.Notifier(self.Adaptee())
+		result = o.getter()
+		self.assertEqual(result, "Getting")
+		self.assertFalse(o._notified)
+
+	def testSetter(self) :
+		o  = self.Notifier(self.Adaptee())
+		result = o.setter("param")
+		self.assertEqual(result, "Setting param notifier")
+		self.assertTrue(o._notified)
+
+
+if __name__ == "__main__" :
+	unittest.main()
+
