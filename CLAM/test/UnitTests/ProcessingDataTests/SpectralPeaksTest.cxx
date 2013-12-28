361bad4d79318fa7d94e76b0d9b4b37c5b421358
David García Garzón <vokimon@users.noreply.github.com>
2013-12-28 23:31:22 +0000

    * SpectralPeakArray::operator+: Fixed phase rubbish and the drop of the last peak

--- CLAM/test/UnitTests/ProcessingDataTests/SpectralPeaksTest.cxx
+++ CLAM/test/UnitTests/ProcessingDataTests/SpectralPeaksTest.cxx
@@ -13,6 +13,13 @@ class CLAMTest::SpectralPeaksTest : public CppUnit::TestFixture
 	CPPUNIT_TEST_SUITE( SpectralPeaksTest );
 
 	CPPUNIT_TEST(test_asString_withEmptyArray);
+	CPPUNIT_TEST(test_asString_withSingleElementArray);
+	CPPUNIT_TEST(test_asString_withManyElementArray);
+	CPPUNIT_TEST(test_asString_withEmptyPeaks);
+	CPPUNIT_TEST(test_asString_withPeaks);
+	CPPUNIT_TEST(test_peakAddition_whenDisjoint);
+	CPPUNIT_TEST(test_peakAddition_whenInterleaved);
+	CPPUNIT_TEST(test_peakAddition_whenCoincident);
 
 	CPPUNIT_TEST_SUITE_END();
 
@@ -30,16 +37,47 @@ private:
 	{
 		std::ostringstream os;
 		os << "[";
-		os << "]";
+		for (unsigned i=0; i<array.Size(); i++)
+			os << " " << array[i];
+		os << " ]";
 		return os.str();
 	}
 
 	std::string asString(const SpectralPeaks & peaks)
 	{
 		std::ostringstream os;
-		os << "[";
-		os << "]";
+		if (peaks.HasIndexArray())
+			os << "Indices: " << asString(peaks.GetIndexArray()) << std::endl;
+		if (peaks.HasFreqBuffer())
+			os << "Frequencies: " << asString(peaks.GetFreqBuffer()) << std::endl;
+		if (peaks.HasMagBuffer())
+			os << "Magnitudes: " << asString(peaks.GetMagBuffer()) << std::endl;
+		if (peaks.HasPhaseBuffer())
+			os << "Phases: " << asString(peaks.GetPhaseBuffer()) << std::endl;
 		return os.str();
+	}
+
+	SpectralPeaks createPeaks()
+	{
+		SpectralPeaks peaks;
+		peaks.AddAll();
+		peaks.UpdateData();
+		return peaks;
+	}
+
+	void addPeak(SpectralPeaks & peaks,
+		unsigned index,
+		float frequency,
+		float magnitude,
+		float phase)
+	{
+		CLAM::SpectralPeak peak;
+		peak.AddAll();
+		peak.UpdateData();
+		peak.SetFreq(frequency);
+		peak.SetMag(magnitude);
+		peak.SetPhase(phase);
+		peaks.AddSpectralPeak(peak, true, index);
 	}
 
 	void test_asString_withEmptyArray()
@@ -47,11 +85,113 @@ private:
 		CLAM::Array<float> data;
 		CPPUNIT_ASSERT_EQUAL(
 			std::string(
-			"[]"
+			"[ ]"
+			), asString(data));
+	}
+	void test_asString_withSingleElementArray()
+	{
+		CLAM::Array<float> data;
+		data.AddElem(4.2);
+		CPPUNIT_ASSERT_EQUAL(
+			std::string(
+			"[ 4.2 ]"
 			), asString(data));
 	}
 
+	void test_asString_withManyElementArray()
+	{
+		CLAM::Array<float> data;
+		data.AddElem(4.2);
+		data.AddElem(5.6);
+		CPPUNIT_ASSERT_EQUAL(
+			std::string(
+			"[ 4.2 5.6 ]"
+			), asString(data));
+	}
 
+	void test_asString_withEmptyPeaks()
+	{
+		SpectralPeaks peaks;
+		peaks.AddAll();
+		peaks.UpdateData();
+		CPPUNIT_ASSERT_EQUAL(
+			std::string(
+			"Indices: [ ]\n"
+			"Frequencies: [ ]\n"
+			"Magnitudes: [ ]\n"
+			"Phases: [ ]\n"
+			), asString(peaks));
+	}
+	void test_asString_withPeaks()
+	{
+		SpectralPeaks peaks = createPeaks();
+		addPeak(peaks, 1, 10, 1000, .1);
+		addPeak(peaks, 2, 20, 2000, .2);
+		CPPUNIT_ASSERT_EQUAL(
+			std::string(
+			"Indices: [ 1 2 ]\n"
+			"Frequencies: [ 10 20 ]\n"
+			"Magnitudes: [ 1000 2000 ]\n"
+			"Phases: [ 0.1 0.2 ]\n"
+			), asString(peaks));
+	}
+	void test_peakAddition_whenDisjoint()
+	{
+		SpectralPeaks peaks1 = createPeaks();
+		addPeak(peaks1, 1, 10, 1000, .1);
+		addPeak(peaks1, 2, 20, 2000, .2);
+
+		SpectralPeaks peaks2 = createPeaks();
+		addPeak(peaks2, 3, 30, 3000, .3);
+		addPeak(peaks2, 4, 40, 4000, .4);
+
+		SpectralPeaks result = peaks1 + peaks2;
+		CPPUNIT_ASSERT_EQUAL(
+			std::string(
+			"Indices: [ 2 4 7 9 ]\n"
+			"Frequencies: [ 10 20 30 40 ]\n"
+			"Magnitudes: [ 1000 2000 3000 4000 ]\n"
+			"Phases: [ 0.1 0.2 0.3 0.4 ]\n"
+			), asString(result));
+	}
+	void test_peakAddition_whenInterleaved()
+	{
+		SpectralPeaks peaks1 = createPeaks();
+		addPeak(peaks1, 1, 10, 1000, .1);
+		addPeak(peaks1, 3, 30, 3000, .3);
+
+		SpectralPeaks peaks2 = createPeaks();
+		addPeak(peaks2, 2, 20, 2000, .2);
+		addPeak(peaks2, 4, 40, 4000, .4);
+
+		SpectralPeaks result = peaks1 + peaks2;
+		CPPUNIT_ASSERT_EQUAL(
+			std::string(
+			"Indices: [ 2 5 6 9 ]\n"
+			"Frequencies: [ 10 20 30 40 ]\n"
+			"Magnitudes: [ 1000 2000 3000 4000 ]\n"
+			"Phases: [ 0.1 0.2 0.3 0.4 ]\n"
+			), asString(result));
+	}
+	void test_peakAddition_whenCoincident()
+	{
+		SpectralPeaks peaks1 = createPeaks();
+		addPeak(peaks1, 1, 10, 1000, .1);
+		addPeak(peaks1, 2, 20, 2000, .2);
+
+		SpectralPeaks peaks2 = createPeaks();
+		addPeak(peaks2, 1, 10, 1001, .11);
+		addPeak(peaks2, 3, 30, 3000, .3);
+
+		SpectralPeaks result = peaks1 + peaks2;
+		CPPUNIT_ASSERT_EQUAL(
+			std::string(
+			"Indices: [ 2 3 4 7 ]\n"
+			"Frequencies: [ 10 10 20 30 ]\n"
+			"Magnitudes: [ 1000 1001 2000 3000 ]\n"
+			"Phases: [ 0.1 0.11 0.2 0.3 ]\n"
+			), asString(result));
+	}
 };
 
 
